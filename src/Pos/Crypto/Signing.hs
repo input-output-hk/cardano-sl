48994d4ed520420e80259ef672ff46463fbee07d
Artyom <yom@artyom.me>
2016-10-09 12:14:54 +0300

    [POS-20] Optimise serialization instances

--- src/Pos/Crypto/Signing.hs
+++ src/Pos/Crypto/Signing.hs
@@ -32,14 +32,19 @@ module Pos.Crypto.Signing
 import qualified Crypto.Sign.Ed25519    as Ed25519
 import           Data.Binary            (Binary)
 import qualified Data.Binary            as Binary
+import qualified Data.Binary.Get        as Binary
+import qualified Data.Binary.Put        as Binary
+import qualified Data.ByteString        as BS
 import qualified Data.ByteString.Lazy   as BSL
 import           Data.Coerce            (coerce)
 import           Data.Hashable          (Hashable)
 import           Data.SafeCopy          (base, deriveSafeCopySimple)
 import           Data.SafeCopy          (SafeCopy (..))
+import qualified Data.Serialize         as Cereal
 import qualified Data.Text.Buildable    as Buildable
 import           Data.Text.Lazy.Builder (Builder)
-import           Formatting             (Format, bprint, fitLeft, later, (%), (%.))
+import           Formatting             (Format, bprint, fitLeft, int, later, sformat,
+                                         stext, (%), (%.))
 import           Universum
 
 import qualified Serokell.Util.Base64   as Base64 (decode, encode)
@@ -56,31 +61,75 @@ instance Hashable Ed25519.PublicKey
 instance Hashable Ed25519.SecretKey
 instance Hashable Ed25519.Signature
 
-instance Binary Ed25519.PublicKey
-instance Binary Ed25519.SecretKey
-instance Binary Ed25519.Signature
-
 instance NFData Ed25519.PublicKey
 instance NFData Ed25519.SecretKey
 instance NFData Ed25519.Signature
+
+secretKeyLength, publicKeyLength, signatureLength :: Int
+secretKeyLength = 64
+publicKeyLength = 32
+signatureLength = 64
+
+putAssertLength :: Monad m => Text -> Int -> ByteString -> m ()
+putAssertLength typeName expectedLength bs =
+    when (BS.length bs /= expectedLength) $ panic $
+        sformat ("put@"%stext%": expected length "%int%", not "%int)
+                typeName expectedLength (BS.length bs)
+
+-- Binary
+
+instance Binary Ed25519.PublicKey where
+    put (Ed25519.PublicKey k) = do
+        putAssertLength "PublicKey" publicKeyLength k
+        Binary.putByteString k
+    get = Ed25519.PublicKey <$> Binary.getByteString publicKeyLength
+
+instance Binary Ed25519.SecretKey where
+    put (Ed25519.SecretKey k) = do
+        putAssertLength "SecretKey" secretKeyLength k
+        Binary.putByteString k
+    get = Ed25519.SecretKey <$> Binary.getByteString secretKeyLength
+
+instance Binary Ed25519.Signature where
+    put (Ed25519.Signature s) = do
+        putAssertLength "Signature" signatureLength s
+        Binary.putByteString s
+    get = Ed25519.Signature <$> Binary.getByteString signatureLength
+
+-- Cereal
+
+instance Cereal.Serialize Ed25519.PublicKey where
+    put (Ed25519.PublicKey k) = do
+        putAssertLength "PublicKey" publicKeyLength k
+    get = Ed25519.PublicKey <$> Cereal.getByteString publicKeyLength
+
+instance Cereal.Serialize Ed25519.SecretKey where
+    put (Ed25519.SecretKey k) = do
+        putAssertLength "SecretKey" secretKeyLength k
+        Cereal.putByteString k
+    get = Ed25519.SecretKey <$> Cereal.getByteString secretKeyLength
+
+instance Cereal.Serialize Ed25519.Signature where
+    put (Ed25519.Signature s) = do
+        putAssertLength "Signature" signatureLength s
+        Cereal.putByteString s
+    get = Ed25519.Signature <$> Cereal.getByteString signatureLength
 
 ----------------------------------------------------------------------------
 -- Keys, key generation & printing & decoding
 ----------------------------------------------------------------------------
 
 newtype PublicKey = PublicKey Ed25519.PublicKey
-    deriving (Eq, Ord, Show, Binary, NFData, Hashable)
+    deriving (Eq, Ord, Show, Binary, Cereal.Serialize, NFData, Hashable)
 
 newtype SecretKey = SecretKey Ed25519.SecretKey
-    deriving (Eq, Ord, Show, Binary, NFData, Hashable)
+    deriving (Eq, Ord, Show, Binary, Cereal.Serialize, NFData, Hashable)
 
 instance SafeCopy PublicKey where
-    putCopy = putCopyBinary
-    getCopy = getCopyBinary "PublicKey"
+    -- will use Cereal.Serialize instance by default
 
 instance SafeCopy SecretKey where
-    putCopy = putCopyBinary
-    getCopy = getCopyBinary "SecretKey"
+    -- will use Cereal.Serialize instance by default
 
 -- | Generate a public key from a secret key. Fast (it just drops some bytes
 -- off the secret key).

4a46e291316e1cac89813d8b3b9acf2ce8cac692
Ivan Gromakovskii <gie1994@gmail.com>
2016-10-14 14:40:27 +0300

    [POS-14] Change structure of BlockStorage

--- src/Pos/State/Storage/Block.hs
+++ src/Pos/State/Storage/Block.hs
@@ -13,33 +13,30 @@ module Pos.State.Storage.Block
        , HasBlockStorage (blockStorage)
 
        , getBlock
-       , getBlockByIndex
        , getLeaders
 
        , blkRollback
        ) where
 
-import           Control.Lens  (ix, makeClassy, preview, views, (^.))
+import           Control.Lens  (at, ix, makeClassy, preview, view, views, (^.))
 import           Data.Default  (Default, def)
 import           Data.SafeCopy (base, deriveSafeCopySimple)
 import           Data.Vector   (Vector)
 import           Universum
 
+import           Pos.Crypto    (hash)
 import           Pos.Genesis   (genesisLeaders)
-import           Pos.Types     (Block, EpochIndex, HeaderHash, SlotLeaders, blockLeaders,
-                                mkGenesisBlock)
+import           Pos.Types     (Block, EpochIndex, HeaderHash, SlotLeaders, blockHeader,
+                                blockLeaders, mkGenesisBlock)
 
 data BlockStorage = BlockStorage
-    { -- | The best valid blockchain known to the node. We should take
-      -- into account that we are dealing with tree, not list. This
-      -- list is the best chain. We use builtin lists for simplicity,
-      -- because we don't care about performance for now. Head of list
-      -- is the most recent block.
-      _blkBlocks         :: ![Block]
-    , -- | Extra blocks from alternative chains. It serves as cache basically.
-      _blkExtraBlocks    :: !(HashMap HeaderHash Block)
-    , -- | Indices of genesis blocks.
-      _blkGenesisIndices :: !(Vector Word)
+    { -- | All blocks known to the node. Blocks have pointers to other
+      -- blocks and can be easily traversed.
+      _blkBlocks        :: !(HashMap HeaderHash Block)
+    , -- | Hashes of genesis blocks in the __best chain__.
+      _blkGenesisBlocks :: !(Vector HeaderHash)
+    , -- | Hash of the head in the __best chain__.
+      _blkHead          :: !HeaderHash
     }
 
 makeClassy ''BlockStorage
@@ -48,30 +45,27 @@ deriveSafeCopySimple 0 'base ''BlockStorage
 instance Default BlockStorage where
     def =
         BlockStorage
-        { _blkBlocks = [Left (mkGenesisBlock Nothing 0 genesisLeaders)]
-        , _blkExtraBlocks = mempty
-        , _blkGenesisIndices = [0]
+        { _blkBlocks = [(genesisBlock0Hash, genesisBlock0)]
+        , _blkGenesisBlocks = [genesisBlock0Hash]
+        , _blkHead = genesisBlock0Hash
         }
+      where
+        genesisBlock0 = Left (mkGenesisBlock Nothing 0 genesisLeaders)
+        genesisBlock0Hash = hash $ genesisBlock0 ^. blockHeader
 
 type Query a = forall m x. (HasBlockStorage x, MonadReader x m) => m a
 type Update a = forall m x. (HasBlockStorage x, MonadState x m) => m a
 
 -- | Get block by hash of its header.
 getBlock :: HeaderHash -> Query (Maybe Block)
-getBlock _ = notImplemented
-
--- | Get i-th block from the best know chain. 0-th block is genesis
--- block for 0-th epoch.
-getBlockByIndex :: Word -> Query (Maybe Block)
-getBlockByIndex (fromIntegral -> i) =
-    views blkBlocks (\b -> b `atMay` (length b - i - 1))
+getBlock h = view (blkBlocks . at h)
 
 -- | Get list of slot leaders for the given epoch. Empty list is returned
 -- if no information is available.
 getLeaders :: EpochIndex -> Query SlotLeaders
 getLeaders (fromIntegral -> epoch) = do
-    blkIdx <- preview (blkGenesisIndices . ix epoch)
-    maybe (pure mempty) (fmap leadersFromBlock . getBlockByIndex) blkIdx
+    blkIdx <- preview (blkGenesisBlocks . ix epoch)
+    maybe (pure mempty) (fmap leadersFromBlock . getBlock) blkIdx
   where
     leadersFromBlock (Just (Left genBlock)) = genBlock ^. blockLeaders
     leadersFromBlock _                      = mempty

326c1659bd67b337a08898e1e05aa282216c4254
Ivan Gromakovskii <gie1994@gmail.com>
2016-10-19 20:10:50 +0300

    [POS-28] Move FtsSeed stuff to Mpc, genFtsSeed → genCommitmentAndOpening

--- src/Pos/Types/Mpc.hs
+++ src/Pos/Types/Mpc.hs
@@ -1,15 +1,41 @@
 -- | Function related to Mpc messages.
 
 module Pos.Types.Mpc
-       ( verifyCommitment
+       ( ftsSeedLength
+       , secretToFtsSeed
+       , genCommitmentAndOpening
+       , verifyCommitment
        , verifyOpening
        ) where
 
+import           Control.Lens        (over, _1)
+import qualified Data.Binary         as Bin
 import qualified Data.HashMap.Strict as HM
 import           Universum
 
-import           Pos.Crypto          (verifyEncShare, verifySecretProof)
-import           Pos.Types.Types     (Commitment (..), Opening (..))
+import           Pos.Crypto          (EncShare, Secret, SecretSharingExtra, Threshold,
+                                      VssPublicKey, genSharedSecret, runSecureRandom,
+                                      verifyEncShare, verifySecretProof)
+import           Pos.Types.Types     (Commitment (..), FtsSeed (..), Opening (..))
+
+-- | Length of FtsSeed which is currently constant known in compile
+-- time.
+-- FIXME: do something!
+ftsSeedLength :: Integral a => a
+ftsSeedLength = 40
+
+-- | Convert Secret to FtsSeed.
+-- FIXME: result is 33 bytes long!
+secretToFtsSeed :: Secret -> FtsSeed
+secretToFtsSeed = FtsSeed . toS . Bin.encode
+
+-- | Generate securely random FtsSeed.
+genCommitmentAndOpening
+    :: MonadIO m
+    => Threshold -> [VssPublicKey] -> m (Commitment, Opening)
+genCommitmentAndOpening n = notImplemented
+    -- liftIO .
+    -- runSecureRandom . fmap (over _1 secretToFtsSeed) . genSharedSecret n
 
 -- | Verify that Commitment is correct.
 verifyCommitment :: Commitment -> Bool

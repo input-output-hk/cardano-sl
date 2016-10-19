113ef2d5357555c244f27cd69d1db99e42bb0069
Ivan Gromakovskii <gie1994@gmail.com>
2016-10-19 19:48:30 +0300

    [POS-28] Add verifyCommitment

--- src/Pos/Types/Mpc.hs
+++ src/Pos/Types/Mpc.hs
@@ -1,14 +1,23 @@
 -- | Function related to Mpc messages.
 
 module Pos.Types.Mpc
-       ( verifyOpening
+       ( verifyCommitment
+       , verifyOpening
        ) where
 
+import qualified Data.HashMap.Strict as HM
 import           Universum
 
-import           Pos.Crypto      (verifySecretProof)
-import           Pos.Types.Types (Commitment (..), Opening (..))
+import           Pos.Crypto          (verifyEncShare, verifySecretProof)
+import           Pos.Types.Types     (Commitment (..), Opening (..))
 
+-- | Verify that Commitment is correct.
+verifyCommitment :: Commitment -> Bool
+verifyCommitment Commitment {..} = all verifyCommitmentDo $ HM.toList commShares
+  where
+    verifyCommitmentDo = uncurry (verifyEncShare commExtra)
+
+-- | Verify that Secret provided with Opening corresponds to given commitment.
 verifyOpening :: Commitment -> Opening -> Bool
 verifyOpening Commitment {..} (Opening secret) =
     verifySecretProof commExtra secret commProof

8952d4ad708a9673986c82202698da244cf9266e
Ivan Gromakovskii <gie1994@gmail.com>
2016-10-20 01:35:49 +0300

    [POS-14] Implement handleBlockHeader

--- src/Pos/Communication/Server/Block.hs
+++ src/Pos/Communication/Server/Block.hs
@@ -2,21 +2,27 @@
 
 module Pos.Communication.Server.Block
        ( blockListeners
+
+       , handleBlockHeader -- tmp
        ) where
 
-import           Control.TimeWarp.Rpc    (Listener (..))
+import           Control.TimeWarp.Rpc    (Listener (..), MonadDialog, MonadResponse,
+                                          reply)
 import           Universum
 
 import           Pos.Communication.Types (RequestBlock (..), SendBlock (..),
                                           SendBlockHeader (..))
--- import           Pos.Crypto              (hash)
+import           Pos.Crypto              (hash)
 import qualified Pos.State               as St
 import           Pos.WorkMode            (WorkMode)
 
 -- | Listeners for requests related to blocks processing.
 blockListeners :: WorkMode m => [Listener m]
 blockListeners =
-    [Listener handleBlock, Listener handleBlockHeader, Listener handleBlockRequest]
+    [ Listener handleBlock
+    -- , Listener handleBlockHeader
+    , Listener handleBlockRequest
+    ]
 
 handleBlock :: WorkMode m => SendBlock -> m ()
 handleBlock (SendBlock block) = do
@@ -24,13 +30,11 @@ handleBlock (SendBlock block) = do
     notImplemented
 
 handleBlockHeader
-    :: WorkMode m
+    :: (WorkMode m, MonadResponse m, MonadDialog m)  -- TODO: MonadDialog should be part of WorkMode
     => SendBlockHeader -> m ()
 handleBlockHeader (SendBlockHeader header) = whenM (St.mayBlockBeUseful header) $ do
-    -- let h = hash $ Right header
-    -- reply (RequestBlock h)
-    -- _ <- St.processNewBlocks [blk]
-    notImplemented
+    let h = hash $ Right header
+    reply (RequestBlock h)
 
 handleBlockRequest
     :: WorkMode m

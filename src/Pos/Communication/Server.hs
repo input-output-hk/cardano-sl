45dae593fa56a0c6e0545c7ef5f6e28df88d999d
Ivan Gromakovskii <gie1994@gmail.com>
2016-10-20 19:32:40 +0300

    Make serve blocking

--- src/Pos/Communication/Server.hs
+++ src/Pos/Communication/Server.hs
@@ -4,10 +4,11 @@ module Pos.Communication.Server
        ( serve
        ) where
 
+import           Control.TimeWarp.Timed         (sleepForever)
 import           Universum
 
 import           Pos.Communication.Server.Block ()
 import           Pos.WorkMode                   (WorkMode)
 
 serve :: WorkMode m => m ()
-serve = pure ()
+serve = sleepForever

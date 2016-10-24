93ef50dca3cfceac4f0e9be65bd8749329db7154
Ivan Gromakovskii <gie1994@gmail.com>
2016-10-24 03:39:55 +0300

    Implement serve

--- src/Pos/Communication/Server.hs
+++ src/Pos/Communication/Server.hs
@@ -4,11 +4,14 @@ module Pos.Communication.Server
        ( serve
        ) where
 
-import           Control.TimeWarp.Timed         (sleepForever)
--- import           Universum
+import           Control.TimeWarp.Rpc           (listen)
+import           Universum
 
-import           Pos.Communication.Server.Block ()
+import           Pos.Communication.Server.Block (blockListeners)
+import           Pos.Communication.Server.Mpc   (mpcListeners)
+import           Pos.Communication.Server.Tx    (txListeners)
 import           Pos.WorkMode                   (WorkMode)
 
+-- TODO: not hardcode port.
 serve :: WorkMode m => m ()
-serve = sleepForever
+serve = listen 322 $ mconcat [blockListeners, mpcListeners, txListeners]

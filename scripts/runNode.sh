bfbd3b7a10dd82e9d297ee8fc1481b3468522112
George Agapov <george.agapov@gmail.com>
2016-10-30 01:18:46 +0300

    [POS-31] Debug convenience modifications

--- scripts/runNode.sh
+++ scripts/runNode.sh
@@ -20,4 +20,4 @@ fi
 $(find_binary pos-node) --db-path pos-db$i --rebuild-db --vss-genesis $i \
   --spending-genesis $i --port $port --peer '127.0.0.1:2000/ABOtPlQMv123_4wzfgjAzvsT2LE=' \
   $logs $st \
-  | tee logs/node-$i-`date '+%F_%H%M%S'`.log
+  2>&1 | tee logs/node-$i-`date '+%F_%H%M%S'`.log

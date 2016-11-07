caf92c7b8a5a2e7e1c4d47706457fc3da4e0acbe
Ivan Gromakovskii <gie1994@gmail.com>
2016-11-07 21:11:56 +0300

    [CSL-66] Implement BitcoinStakes

--- src/Pos/Genesis.hs
+++ src/Pos/Genesis.hs
@@ -17,11 +17,12 @@ module Pos.Genesis
 
 
 import           Data.Default       (Default (def))
-import           Data.List          (genericTake)
+import           Data.List          (genericLength, genericReplicate)
 import qualified Data.List.NonEmpty as NE
 import qualified Data.Map.Strict    as M
 import qualified Data.Text          as T
 import           Formatting         (int, sformat, (%))
+import           Serokell.Util      (enumerate)
 import           Universum
 
 import           Pos.Constants      (epochSlots, genesisN)
@@ -63,18 +64,47 @@ data StakeDistribution
     = FlatStakes !Word     -- number of stakeholders
                  !Coin     -- total number of coins
     | BitcoinStakes !Word  -- number of stakeholders
-                    !Coin  -- total number of coins
 
 instance Default StakeDistribution where
     def = FlatStakes 3 30000
 
-genesisUtxo :: StakeDistribution -> Utxo
-genesisUtxo (FlatStakes stakeholders coins) =
-    M.fromList . genericTake stakeholders $
-    map (\a -> ((unsafeHash a, 0), TxOut a val)) genesisAddresses
+bitcoinDistribution20 :: [Coin]
+bitcoinDistribution20 = [200, 163, 120, 105, 78, 76, 57, 50, 46, 31, 26, 13, 11, 11, 7, 4, 2, 0, 0, 0]
+
+stakeDistribution :: StakeDistribution -> [Coin]
+stakeDistribution (FlatStakes stakeholders coins) =
+    genericReplicate stakeholders val
   where
     val = coins `div` fromIntegral stakeholders
-genesisUtxo (BitcoinStakes _ _) = notImplemented
+stakeDistribution (BitcoinStakes stakeholders)
+    | stakeholders < 20 = stakeDistribution (FlatStakes stakeholders 1000)
+    | stakeholders == 20 = bitcoinDistribution20
+    | otherwise =
+        foldl' (bitcoinDistributionImpl ratio) [] $
+        enumerate bitcoinDistribution20
+  where
+    ratio = fromIntegral stakeholders / 20
+
+bitcoinDistributionImpl :: Double -> [Coin] -> (Int, Coin) -> [Coin]
+bitcoinDistributionImpl ratio coins (coinIdx, coin) =
+    coins ++ toAddValMax : replicate (toAddNum - 1) toAddValMin
+  where
+    toAddNumMax = ceiling ratio
+    toAddNumMin = floor ratio
+    toAddNum :: Int
+    toAddNum =
+        if genericLength coins + realToFrac toAddNumMax >
+           realToFrac (coinIdx + 1) * ratio
+            then toAddNumMin
+            else toAddNumMax
+    toAddValMin = coin `div` fromIntegral toAddNum
+    toAddValMax = coin - toAddValMin * (fromIntegral toAddNum - 1)
+
+genesisUtxo :: StakeDistribution -> Utxo
+genesisUtxo sd =
+    M.fromList . zipWith zipF (stakeDistribution sd) $ genesisAddresses
+  where
+    zipF coin addr = ((unsafeHash addr, 0), TxOut addr coin)
 
 -- | For every static stakeholder it generates `k` coins, but in `k`
 -- transaction (1 coin each), where `k` is input parameter (depends on

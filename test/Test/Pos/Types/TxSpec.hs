65ff5b827c26bee5c9cb9923eafb297c768a95d2
Alexandre Rodrigues <alexandrer_b@outlook.com>
2016-11-17 05:12:27 +0000

    [POS-22] Refactor transaction testing module

--- test/Test/Pos/Types/TxSpec.hs
+++ test/Test/Pos/Types/TxSpec.hs
@@ -49,14 +49,18 @@ invalidateBadTx tx@Tx{..} =
   where
     negOutputs = fmap (\(TxOut a c) -> TxOut a (negate c)) txOutputs
 
-txChecksum :: [Maybe (TxIn, TxOut)] -> [TxOut] -> Bool
-txChecksum extendedInputs txOuts =
-    let inpSum = sum $ fmap (toInteger . txOutValue . snd) $ catMaybes extendedInputs
-        outSum = sum $ fmap (toInteger . txOutValue) txOuts
-    in inpSum >= outSum
+type TxVerifyingTools = (Tx, TxIn -> Maybe TxOut, [Maybe (TxIn, TxOut)])
 
-individualTxPropertyVerifier :: GoodTx -> Bool
-individualTxPropertyVerifier (getGoodTx -> ls) =
+-- | This function takes the list inside a 'GoodTx' and related types, and
+-- turns it into something 'verifyTx' can use:
+--
+-- ★ the transaction that the list holds
+-- ★ the input resolver associated with that transaction
+-- ★ the list of resolved inputs with all inputs in the transaction
+getTxFromGoodTx
+    :: [(Tx, TxIn, TxOut)]
+    -> TxVerifyingTools
+getTxFromGoodTx ls =
     let txOutputs = fmap (view _3) ls
         txInputs = fmap (view _2) ls
         inpResolver :: TxIn -> Maybe TxOut
@@ -64,8 +68,28 @@ individualTxPropertyVerifier (getGoodTx -> ls) =
         extendInput txIn = (txIn,) <$> inpResolver txIn
         extendedInputs :: [Maybe (TxIn, TxOut)]
         extendedInputs = fmap extendInput txInputs
-        hasGoodSum = txChecksum extendedInputs txOutputs
-        hasGoodStructure = isVerSuccess $ verifyTxAlone Tx{..}
+    in (Tx {..}, inpResolver, extendedInputs)
+
+-- | This function takes a list of resolved inputs from a transaction, that
+-- same transaction's outputs, and verifies that the input sum is greater than
+-- the output sum.
+txChecksum :: [Maybe (TxIn, TxOut)] -> [TxOut] -> Bool
+txChecksum extendedInputs txOuts =
+    let inpSum = sum $ fmap (toInteger . txOutValue . snd) $ catMaybes extendedInputs
+        outSum = sum $ fmap (toInteger . txOutValue) txOuts
+    in inpSum >= outSum
+
+-- | This function, used in 'verifyGoodTx', takes a 'GoodTx' and checks that
+-- each property verified by 'verifyTx' holds, meaning:
+--
+-- ★ sum of inputs ≥ sum of outputs;
+-- ★ every input is signed properly;
+-- ★ every input is a known unspent output.
+-- It also checks that it has good structure w.r.t. 'verifyTxAlone'.
+individualTxPropertyVerifier :: TxVerifyingTools -> Bool
+individualTxPropertyVerifier (tx@Tx{..}, inpResolver, extendedInputs) =
+    let hasGoodSum = txChecksum extendedInputs txOutputs
+        hasGoodStructure = isVerSuccess $ verifyTxAlone tx
         mapFun =
             \maybeTxPair ->
                 case maybeTxPair of
@@ -79,26 +103,17 @@ individualTxPropertyVerifier (getGoodTx -> ls) =
 
 validateGoodTx :: GoodTx -> Bool
 validateGoodTx g@(getGoodTx -> ls) =
-    let txOutputs = fmap (view _3) ls
-        txInputs = fmap (view _2) ls
-        resolveFun (Tx _ txOut, tInp, _) = (tInp, head txOut)
-        inpResolver :: TxIn -> Maybe TxOut
-        inpResolver = join . flip lookup (map resolveFun ls)
-        transactionIsVerified = isVerSuccess $ verifyTx inpResolver $ Tx{..}
-        transactionReallyIsGood = individualTxPropertyVerifier g
+    let triple@(tx, inpResolver, extendedInputs) =
+            getTxFromGoodTx ls
+        transactionIsVerified = isVerSuccess $ verifyTx inpResolver tx
+        transactionReallyIsGood = individualTxPropertyVerifier triple
     in  transactionIsVerified == transactionReallyIsGood
 
 overflowTx :: OverflowTx -> Bool
 overflowTx (getOverflowTx -> ls) =
-    let txOutputs = fmap (view _3) ls
-        txInputs = fmap (view _2) ls
-        resolveFun (Tx _ txOut, tInp, _) = (tInp, head txOut)
-        inpResolver :: TxIn -> Maybe TxOut
-        inpResolver = join . flip lookup (map resolveFun ls)
-        extendInput txIn = (txIn,) <$> inpResolver txIn
-        extendedInputs :: [Maybe (TxIn, TxOut)]
-        extendedInputs = fmap extendInput txInputs
-        transactionIsNotVerified = isVerFailure $ verifyTx inpResolver Tx{..}
+    let (tx@Tx{..}, inpResolver, extendedInputs) =
+            getTxFromGoodTx ls
+        transactionIsNotVerified = isVerFailure $ verifyTx inpResolver tx
         inpSumLessThanOutSum = not $ txChecksum extendedInputs txOutputs
     in inpSumLessThanOutSum == transactionIsNotVerified
 
@@ -111,14 +126,8 @@ signatureIsNotValid _ _ = False
 
 badSigsTx :: BadSigsTx -> Bool
 badSigsTx (getBadSigsTx -> ls) =
-    let txOutputs = fmap (view _3) ls
-        txInputs = fmap (view _2) ls
-        resolveFun (Tx _ txOut, tInp, _) = (tInp, head txOut)
-        inpResolver :: TxIn -> Maybe TxOut
-        inpResolver = join . flip lookup (map resolveFun ls)
-        extendInput txIn = (txIn,) <$> inpResolver txIn
-        extendedInputs :: [Maybe (TxIn, TxOut)]
-        extendedInputs = fmap extendInput txInputs
-        transactionIsNotVerified = isVerFailure $ verifyTx inpResolver Tx{..}
+    let (tx@Tx{..}, inpResolver, extendedInputs) =
+            getTxFromGoodTx ls
+        transactionIsNotVerified = isVerFailure $ verifyTx inpResolver tx
         noSignatureIsValid = all (signatureIsNotValid txOutputs) extendedInputs
     in noSignatureIsValid == transactionIsNotVerified

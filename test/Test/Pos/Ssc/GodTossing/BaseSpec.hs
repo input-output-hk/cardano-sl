ea776ac9bd5971ea879e38a5eaa03a169dca6f01
Alexandre <alex_balde12@hotmail.com>
2016-11-21 04:57:48 +0000

    [CSL-75] Move and rename MpcSpec module
    Signed-off-by: Alexandre <alex_balde12@hotmail.com>

--- test/Test/Pos/Ssc/GodTossing/BaseSpec.hs
+++ test/Test/Pos/Ssc/GodTossing/BaseSpec.hs
@@ -1,29 +1,27 @@
--- | Specification of Pos.Types.Mpc
+-- | Specification of Pos.Ssc.GodTossing.Base
 
-module Test.Pos.Types.MpcSpec
+module Test.Pos.Ssc.GodTossing.Base
        ( spec
        ) where
 
 import           Pos.Crypto            (SecretKey, sign, toPublic)
-import           Pos.Ssc.GodTossing  (Commitment, CommitmentOpening (..),
+import           Pos.Ssc.GodTossing    (Commitment, CommitmentOpening (..),
                                         verifyCommitment, verifyCommitmentSignature,
                                         verifyOpening)
-import           Pos.Types             (EpochIndex, SharedSeed (..))
+import           Pos.Types             (EpochIndex)
 
 import           Test.Hspec            (Spec, describe)
 import           Test.Hspec.QuickCheck (prop)
 import           Universum
 
 spec :: Spec
-spec = describe "Types.Mpc" $ do
+spec = describe "Ssc.GodTossing.Base" $ do
     describe "verifyCommitment" $ do
         prop description_verifiesOkComm verifiesOkComm
     describe "verifyCommitmentSignature" $ do
         prop description_verifiesOkCommSig verifiesOkCommSig
     describe "verifyOpening" $ do
         prop description_verifiesOkOpening verifiesOkOpening
-    describe "xorSharedSeed" $ do
-        prop description_xorFormsAbelianGroup xorFormsAbelianGroup
   where
     description_verifiesOkComm =
         "successfully verifies a correct commitment, and fails to verify an incorrect \
@@ -33,9 +31,6 @@ spec = describe "Types.Mpc" $ do
     description_verifiesOkOpening =
         "successfully verifies that an opening corresponding to the given commitment \
         \ does indeed belong to it"
-    description_xorFormsAbelianGroup =
-        "under the xor operation, the set of ftsSeedLength-byte SharedSeeds is an \
-        \ abelian group"
 
 verifiesOkComm :: CommitmentOpening -> Bool
 verifiesOkComm CommitmentOpening{..} =
@@ -50,24 +45,3 @@ verifiesOkCommSig sk comm epoch =
 verifiesOkOpening :: CommitmentOpening -> Bool
 verifiesOkOpening CommitmentOpening{..} =
     verifyOpening coCommitment coOpening
-
-xorFormsAbelianGroup :: SharedSeed -> SharedSeed -> SharedSeed -> Bool
-xorFormsAbelianGroup fts1 fts2 fts3 =
-    let isAssociative =
-            let assoc1 = (fts1 <> fts2) <> fts3
-                assoc2 = fts1 <> (fts2 <> fts3)
-            in assoc1 == assoc2
-        hasIdentity =
-            let id1 = mempty <> fts1
-                id2 = fts1 <> mempty
-            in (fts1 == id1) && (fts1 == id2)
-        hasInverses =
-            let inv1 = fts1 <> fts2
-                inv2 = inv1 <> fts2
-                inv3 = fts1 <> inv1
-            in inv2 == fts1 && inv3 == fts2
-        isCommutative =
-            let comm1 = fts1 <> fts2
-                comm2 = fts2 <> fts1
-            in comm1 == comm2
-    in isAssociative && hasIdentity && hasInverses && isCommutative

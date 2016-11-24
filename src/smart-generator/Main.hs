9821139e7b5f40ee5cf438a6534c9e08dfcacb2d
Dmitry Mukhutdinov <flyingleafe@gmail.com>
2016-11-24 23:39:17 +0300

    [CSL-163] Move options to separate file

--- src/smart-generator/Main.hs
+++ src/smart-generator/Main.hs
@@ -8,32 +8,25 @@ import           Control.Lens           ((^.))
 import           Control.Monad          (when)
 import           Control.TimeWarp.Rpc   (NetworkAddress)
 import           Control.TimeWarp.Timed (for, fork_, ms, repeatForever, sec, wait)
-import           Data.Aeson             (encode)
 import           Data.Array.IO          (IOArray)
 import           Data.Array.MArray      (MArray (..), newListArray, readArray, writeArray)
-import qualified Data.ByteString.Lazy   as LBS
 import           Data.Default           (def)
 import qualified Data.HashMap.Strict    as M
 import           Data.IORef             (IORef, modifyIORef', newIORef, readIORef,
                                          writeIORef)
 import           Data.List              (head, intersect, tail, (!!))
-import           Data.Monoid            ((<>))
 import           Data.Text.IO           (appendFile, writeFile)
 import           Data.Time.Clock.POSIX  (getPOSIXTime)
 import           Formatting             (build, fixed, float, int, sformat, (%))
-import           Options.Applicative    (Parser, ParserInfo, auto, execParser, fullDesc,
-                                         help, helper, info, long, many, metavar, option,
-                                         progDesc, short, showDefault, switch, value)
-import           Serokell.Util.OptParse (fromParsec, strOption)
+import           Options.Applicative    (execParser)
 import           System.Wlog            (logInfo, logWarning)
 import           Test.QuickCheck        (arbitrary, generate)
-import           Universum              hiding (head, (<>))
+import           Universum              hiding (head)
 
-import           Pos.CLI                (dhtNodeParser, sscAlgoParser)
 import           Pos.Communication      (allListeners)
 import           Pos.Constants          (k, slotDuration)
 import           Pos.Crypto             (KeyPair (..), SecretKey, hash, sign, unsafeHash)
-import           Pos.DHT                (DHTNode, DHTNodeType (..), ListenerDHT, dhtAddr,
+import           Pos.DHT                (DHTNodeType (..), ListenerDHT, dhtAddr,
                                          discoverPeers, mapListenerDHT)
 import           Pos.DHT.Real           (KademliaDHTInstance)
 import           Pos.Genesis            (StakeDistribution (..), genesisAddresses,
@@ -54,119 +47,7 @@ import           Pos.Types              (Address, SlotId (..), Tx (..), TxId, Tx
 import           Pos.Util.JsonLog       ()
 import           Pos.WorkMode           (ProductionMode, RealMode, WorkMode)
 
------------------------------------------------------------------------
--- CLI options
------------------------------------------------------------------------
-data GenOptions = GenOptions
-    { goGenesisIdx         :: !Word       -- ^ Index in genesis key pairs.
-    -- , goRemoteAddr  :: !NetworkAddress -- ^ Remote node address
-    , goDHTPeers           :: ![DHTNode]  -- ^ Initial DHT nodes
-    , goRoundDuration      :: !Double     -- ^ Number of seconds per round
-    , goRoundNumber        :: !Int        -- ^ Number of rounds
-    , goTxFrom             :: !Int        -- ^ Start from UTXO transaction #x
-    , goInitBalance        :: !Int        -- ^ Total coins in init utxo per address
-    , goInitTps            :: !Double     -- ^ Start TPS rate (it adjusts over time)
-    , goTpsIncreaseStep    :: !Double     -- ^ When system is stable, increase TPS in next round by this value
-    , goPropThreshold      :: !Int
-    , goSingleRecipient    :: !Bool       -- ^ Send to only 1 node if flag is set
-    , goDhtExplicitInitial :: !Bool
-    , goLogConfig          :: !(Maybe FilePath)
-    , goLogsPrefix         :: !(Maybe FilePath)
-    , goJLFile             :: !(Maybe FilePath)
-    , goSscAlgo            :: !SscAlgo
-    , goFlatDistr          :: !(Maybe (Int, Int))
-    , goBitcoinDistr       :: !(Maybe (Int, Int))
-    }
-
-optionsParser :: Parser GenOptions
-optionsParser = GenOptions
-    <$> option auto
-            (short 'i'
-          <> long "index"
-          <> metavar "INT"
-          <> help "Index in list of genesis key pairs")
-    -- <*> option (fromParsec addrParser)
-    --         (long "peer"
-    --       <> metavar "HOST:PORT"
-    --       <> help "Node address to ZERG RUSH")
-    <*> many (option (fromParsec dhtNodeParser) $
-             long "peer"
-          <> metavar "HOST:PORT/HOST_ID"
-          <> help "Initial DHT peer (may be many)")
-    <*> option auto
-            (short 'd'
-          <> long "round-duration"
-          <> help "Duration of one testing round")
-    <*> option auto
-            (short 'N'
-          <> long "round-number"
-          <> help "Number of testing rounds")
-    <*> option auto
-            (long "tx-from-n"
-          <> value 0
-          <> help "From which transaction in utxo to start")
-    <*> option auto
-            (long "init-money"
-          <> help "How many coins node has in the beginning")
-    <*> option auto
-            (short 't'
-          <> long "tps"
-          <> metavar "DOUBLE"
-          <> help "TPS (transactions per second)")
-    <*> option auto
-            (short 'S'
-          <> long "tps-step"
-          <> value 10
-          <> metavar "DOUBLE"
-          <> help "TPS increase delta on stable system")
-    <*> option auto
-            (short 'P'
-          <> long "propagate-threshold"
-          <> value 1
-          <> help "Approximate number of slots needed to propagate transactions across the network")
-    <*> switch
-        (long "single-recipient" <>
-         help "Send transactions only to one of nodes")
-    <*> switch
-        (long "explicit-initial" <>
-         help
-             "Explicitely contact to initial peers as to neighbors (even if they appeared offline once)")
-    <*> optional (strOption $
-                  long "log-config"
-               <> metavar "FILEPATH"
-               <> help "Path to logger configuration")
-    <*> optional (strOption $
-                  long "logs-prefix"
-               <> metavar "FILEPATH"
-               <> help "Prefix to logger output path")
-    <*> optional (strOption $
-                  long "json-log"
-               <> metavar "FILEPATH"
-               <> help "Path to json log file")
-    <*> option (fromParsec sscAlgoParser)
-        (long "ssc-algo"
-      <> metavar "ALGO"
-      <> value GodTossingAlgo
-      <> showDefault
-      <> help "Shared Seed Calculation algorithm which nodes will use")
-    <*> optional
-        (option auto $
-         mconcat
-            [ long "flat-distr"
-            , metavar "(INT,INT)"
-            , help "Use flat stake distribution with given parameters (nodes, coins)"
-            ])
-    <*> optional
-        (option auto $
-         mconcat
-            [ long "bitcoin-distr"
-            , metavar "(INT,INT)"
-            , help "Use bitcoin stake distribution with given parameters (nodes, coins)"
-            ])
-
-optsInfo :: ParserInfo GenOptions
-optsInfo = info (helper <*> optionsParser) $
-    fullDesc `mappend` progDesc "Stupid transaction generator"
+import           GenOptions             (GenOptions (..), optsInfo)
 
 ----------------------------------------------------------------------------------------------------
 -- Transactions generation

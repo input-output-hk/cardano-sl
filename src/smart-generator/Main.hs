fccd56dce4506724182c6ce65359117b9fd87c54
Dmitry Mukhutdinov <flyingleafe@gmail.com>
2016-11-26 12:19:19 +0300

    [CSL-190] Change rounds model

--- src/smart-generator/Main.hs
+++ src/smart-generator/Main.hs
@@ -106,12 +106,9 @@ runSmartGen inst np@NodeParams{..} opts@GenOptions{..} =
     -- Start writing tps file
     liftIO $ writeFile (logsFilePrefix </> tpsCsvFile) tpsCsvHeader
 
-    initialT <- getPosixMs
-    let startMeasurementsT =
-            initialT + (k + goPropThreshold) * fromIntegral (slotDuration `div` ms 1)
-        roundDuration =
-                fromIntegral ((k + goPropThreshold) * (goRoundPeriodRate + 1)) *
-                fromIntegral (slotDuration `div` sec 1)
+    let phaseDuration = fromIntegral (k + goPropThreshold) * slotDuration
+        roundDuration = fromIntegral (goRoundPeriodRate + 1) *
+                        fromIntegral (phaseDuration `div` sec 1)
 
     void $ forFold (goInitTps, goTpsIncreaseStep) [1 .. goRoundNumber] $
         \(goTPS, increaseStep) (roundNum :: Int) -> do
@@ -130,6 +127,9 @@ runSmartGen inst np@NodeParams{..} opts@GenOptions{..} =
         wait $ for (round $ goRoundPause * fromIntegral (sec 1) :: Microsecond)
 
         beginT <- getPosixMs
+        let startMeasurementsT =
+                beginT + (k + goPropThreshold) * fromIntegral (slotDuration `div` ms 1)
+
         forM_ [0 .. txNum - 1] $ \(idx :: Int) -> do
             preStartT <- getPosixMs
             logInfo $ sformat ("CURRENT TXNUM: "%int) txNum
@@ -178,6 +178,10 @@ runSmartGen inst np@NodeParams{..} opts@GenOptions{..} =
         -- We collect tables of really generated tps
         liftIO $ appendFile (logsFilePrefix </> tpsCsvFile) $
             tpsCsvFormat (globalTime, goTPS, realTPS)
+
+        -- Wait for 1 phase (to get all the last sent transactions)
+        logInfo "Pausing transaction spawning for 1 phase"
+        wait $ for phaseDuration
 
         return (newTPS, newStep)
 

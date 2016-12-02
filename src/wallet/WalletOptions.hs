c6afa546aeeb65f5be61f1f94fbcf14b1945011d
Ivan Gromakovskii <gie1994@gmail.com>
2016-12-03 00:54:47 +0300

    [CSL-236] Add cabal flags to build wallet and web

--- src/wallet/WalletOptions.hs
+++ src/wallet/WalletOptions.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE CPP #-}
+
+#ifdef WITH_WALLET
 -- | Wallet's command line options.
 
 module WalletOptions
@@ -19,7 +22,9 @@ data WalletCommand
                , stDHTPeers   :: ![DHTNode]
                , stLogConfig  :: !(Maybe FilePath)
                , stLogsPrefix :: !(Maybe FilePath)}
+#ifdef WITH_WEB
     | ServeWallet { swPort :: !Word16}
+#endif
 
 commandParser :: Opts.Parser WalletCommand
 commandParser =
@@ -28,9 +33,11 @@ commandParser =
              [ Opts.command
                    "submit"
                    (Opts.info submitTxOpts (Opts.progDesc "Submit transactions"))
+#ifdef WITH_WEB
              , Opts.command
                    "serve"
                    (Opts.info serveWalletOpts (Opts.progDesc "Run web server"))
+#endif
              ])
   where
     submitTxOpts =
@@ -54,6 +61,7 @@ commandParser =
             (Opts.option Opts.auto $
              Opts.long "logs-prefix" <> Opts.metavar "FILEPATH" <>
              Opts.help "Prefix to logger output path")
+#ifdef WITH_WEB
     serveWalletOpts =
         ServeWallet <$>
         Opts.option
@@ -65,6 +73,7 @@ commandParser =
                  , Opts.value 8090
                  , Opts.showDefault
                  ])
+#endif
     peerHelpMsg =
         "Peer to connect to for initial peer discovery. Format example: \"localhost:1234/MHdtsP-oPf7UWly7QuXnLK5RDB8=\""
 
@@ -81,3 +90,4 @@ getWalletOptions =
     Opts.info
         (Opts.helper <*> optionsParser)
         (Opts.fullDesc `mappend` Opts.progDesc "Stupid wallet")
+#endif

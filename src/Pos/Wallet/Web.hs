3bb6c883ba8c3aa691ac01a5b26e3cc74ba9e7ad
Ivan Gromakovskii <gie1994@gmail.com>
2016-12-02 23:35:18 +0300

    [CSL-224] Add documentation of wallet web api

--- src/Pos/Wallet/Web.hs
+++ src/Pos/Wallet/Web.hs
@@ -5,4 +5,5 @@ module Pos.Wallet.Web
        ) where
 
 import           Pos.Wallet.Web.Api    as Exports
+import           Pos.Wallet.Web.Doc    as Exports
 import           Pos.Wallet.Web.Server as Exports

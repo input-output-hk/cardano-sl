d8072b238e0afa5bba724d73d60409a5235e0b30
Dmitry Mukhutdinov <flyingleafe@gmail.com>
2016-12-09 00:22:34 +0300

    [CSL-255] Endpoint for handling requests of blockchain parts

--- src/Pos/Communication/Server/Block.hs
+++ src/Pos/Communication/Server/Block.hs
@@ -12,6 +12,7 @@ module Pos.Communication.Server.Block
        , handleBlock
        , handleBlockHeader
        , handleBlockRequest
+       , handleBlockchainPartRequest
        ) where
 
 import           Control.Lens              ((^.))
@@ -27,8 +28,9 @@ import           System.Wlog               (logDebug, logError, logInfo, logNoti
 import           Universum
 
 import           Pos.Communication.Methods (announceBlock)
-import           Pos.Communication.Types   (RequestBlock (..), ResponseMode,
-                                            SendBlock (..), SendBlockHeader (..))
+import           Pos.Communication.Types   (RequestBlock (..), RequestBlockchainPart (..),
+                                            ResponseMode, SendBlock (..),
+                                            SendBlockHeader (..), SendBlockchainPart (..))
 import           Pos.Crypto                (hash, shortHashF)
 import           Pos.DHT                   (ListenerDHT (..), replyToNode)
 import           Pos.Slotting              (getCurrentSlot)
@@ -48,6 +50,7 @@ blockListeners =
     [ ListenerDHT handleBlock
     , ListenerDHT handleBlockHeader
     , ListenerDHT handleBlockRequest
+    , ListenerDHT handleBlockchainPartRequest
     ]
 
 -- | Handler 'SendBlock' event.
@@ -159,3 +162,22 @@ handleBlockRequest (RequestBlock h) = do
     sendBlockBack block = do
         logDebug $ sformat ("Sending block " %build % " in reply") h
         replyToNode $ SendBlock block
+
+-- | Handle 'RequestBlockchainPart' message
+handleBlockchainPartRequest
+    :: ResponseMode ssc m
+    => RequestBlockchainPart ssc -> m ()
+handleBlockchainPartRequest RequestBlockchainPart {..} = do
+    logDebug $ sformat ("Blockchain part (range "%build%".."%build%
+                        ", count "%build%" is requested")
+        rbFromBlock rbUntilBlock rbCount
+    either logErr sendChainPart =<< St.getChainPart rbFromBlock rbUntilBlock rbCount
+  where
+    logErr = logWarning . sformat ("Error while fetching part of blockchain: "%stext)
+    sendChainPart cp = do
+        let fstH = headerHash <$> head cp
+            lc = length cp
+        logDebug $ sformat ("Sending chain part of length "%int%
+                            ", starting with "%build) lc fstH
+        replyToNode $ SendBlockchainPart cp
+

curtisz/pos-haskell-prototype

[CSL-242] Refactor lightweight crypto types

src/Pos/Crypto/Arbitrary.hs
@@ -1,4 +1,6 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeApplications #-}
+
 -- | `Arbitrary` instances for using in tests and benchmarks
 
 module Pos.Crypto.Arbitrary
@@ -18,11 +20,10 @@ import           Pos.Crypto.SecretSharing     (EncShare,Secret, SecretProof,
                                                SecretSharingExtra, Share, VssKeyPair,
                                                VssPublicKey, decryptShare, genSharedSecret,
                                                toVssPublicKey, vssKeyGen)
-import           Pos.Crypto.SerTypes          (LEncShare, LSecret, LSecretProof,
-                                               LSecretSharingExtra, LShare, LVssPublicKey)
+import           Pos.Crypto.SerTypes          ()
 import           Pos.Crypto.Signing           (PublicKey, SecretKey, Signature, Signed,
                                                keyGen, mkSigned, sign)
-import           Pos.Util                     (Serialized (..))
+import           Pos.Util                     (Lightweight (..), Serialized (..))
 import           Pos.Util.Arbitrary           (Nonrepeating (..), sublistN, unsafeMakePool)
 
 {- A note on 'Arbitrary' instances
@@ -80,7 +81,7 @@ instance Arbitrary VssKeyPair where
 instance Arbitrary VssPublicKey where
     arbitrary = toVssPublicKey <$> arbitrary
 
-instance Arbitrary LVssPublicKey where
+instance Arbitrary (Lightweight VssPublicKey) where
     arbitrary = serialize @VssPublicKey <$> arbitrary
 
 instance Nonrepeating VssKeyPair where
@@ -112,16 +113,16 @@ sharedSecrets =
 instance Arbitrary SecretSharingExtra where
     arbitrary = elements $ fmap (view _1) sharedSecrets
 
-instance Arbitrary LSecretSharingExtra where
+instance Arbitrary (Lightweight SecretSharingExtra) where
     arbitrary = serialize @SecretSharingExtra <$> arbitrary
 
-instance Arbitrary LSecretProof where
+instance Arbitrary (Lightweight SecretProof) where
     arbitrary = serialize @SecretProof <$> arbitrary
 
 instance Arbitrary Secret where
     arbitrary = elements $ fmap (view _2) sharedSecrets
 
-instance Arbitrary LSecret where
+instance Arbitrary (Lightweight Secret) where
     arbitrary = serialize @Secret <$> arbitrary
 
 instance Arbitrary SecretProof where
@@ -130,11 +131,11 @@ instance Arbitrary SecretProof where
 instance Arbitrary EncShare where
     arbitrary = elements $ concat $ fmap (view _4) sharedSecrets
 
-instance Arbitrary LEncShare where
+instance Arbitrary (Lightweight EncShare) where
     arbitrary = serialize @EncShare <$> arbitrary
 
 instance Arbitrary Share where
     arbitrary = unsafePerformIO <$> (decryptShare <$> arbitrary <*> arbitrary)
 
-instance Arbitrary LShare where
+instance Arbitrary (Lightweight Share) where
     arbitrary = serialize @Share <$> arbitrary

src/Pos/Ssc/GodTossing/Types/Base.hs
@@ -26,10 +26,11 @@ import           Data.SafeCopy       (base, deriveSafeCopySimple)
 import           Data.Text.Buildable (Buildable (..))
 import           Universum
 
-import           Pos.Crypto          (LEncShare, LSecret, LSecretProof,
-                                      LSecretSharingExtra, LShare, LVssPublicKey,
-                                      PublicKey, SecretKey, Signature, sign, toPublic)
+import           Pos.Crypto          (EncShare, PublicKey, Secret, Share, SecretKey,
+                                      SecretProof, SecretSharingExtra, Signature,
+                                      VssPublicKey, sign, toPublic)
 import           Pos.Types.Types     (Address (..), EpochIndex)
+import           Pos.Util            (Lightweight (..))
 
 ----------------------------------------------------------------------------
 -- Types, instances
@@ -40,9 +41,9 @@ type PKSet = HashSet Address
 -- | Commitment is a message generated during the first stage of
 -- MPC. It contains encrypted shares and proof of secret.
 data Commitment = Commitment
-    { commExtra  :: !LSecretSharingExtra
-    , commProof  :: !LSecretProof
-    , commShares :: !(HashMap LVssPublicKey LEncShare)
+    { commExtra  :: !(Lightweight SecretSharingExtra)
+    , commProof  :: !(Lightweight SecretProof)
+    , commShares :: !(HashMap (Lightweight VssPublicKey) (Lightweight EncShare))
     } deriving (Show, Eq, Generic)
 
 instance Binary Commitment
@@ -57,7 +58,7 @@ type CommitmentsMap = HashMap Address SignedCommitment
 
 -- | Opening reveals secret.
 newtype Opening = Opening
-    { getOpening :: LSecret
+    { getOpening :: (Lightweight Secret)
     } deriving (Show, Eq, Generic, Binary, Buildable)
 
 type OpeningsMap = HashMap Address Opening
@@ -69,7 +70,7 @@ type OpeningsMap = HashMap Address Opening
 -- Specifically, if node identified by 'Address' X has received a share
 -- from node identified by key Y, this share will be at @sharesMap ! X ! Y@.
 
-type InnerSharesMap = HashMap Address LShare
+type InnerSharesMap = HashMap Address (Lightweight Share)
 
 type SharesMap = HashMap Address InnerSharesMap
 
@@ -83,14 +84,14 @@ type SharesMap = HashMap Address InnerSharesMap
 -- Other nodes accept this certificate if it is valid and if node really
 -- has some stake.
 data VssCertificate = VssCertificate
-    { vcVssKey     :: !LVssPublicKey
-    , vcSignature  :: !(Signature LVssPublicKey)
+    { vcVssKey     :: !(Lightweight VssPublicKey)
+    , vcSignature  :: !(Signature (Lightweight VssPublicKey))
     , vcSigningKey :: !PublicKey
     } deriving (Show, Eq, Generic)
 
 instance Binary VssCertificate
 
-mkVssCertificate :: SecretKey -> LVssPublicKey -> VssCertificate
+mkVssCertificate :: SecretKey -> (Lightweight VssPublicKey) -> VssCertificate
 mkVssCertificate sk vk = VssCertificate vk (sign sk vk) $ toPublic sk
 
 -- | VssCertificatesMap contains all valid certificates collected

input-output-hk/cardano-sl

src/Pos/Crypto/Arbitrary.hs
@@ -1,4 +1,6 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeApplications #-}
+
 -- | `Arbitrary` instances for using in tests and benchmarks
 
 module Pos.Crypto.Arbitrary
@@ -18,11 +20,10 @@ import           Pos.Crypto.SecretSharing     (EncShare,Secret, SecretProof,
                                                SecretSharingExtra, Share, VssKeyPair,
                                                VssPublicKey, decryptShare, genSharedSecret,
                                                toVssPublicKey, vssKeyGen)
-import           Pos.Crypto.SerTypes          (LEncShare, LSecret, LSecretProof,
-                                               LSecretSharingExtra, LShare, LVssPublicKey)
+import           Pos.Crypto.SerTypes          ()
 import           Pos.Crypto.Signing           (PublicKey, SecretKey, Signature, Signed,
                                                keyGen, mkSigned, sign)
-import           Pos.Util                     (Serialized (..))
+import           Pos.Util                     (AsBinary (..), Serialized (..))
 import           Pos.Util.Arbitrary           (Nonrepeating (..), sublistN, unsafeMakePool)
 
 {- A note on 'Arbitrary' instances
@@ -80,7 +81,7 @@ instance Arbitrary VssKeyPair where
 instance Arbitrary VssPublicKey where
     arbitrary = toVssPublicKey <$> arbitrary
 
-instance Arbitrary LVssPublicKey where
+instance Arbitrary (AsBinary VssPublicKey) where
     arbitrary = serialize @VssPublicKey <$> arbitrary
 
 instance Nonrepeating VssKeyPair where
@@ -112,16 +113,16 @@ sharedSecrets =
 instance Arbitrary SecretSharingExtra where
     arbitrary = elements $ fmap (view _1) sharedSecrets
 
-instance Arbitrary LSecretSharingExtra where
+instance Arbitrary (AsBinary SecretSharingExtra) where
     arbitrary = serialize @SecretSharingExtra <$> arbitrary
 
-instance Arbitrary LSecretProof where
+instance Arbitrary (AsBinary SecretProof) where
     arbitrary = serialize @SecretProof <$> arbitrary
 
 instance Arbitrary Secret where
     arbitrary = elements $ fmap (view _2) sharedSecrets
 
-instance Arbitrary LSecret where
+instance Arbitrary (AsBinary Secret) where
     arbitrary = serialize @Secret <$> arbitrary
 
 instance Arbitrary SecretProof where
@@ -130,11 +131,11 @@ instance Arbitrary SecretProof where
 instance Arbitrary EncShare where
     arbitrary = elements $ concat $ fmap (view _4) sharedSecrets
 
-instance Arbitrary LEncShare where
+instance Arbitrary (AsBinary EncShare) where
     arbitrary = serialize @EncShare <$> arbitrary
 
 instance Arbitrary Share where
     arbitrary = unsafePerformIO <$> (decryptShare <$> arbitrary <*> arbitrary)
 
-instance Arbitrary LShare where
+instance Arbitrary (AsBinary Share) where
     arbitrary = serialize @Share <$> arbitrary

src/Pos/Ssc/GodTossing/Types/Base.hs
@@ -26,10 +26,11 @@ import           Data.SafeCopy       (base, deriveSafeCopySimple)
 import           Data.Text.Buildable (Buildable (..))
 import           Universum
 
-import           Pos.Crypto          (LEncShare, LSecret, LSecretProof,
-                                      LSecretSharingExtra, LShare, LVssPublicKey,
-                                      PublicKey, SecretKey, Signature, sign, toPublic)
+import           Pos.Crypto          (EncShare, PublicKey, Secret, Share, SecretKey,
+                                      SecretProof, SecretSharingExtra, Signature,
+                                      VssPublicKey, sign, toPublic)
 import           Pos.Types.Types     (Address (..), EpochIndex)
+import           Pos.Util            (AsBinary (..))
 
 ----------------------------------------------------------------------------
 -- Types, instances
@@ -40,9 +41,9 @@ type PKSet = HashSet Address
 -- | Commitment is a message generated during the first stage of
 -- MPC. It contains encrypted shares and proof of secret.
 data Commitment = Commitment
-    { commExtra  :: !LSecretSharingExtra
-    , commProof  :: !LSecretProof
-    , commShares :: !(HashMap LVssPublicKey LEncShare)
+    { commExtra  :: !(AsBinary SecretSharingExtra)
+    , commProof  :: !(AsBinary SecretProof)
+    , commShares :: !(HashMap (AsBinary VssPublicKey) (AsBinary EncShare))
     } deriving (Show, Eq, Generic)
 
 instance Binary Commitment
@@ -57,7 +58,7 @@ type CommitmentsMap = HashMap Address SignedCommitment
 
 -- | Opening reveals secret.
 newtype Opening = Opening
-    { getOpening :: LSecret
+    { getOpening :: (AsBinary Secret)
     } deriving (Show, Eq, Generic, Binary, Buildable)
 
 type OpeningsMap = HashMap Address Opening
@@ -69,7 +70,7 @@ type OpeningsMap = HashMap Address Opening
 -- Specifically, if node identified by 'Address' X has received a share
 -- from node identified by key Y, this share will be at @sharesMap ! X ! Y@.
 
-type InnerSharesMap = HashMap Address LShare
+type InnerSharesMap = HashMap Address (AsBinary Share)
 
 type SharesMap = HashMap Address InnerSharesMap
 
@@ -83,14 +84,14 @@ type SharesMap = HashMap Address InnerSharesMap
 -- Other nodes accept this certificate if it is valid and if node really
 -- has some stake.
 data VssCertificate = VssCertificate
-    { vcVssKey     :: !LVssPublicKey
-    , vcSignature  :: !(Signature LVssPublicKey)
+    { vcVssKey     :: !(AsBinary VssPublicKey)
+    , vcSignature  :: !(Signature (AsBinary VssPublicKey))
     , vcSigningKey :: !PublicKey
     } deriving (Show, Eq, Generic)
 
 instance Binary VssCertificate
 
-mkVssCertificate :: SecretKey -> LVssPublicKey -> VssCertificate
+mkVssCertificate :: SecretKey -> (AsBinary VssPublicKey) -> VssCertificate
 mkVssCertificate sk vk = VssCertificate vk (sign sk vk) $ toPublic sk
 
 -- | VssCertificatesMap contains all valid certificates collected
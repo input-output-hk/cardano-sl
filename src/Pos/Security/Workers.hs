b30b07addc634eeabe1ad0984b086b75f2b1b79d
Zakhar Voit <zakharvoit@gmail.com>
2016-12-12 21:34:20 +0300

    [CSL-267] Add processing of deeper blocks

--- src/Pos/Security/Workers.hs
+++ src/Pos/Security/Workers.hs
@@ -6,25 +6,26 @@ module Pos.Security.Workers
        ) where
 
 import           Control.Lens                      ((^.))
-import           Control.Monad.Trans.State         (StateT (..), get, modify, put)
+import           Control.Monad.Trans.State         (StateT (..), get, put)
 import qualified Data.HashMap.Strict               as HM
 import           Data.Tagged                       (Tagged (..))
 import           Formatting                        (build, int, sformat, (%))
 import           System.Wlog                       (logWarning)
 import           Universum                         hiding (modify)
 
+import           Pos.Constants                     (k)
 import           Pos.Slotting                      (onNewSlot)
 import           Pos.Ssc.Class.Types               (Ssc (..))
-import           Pos.Ssc.GodTossing.Functions      (isOpeningId)
 import           Pos.Ssc.GodTossing.Types.Instance ()
 import           Pos.Ssc.GodTossing.Types.Type     (SscGodTossing)
 import           Pos.Ssc.GodTossing.Types.Types    (GtPayload (..))
 import           Pos.Ssc.NistBeacon                (SscNistBeacon)
-import           Pos.State                         (getHeadBlock)
-import           Pos.Types                         (Address, SlotId, MainBlock, blockMpc,
+import           Pos.State                         (getHeadBlock, getBlockByDepth)
+import           Pos.Types                         (Address, EpochIndex, SlotId (..),
+                                                    MainBlock, blockMpc,
                                                     flattenSlotId, flattenSlotId,
-                                                    gbHeader, headerSlot,
-                                                    makePubKeyAddress, slotIdF)
+                                                    gbHeader, headerSlot, makePubKeyAddress,
+                                                    slotIdF)
 import           Pos.WorkMode                      (WorkMode, getNodeContext, ncPublicKey)
 
 class Ssc ssc => SecurityWorkersClass ssc where
@@ -65,38 +66,42 @@ checkForIgnoredCommitmentsWorker :: forall m. WorkMode SscGodTossing m => m ()
 checkForIgnoredCommitmentsWorker =
   runStateT (onNewSlot True checkForIgnoredCommitmentsWorkerImpl) 0 >> return ()
 
-type EpochsCount = Word64
-
 checkForIgnoredCommitmentsWorkerImpl ::
   forall m. WorkMode SscGodTossing m
-  => SlotId -> StateT EpochsCount m ()
+  => SlotId -> StateT EpochIndex m ()
 checkForIgnoredCommitmentsWorkerImpl slotId = do
-  headBlock <- getHeadBlock
-  case headBlock of
-    Left _ -> return ()
-    Right blk -> processBlock slotId blk
+  commitmentIgnored <- not <$> isCommitmentIncludedInAnyPreviousBlock slotId
+  lastCommitment <- get
+  when (and [commitmentIgnored, siEpoch slotId - lastCommitment > commitmentsAreIgnoredThreshold]) $ do
+    lift reportAboutEclipsed
+
+isCommitmentIncludedInAnyPreviousBlock ::
+  forall m. WorkMode SscGodTossing m
+  => SlotId -> StateT EpochIndex m Bool
+isCommitmentIncludedInAnyPreviousBlock slotId = do
+  fmap or $ forM [0 .. k - 1] $ \depth -> do
+    headBlock <- getBlockByDepth depth
+    case headBlock of
+      Just (Right blk) -> logWarning (sformat ("Malicious: processing depth: "%int) depth) >> processBlock slotId blk
+      _ -> return False
 
 isCommitmentInPayload  :: forall m. WorkMode SscGodTossing m => Address -> GtPayload -> m Bool
 isCommitmentInPayload addr p@(CommitmentsPayload commitments _) = do
   logWarning $ sformat ("Malicious commitment: "%build) p
   return $ HM.member addr commitments
+
 isCommitmentInPayload _ p = do
   logWarning $ sformat ("Malicious other-type: "%build) p
   return False
 
 processBlock ::
   forall m. WorkMode SscGodTossing m
-  => SlotId -> MainBlock SscGodTossing -> StateT EpochsCount m ()
+  => SlotId -> MainBlock SscGodTossing -> StateT EpochIndex m Bool
 processBlock slotId block = do
   ourAddr <- makePubKeyAddress . ncPublicKey <$> getNodeContext
-  -- let commitmentInBlockchain = isCommitmentInPayload ourAddr (block ^. blockMpc)
   commitmentInBlockchain <- lift $ isCommitmentInPayload ourAddr (block ^. blockMpc)
   when commitmentInBlockchain $
-    put 0
-  -- TODO(voit): Check if isOpeningId is the correct time to check about commitment
+    put $ siEpoch slotId
   epochs <- get
   logWarning $ sformat ("Malicious: slot="%slotIdF%", epochs="%int%", hasCommitment="%build%", ourAddr="%build) slotId epochs commitmentInBlockchain ourAddr
-  when (and [isOpeningId slotId, not commitmentInBlockchain]) $ do
-    modify (+1)
-    when (epochs > commitmentsAreIgnoredThreshold) $
-      lift reportAboutEclipsed
+  return commitmentInBlockchain

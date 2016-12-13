ae788a5f7d3f7ea6f237bfb9ffd6202e5ba17b15
Ivan Gromakovskii <gie1994@gmail.com>
2016-12-13 20:43:07 +0300

    [CSL-279] Change Blocks DB to work with StoredBlocks

--- src/Pos/Modern/DB/Block.hs
+++ src/Pos/Modern/DB/Block.hs
@@ -2,7 +2,9 @@
 
 module Pos.Modern.DB.Block
        ( getBlock
+       , getStoredBlock
        , getUndo
+       , isBlockInMainChain
        ) where
 
 import           Universum
@@ -10,6 +12,7 @@ import           Universum
 import           Pos.Binary              (Bi, encodeStrict)
 import           Pos.Modern.DB.Class     (MonadDB, getBlockDB)
 import           Pos.Modern.DB.Functions (rocksGetBi)
+import           Pos.Modern.DB.Types     (StoredBlock (..))
 import           Pos.Ssc.Class           (Ssc)
 import           Pos.Types               (Block, HeaderHash, Undo)
 
@@ -18,14 +21,26 @@ getBi
     => ByteString -> m (Maybe v)
 getBi k = rocksGetBi k =<< getBlockDB
 
+-- | Get StoredBlock by hash from Block DB.
+getStoredBlock
+    :: (Ssc ssc, MonadDB ssc m)
+    => HeaderHash ssc -> m (Maybe (StoredBlock ssc))
+getStoredBlock = getBi . ("b" <>) . encodeStrict
+
 -- | Get block with given hash from Block DB.
 getBlock
     :: (Ssc ssc, MonadDB ssc m)
     => HeaderHash ssc -> m (Maybe (Block ssc))
-getBlock = getBi . ("b" <>) . encodeStrict
+getBlock = fmap (fmap sbBlock) . getStoredBlock
+
+-- | Get block with given hash from Block DB.
+isBlockInMainChain
+    :: (Ssc ssc, MonadDB ssc m)
+    => HeaderHash ssc -> m Bool
+isBlockInMainChain = fmap (maybe True sbInMain) . getStoredBlock
 
 -- | Get undo data for block with given hash from Block DB.
 getUndo
-    :: (Ssc ssc, MonadDB ssc m)
+    :: (MonadDB ssc m)
     => HeaderHash ssc -> m (Maybe Undo)
 getUndo = getBi . ("u" <>) . encodeStrict

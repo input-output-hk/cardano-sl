dacee55c58c9fd82f3d1f9cee294b1aca2067cf9
Ivan Gromakovskii <gie1994@gmail.com>
2016-12-14 00:18:00 +0300

    Hopefully fix build

--- src/Pos/Wallet/WalletMode.hs
+++ src/Pos/Wallet/WalletMode.hs
@@ -8,7 +8,9 @@ module Pos.Wallet.WalletMode
        ) where
 
 import           Pos.Context           (WithNodeContext)
+#ifdef WITH_ROCKS
 import qualified Pos.Modern.DB         as Modern
+#endif
 import           Pos.Ssc.Class.Storage (SscStorageMode)
 import           Pos.State             (MonadDB)
 import           Pos.Txp.LocalData     (MonadTxLD)

aa694e4a7dbe35322ea30afa7045b73e4a769d50
Ivan Gromakovskii <gie1994@gmail.com>
2016-12-13 18:29:32 +0300

    [CSL-279] Add very simple interface to Blocks DB

---
+++ src/Pos/Modern/DB/Block.hs
@@ -0,0 +1,31 @@
+-- | Interface to Blocks DB.
+
+module Pos.Modern.DB.Block
+       ( getBlock
+       , getUndo
+       ) where
+
+import           Universum
+
+import           Pos.Binary              (Bi, encodeStrict)
+import           Pos.Modern.DB.Class     (MonadDB, getBlockDB)
+import           Pos.Modern.DB.Functions (rocksGetBi)
+import           Pos.Ssc.Class           (Ssc)
+import           Pos.Types               (Block, HeaderHash, Undo)
+
+getBi
+    :: (MonadDB ssc m, Bi v)
+    => ByteString -> m (Maybe v)
+getBi k = rocksGetBi k =<< getBlockDB
+
+-- | Get block with given hash from Block DB.
+getBlock
+    :: (Ssc ssc, MonadDB ssc m)
+    => HeaderHash ssc -> m (Maybe (Block ssc))
+getBlock = getBi . ("b" <>) . encodeStrict
+
+-- | Get undo data for block with given hash from Block DB.
+getUndo
+    :: (Ssc ssc, MonadDB ssc m)
+    => HeaderHash ssc -> m (Maybe Undo)
+getUndo = getBi . ("u" <>) . encodeStrict

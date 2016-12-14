5c7faf361aba12d301e7f110f0e04a7b2bc065c4
Ivan Gromakovskii <gie1994@gmail.com>
2016-12-15 01:08:15 +0300

    [CSL-306] Add dummy Pos.Modern.Blockp.Listeners

--- src/Pos/Modern/Blockp/Server.hs
+++ src/Pos/Modern/Blockp/Server.hs
@@ -1,7 +1,9 @@
 -- | Re-exports Blockp server functionality.
 
 module Pos.Modern.Blockp.Server
-       ( module Pos.Modern.Blockp.Server.State
+       ( module Pos.Modern.Blockp.Server.Listeners
+       , module Pos.Modern.Blockp.Server.State
        ) where
 
+import           Pos.Modern.Blockp.Server.Listeners
 import           Pos.Modern.Blockp.Server.State

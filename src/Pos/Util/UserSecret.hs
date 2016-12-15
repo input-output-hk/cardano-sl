d2bc502be8c6ff9f26e003a506d43800666896c0
Dmitry Mukhutdinov <flyingleafe@gmail.com>
2016-12-16 01:49:55 +0300

    [CSL-263] Storage for secret keys ready

--- src/Pos/Util/UserSecret.hs
+++ src/Pos/Util/UserSecret.hs
@@ -15,11 +15,12 @@ module Pos.Util.UserSecret
 import           Control.Lens         (makeLenses, to, (.~), (^.))
 import           Control.Monad.Fail   (MonadFail, fail)
 import qualified Data.ByteString.Lazy as BSL
+import           Data.Default         (Default (..))
 import           System.FileLock      (FileLock, SharedExclusive (..), lockFile,
                                        unlockFile, withFileLock)
 import           Universum
 
-import           Pos.Binary           (Bi (..), decode, encode)
+import           Pos.Binary           (Bi (..), decode, decodeFull, encode)
 import           Pos.Crypto           (SecretKey)
 
 -- | User secret data. Includes secret keys only for now (not
@@ -38,20 +39,22 @@ makeLenses ''UserSecret
 canWrite :: UserSecret -> Bool
 canWrite u = u ^. usLock . to isJust
 
+instance Default UserSecret where
+    def = UserSecret [] "" Nothing
+
 -- | It's not network/system-related, so instance shouldn't be under
 -- @Pos.Binary.*@.
 instance Bi UserSecret where
     put UserSecret{..} = put _usKeys
-    get = do
-        _usKeys <- get
-        pure $ UserSecret { _usPath = "", _usLock = Nothing, ..}
+    get = get >>= pure . flip (usKeys .~) def
 
 -- | Reads user secret from the given file.
+-- If the file does not exist/is empty, returns empty user secret
 peekUserSecret :: (MonadIO m) => FilePath -> m UserSecret
 peekUserSecret path =
     liftIO $ withFileLock path Shared $ const $ do
-        content <- decode <$> BSL.readFile path
-        pure $ content & usPath .~ path
+        econtent <- decodeFull <$> BSL.readFile path
+        pure $ either (const def) identity econtent & usPath .~ path
 
 -- | Read user secret putting an exclusive lock on it. To unlock, use
 -- 'writeUserSecret' or 'writeUserSecretRelease'.
@@ -62,17 +65,18 @@ takeUserSecret path = liftIO $ do
     pure $ content & usPath .~ path
                    & usLock .~ Just l
 
--- | Writes user secret if file is locked by this process.
+-- | Writes user secret .
 writeUserSecret :: (MonadIO m, MonadFail m) => UserSecret -> m ()
 writeUserSecret u
-    | not (canWrite u) = fail "writeUserSecret: UserSecret is not writable"
-    | otherwise = liftIO $ BSL.writeFile (u ^. usPath) $ encode u
+    | canWrite u = fail "writeUserSecret: UserSecret is already locked"
+    | otherwise = liftIO $ withFileLock (u ^. usPath) Exclusive $ const $
+                  BSL.writeFile (u ^. usPath) $ encode u
 
 -- | Writes user secret and releases the lock. UserSecret can't be
 -- used after this function call anymore.
 writeUserSecretRelease :: (MonadIO m, MonadFail m) => UserSecret -> m ()
 writeUserSecretRelease u
-    | not (canWrite u) = fail "writeUserSecret: UserSecret is not writable"
+    | not (canWrite u) = fail "writeUserSecretRelease: UserSecret is not writable"
     | otherwise = do
         writeUserSecret u
         liftIO $ unlockFile
--- src/Pos/Wallet/KeyStorage.hs
+++ src/Pos/Wallet/KeyStorage.hs
@@ -6,13 +6,17 @@
 {-# LANGUAGE UndecidableInstances  #-}
 
 module Pos.Wallet.KeyStorage
-       (
+       ( MonadKeys (..)
+       , KeyStorage (..)
+       , runKeyStorage
        ) where
 
 import qualified Control.Concurrent.STM    as STM
-import           Control.Lens              (iso)
+import           Control.Lens              (iso, use, (%=), (<>=))
 import           Control.Monad.Catch       (MonadCatch, MonadMask, MonadThrow)
-import           Control.Monad.Reader      (ReaderT (ReaderT), ask)
+import           Control.Monad.Fail        (MonadFail)
+import           Control.Monad.Reader      (ReaderT (..), ask)
+import           Control.Monad.State       (MonadState (..))
 import           Control.Monad.Trans.Class (MonadTrans)
 import           Control.TimeWarp.Rpc      (MonadDialog, MonadTransfer (..))
 import           Control.TimeWarp.Timed    (MonadTimed (..), ThreadId)
@@ -21,6 +25,7 @@ import           System.Wlog               (CanLog, HasLoggerName)
 import           Universum
 
 import           Pos.Context               (WithNodeContext)
+import           Pos.Crypto                (SecretKey)
 #ifdef WITH_ROCKS
 import qualified Pos.Modern.DB             as Modern (MonadDB)
 import qualified Pos.Modern.Txp.Class      as Modern (MonadTxpLD)
@@ -29,15 +34,24 @@ import           Pos.DHT.Model             (MonadDHT, MonadMessageDHT)
 import           Pos.Slotting              (MonadSlots)
 import           Pos.Ssc.Class             (MonadSscLD)
 import           Pos.State                 (MonadDB)
+import           Pos.Util                  ()
 import           Pos.Util.JsonLog          (MonadJL)
-import           Pos.Util.UserSecret       (UserSecret, peekUserSecret, takeUserSecret,
-                                            writeUserSecret, writeUserSecretRelease)
+import           Pos.Util.UserSecret       (UserSecret, peekUserSecret, usKeys,
+                                            writeUserSecret)
+
+-- | Typeclass of monad with access to secret keys
+class Monad m => MonadKeys m where
+    getSecretKeys :: m [SecretKey]
+    addSecretKey :: SecretKey -> m ()
+    deleteSecretKey :: Int -> m ()
 
 newtype KeyStorage m a = KeyStorage
     { getKeyStorage :: ReaderT (STM.TVar UserSecret) m a
-    } deriving (Functor, Applicative, Monad, MonadTrans, MonadTimed, MonadThrow, MonadSlots,
-                MonadCatch, MonadIO, HasLoggerName, MonadDialog s p, WithNodeContext ssc, MonadJL,
-                MonadDB ssc, CanLog, MonadMask, MonadDHT, MonadMessageDHT s, MonadSscLD ssc)
+    } deriving (Functor, Applicative, Monad, MonadTrans, MonadTimed,
+                MonadThrow, MonadSlots, MonadFail, MonadCatch, MonadIO,
+                HasLoggerName, MonadDialog s p, WithNodeContext ssc,
+                MonadJL, MonadDB ssc, CanLog, MonadMask, MonadDHT,
+                MonadMessageDHT s, MonadSscLD ssc)
 
 type instance ThreadId (KeyStorage m) = ThreadId m
 
@@ -47,7 +61,23 @@ instance Monad m => WrappedM (KeyStorage m) where
 
 instance MonadTransfer s m => MonadTransfer s (KeyStorage m)
 
+instance (MonadIO m, MonadFail m) => MonadState UserSecret (KeyStorage m) where
+    get = KeyStorage ask >>= atomically . STM.readTVar
+    put s = KeyStorage ask >>= atomically . flip STM.writeTVar s >>
+            writeUserSecret s
+
 #ifdef WITH_ROCKS
 deriving instance Modern.MonadDB ssc m => Modern.MonadDB ssc (KeyStorage m)
 deriving instance Modern.MonadTxpLD ssc m => Modern.MonadTxpLD ssc (KeyStorage m)
 #endif
+
+runKeyStorage :: MonadIO m => FilePath -> KeyStorage m a -> m a
+runKeyStorage fp (KeyStorage r) =
+    peekUserSecret fp >>= liftIO . STM.newTVarIO >>= runReaderT r
+
+instance (MonadIO m, MonadFail m) => MonadKeys (KeyStorage m) where
+    getSecretKeys = use usKeys
+    addSecretKey sk = usKeys <>= [sk]
+    deleteSecretKey i = usKeys %= deleteAt i
+      where deleteAt j ls = let (l, r) = splitAt j ls
+                            in l ++ drop 1 r

8b54214a605d864dc2346f8d29febd0904d3bfe2
Ivan Gromakovskii <gie1994@gmail.com>
2016-12-16 21:51:40 +0300

    [CSL-280] Add documentation for TxpLD with invariants

--- src/Pos/Modern/Txp/Class.hs
+++ src/Pos/Modern/Txp/Class.hs
@@ -17,6 +17,16 @@ import           Control.Monad.Trans          (MonadTrans)
 import           Pos.Modern.Txp.Storage.Types (MemPool, UtxoView)
 import           Pos.Types                    (HeaderHash)
 
+-- | LocalData of transactions processing.
+-- There are two invariants which must hold for local data
+-- (where uv is UtxoView, memPool is MemPool and tip is HeaderHash):
+-- 1. Suppose 'blks' is sequence of blocks from the very beggining up
+-- to 'tip'. If one applies 'blks' to genesis Utxo, resulting Utxo
+-- (let's call it 'utxo1') will be such that all transactions from
+-- 'memPool' are valid with respect to it.
+-- 2. If one applies all transactions from 'memPool' to 'utxo1',
+-- resulting Utxo will be equivalent to 'uv' with respect to
+-- MonadUtxo.
 type TxpLD ssc = (UtxoView ssc, MemPool, HeaderHash ssc)
 
 class Monad m => MonadTxpLD ssc m | m -> ssc where
@@ -33,7 +43,7 @@ class Monad m => MonadTxpLD ssc m | m -> ssc where
     default getUtxoView :: MonadTrans t => t m (UtxoView ssc)
     getUtxoView = lift  getUtxoView
 
-    default setUtxoView :: MonadTrans t =>UtxoView ssc -> t m ()
+    default setUtxoView :: MonadTrans t => UtxoView ssc -> t m ()
     setUtxoView = lift . setUtxoView
 
     default getMemPool :: MonadTrans t => t m MemPool

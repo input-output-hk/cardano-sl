a70ed9f78e35726f40dd248c7b9c3664b5005b8b
brunjlar <brunjlar@gmail.com>
2016-12-16 01:33:03 +0100

    [CSL-110] improved script (only partially done)

--- src/checks/Main.hs
+++ src/checks/Main.hs
@@ -3,19 +3,83 @@
 
 {-# LANGUAGE OverloadedStrings #-}
 
-import qualified Control.Foldl as Fold
+import qualified Control.Foldl as F
+import qualified Data.Map      as M
+import qualified Data.Text     as T
 import           Turtle
 import           Prelude       hiding (FilePath)
 
+main = putStrLn "to be implemented!"
+
+{-
 main :: IO ()
 main = do
     xs <- arguments
     case xs of
         [folder, file] -> pandoc file $ folderMarkDown folder
         _              -> err "Expected source folder and output file as arguments."
+-}
 
-data Check = Module !Text | CheckHeader !Int !Text | CheckItem !Int !Text
+type Tag = Text
 
+type ModuleName = Text
+
+type ErrorMessage = Text
+
+data Check = Check
+    { chTag  :: !(Maybe Tag)
+    , chPos  :: !Int
+    , chBody :: ![Either Tag Text]
+    } deriving Show
+
+data Module = Module
+    { modName   :: !ModuleName
+    , modChecks :: ![Check]
+    } deriving Show
+
+toModule :: [Text] -> Either ErrorMessage Module
+toModule xs = Module <$> getModuleName xs <*> pure (getChecks xs)
+
+  where
+
+    getModuleName :: [Text] -> Either ErrorMessage ModuleName
+    getModuleName []     = Left "no module name"
+    getModuleName (t:ts) = case match moduleNamePattern t of
+        [y] -> Right y
+        _   -> getModuleName ts
+
+    moduleNamePattern :: Pattern ModuleName
+    moduleNamePattern = text "module " *> star (noneOf " ") <* (char ' ' <* chars <|> eof *> pure ' ')
+
+    getChecks :: [Text] -> [Check]
+    getChecks = loop [] 1
+
+    loop :: [Check] -> Int -> [Text] -> [Check]
+    loop acc _ []     = reverse acc
+    loop acc i (t:ts) = case match checkPattern t of
+        [c]
+            | T.null c        -> loop  acc (succ i)                                       ts
+            | T.head c == '@' -> loop' acc (succ i) i (Just $ T.tail c) []                ts
+            | T.head c == '#' -> loop' acc (succ i) i Nothing           [Left $ T.tail c] ts
+            | otherwise       -> loop' acc (succ i) i Nothing           [Right c]         ts
+        _                     -> loop  acc (succ i)                                       ts
+
+    loop' :: [Check] -> Int -> Int -> Maybe Tag -> [Either Tag Text] -> [Text] -> [Check]
+    loop' acc _ j mt bs []       = reverse (Check mt j (reverse bs) : acc)
+    loop' acc i j mt bs (t : ts) = case match checkComment t of
+        [c]
+            | T.null c        -> loop  (Check mt j (reverse bs) : acc) (succ i)                             ts
+            | T.head c == '#' -> loop' acc                             (succ i) j mt (Left (T.tail c) : bs) ts
+            | otherwise       -> loop' acc                             (succ i) j mt (Right c : bs)         ts
+        _                     -> loop  (Check mt j (reverse bs) : acc) (succ i)                             ts
+
+    checkPattern :: Pattern Text
+    checkPattern = chars *> text "-- CHECK: " *> chars
+
+    checkComment :: Pattern Text
+    checkComment = chars *> text "-- " *> chars
+
+{-
 hsFiles :: FilePath -> Shell FilePath
 hsFiles folder = do
     file <- lstree folder
@@ -69,8 +133,4 @@ handleFile file = do
 
     line :: Int -> Text
     line n = " _(line " <> fromString (show n) <> ")_"
-
-{-
-handleFolder :: FilePath -> IO ()
-handleFolder folder = stdout $ return "# Checks" <|> (hsFiles folder >>= handleFile)
--}
+    -}

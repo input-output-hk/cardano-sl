c10eaaf03422011c0899754a4b74070173fb4e1c
Ante Kegalj <akegalj@gmail.com>
2016-12-16 15:19:48 +0100

    Remove not needed debug

--- daedalus/src/Daedalus/BackendApi.purs
+++ daedalus/src/Daedalus/BackendApi.purs
@@ -9,17 +9,12 @@ import Data.Tuple (Tuple)
 import Network.HTTP.Affjax (AJAX, get)
 import Pos.Types.Types (Coin)
 import Pos.Wallet.Web.ClientTypes (CAddress)
-import Debug.Trace (traceAnyM)
 
 -- TODO: remove traces, they are adding to increase verbosity in development
 makeRequest :: forall eff a. (Generic a) => String -> Aff (ajax :: AJAX | eff) (Either String a)
 makeRequest url = do
-    traceAnyM $ "makeRequest: Starting request " <> url
     res <- attempt $ get url
-    traceAnyM "makeRequest: Got reponse"
-    traceAnyM res
-    traceAnyM "makeRequest: Result of decoding"
-    traceAnyM $ either (Left <<< show) decode res
+    pure $ either (Left <<< show) decode res
     where
       decode res' = decodeJson res'.response
 

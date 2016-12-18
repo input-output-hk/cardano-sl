146c0c5668dd7709e12fa06cf8004abf0c9f4994
Ivan Gromakovskii <gie1994@gmail.com>
2016-12-18 22:07:47 +0300

    [CSL-306] Add logic of unsolicited headers processing

--- src/Pos/Block/Logic.hs
+++ src/Pos/Block/Logic.hs
@@ -3,19 +3,78 @@
 -- | Logic of blocks processing.
 
 module Pos.Block.Logic
-       ( verifyBlock
+       ( ClassifyHeaderRes (..)
        , applyBlock
+       , classifyNewHeader
+       , verifyBlock
        ) where
 
 import           Control.Lens           ((^.))
 import           Control.Monad.Catch    (onException)
+import           Data.Default           (Default (def))
+import qualified Data.Text              as T
+import           Serokell.Util.Verify   (VerificationRes (..))
 import           Universum
 
 import           Pos.Context            (putBlkSemaphore, takeBlkSemaphore)
 import qualified Pos.Modern.DB          as DB
 import           Pos.Modern.Txp.Storage (txApplyBlocks)
-import           Pos.Types              (Block, headerHash, prevBlockL)
+import           Pos.Slotting           (getCurrentSlot)
+import           Pos.Types              (Block, BlockHeader, VerifyHeaderParams (..),
+                                         blockHeader, difficultyL, headerHash, headerSlot,
+                                         prevBlockL, verifyHeader, vhpVerifyConsensus)
 import           Pos.WorkMode           (WorkMode)
+
+-- | Result of header classification.
+data ClassifyHeaderRes
+    = CHRcontinues      -- ^ Header continues our main chain.
+    | CHRalternative    -- ^ Header continues alternative chain which
+                        -- is more difficult.
+    | CHRuseless !Text  -- ^ Header is useless.
+    | CHRinvalid !Text  -- ^ Header is invalid.
+
+-- | Make `ClassifyHeaderRes` from list of error messages using
+-- `CHRinvalid` constructor. Intended to be used with `VerificationRes`.
+-- Note: this version forces computation of all error messages. It can be
+-- made more efficient but less informative by using head, for example.
+mkCHRinvalid :: [Text] -> ClassifyHeaderRes
+mkCHRinvalid = CHRinvalid . T.intercalate "; "
+
+-- | Classify new header announced by some node. Result is represented
+-- as ClassifyHeaderRes type.
+classifyNewHeader
+    :: (WorkMode ssc m)
+    => BlockHeader ssc -> m ClassifyHeaderRes
+-- Genesis headers seem useless, we can create them by ourselves.
+classifyNewHeader (Left _) = pure $ CHRuseless "genesis header is useless"
+classifyNewHeader (Right header) = do
+    curSlot <- getCurrentSlot
+    -- First of all we check whether header is from current slot and
+    -- ignore it if it's not.
+    if curSlot == header ^. headerSlot
+        then classifyNewHeaderDo <$> DB.getTip <*> DB.getTipBlock
+        else return $ CHRuseless "header is not for current slot"
+  where
+    classifyNewHeaderDo tip tipBlock
+        -- If header's parent is our tip, we verify it against tip's header.
+        | tip == header ^. prevBlockL =
+            let vhp =
+                    def
+                    { vhpVerifyConsensus = True
+                    , vhpPrevHeader = Just $ tipBlock ^. blockHeader
+                    }
+                verRes = verifyHeader vhp (Right header)
+            in case verRes of
+                   VerSuccess        -> CHRcontinues
+                   VerFailure errors -> mkCHRinvalid errors
+        -- If header's parent is not our tip, we check whether it's
+        -- more difficult than our main chain.
+        | tipBlock ^. difficultyL < header ^. difficultyL = CHRalternative
+        -- If header can't continue main chain and is not more
+        -- difficult than main chain, it's useless.
+        | otherwise =
+            CHRuseless $
+            "header doesn't continue main chain and is not more difficult"
 
 verifyBlock
     :: (WorkMode ssc m)
@@ -34,7 +93,7 @@ applyBlock :: (WorkMode ssc m) => Block ssc -> m Bool
 applyBlock blk = do
     tip <- takeBlkSemaphore
     let putBack = putBlkSemaphore tip
-    let putNew = putBlkSemaphore (headerHash blk)
+    let putNew = putBlkSemaphore $ headerHash blk
     if blk ^. prevBlockL == tip
         then True <$ onException (applyBlockDo blk >> putNew) putBack
         else False <$ putBack
--- src/Pos/Block/Server/Listeners.hs
+++ src/Pos/Block/Server/Listeners.hs
@@ -10,11 +10,18 @@ module Pos.Block.Server.Listeners
        ( blockListeners
        ) where
 
+import           Data.List.NonEmpty       (NonEmpty ((:|)))
+import           Formatting               (sformat, stext, (%))
+import           System.Wlog              (logDebug)
 import           Universum
 
 import           Pos.Binary.Communication ()
-import           Pos.Communication.Types  (MutSocketState, ResponseMode, SendBlock (..))
+import           Pos.Block.Logic          (ClassifyHeaderRes (..), classifyNewHeader)
+import           Pos.Communication.Types  (MsgBlockHeaders (..), MutSocketState,
+                                           ResponseMode, SendBlock (..))
+import           Pos.Crypto               (shortHashF)
 import           Pos.DHT.Model            (ListenerDHT (..), MonadDHTDialog)
+import           Pos.Types                (BlockHeader, headerHash)
 import           Pos.WorkMode             (WorkMode)
 
 -- | Listeners for requests related to blocks processing.
@@ -22,8 +29,35 @@ blockListeners
     :: (MonadDHTDialog (MutSocketState ssc) m, WorkMode ssc m)
     => [ListenerDHT (MutSocketState ssc) m]
 blockListeners =
-    [ ListenerDHT handleBlock
+    [ ListenerDHT handleBlockHeaders
+    , ListenerDHT handleBlock
     ]
+
+handleBlockHeaders
+    :: forall ssc m.
+       (ResponseMode ssc m)
+    => MsgBlockHeaders ssc -> m ()
+handleBlockHeaders (MsgBlockHeaders headers) = do
+    -- TODO: decide what to do depending on socket state
+    handleUnsolicitedHeaders headers
+
+handleUnsolicitedHeaders
+    :: forall ssc m.
+       (ResponseMode ssc m)
+    => NonEmpty (BlockHeader ssc) -> m ()
+handleUnsolicitedHeaders (header :| []) = do
+    classificationRes <- classifyNewHeader header
+    case classificationRes of
+        CHRcontinues -> pass -- TODO: request block
+        CHRalternative -> pass -- TODO: request multiple blocks or headers, dunno
+        CHRuseless reason ->
+            logDebug $
+            sformat
+                ("Header "%shortHashF%" is useless for the following reason: "%stext)
+                (headerHash header) reason
+        CHRinvalid _ -> pass -- TODO: ban node for sending invalid block.
+-- TODO: ban node for sending more than one unsolicited header.
+handleUnsolicitedHeaders _ = pass
 
 handleBlock
     :: forall ssc m.

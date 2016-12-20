57f23954eda725f316d41d6d57c8655060bfb7b0
Domen Kožar <domen@dev.si>
2016-12-20 22:24:52 +0100

    nix-shell: pin nixpkgs commit for determinism (thus sanity)

--- shell.nix
+++ shell.nix
@@ -1,13 +1,15 @@
-with import <nixpkgs> { };
+with import (fetchTarball https://github.com/NixOS/nixpkgs/archive/5ed1aee3af6ce80c63e534fc26d7ee611d4ca50d.tar.gz) { };
+
 let
   hsPkgs = haskell.packages.ghc801;
 in
   haskell.lib.buildStackProject {
      name = "cardano-sl";
      ghc = hsPkgs.ghc;
-     # cabal-install and stack pull in lots of dependencies on OSX so skip them
      buildInputs = [
        zlib openssh autoreconfHook openssl
-       gmp rocksdb git ] ++ (lib.optionals stdenv.isLinux [ cabal-install stack ]);
+       gmp rocksdb git
+     # cabal-install and stack pull in lots of dependencies on OSX so skip them
+     ] ++ (lib.optionals stdenv.isLinux [ cabal-install stack ]);
      LANG = "en_US.UTF-8";
   }

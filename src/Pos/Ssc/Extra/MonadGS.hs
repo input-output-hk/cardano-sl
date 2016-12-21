d0e19a26f0e82865d88ff187ecf088202055bfd9
Ivan Gromakovskii <gie1994@gmail.com>
2016-12-22 01:26:37 +0300

    [CSL-365] Add wrappers for content of SscStorageClassM

--- src/Pos/Ssc/Extra/MonadGS.hs
+++ src/Pos/Ssc/Extra/MonadGS.hs
@@ -16,21 +16,27 @@ module Pos.Ssc.Extra.MonadGS
        ( MonadSscGS (..)
        , sscRunGlobalQuery
        , sscRunGlobalModify
+
+       , sscApplyBlocks
        , sscCalculateSeed
+       , sscRollback
+       , sscVerifyBlocks
        ) where
 
-import           Control.Monad.Except  (ExceptT)
-import           Control.Monad.Trans   (MonadTrans)
-import           Control.TimeWarp.Rpc  (ResponseT)
+import           Control.Monad.Except    (ExceptT)
+import           Control.Monad.Trans     (MonadTrans)
+import           Control.TimeWarp.Rpc    (ResponseT)
+import           Serokell.Util           (VerificationRes)
 import           Universum
 
-import           Pos.Crypto            (Threshold)
-import           Pos.DHT.Model.Class   (DHTResponseT)
-import           Pos.DHT.Real          (KademliaDHT)
-import           Pos.Ssc.Class.Helpers (SscHelpersClassM (sscCalculateSeedQ))
-import           Pos.Ssc.Class.Types   (Ssc (..))
-import           Pos.Types.Types       (Address, EpochIndex, HeaderHash, SharedSeed,
-                                        SlotLeaders, Utxo)
+import           Pos.Crypto              (Threshold)
+import           Pos.DHT.Model.Class     (DHTResponseT)
+import           Pos.DHT.Real            (KademliaDHT)
+import           Pos.Ssc.Class.Helpers   (SscHelpersClassM (sscCalculateSeedQ))
+import           Pos.Ssc.Class.Storage   (SscStorageClassM (..))
+import           Pos.Ssc.Class.Types     (Ssc (..))
+import           Pos.State.Storage.Types (AltChain)
+import           Pos.Types.Types         (EpochIndex, SharedSeed)
 
 class Monad m => MonadSscGS ssc m | m -> ssc where
     getGlobalState    :: m (SscGlobalStateM ssc)
@@ -70,3 +76,21 @@ sscCalculateSeed
        (MonadSscGS ssc m, SscHelpersClassM ssc)
     => EpochIndex -> Threshold -> m (Either (SscSeedError ssc) SharedSeed)
 sscCalculateSeed e = sscRunGlobalQuery . sscCalculateSeedQ @ssc e
+
+sscApplyBlocks
+    :: forall ssc m.
+       (MonadSscGS ssc m, SscStorageClassM ssc)
+    => AltChain ssc -> m ()
+sscApplyBlocks = sscRunGlobalModify . sscApplyBlocksM @ssc
+
+sscRollback
+    :: forall ssc m.
+       (MonadSscGS ssc m, SscStorageClassM ssc)
+    => AltChain ssc -> m ()
+sscRollback = sscRunGlobalModify . sscRollbackM @ssc
+
+sscVerifyBlocks
+    :: forall ssc m.
+       (MonadSscGS ssc m, SscStorageClassM ssc)
+    => AltChain ssc -> m VerificationRes
+sscVerifyBlocks = sscRunGlobalQuery . sscVerifyBlocksM @ssc

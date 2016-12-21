7cd0b7c8af0c1166f2267527426608ac40757a6e
Ilya Peresadin <pva701@gmail.com>
2016-12-21 18:03:32 +0300

    [CSL-355] Added slot check to Block/Worker

--- src/Pos/Block/Worker.hs
+++ src/Pos/Block/Worker.hs
@@ -17,6 +17,7 @@ import           Control.Monad.State          (get)
 import qualified Data.HashMap.Strict          as HM
 import qualified Data.List.NonEmpty           as NE
 import           Serokell.Util.Exceptions     ()
+import           System.Wlog                  (logDebug)
 import           Universum
 
 import           Pos.Binary.Communication     ()
@@ -35,22 +36,26 @@ import           Pos.WorkMode                 (WorkMode)
 
 lpcOnNewSlot :: WorkMode ssc m => SlotId -> m () --Leaders and Participants computation
 lpcOnNewSlot SlotId{..} = withBlkSemaphore $ \tip -> do
-    blockUndos <- loadLastNBlocksWithUndo tip k
-    rollbackBlocks blockUndos
-    -- [CSL-93] Use eligibility threshold here
-    richmen <- getRichmen 0
-    let threshold = getThreshold $ length richmen -- no, its wrong.....
-    --mbSeed <- sscCalculateSeed siEpoch threshold -- SscHelperClassM needded
-    let mbSeed = notImplemented
-    leaders <-
-        case mbSeed of
-          Left e     -> panic "SSC couldn't compute seed"
-          Right seed -> mapUtxoIterator @(TxIn, TxOut) @TxOut
-                        (followTheSatoshiM seed notImplemented) snd --balance
-    nc <- getNodeContext
-    liftIO $ putMVar (ncSscLeaders nc) leaders
-    liftIO $ putMVar (ncSscParticipants nc) richmen
-    applyBlocks (map fst blockUndos)
+    if siSlot == 0 then do
+        logDebug $ "It's time to compute leaders and parts"
+        blockUndos <- loadLastNBlocksWithUndo tip k
+        rollbackBlocks blockUndos
+        -- [CSL-93] Use eligibility threshold here
+        richmen <- getRichmen 0
+        let threshold = getThreshold $ length richmen -- no, its wrong.....
+        --mbSeed <- sscCalculateSeed siEpoch threshold -- SscHelperClassM needded
+        let mbSeed = notImplemented
+        leaders <-
+            case mbSeed of
+              Left e     -> panic "SSC couldn't compute seed"
+              Right seed -> mapUtxoIterator @(TxIn, TxOut) @TxOut
+                            (followTheSatoshiM seed notImplemented) snd --balance
+        nc <- getNodeContext
+        liftIO $ putMVar (ncSscLeaders nc) leaders
+        liftIO $ putMVar (ncSscParticipants nc) richmen
+        applyBlocks (map fst blockUndos)
+    else
+        logDebug $ "It is too early compute leaders and parts"
     pure tip
 
 -- | Second argument - T, min money.

755ffbe944f7bed0f4829ef892d1e28001976593
Artyom <yom@artyom.me>
2016-12-22 18:52:40 +0300

    [CSL-328] Move 'Script' into Pos.Script.Type

--- src/Pos/Script.hs
+++ src/Pos/Script.hs
@@ -32,10 +32,8 @@ import qualified Utils.Names           as Names
 import qualified Utils.Vars            as Vars
 
 import           Pos.Binary.Class      (Bi)
+import           Pos.Script.Type       (Script)
 import           Pos.Util              (getCopyBinary, putCopyBinary)
-
--- | A script for inclusion into a transaction.
-type Script = PLCore.Program
 
 -- | Parse a script intended to serve as a validator (or “lock”) in a
 -- transaction output.
@@ -76,74 +74,3 @@ txScriptCheck validator redeemer = do
     (script, env) <- PL.buildValidationScript validator redeemer
     result <- PL.checkValidationResult (script, env)
     if result then Right () else Left "result of evaluation is 'failure'"
-
-----------------------------------------------------------------------------
--- Orphan instances, to be included into plutus-prototype
-----------------------------------------------------------------------------
-
-deriving instance Show PLCore.PrimData
-
-deriving instance (Show a, Show (PLCore.ClauseF a)) => Show (PLCore.TermF a)
-deriving instance (Eq a, Eq (PLCore.ClauseF a)) => Eq (PLCore.TermF a)
-
-deriving instance Eq PLTypes.TyConSig
-deriving instance Eq PLTypes.ConSig
-deriving instance Eq PLCore.TermDeclaration
-deriving instance Eq PLCore.Program
-
-deriveEq1 ''PLCore.PatternF
-deriveEq1 ''PLCore.ClauseF
-deriveEq1 ''PLCore.TermF
-deriveEq1 ''PLTypes.TypeF
-
-deriving instance Show (f PLCore.PatternF) => Show (PLCore.PatternF (f PLCore.PatternF))
-
-deriving instance Show r => Show (PLCore.ClauseF r)
-
-instance Binary Vars.FreeVar
-instance Binary Vars.MetaVar
-instance Binary Vars.BoundVar
-instance Binary PLTypes.TyConSig
-instance Binary PLTypes.ConSig
-instance Binary a => Binary (Names.Sourced a)
-instance Binary ABT.Variable
-instance Binary (f (ABT.Scope f)) => Binary (ABT.ABT f)
-instance Binary (f (ABT.Scope f)) => Binary (ABT.Scope f)
-instance Binary r => Binary (PLCore.ClauseF r)
-instance Binary a => Binary (PLCore.TermF a)
-instance Binary a => Binary (PLCore.PatternF a)
-instance Binary a => Binary (PLTypes.TypeF a)
-instance Binary PLCore.TermDeclaration
-instance Binary PLCore.PrimData
-instance Binary PLCore.Program
-
-instance NFData Vars.FreeVar
-instance NFData Vars.MetaVar
-instance NFData Vars.BoundVar
-instance NFData PLTypes.TyConSig
-instance NFData PLTypes.ConSig
-instance NFData a => NFData (Names.Sourced a)
-instance NFData ABT.Variable
-instance NFData (f (ABT.Scope f)) => NFData (ABT.ABT f)
-instance NFData (f (ABT.Scope f)) => NFData (ABT.Scope f)
-instance NFData r => NFData (PLCore.ClauseF r)
-instance NFData a => NFData (PLCore.TermF a)
-instance NFData a => NFData (PLCore.PatternF a)
-instance NFData a => NFData (PLTypes.TypeF a)
-instance NFData PLCore.TermDeclaration
-instance NFData PLCore.PrimData
-instance NFData Script
-
-instance Bi PLCore.Term => SafeCopy PLCore.Term where
-    getCopy = getCopyBinary "Term"
-    putCopy = putCopyBinary
-
-instance Bi PLCore.Program => SafeCopy PLCore.Program where
-    getCopy = getCopyBinary "Program"
-    putCopy = putCopyBinary
-
-instance Hashable PLCore.Term where
-    hashWithSalt s = hashWithSalt s . Binary.encode
-
-instance Hashable PLCore.Program where
-    hashWithSalt s = hashWithSalt s . Binary.encode

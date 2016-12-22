b9eecb123dfc4d5259969e81a522cf4c2680114e
Ivan Gromakovskii <gie1994@gmail.com>
2016-12-23 02:31:06 +0300

    [CSL-345] Move Pos.Worker.Block to Pos.Block.Worker, minor changes

--- src/Pos/Block/Worker.hs
+++ src/Pos/Block/Worker.hs
@@ -3,12 +3,156 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE MultiWayIf            #-}
 {-# LANGUAGE RankNTypes            #-}
-{-# LANGUAGE ScopedTypeVariables   #-}
 {-# LANGUAGE TupleSections         #-}
 {-# LANGUAGE TypeFamilies          #-}
 
 -- | Block processing related workers.
 
 module Pos.Block.Worker
-       (
+       ( blkOnNewSlot
+       , blkWorkers
        ) where
+
+import           Control.Lens              (ix, (^.), (^?))
+import           Control.TimeWarp.Timed    (Microsecond, for, repeatForever, wait)
+import qualified Data.HashMap.Strict       as HM
+import           Data.Tagged               (untag)
+import           Formatting                (build, sformat, shown, (%))
+import           Serokell.Util             (VerificationRes (..), listJson)
+import           Serokell.Util.Exceptions  ()
+import           System.Wlog               (dispatchEvents, logDebug, logInfo, logWarning)
+import           Universum
+
+import           Pos.Binary.Communication  ()
+import           Pos.Communication.Methods (announceBlock)
+import           Pos.Constants             (networkDiameter, slotDuration)
+import           Pos.Context               (getNodeContext, ncPropagation, ncPublicKey,
+                                            ncSecretKey)
+import           Pos.Crypto                (ProxySecretKey, WithHash (WithHash),
+                                            pskIssuerPk, pskOmega)
+import           Pos.DB.Misc               (getProxySecretKeys)
+import           Pos.Slotting              (MonadSlots (getCurrentTime), getSlotStart,
+                                            onNewSlot)
+import           Pos.Ssc.Class             (sscApplyGlobalState, sscGetLocalPayload,
+                                            sscVerifyPayload)
+import           Pos.State                 (createNewBlock, getGlobalMpcData,
+                                            getHeadBlock, getLeaders, processNewSlot)
+import           Pos.Txp.LocalData         (getLocalTxs)
+import           Pos.Types                 (EpochIndex, SlotId (..),
+                                            Timestamp (Timestamp), blockMpc, gbHeader,
+                                            slotIdF, topsortTxs)
+import           Pos.Types.Address         (addressHash)
+import           Pos.Util                  (logWarningWaitLinear)
+import           Pos.Util.JsonLog          (jlCreatedBlock, jlLog)
+import           Pos.WorkMode              (WorkMode)
+
+-- | All workers specific to block processing.
+blkWorkers :: WorkMode ssc m => [m ()]
+blkWorkers = [blocksTransmitter, blkOnNewSlotWorker]
+
+blkOnNewSlotWorker :: WorkMode ssc m => m ()
+blkOnNewSlotWorker = onNewSlot True blkOnNewSlot
+
+-- Action which should be done when new slot starts.
+blkOnNewSlot :: WorkMode ssc m => SlotId -> m ()
+blkOnNewSlot slotId@SlotId {..} = do
+    -- First of all we create genesis block if necessary.
+    (mGenBlock, pnsLog) <- processNewSlot slotId
+    dispatchEvents pnsLog
+    forM_ mGenBlock $ \createdBlk -> do
+        logInfo $ sformat ("Created genesis block:\n" %build) createdBlk
+        jlLog $ jlCreatedBlock (Left createdBlk)
+
+    -- Then we get leaders for current epoch.
+    leadersMaybe <- getLeaders siEpoch
+    case leadersMaybe of
+        -- If we don't know leaders, we can't do anything.
+        Nothing -> logWarning "Leaders are not known for new slot"
+        -- If we know leaders, we check whether we are leader and
+        -- create a new block if we are.
+        Just leaders -> do
+            let logLeadersF = if siSlot == 0 then logInfo else logDebug
+            logLeadersF (sformat ("Slot leaders: "%listJson) leaders)
+            ourPkHash <- addressHash . ncPublicKey <$> getNodeContext
+            let leader = leaders ^? ix (fromIntegral siSlot)
+            proxyCerts <- getProxySecretKeys
+            let validCerts =
+                    filter (\pSk -> let (w0,w1) = pskOmega pSk
+                                    in siEpoch >= w0 && siEpoch <= w1) proxyCerts
+                validCert =
+                    find (\pSk -> Just (addressHash $ pskIssuerPk pSk) == leader)
+                         validCerts
+            if | leader == Just ourPkHash -> onNewSlotWhenLeader slotId Nothing
+               | isJust validCert -> onNewSlotWhenLeader slotId validCert
+               | otherwise -> pure ()
+
+onNewSlotWhenLeader
+    :: WorkMode ssc m
+    => SlotId
+    -> Maybe (ProxySecretKey (EpochIndex, EpochIndex))
+    -> m ()
+onNewSlotWhenLeader slotId pSk = do
+    logInfo $
+        maybe
+        (sformat
+            ("I'm the leader for the slot " %slotIdF % ", will create block soon.")
+            slotId)
+        (sformat
+            ("I have a right to create a delegated block for the slot "%slotIdF%
+             "using proxy signature key"%build%", will do it soon") slotId)
+        pSk
+    whenJust pSk $ logInfo . sformat ("Will use proxy signature key "%build)
+    nextSlotStart <- getSlotStart (succ slotId)
+    currentTime <- getCurrentTime
+    let timeToCreate =
+            max currentTime (nextSlotStart - Timestamp networkDiameter)
+        Timestamp timeToWait = timeToCreate - currentTime
+    logInfo $
+        sformat ("Waiting for "%shown%" before creating block") timeToWait
+    wait (for timeToWait)
+    let verifyCreatedBlock blk =
+            untag sscVerifyPayload
+            (blk ^. gbHeader) (blk ^. blockMpc)
+    let onNewSlotWhenLeaderDo = do
+            logInfo "It's time to create a block for current slot"
+            sk <- ncSecretKey <$> getNodeContext
+            let whenCreated createdBlk = do
+                    logInfo $
+                        sformat ("Created a new block:\n" %build) createdBlk
+                    jlLog $ jlCreatedBlock (Right createdBlk)
+                    case verifyCreatedBlock createdBlk of
+                        VerSuccess -> return ()
+                        VerFailure warnings -> logWarning $ sformat
+                            ("New block failed some checks: "%listJson)
+                            warnings
+                    globalData <- logWarningWaitLinear 6 "getGlobalMpcData"
+                        getGlobalMpcData
+                    logWarningWaitLinear 7 "sscApplyGlobalState" $
+                        sscApplyGlobalState globalData
+                    announceBlock $ createdBlk ^. gbHeader
+            let whenNotCreated = logWarning . (mappend "I couldn't create a new block: ")
+            sscData <- sscGetLocalPayload slotId
+            localTxs <- HM.toList <$> getLocalTxs
+            let panicTopsort = panic "Topology of local transactions is broken!"
+            let convertTx (txId, (tx, _)) = WithHash tx txId
+            let sortedTxs = fromMaybe panicTopsort $
+                            topsortTxs convertTx localTxs
+            createdBlock <- createNewBlock sortedTxs sk pSk slotId sscData
+            either whenNotCreated whenCreated createdBlock
+    logWarningWaitLinear 8 "onNewSlotWhenLeader" onNewSlotWhenLeaderDo
+
+blocksTransmitterInterval :: Microsecond
+blocksTransmitterInterval = slotDuration `div` 2
+
+blocksTransmitter :: WorkMode ssc m => m ()
+blocksTransmitter = whenM (ncPropagation <$> getNodeContext) impl
+  where
+    impl = repeatForever blocksTransmitterInterval onError $
+        do headBlock <- getHeadBlock
+           case headBlock of
+               Left _          -> logDebug "Head block is genesis block ⇒ no announcement"
+               Right mainBlock -> do
+                   announceBlock (mainBlock ^. gbHeader)
+    onError e =
+        blocksTransmitterInterval <$
+        logWarning (sformat ("Error occured in blocksTransmitter: " %build) e)

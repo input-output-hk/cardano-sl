994d18fe83383d15a19f450010ca037e9e784e77
Ivan Gromakovskii <gie1994@gmail.com>
2016-12-24 21:58:41 +0300

    Remove useless

--- src/Pos/Launcher/Runner.hs
+++ src/Pos/Launcher/Runner.hs
@@ -152,7 +152,7 @@ runRawRealMode
 runRawRealMode inst np@NodeParams {..} sscnp listeners action = runResourceT $ do
     putText $ "Running listeners number: " <> show (length listeners)
     lift $ setupLoggers lp
-    modernDBs <- Modern.openNodeDBs (npDbPathM </> "rocksDBzhogovo") npCustomUtxo
+    modernDBs <- Modern.openNodeDBs npDbPathM npCustomUtxo
     initTip <- Modern.runDBHolder modernDBs Modern.getTip
     initGS <- Modern.runDBHolder modernDBs (sscLoadGlobalState @ssc initTip)
     initNC <- sscCreateNodeContext @ssc sscnp

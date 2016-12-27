a362c4bb77d4c45d5e702c0d5626d7e46b718eb8
Dmitry Kovanikov <kovanikov@gmail.com>
2016-12-28 00:50:52 +0300

    [TW-101] Fix -r option & refactor sender

--- bench/Network/Sender/Main.hs
+++ bench/Network/Sender/Main.hs
@@ -1,18 +1,23 @@
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
-{-# LANGUAGE TypeApplications      #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeApplications      #-}
 
 module Main where
 
-import           Control.Applicative        (empty)
+import           Control.Applicative        (empty, liftA2)
 import qualified Control.Exception.Lifted   as Exception
-import           Control.Monad              (forM, forM_, liftM2)
+import           Control.Lens               (makeLenses, (+=))
+import           Control.Monad              (forM, forM_)
 import           Control.Monad.Random       (evalRandT, getRandomR)
-import           Control.Monad.Trans        (lift, liftIO)
+import           Control.Monad.State        (evalStateT, get, put)
+import           Control.Monad.Trans        (MonadIO (liftIO), lift)
 
-import           Data.Time.Units            (Second)
+import           Data.Time.Units            (Microsecond, Second, convertUnit,
+                                             fromMicroseconds)
 import           GHC.IO.Encoding            (setLocaleEncoding, utf8)
+import qualified Network.Transport.TCP      as TCP
 import           Options.Applicative.Simple (simpleOptions)
 import           Serokell.Util.Concurrent   (threadDelay)
 import           System.Random              (mkStdGen)
@@ -20,15 +25,15 @@ import           System.Wlog                (LoggerNameBox, usingLoggerName)
 
 import           Mockable.Class             (Mockable (..))
 import           Mockable.Exception         (Catch (..))
-
-import           Bench.Network.Commons      (MeasureEvent (..), Payload (..), Ping (..),
-                                             Pong (..), loadLogConfig, logMeasure)
 import qualified Network.Transport.Abstract as NT
 import           Network.Transport.Concrete (concrete)
-import qualified Network.Transport.TCP      as TCP
 import           Node                       (Listener (..), ListenerAction (..), sendTo,
                                              startNode, stopNode)
 import           Node.Internal              (NodeId (..))
+
+import           Bench.Network.Commons      (MeasureEvent (..), Payload (..), Ping (..),
+                                             Pong (..), curTimeMcs, loadLogConfig,
+                                             logMeasure)
 import           SenderOptions              (Args (..), argsParser)
 
 instance Mockable Catch (LoggerNameBox IO) where
@@ -36,9 +41,18 @@ instance Mockable Catch (LoggerNameBox IO) where
 
 type Header = ()
 
-sendDelay :: Maybe Int -> Second
-sendDelay Nothing     = 0
-sendDelay (Just rate) = fromIntegral $ 1000000 `div` rate
+data PingState = PingState
+    { _lastResetMcs    :: !Microsecond
+    , _currentMessages :: !Word
+    }
+
+makeLenses ''PingState
+
+oneSecondMcs :: Microsecond
+oneSecondMcs = convertUnit @Second 1
+
+curTimeUnitsMcs :: MonadIO m => m Microsecond
+curTimeUnitsMcs = fromMicroseconds <$> curTimeMcs
 
 main :: IO ()
 main = do
@@ -53,27 +67,31 @@ main = do
     loadLogConfig logsPrefix logConfig
     setLocaleEncoding utf8
 
-    Right transport_ <- TCP.createTransport "0.0.0.0" "3432"
-        TCP.defaultTCPParameters
+    Right transport_ <- TCP.createTransport "0.0.0.0" "3432" TCP.defaultTCPParameters
     let transport = concrete transport_
 
     let prngNode = mkStdGen 0
     let prngWork = mkStdGen 1
     let nodeIds  = [ NodeId $ TCP.encodeEndPointAddress host (show port) 0
                    | (host, port) <- recipients ]
-    let delay    = sendDelay msgRate
     let tasksIds = [[tid, tid + threadNum .. msgNum] | tid <- [1..threadNum]]
 
     usingLoggerName "sender" $ do
         Right endPoint <- NT.newEndPoint transport
-        drones <- forM nodeIds (startDrone endPoint)
-        senderNode <- startNode @Header endPoint prngNode
-            -- TODO: is it good idea to start (recipients number * thread number) threads?
-            (liftM2 (pingSender prngWork payloadBound delay)
-                tasksIds
-                (zip [0, msgNum..] nodeIds))
-            Nothing
-            [Listener "pong" pongListener]
+        drones         <- forM nodeIds (startDrone endPoint)
+        startTime      <- curTimeUnitsMcs
+
+        -- TODO: is it good idea to start (recipients number * thread number) threads?
+        let pingWorkers = liftA2 (pingSender prngWork payloadBound startTime msgRate)
+                                 tasksIds
+                                 (zip [0, msgNum..] nodeIds)
+        senderNode <- startNode
+                          @Header
+                          endPoint
+                          prngNode
+                          pingWorkers
+                          Nothing
+                          [Listener "pong" pongListener]
 
         threadDelay (fromIntegral duration :: Second)
         forM_ drones stopDrone
@@ -82,16 +100,28 @@ main = do
     pongListener = ListenerActionOneMsg $ \_ _ (Pong mid payload) ->
         logMeasure PongReceived mid payload
 
-    pingSender gen payloadBound delay msgIds (msgStartId, peerId) sendActions = do
-        flip evalRandT gen . forM_ msgIds $
-            \msgId -> do
-                let sMsgId = msgStartId + msgId
-                payload <- liftIO $ Payload <$> getRandomR (0, payloadBound)
-                lift $ logMeasure PingSent sMsgId payload
-                -- TODO: better to use `connect` + `send`,
-                -- but `connect` is not implemented yet
-                lift $ sendTo sendActions peerId "ping" () $ Ping sMsgId payload
-                threadDelay delay
+    pingSender gen payloadBound startTimeMcs msgRate msgIds (msgStartId, peerId) sendActions =
+        (`evalRandT` gen) . (`evalStateT` PingState startTimeMcs 0) . forM_ msgIds $ \msgId -> do
+            let sMsgId = msgStartId + msgId
+            payload   <- liftIO $ Payload <$> getRandomR (0, payloadBound)
+            lift . lift $ logMeasure PingSent sMsgId payload
+            -- TODO: better to use `connect` + `send`,
+            -- but `connect` is not implemented yet
+            lift . lift $ sendTo sendActions peerId "ping" () $ Ping sMsgId payload
+
+            PingState{..}    <- get
+            curTime          <- curTimeUnitsMcs
+            let fromLastReset = curTime - _lastResetMcs
+
+            if fromLastReset >= oneSecondMcs then
+                put $ PingState curTime 0
+            else if _currentMessages >= msgRate then do
+                let waitToNextSecond = oneSecondMcs - fromLastReset
+                threadDelay waitToNextSecond
+                freshTime <- curTimeUnitsMcs
+                put $ PingState freshTime 0
+            else
+                currentMessages += 1
 
     startDrone endPoint (NodeId addr) = do
         Right conn <- NT.connect endPoint addr NT.ReliableOrdered (NT.ConnectHints Nothing)

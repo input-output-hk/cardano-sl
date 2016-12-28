d9805f27adbe40a20b5aaf3606d4029705b88a2f
Mikhail Volkhov <volhovm.cs@gmail.com>
2016-12-28 23:38:29 +0300

    [CSL-343] Add basic protocol types

---
+++ src/Pos/Communication/Types/Protocol.hs
@@ -0,0 +1,26 @@
+-- | Protocol/versioning related communication types.
+
+module Pos.Communication.Types.Protocol where
+
+import           Control.TimeWarp.Rpc (Message (..), messageName')
+import           Universum
+
+import           Pos.Types            (ProtocolVersion)
+
+-- | Version request message. 'VersionResp' is expected as response.
+data VersionReq = VerlionReq
+    deriving (Generic)
+
+instance Message VersionReq where
+    messageName _ = "VersionReq"
+    formatMessage = messageName'
+
+-- | Version response (on 'VersionReq' response).
+data VersionResp = VersionResp
+    { vRespMagic           :: Int32
+    , vRespProtocolVersion :: ProtocolVersion
+    } deriving (Generic)
+
+instance Message VersionResp where
+    messageName _ = "VersionResp"
+    formatMessage = messageName'

ff1e2bc918aa752ffaf1a93db4057981d5bfde0b
Jens Krause <email@jkrause.io>
2016-12-29 18:12:35 +0100

    uncurry multiple arguments

--- daedalus/src/Daedalus/ClientApi.purs
+++ daedalus/src/Daedalus/ClientApi.purs
@@ -1,36 +1,39 @@
 module Daedalus.ClientApi where
 
 import Prelude
+import Daedalus.BackendApi as B
 import Control.Monad.Eff (Eff)
 import Control.Promise (Promise, fromAff)
-import Daedalus.BackendApi as B
-import Data.Tuple (Tuple)
+import Daedalus.Types (mkCAddress, mkCoin, mkCWalletMeta)
 import Data.Argonaut (Json)
-import Network.HTTP.Affjax (AJAX)
-import Daedalus.Types (mkCAddress, mkCoin, CAddress, Coin, CWallet, CTx, mkCWalletMeta)
 import Data.Argonaut.Generic.Aeson (encodeJson)
+import Data.Function.Uncurried (Fn4, mkFn4, Fn3, mkFn3)
+import Network.HTTP.Affjax (AJAX)
 
 getWallets :: forall eff. Eff(ajax :: AJAX | eff) (Promise Json)
 getWallets = fromAff $ map encodeJson B.getWallets
 
-getWallet :: forall eff. String -> Eff(ajax :: AJAX | eff) (Promise Json)
+getWallet :: forall eff. String -> (Eff(ajax :: AJAX | eff) (Promise Json))
 getWallet = fromAff <<< map encodeJson <<< B.getWallet <<< mkCAddress
 
 getHistory :: forall eff. String -> Eff(ajax :: AJAX | eff) (Promise Json)
 getHistory = fromAff <<< map encodeJson <<< B.getHistory <<< mkCAddress
 
-send :: forall eff. String -> String -> Int -> Eff(ajax :: AJAX | eff) (Promise Json)
-send addrFrom addrTo amount = fromAff <<< map encodeJson $
+send :: forall eff. Fn3 String String Int (Eff(ajax :: AJAX | eff) (Promise Json))
+send = mkFn3 \addrFrom addrTo amount -> fromAff <<< map encodeJson $
     B.send
         (mkCAddress addrFrom)
         (mkCAddress addrTo)
         (mkCoin amount)
 
-newWallet :: forall eff. String -> String -> String -> Eff(ajax :: AJAX | eff) (Promise Json)
-newWallet wType wCurrency wName = fromAff <<< map encodeJson <<< B.newWallet $ mkCWalletMeta wType wCurrency wName
+newWallet :: forall eff. Fn3 String String String
+  (Eff(ajax :: AJAX | eff) (Promise Json))
+newWallet = mkFn3 \wType wCurrency wName -> fromAff <<< map encodeJson <<<
+    B.newWallet $ mkCWalletMeta wType wCurrency wName
 
-updateWallet :: forall eff. String -> String -> String -> String -> Eff(ajax :: AJAX | eff) (Promise Json)
-updateWallet addr wType wCurrency wName = fromAff <<< map encodeJson <<<
+updateWallet :: forall eff. Fn4 String String String String
+  (Eff(ajax :: AJAX | eff) (Promise Json))
+updateWallet = mkFn4 \addr wType wCurrency wName -> fromAff <<< map encodeJson <<<
     B.updateWallet
         (mkCAddress addr)
         $ mkCWalletMeta wType wCurrency wName
@@ -42,5 +45,5 @@ updateWallet addr wType wCurrency wName = fromAff <<< map encodeJson <<<
 --         (mkCTxId ctxId)
 --         $ mkCWalletMeta wType wCurrency wName
 --
-deleteWallet :: forall eff. String -> Eff(ajax :: AJAX | eff) (Promise Unit)
+deleteWallet :: forall eff. String -> (Eff(ajax :: AJAX | eff) (Promise Unit))
 deleteWallet = fromAff <<< B.deleteWallet <<< mkCAddress

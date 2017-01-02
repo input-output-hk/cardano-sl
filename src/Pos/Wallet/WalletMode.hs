a743bb157dbb017128293bfb8583bc079bf88277
Dmitry Mukhutdinov <flyingleafe@gmail.com>
2017-01-02 16:29:03 +0300

    [CSL-391] Derive recent history from local txs

--- src/Pos/Wallet/WalletMode.hs
+++ src/Pos/Wallet/WalletMode.hs
@@ -15,7 +15,6 @@ module Pos.Wallet.WalletMode
        ) where
 
 import           Control.Monad                 (fail)
-import           Control.Monad.Loops           (unfoldrM)
 import           Control.Monad.Trans           (MonadTrans)
 import           Control.Monad.Trans.Maybe     (MaybeT (..))
 import           Control.TimeWarp.Rpc          (Dialog, Transfer)
@@ -23,6 +22,7 @@ import qualified Data.HashMap.Strict           as HM
 import qualified Data.Map                      as M
 import           Universum
 
+import Pos.Crypto (WithHash (..))
 import           Pos.Communication.Types.State (MutSocketState)
 import qualified Pos.Context                   as PC
 import           Pos.DB                        (MonadDB)
@@ -32,12 +32,12 @@ import           Pos.DHT.Real                  (KademliaDHT)
 import           Pos.Ssc.Class.Types           (Ssc)
 import           Pos.Ssc.Extra                 (SscHolder (..))
 import           Pos.Ssc.GodTossing            (SscGodTossing)
-import           Pos.Txp.Class                 (getUtxoView)
+import           Pos.Txp.Class                 (getUtxoView, getMemPool)
 import qualified Pos.Txp.Holder                as Modern
 import           Pos.Txp.Logic                 (processTx)
-import           Pos.Txp.Types                 (UtxoView (..))
+import           Pos.Txp.Types                 (UtxoView (..), localTxs)
 import           Pos.Types                     (Address, Coin, Tx, TxAux, TxId, Utxo,
-                                                runUtxoStateT, toPair, txOutValue)
+                                                runUtxoStateT, toPair, txOutValue, evalUtxoStateT)
 import           Pos.Types.Utxo.Functions      (belongsTo, filterUtxoByAddr)
 import           Pos.WorkMode                  (MinWorkMode)
 
@@ -45,7 +45,7 @@ import           Pos.Wallet.Context            (ContextHolder, WithWalletContext
 import           Pos.Wallet.KeyStorage         (KeyStorage, MonadKeys)
 import           Pos.Wallet.State              (WalletDB)
 import qualified Pos.Wallet.State              as WS
-import           Pos.Wallet.Tx.Pure            (deriveAddrHistory)
+import           Pos.Wallet.Tx.Pure            (deriveAddrHistory, deriveAddrHistoryPartial, getRelatedTxs)
 
 -- | A class which have the methods to get state of address' balance
 class Monad m => MonadBalances m where
@@ -128,11 +128,17 @@ instance (Ssc ssc, MonadDB ssc m, MonadThrow m) => MonadTxHistory (Modern.TxpLDH
                 case nblk of
                     Nothing -> return []
                     Just (next, blk) -> do
-                        txs <- deriveAddrHistory addr [blk]
+                        txs <- deriveAddrHistoryPartial [] addr [blk]
                         (++ txs) <$> blockFetcher next
+            localFetcher blkTxs = do
+                let mp (txid, (tx, txw, txd)) = (WithHash tx txid, txw, txd)
+                ltxs <- HM.toList . localTxs <$> lift (lift getMemPool)
+                txs <- getRelatedTxs addr $ map mp ltxs
+                return $ txs ++ blkTxs
 
-        result <- runMaybeT $ runUtxoStateT (blockFetcher bot) genUtxo
-        return $ maybe [] fst result
+        result <- runMaybeT $
+                  evalUtxoStateT (blockFetcher bot >>= localFetcher) genUtxo
+        maybe (fail "deriveAddrHistory: Nothing") return result
 
     saveTx txw = () <$ processTx txw
 

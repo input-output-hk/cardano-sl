cb50080feb67e556e2b3eab152a4b4bcfb130b9d
Dominik Guzei <dominik.guzei@gmail.com>
2017-01-04 18:27:27 +0100

    remove generated source files when rebuilding daedalus bridge

--- scripts/build-daedalus-bridge.sh
+++ scripts/build-daedalus-bridge.sh
@@ -1,3 +1,4 @@
+rm -rf daedalus/src/Generated/
 stack build --flag cardano-sl:with-wallet --flag cardano-sl:with-web
 stack exec -- cardano-wallet-hs2purs
 cd daedalus && npm install && npm run build:prod

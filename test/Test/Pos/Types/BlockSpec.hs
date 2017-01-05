2abb22748649eac4b96e44292828d39e5d7e400b
Mikhail Volkhov <volhovm.cs@gmail.com>
2017-01-05 12:22:28 +0300

    Fix warnings

--- test/Test/Pos/Types/BlockSpec.hs
+++ test/Test/Pos/Types/BlockSpec.hs
@@ -4,17 +4,17 @@ module Test.Pos.Types.BlockSpec
        ( spec
        ) where
 
-import           Test.Hspec            (Spec)
+import           Test.Hspec (Spec)
 import           Universum
 
-import           Pos.Ssc.Class.Helpers (SscHelpersClass (..))
-import           Pos.Ssc.Class.Types   (Ssc (..))
-import qualified Pos.Types             as T
+--import           Pos.Ssc.Class.Helpers (SscHelpersClass (..))
+--import           Pos.Ssc.Class.Types   (Ssc (..))
+--import qualified Pos.Types             as T
 
 spec :: Spec
 spec = pure ()
 
-validateGoodHeader
-    :: (SscHelpersClass ssc, T.BiSsc ssc)
-    => T.VerifyBlockParams ssc -> T.BlockHeader ssc -> Bool
-validateGoodHeader = undefined
+--validateGoodHeader
+--    :: (SscHelpersClass ssc, T.BiSsc ssc)
+--    => T.VerifyBlockParams ssc -> T.BlockHeader ssc -> Bool
+--validateGoodHeader = undefined

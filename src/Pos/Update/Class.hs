fa45c65c1d094fd9fb257332390a2b445e8a16d5
Ivan Gromakovskii <gie1994@gmail.com>
2017-01-06 00:33:23 +0300

    [CSL-349] Add UpdateSystem type class and transformer

---
+++ src/Pos/Update/Class.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE TypeFamilies         #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+-- | Type class necessary for Update System.
+
+module Pos.Update.Class
+       ( MonadUS (..)
+       ) where
+
+import           Control.Concurrent.STM (TVar)
+import           Control.Monad.Trans    (MonadTrans)
+import           Universum
+
+import           Pos.DHT.Model.Class    (DHTResponseT)
+import           Pos.DHT.Real           (KademliaDHT)
+import           Pos.Update.State       (MemState)
+
+-- | Equivalent of @MonadReader (TVar MemState) m@.
+class Monad m => MonadUS m where
+    askUSMemState :: m (TVar MemState)
+    -- ^ Retrieve 'TVar' on 'Pos.Update.State.MemState'.
+
+    default askUSMemState
+        :: (MonadTrans t, MonadUS m', t m' ~ m) => m (TVar MemState)
+    askUSMemState = lift askUSMemState
+    -- ^ Default implementation for 'MonadTrans'.
+
+instance MonadUS m => MonadUS (ReaderT s m)
+instance MonadUS m => MonadUS (StateT s m)
+instance MonadUS m => MonadUS (DHTResponseT s m)
+instance MonadUS m => MonadUS (KademliaDHT m)
---
+++ src/Pos/Update/Holder.hs
@@ -0,0 +1,74 @@
+{-# LANGUAGE TypeFamilies         #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+-- | MonadTransformer which implements MonadUS based on ReaderT.
+
+module Pos.Update.Holder
+       ( USHolder (..)
+       , runUSHolder
+       , runUSHolderFromTVar
+       ) where
+
+import           Control.Concurrent.STM      (TVar, newTVarIO)
+import           Control.Lens                (iso)
+import           Control.Monad.Base          (MonadBase (..))
+import           Control.Monad.Trans.Class   (MonadTrans)
+import           Control.Monad.Trans.Control (ComposeSt, MonadBaseControl (..),
+                                              MonadTransControl (..), StM,
+                                              defaultLiftBaseWith, defaultLiftWith,
+                                              defaultRestoreM, defaultRestoreT)
+import           Control.TimeWarp.Rpc        (MonadDialog, MonadTransfer (..))
+import           Control.TimeWarp.Timed      (MonadTimed (..), ThreadId)
+import           Data.Default                (Default (def))
+import           Serokell.Util.Lens          (WrappedM (..))
+import           System.Wlog                 (CanLog, HasLoggerName)
+import           Universum
+
+import           Pos.Context                 (WithNodeContext)
+import           Pos.DB.Class                (MonadDB)
+import           Pos.Slotting                (MonadSlots (..))
+import           Pos.Ssc.Extra               (MonadSscGS (..), MonadSscLD (..))
+import           Pos.Txp.Class               (MonadTxpLD (..))
+import           Pos.Types.Utxo.Class        (MonadUtxo, MonadUtxoRead)
+import           Pos.Update.Class            (MonadUS (..))
+import           Pos.Update.State            (MemState)
+import           Pos.Util.JsonLog            (MonadJL (..))
+
+-- | Trivial monad transformer based on @ReaderT (TVar MemState)@.
+newtype USHolder m a = USHolder
+    { getUSHolder :: ReaderT (TVar MemState) m a
+    } deriving (Functor, Applicative, Monad, MonadTrans, MonadTimed,
+                MonadThrow, MonadSlots, MonadCatch, MonadIO, MonadFail,
+                HasLoggerName, MonadDialog s p, WithNodeContext ssc, MonadJL,
+                MonadDB ssc, CanLog, MonadMask, MonadSscLD kek, MonadSscGS ssc,
+                MonadUtxoRead, MonadUtxo, MonadTxpLD ssc, MonadBase io)
+
+instance Monad m => MonadUS (USHolder m) where
+    askUSMemState = USHolder ask
+
+instance MonadTransfer s m => MonadTransfer s (USHolder m)
+
+type instance ThreadId (USHolder m) = ThreadId m
+
+instance Monad m => WrappedM (USHolder m) where
+    type UnwrappedM (USHolder m) = ReaderT (TVar MemState) m
+    _WrappedM = iso getUSHolder USHolder
+
+instance MonadTransControl USHolder where
+    type StT (USHolder) a = StT (ReaderT (TVar MemState)) a
+    liftWith = defaultLiftWith USHolder getUSHolder
+    restoreT = defaultRestoreT USHolder
+
+instance MonadBaseControl IO m => MonadBaseControl IO (USHolder m) where
+    type StM (USHolder m) a = ComposeSt USHolder m a
+    liftBaseWith     = defaultLiftBaseWith
+    restoreM         = defaultRestoreM
+
+-- | Run USHolder using default (empty) MemState.
+runUSHolder :: MonadIO m => USHolder m a -> m a
+runUSHolder action =
+    liftIO (newTVarIO def) >>= runReaderT (getUSHolder action)
+
+-- | Run USHolder using existing TVar with MemState.
+runUSHolderFromTVar :: TVar MemState -> USHolder m a -> m a
+runUSHolderFromTVar var action = runReaderT (getUSHolder action) var

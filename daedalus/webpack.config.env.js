0221d96f036ba44a61042117b3b6e67002e7a771
Jens Krause <email@jkrause.io>
2017-01-10 16:20:56 +0100

    rollback ps-websocket-simple to @0.5.0

--- daedalus/webpack.config.env.js
+++ daedalus/webpack.config.env.js
@@ -16,9 +16,6 @@ export default {
       filename: '[name]-[hash].min.js',
     }
   ),
-  externals: {
-    "ws": "ws"
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html',

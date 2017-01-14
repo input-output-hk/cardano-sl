7b2c5f8c12bff4b6eb8c4697bc170a2d0ad391c1
Mikhail Volkhov <volhovm.cs@gmail.com>
2017-01-15 02:18:20 +0300

    Fix ExponentialStakes binary instance warning

--- src/Pos/Binary/Genesis.hs
+++ src/Pos/Binary/Genesis.hs
@@ -21,11 +21,13 @@ instance Bi StakeDistribution where
         0 -> FlatStakes <$> getUVI <*> get
         1 -> BitcoinStakes <$> getUVI <*> get
         2 -> TestnetStakes <$> get <*> getUVI <*> getUVI
+        3 -> pure ExponentialStakes
         _ -> fail "Pos.Binary.Genesis: StakeDistribution: invalid tag"
     put (FlatStakes n total)      = putWord8 0 >> putUVI n >> put total
     put (BitcoinStakes n total)   = putWord8 1 >> putUVI n >> put total
     put (TestnetStakes total m n) = putWord8 2 >> put total >>
                                     putUVI m >> putUVI n
+    put ExponentialStakes         = putWord8 3
 
 instance Bi GenesisData where
     get = GenesisData <$> get <*> get <*> get

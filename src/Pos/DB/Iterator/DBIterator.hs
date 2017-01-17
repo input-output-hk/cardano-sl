4cb83e62458527e68597f490b7962625b9600e23
Ivan Gromakovskii <gie1994@gmail.com>
2017-01-18 02:25:25 +0300

    [CSL-442] Cosmetics in DBIterator

--- src/Pos/DB/Iterator/DBIterator.hs
+++ src/Pos/DB/Iterator/DBIterator.hs
@@ -26,12 +26,12 @@ import           Pos.DB.Error          (DBError (DBMalformed))
 import           Pos.DB.Functions      (rocksDecodeMaybe, rocksDecodeMaybeWP)
 import           Pos.DB.Iterator.Class (DBIteratorClass (..), IterType)
 import           Pos.DB.Types          (DB (..))
+import           Pos.Util              (maybeThrow)
 import           Pos.Util.Iterator     (MonadIterator (..))
 
-newtype DBIterator i m a = DBIterator
-    { getDBIterator :: ReaderT Rocks.Iterator m a
-    } deriving (Functor, Applicative, Monad, MonadIO, MonadTrans
-               , MonadThrow, MonadCatch)
+----------------------------------------------------------------------------
+-- ParseResult
+----------------------------------------------------------------------------
 
 -- | RocksDB key value iteration errors.
 data ParseResult a = FetchError  -- RocksDB internal error
@@ -42,6 +42,18 @@ data ParseResult a = FetchError  -- RocksDB internal error
                                  -- but encounter balance (key, value))
                    | Success a   -- Element is fetched and decoded successfully.
     deriving Show
+
+----------------------------------------------------------------------------
+-- DBIterator
+----------------------------------------------------------------------------
+
+newtype DBIterator i m a = DBIterator
+    { getDBIterator :: ReaderT Rocks.Iterator m a
+    } deriving (Functor, Applicative, Monad, MonadIO, MonadTrans
+               , MonadThrow, MonadCatch)
+
+deriving instance MonadMask m => MonadMask (DBIterator i m)
+deriving instance MonadDB ssc m => MonadDB ssc (DBIterator i m)
 
 -- | Iterator by keys of type @k@ and values of type @v@.
 instance ( Bi k, Bi v
@@ -60,9 +72,26 @@ instance ( Bi k, Bi v
         if | Nothing <- entryStr -> pure Nothing -- end of Database is reached
            | Just (key, val) <- entryStr,
              BS.isPrefixOf (iterKeyPrefix @i Proxy) key ->
-                maybe (throwM $ DBMalformed "Invalid entry") (pure . Just) $
-                    (,) <$> rocksDecodeMaybeWP @i key <*> rocksDecodeMaybe val
-            | otherwise -> pure Nothing -- all entries with specified prefix was viewed
+               Just <$>
+               maybeThrow
+                   (DBMalformed "Invalid entry")
+                   ((,) <$> rocksDecodeMaybeWP @i key <*> rocksDecodeMaybe val)
+            -- all entries with specified prefix have been viewed
+            | otherwise -> pure Nothing
+
+-- | Run DBIterator by `DB ssc`.
+runIterator :: forall i a m ssc . (MonadIO m, MonadMask m, DBIteratorClass i)
+             => DBIterator i m a -> DB ssc -> m a
+runIterator dbIter DB {..} =
+    bracket (Rocks.createIter rocksDB rocksReadOpts) Rocks.releaseIter run
+  where
+    run it = do
+        Rocks.iterSeek it (iterKeyPrefix @i Proxy)
+        runReaderT (getDBIterator dbIter) it
+
+----------------------------------------------------------------------------
+-- DBMapIterator
+----------------------------------------------------------------------------
 
 -- | Encapsulate `map f elements`, where @elements@ - collection elements of type @a@.
 -- Holds `DBIterator m a` and apply f for every `nextItem` and `curItem` call.
@@ -85,19 +114,8 @@ instance ( Monad m
     nextItem = DBMapIterator $ ReaderT $ \f -> fmap f <$> nextItem
     curItem = DBMapIterator $ ReaderT $ \f -> fmap f <$> curItem
 
-deriving instance MonadMask m => MonadMask (DBIterator i m)
 deriving instance MonadMask m => MonadMask (DBMapIterator i v m)
-
-deriving instance MonadDB ssc m => MonadDB ssc (DBIterator i m)
 deriving instance MonadDB ssc m => MonadDB ssc (DBMapIterator i v m)
-
--- | Run DBIterator by `DB ssc`.
-runIterator :: forall i a m ssc . (MonadIO m, MonadMask m, DBIteratorClass i)
-             => DBIterator i m a -> DB ssc -> m a
-runIterator dbIter DB{..} =
-    bracket (Rocks.createIter rocksDB rocksReadOpts) (Rocks.releaseIter)
---            (\it -> Rocks.iterFirst it >> runReaderT (getDBIterator dbIter) it)
-            (\it -> Rocks.iterSeek it (iterKeyPrefix @i Proxy) >> runReaderT (getDBIterator dbIter) it)
 
 -- | Run DBMapIterator by `DB ssc`.
 mapIterator :: forall i v m ssc a . (MonadIO m, MonadMask m, DBIteratorClass i)

3113c6bda617171cc12ed71e40d495e85aea93f1
Ivan Gromakovskii <gie1994@gmail.com>
2017-01-18 20:10:16 +0300

    [CSL-516] Add MemState.Functions to .cabal, make it compile

--- src/Pos/Update/MemState/Functions.hs
+++ src/Pos/Update/MemState/Functions.hs
@@ -4,15 +4,16 @@ module Pos.Update.MemState.Functions
        ( withUSLock
        ) where
 
-import qualified Control.Concurrent.Lock   as Lock
-import           Control.Monad.Catch       (MonadMask, bracket_)
+import qualified Control.Concurrent.Lock      as Lock
+import           Control.Monad.Catch          (MonadMask, bracket_)
 import           Universum
 
-import           Pos.Update.MemState.Class (MonadUSMem (askUSMemVar))
+import           Pos.Update.MemState.Class    (MonadUSMem (askUSMemVar))
+import           Pos.Update.MemState.MemState (MemVar (..))
 
 withUSLock
     :: (MonadUSMem m, MonadIO m, MonadMask m)
     => m a -> m a
 withUSLock action = do
     lock <- mvLock <$> askUSMemVar
-    bracket_ (Lock.acquire lock) (Lock.release lock) action
+    bracket_ (liftIO $ Lock.acquire lock) (liftIO $ Lock.release lock) action

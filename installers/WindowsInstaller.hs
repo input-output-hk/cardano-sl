e2286241de58e2ff46ae8b8522f719b2e9ca82bd
Domen Kožar <domen@dev.si>
2017-01-18 18:03:46 +0100

    WindowsInstaller: add Uninstaller

--- installers/WindowsInstaller.hs
+++ installers/WindowsInstaller.hs
@@ -1,37 +1,41 @@
 module WindowsInstaller where
 
 import           Development.NSIS
-import           Development.NSIS.Plugins.EnvVarUpdate
-import           System.Directory (copyFile)
-import           Turtle
-
+import           Turtle (echo, procs)
 
 writeNSIS :: IO ()
 writeNSIS = writeFile "daedalus.nsi" $ nsis $ do
-     name "Daedalus"                  -- The name of the installer
-     outFile "daedalus-win64-installer.exe"           -- Where to produce the installer
-     installDir "$PROGRAMFILES\\Daedalus"   -- The default installation directory
-     requestExecutionLevel Highest       -- Request application privileges for Windows Vista
+    name "Daedalus"                  -- The name of the installer
+    outFile "daedalus-win64-installer.exe"           -- Where to produce the installer
+    installDir "$PROGRAMFILES\\Daedalus"   -- The default installation directory
+    requestExecutionLevel Highest     
 
-     page Directory                   -- Pick where to install
-     page InstFiles                   -- Give a progress bar while installing
+    page Directory                   -- Pick where to install
+    page InstFiles                   -- Give a progress bar while installing
 
-     section "" [Required] $ do
-         setOutPath "$INSTDIR"        -- Where to install files in this section
-         createDirectory "$APPDATA\\Daedalus\\DB"
-         createDirectory "$APPDATA\\Daedalus\\Wallet" 
-         createDirectory "$APPDATA\\Daedalus\\Logs"
-         createDirectory "$APPDATA\\Daedalus\\Secrets"
-         createShortcut "$DESKTOP\\Daedalus.lnk"
-           [Target "$INSTDIR\\Daedalus.exe"
-           , IconFile "$INSTDIR\\Daedalus.exe"
-           , IconIndex 0
-           ]
-         file [] "cardano-node.exe"
-         file [Recursive] "dlls\\"
-         file [] "..\\log-config-prod.yaml"
-         file [Recursive] "C:\\daedalus\\release\\win32-x64\\Daedalus-win32-x64\\"
+    section "" [Required] $ do
+        setOutPath "$INSTDIR"        -- Where to install files in this section
+        createDirectory "$APPDATA\\Daedalus\\DB"
+        createDirectory "$APPDATA\\Daedalus\\Wallet" 
+        createDirectory "$APPDATA\\Daedalus\\Logs"
+        createDirectory "$APPDATA\\Daedalus\\Secrets"
+        createShortcut "$DESKTOP\\Daedalus.lnk"
+          [Target "$INSTDIR\\Daedalus.exe"
+          , IconFile "$INSTDIR\\Daedalus.exe"
+          , IconIndex 0
+          ]
+        file [] "cardano-node.exe"
+        file [Recursive] "dlls\\"
+        file [] "..\\log-config-prod.yaml"
+        file [Recursive] "C:\\daedalus\\release\\win32-x64\\Daedalus-win32-x64\\"
+        writeUninstaller "uninstall.exe"
 
+    uninstall $ do
+      delete [Recursive] "$INSTDIR"
+      rmdir [] "$INSTDIR"
+      delete [] "$DESKTOP\\Daedalus.lnk"
+      -- Note: we leave user data alone
+   
 main :: IO ()
 main = do
   echo "Writing daedalus.nsi"

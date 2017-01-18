b7a5a848fc2ba3f680685a2b71dc23a34abedd65
Mikhail Volkhov <volhovm.cs@gmail.com>
2017-01-19 00:28:55 +0300

    Separate Delegation Class/Holder

--- src/Pos/Delegation/Class.hs
+++ src/Pos/Delegation/Class.hs
@@ -6,44 +6,25 @@
 -- delegate certificates (proxy secret keys).
 
 module Pos.Delegation.Class
-       ( DelegationWrap
+       ( DelegationWrap (..)
        , dwProxyMsgCache
        , dwProxyConfCache
        , dwProxySKPool
        , MonadDelegation (..)
-       , DelegationT (..)
-       , runDelegationT
-       , runDelegationTFromTVar
        ) where
 
-import           Control.Concurrent.STM.TVar (TVar, newTVarIO)
-import           Control.Lens                (makeLenses)
-import           Control.Lens                (iso)
-import           Control.Monad.Fix           (MonadFix)
-import           Control.Monad.Trans.Class   (MonadTrans)
-import           Data.Default                (Default (def))
-import           Data.HashMap.Strict         (HashMap)
-import qualified Data.HashMap.Strict         as HM
-import           Data.Time.Clock             (UTCTime)
-import           Mockable                    (ChannelT, MFunctor',
-                                              Mockable (liftMockable), Promise,
-                                              SharedAtomicT, ThreadId,
-                                              liftMockableWrappedM)
-import           Serokell.Util.Lens          (WrappedM (..))
-import           System.Wlog                 (CanLog, HasLoggerName)
+import           Control.Concurrent.STM    (TVar)
+import           Control.Lens              (makeLenses)
+import           Control.Monad.Trans.Class (MonadTrans)
+import           Data.Default              (Default (def))
+import           Data.HashMap.Strict       (HashMap)
+import qualified Data.HashMap.Strict       as HM
+import           Data.Time.Clock           (UTCTime)
 import           Universum
 
-import           Pos.Context                 (WithNodeContext)
-import           Pos.Crypto                  (PublicKey)
-import           Pos.DB.Class                (MonadDB)
-import           Pos.Delegation.Types        (SendProxySK)
-import           Pos.Slotting                (MonadSlots (..))
-import           Pos.Ssc.Extra               (MonadSscGS (..), MonadSscLD (..),
-                                              MonadSscRichmen)
-import           Pos.Txp.Class               (MonadTxpLD (..))
-import           Pos.Types                   (ProxySKEpoch, ProxySKSimple)
-import           Pos.Types.Utxo.Class        (MonadUtxo, MonadUtxoRead)
-import           Pos.Util.JsonLog            (MonadJL (..))
+import           Pos.Crypto                (PublicKey)
+import           Pos.Delegation.Types      (SendProxySK)
+import           Pos.Types                 (ProxySKEpoch, ProxySKSimple)
 
 ---------------------------------------------------------------------------
 -- Delegation in-memory data
@@ -86,47 +67,3 @@ class (Monad m) => MonadDelegation m where
 
 instance MonadDelegation m => MonadDelegation (ReaderT s m)
 instance MonadDelegation m => MonadDelegation (StateT s m)
-
-----------------------------------------------------------------------------
--- Class implementation
-----------------------------------------------------------------------------
-
-type ReaderTCtx = TVar DelegationWrap
-
--- | Wrapper of @ReaderT (TVar DelegationWrap)@, nothing smart.
-newtype DelegationT m a = DelegationT
-    { getDelegationT :: ReaderT ReaderTCtx m a
-    } deriving (Functor, Applicative, Monad, MonadTrans, MonadFix,
-                MonadThrow, MonadSlots, MonadCatch, MonadIO, MonadFail,
-                HasLoggerName, WithNodeContext ssc, MonadJL,
-                CanLog, MonadMask, MonadSscLD ssc, MonadSscGS ssc,
-                MonadSscRichmen, MonadUtxoRead, MonadUtxo, MonadTxpLD ssc)
-
-deriving instance MonadDB ssc m => MonadDB ssc (DelegationT m)
-
-instance (Monad m) => MonadDelegation (DelegationT m) where
-    askDelegationState = DelegationT ask
-
-instance Monad m => WrappedM (DelegationT m) where
-    type UnwrappedM (DelegationT m) = ReaderT ReaderTCtx m
-    _WrappedM = iso getDelegationT DelegationT
-
-type instance ThreadId (DelegationT m) = ThreadId m
-type instance Promise (DelegationT m) = Promise m
-type instance SharedAtomicT (DelegationT m) = SharedAtomicT m
-type instance ChannelT (DelegationT m) = ChannelT m
-
-instance ( Mockable d m
-         , MFunctor' d (ReaderT ReaderTCtx m) m
-         , MFunctor' d (DelegationT m) (ReaderT ReaderTCtx m)
-         ) => Mockable d (DelegationT m) where
-    liftMockable = liftMockableWrappedM
-
--- | Executes delegationT transformer creating tvar from given wrap.
-runDelegationT :: MonadIO m => DelegationWrap -> DelegationT m a -> m a
-runDelegationT wrap action =
-    liftIO (newTVarIO wrap) >>= runReaderT (getDelegationT action)
-
--- | Executes delegation wrap using existing delegation wrap tvar.
-runDelegationTFromTVar :: TVar DelegationWrap -> DelegationT m a -> m a
-runDelegationTFromTVar var action = runReaderT (getDelegationT action) var

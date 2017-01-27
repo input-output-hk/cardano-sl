fa59b280dd777e68aec88d87d6e8001b5a49d429
George Agapov <george.agapov@gmail.com>
2017-01-27 21:04:29 +0300

    Add imports to Mockable aggregate module

--- src/Mockable.hs
+++ src/Mockable.hs
@@ -2,12 +2,14 @@ module Mockable
        ( module M
        ) where
 
-import           Mockable.Channel      as M
-import           Mockable.Class        as M
-import           Mockable.Concurrent   as M
-import           Mockable.CurrentTime  as M
-import           Mockable.Exception    as M
-import           Mockable.Instances    as M
-import           Mockable.Monad        as M
-import           Mockable.Production   as M
-import           Mockable.SharedAtomic as M
+import           Mockable.Channel         as M
+import           Mockable.Class           as M
+import           Mockable.Concurrent      as M
+import           Mockable.CurrentTime     as M
+import           Mockable.Exception       as M
+import           Mockable.Instances       as M
+import           Mockable.Metrics         as M
+import           Mockable.Monad           as M
+import           Mockable.Production      as M
+import           Mockable.SharedAtomic    as M
+import           Mockable.SharedExclusive as M

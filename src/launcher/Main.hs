8848f4484395defa109aa6067df5a1e4c15a0d1d
Artyom <yom@artyom.me>
2017-01-28 18:57:46 +0300

    [launcher] Do horrible things to kill the node

--- src/launcher/Main.hs
+++ src/launcher/Main.hs
@@ -1,14 +1,22 @@
 {-# LANGUAGE MultiWayIf        #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes        #-}
 
-import           Control.Concurrent.Async (cancel, waitAny)
+import           Control.Concurrent.Async hiding (wait)
+import           Data.IORef
 import           Options.Applicative      (Parser, ParserInfo, execParser, fullDesc, help,
                                            helper, info, long, metavar, progDesc,
                                            strOption)
+import qualified System.IO                as IO
+import qualified System.Process           as Process
 import           System.Process.Internals (translate)
 import           Turtle                   hiding (toText)
 import           Universum                hiding (FilePath)
+
+import           Control.Exception        (handle, mask_, throwIO)
+import           Foreign.C.Error          (Errno (..), ePIPE)
+import           GHC.IO.Exception         (IOErrorType (..), IOException (..))
 
 data LauncherOptions = LO
     { loNode          :: Text
@@ -76,7 +84,15 @@ clientScenario nodeCmd walletCmd upd = do
     echo "Running the updater"
     runUpdater upd
     echo "Starting the node"
-    nodeAsync <- fork (shell nodeCmd mempty)
+    nodeHandleRef <- liftIO $
+        newIORef (panic "nodeHandleRef: node wasn't started")
+    -- I don't know why but a process started with turtle just can't be
+    -- killed, so let's do everything manually
+    nodeAsync <- fork (shell' nodeHandleRef nodeCmd mempty)
+    --nodeAsync <- fork $ do
+    --    ph <- Process.spawnCommand nodeCmd
+    --    writeIORef nodeHandleRef ph
+    --    waitForProcess ph
     echo "Starting the wallet"
     walletAsync <- fork (shell walletCmd mempty)
     (someAsync, exitCode) <- liftIO $ waitAny [nodeAsync, walletAsync]
@@ -87,11 +103,16 @@ clientScenario nodeCmd walletCmd upd = do
        | exitCode == ExitFailure 20 -> do
              echo "The wallet has exited with code 20"
              echo "Killing the node"
-             liftIO $ cancel nodeAsync
+             liftIO $ do
+                 Process.terminateProcess =<< readIORef nodeHandleRef
+                 cancel nodeAsync
              clientScenario nodeCmd walletCmd upd
        | otherwise -> do
              printf ("The wallet has exited with "%s%"\n") (show exitCode)
-             liftIO $ cancel nodeAsync
+             echo "Killing the node"
+             liftIO $ do
+                 Process.terminateProcess =<< readIORef nodeHandleRef
+                 cancel nodeAsync
 
 -- | We run the updater and delete the update file if the update was
 -- successful.
@@ -105,3 +126,82 @@ runUpdater (updaterCmd, updateArchive) = do
         -- this will throw an exception if the file doesn't exist but
         -- hopefully if the updater has succeeded it *does* exist
         rm updateArchive
+
+----------------------------------------------------------------------------
+-- Turtle internals, modified to give access to process handles
+----------------------------------------------------------------------------
+
+shell'
+    :: MonadIO io
+    => IORef Process.ProcessHandle
+    -- ^ Where to put process handle
+    -> Text
+    -- ^ Command line
+    -> Shell Line
+    -- ^ Lines of standard input
+    -> io ExitCode
+    -- ^ Exit code
+shell' ref cmdLine =
+    system' ref
+        ( (Process.shell (toString cmdLine))
+            { Process.std_in  = Process.CreatePipe
+            , Process.std_out = Process.Inherit
+            , Process.std_err = Process.Inherit
+            } )
+
+system'
+    :: MonadIO io
+    => IORef Process.ProcessHandle
+    -- ^ Where to put process handle
+    -> Process.CreateProcess
+    -- ^ Command
+    -> Shell Line
+    -- ^ Lines of standard input
+    -> io ExitCode
+    -- ^ Exit code
+system' ref p s = liftIO (do
+    let open = do
+            (m, Nothing, Nothing, ph) <- Process.createProcess p
+            writeIORef ref ph
+            case m of
+                Just hIn -> IO.hSetBuffering hIn IO.LineBuffering
+                _        -> return ()
+            return (m, ph)
+
+    -- Prevent double close
+    mvar <- newMVar False
+    let close handle = do
+            modifyMVar_ mvar (\finalized -> do
+                unless finalized (ignoreSIGPIPE (IO.hClose handle))
+                return True )
+    let close' (Just hIn, ph) = do
+            close hIn
+            Process.terminateProcess ph
+        close' (Nothing , ph) = do
+            Process.terminateProcess ph
+
+    let handle (Just hIn, ph) = do
+            let feedIn :: (forall a. IO a -> IO a) -> IO ()
+                feedIn restore =
+                    restore (ignoreSIGPIPE (outhandle hIn s)) `finally` close hIn
+            mask_ (withAsyncWithUnmask feedIn (\a -> Process.waitForProcess ph <* halt a) )
+        handle (Nothing , ph) = do
+            Process.waitForProcess ph
+
+    bracket open close' handle )
+
+halt :: Async a -> IO ()
+halt a = do
+    m <- poll a
+    case m of
+        Nothing        -> cancel a
+        Just (Left  e) -> throwIO e
+        Just (Right _) -> return ()
+
+ignoreSIGPIPE :: IO () -> IO ()
+ignoreSIGPIPE = handle (\e -> case e of
+    IOError
+        { ioe_type = ResourceVanished
+        , ioe_errno = Just ioe }
+        | Errno ioe == ePIPE -> return ()
+    _ -> throwIO e )

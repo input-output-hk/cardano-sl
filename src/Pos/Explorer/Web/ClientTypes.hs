ff77d77e5c7c7ce07bf358792867108be0707ae3
Dmitry Mukhutdinov <flyingleafe@gmail.com>
2017-02-06 21:24:34 +0300

    [CSE-10] `getBlocks` endpoint logic

--- src/Pos/Explorer/Web/ClientTypes.hs
+++ src/Pos/Explorer/Web/ClientTypes.hs
@@ -5,13 +5,20 @@ module Pos.Explorer.Web.ClientTypes
        , CAddress (..)
        , CTxId (..)
        , CBlockEntry (..)
+       , toBlockEntry
        , CTxEntry (..)
        ) where
 
 import           Universum
 
+import qualified Data.ByteString.Lazy  as BSL
 import           Data.Time.Clock.POSIX (POSIXTime)
-import           Pos.Types             (Coin)
+import           Pos.Binary            (encode)
+import           Pos.Slotting          (MonadSlots (..), getSlotStart)
+import           Pos.Types             (Coin, MainBlock (..), Tx (..), TxOut (..),
+                                        blockTxs, difficultyL, gbHeader, gbhConsensus,
+                                        mcdSlot, mkCoin, sumCoins, unsafeAddCoin,
+                                        unsafeIntegerToCoin)
 
 -- | Client hash
 newtype CHash = CHash Text deriving (Show, Eq, Generic, Buildable, Hashable)
@@ -31,6 +38,24 @@ data CBlockEntry = CBlockEntry
     , cbeSize       :: !Word64
     , cbeRelayedBy  :: !(Maybe Text)
     } deriving (Show, Generic)
+
+toBlockEntry :: MonadSlots m => MainBlock ssc -> m CBlockEntry
+toBlockEntry blk = do
+    blkSlotStart <- getSlotStart $
+                    blk ^. gbHeader . gbhConsensus . mcdSlot
+    let cbeHeight = fromIntegral $ blk ^. difficultyL
+        cbeTimeIssued = (/ 1e6) . fromIntegral $ blkSlotStart
+        txs = blk ^. blockTxs
+        cbeTxNum = fromIntegral $ length txs
+        addCoins c Tx {..} = unsafeAddCoin c $
+            unsafeIntegerToCoin . sumCoins $
+            map txOutValue txOutputs
+        cbeTotalSent = foldl' addCoins (mkCoin 0) txs
+        -- TODO: is there a way to get it more efficiently?
+        -- cbeSize = fromIntegral . BSL.length $ encode blk
+        cbeSize = 0
+        cbeRelayedBy = Nothing
+    return CBlockEntry {..}
 
 -- | List of tx entries is returned from "get latest N transactions" endpoint
 data CTxEntry = CTxEntry
--- src/Pos/Explorer/Web/Server.hs
+++ src/Pos/Explorer/Web/Server.hs
@@ -10,19 +10,27 @@ module Pos.Explorer.Web.Server
        ) where
 
 import           Control.Monad.Catch            (try)
+import           Control.Monad.Loops            (unfoldrM)
+import           Control.Monad.Trans.Maybe      (MaybeT (..))
+import           Data.Maybe                     (fromMaybe)
 import           Network.Wai                    (Application)
 import           Servant.API                    ((:<|>) ((:<|>)))
 import           Servant.Server                 (Handler, Server, ServerT, serve)
 import           Universum
 
 import           Pos.Communication              (SendActions)
+import qualified Pos.DB                         as DB
+import qualified Pos.DB.GState                  as GS
 import           Pos.Ssc.GodTossing             (SscGodTossing)
+import           Pos.Types                      (prevBlockL)
+import           Pos.Util                       (maybeThrow)
 import           Pos.Web                        (serveImpl)
 import           Pos.WorkMode                   (WorkMode)
 
 import           Pos.Explorer.Aeson.ClientTypes ()
 import           Pos.Explorer.Web.Api           (ExplorerApi, explorerApi)
-import           Pos.Explorer.Web.ClientTypes   (CBlockEntry (..), CTxEntry (..))
+import           Pos.Explorer.Web.ClientTypes   (CBlockEntry (..), CTxEntry (..),
+                                                 toBlockEntry)
 import           Pos.Explorer.Web.Error         (ExplorerError (..))
 
 ----------------------------------------------------------------
@@ -43,15 +51,38 @@ explorerApp serv = serve explorerApi <$> serv
 
 explorerHandlers :: ExplorerMode m => SendActions m -> ServerT ExplorerApi m
 explorerHandlers sendActions =
-    catchExplorerError ... getLastBlocks
+    catchExplorerError ... defaultLimit 10 getLastBlocks
     :<|>
-    catchExplorerError ... getLastTxs
+    catchExplorerError ... defaultLimit 10 getLastTxs
   where
     catchExplorerError = try
     f ... g = (f .) . g
 
-getLastBlocks :: ExplorerMode m => Maybe Word -> Maybe Word -> m [CBlockEntry]
-getLastBlocks _ _ = return []
+defaultLimit
+    :: Word                 -- default limit (default offset is always 0)
+    -> (Word -> Word -> a)  -- action to transform
+    -> Maybe Word
+    -> Maybe Word
+    -> a
+defaultLimit lim action mlim moff =
+    action (fromMaybe lim mlim) (fromMaybe 0 moff)
 
-getLastTxs :: ExplorerMode m => Maybe Word -> Maybe Word -> m [CTxEntry]
+getLastBlocks :: ExplorerMode m => Word -> Word -> m [CBlockEntry]
+getLastBlocks lim off = do
+    tip <- GS.getTip
+    let getNextBlk h _ = fmap (view prevBlockL) $
+            DB.getBlockHeader h >>=
+            maybeThrow (Internal "Block database is malformed!")
+    start <- foldlM getNextBlk tip [0..off]
+
+    let unfolder n h = do
+            when (n == 0) $
+                fail "limit!"
+            MaybeT (DB.getBlock h) >>= \case
+                Left gb -> unfolder n (gb ^. prevBlockL)
+                Right mb -> (,) <$> lift (toBlockEntry mb) <*>
+                            pure (n - 1, mb ^. prevBlockL)
+    flip unfoldrM (lim, start) $ \(n, h) -> runMaybeT $ unfolder n h
+
+getLastTxs :: ExplorerMode m => Word -> Word -> m [CTxEntry]
 getLastTxs _ _ = return []

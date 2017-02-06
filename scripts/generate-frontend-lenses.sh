6358c14d733ed5920c75b73c675d89bbf70d993f
Jens Krause <email@jkrause.io>
2017-02-06 11:28:53 +0100

    [CSE-27] Generate lenses of NominalDiffTime

--- scripts/generate-frontend-lenses.sh
+++ scripts/generate-frontend-lenses.sh
@@ -2,15 +2,23 @@
 
 set -e
 
-DIR_TYPES=src/Explorer/Types
-DIR_LENSES=./src/Explorer/Lenses
+# - - - - - - - - - - -
+# Types
+# - - - - - - - - - - -
 
-mkdir -p $DIR_LENSES
+DIR_TYPES=src/Explorer/Types
+DIR_TYPES_LENSES=src/Explorer/Lenses
+
+mkdir -p $DIR_TYPES_LENSES
 
 purescript-derive-lenses \
   < $DIR_TYPES/State.purs \
   --moduleName Explorer.Lenses.State \
-  > $DIR_LENSES/State.purs
+  > $DIR_TYPES_LENSES/State.purs
+
+# - - - - - - - - - - -
+# I18n
+# - - - - - - - - - - -
 
 DIR_I18N=src/Explorer/I18n
 
@@ -18,3 +26,18 @@ purescript-derive-lenses \
   < $DIR_I18N/Types.purs \
   --moduleName Explorer.I18n.Lenses \
   > $DIR_I18N/Lenses.purs
+
+# - - - - - - - - - - -
+# Data.Time
+# - - - - - - - - - - -
+
+DIR_TIME=src/Data/Time
+DIR_TIME_LENSES=$DIR_TIME/Lenses
+
+mkdir -p $DIR_TIME_LENSES
+
+purescript-derive-lenses \
+    < $DIR_TIME/NominalDiffTime.purs \
+    --moduleName Data.Time.NominalDiffTime.Lenses \
+    --moduleImports "import Data.Time.Duration  (Seconds (..))" \
+    > $DIR_TIME_LENSES/NorminalDiffTime.purs

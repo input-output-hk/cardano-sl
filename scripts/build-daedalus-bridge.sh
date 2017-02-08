0196da85bb1613caea8d3465bcd04ec728590202
Darko Mijić <darko.mijic@gmail.com>
2017-02-08 12:23:10 +0100

    Remove generated bridge source before compiling the bridge from shell script for bridge compilation

--- scripts/build-daedalus-bridge.sh
+++ scripts/build-daedalus-bridge.sh
@@ -2,6 +2,7 @@ rm -rf run/* wallet-db/ *key
 set -ex
 pushd cardano-sl
 stack --nix build --flag cardano-sl:with-wallet --flag cardano-sl:with-web
+rm -rf cardano-sl/daedalus/src/Generated/
 stack --nix exec -- cardano-wallet-hs2purs
 pushd daedalus
 nix-shell --run "npm install && npm run build:prod"

2f3c7df7d324bc056fefe0fce856e39a692f6d9f
Mikhail Volkhov <volhovm.cs@gmail.com>
2017-02-09 21:15:24 +0300

    [CSL-678] Minor fix for updateScenario.sh

--- scripts/updateScenario.sh
+++ scripts/updateScenario.sh
@@ -30,7 +30,7 @@ updater=$(find ../cardano-updater/.stack-work/install/ -name "cardano-updater" -
 
 echo "Building cardano-sl"
 stack clean cardano-sl
-grep src/Pos/Constants.hs "BlockVersion 0 0 0" # fails if not found
+grep "BlockVersion 0 0 0" src/Pos/Constants.hs  # fails if not found
 stack build --fast 
 csl_bin=$(find .stack-work/install/ -iname "bin")
 
@@ -130,11 +130,12 @@ pkill webfsd || true
 webfsd -p $serverPort -r webfsfolder 
 echo "Launched webfs server"
 
-# Launcher launching
 
-#echo "Launching launcher"
-#sleep 1
-#rm -rf update-node-tmp.log
-#stack exec cardano-launcher -- --node binaries_v000/cardano-node --node-log-config scripts/update-log-config.yaml -n "--update-server"  -n "http://localhost:$serverPort" -n "--update-latest-path" -n "updateDownloaded.tar" -n "--listen" -n "127.0.0.1:3004" -n "--peer" -n "127.0.0.1:3000/a_P8zb6fNP7I2H54FtGuhqxaMDAwMDAwMDAwMDAwMDA=" -n "--flat-distr" -n "(3,100000)" -n "--rebuild-db" --updater $updater -u "dir" -u "binaries_v000" --node-timeout 5 --report-server http://localhost:8555/ --update-archive updateDownloaded.tar
+# Launcher launching
+ 
+# echo "Launching launcher"
+# sleep 1
+# rm -rf update-node-tmp.log
+# stack exec cardano-launcher -- --node binaries_v000/cardano-node --node-log-config scripts/update-log-config.yaml -n "--update-server"  -n "http://localhost:$serverPort" -n "--update-latest-path" -n "updateDownloaded.tar" -n "--listen" -n "127.0.0.1:3004" -n "--peer" -n "127.0.0.1:3000/a_P8zb6fNP7I2H54FtGuhqxaMDAwMDAwMDAwMDAwMDA=" -n "--flat-distr" -n "(3,100000)" -n "--rebuild-db" --updater $updater -u "dir" -u "binaries_v000" --node-timeout 5 --report-server http://localhost:8555/ --update-archive updateDownloaded.tar
 
 notify-send "updater scenario: ready"

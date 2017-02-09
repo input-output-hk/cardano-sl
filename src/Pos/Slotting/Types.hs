dd209d53f41a8b835cfc35808cd77d9b0cb70348
Ivan Gromakovskii <gie1994@gmail.com>
2017-02-09 14:24:31 +0300

    [CSL-671] Rework 'SlottingData'

--- src/Pos/Slotting/Types.hs
+++ src/Pos/Slotting/Types.hs
@@ -1,16 +1,30 @@
 -- | Core types used in 'Slotting'.
 
 module Pos.Slotting.Types
-       ( SlottingData (..)
+       ( EpochSlottingData (..)
+       , SlottingData (..)
        ) where
 
 import           Data.Time.Units (Millisecond)
 -- import           Universum
 
-import           Pos.Types       (EpochIndex)
+import           Pos.Types       (EpochIndex, Timestamp)
 
+-- | Data which is necessary for slotting and corresponds to a particular epoch.
+data EpochSlottingData = EpochSlottingData
+    { esdSlotDuration :: !Millisecond
+    -- ^ Slot duration actual for given epoch.
+    , esdStart        :: !Timestamp
+    -- ^ Time when epoch starts.
+    }
+
+-- | Data necessary for slotting to work which is basically part of GState.
 data SlottingData = SlottingData
-    { sdEpoch        :: !EpochIndex
-    , sdSlotDuration :: !Millisecond
-    , sdEpochStart   :: !EpochIndex
+    { sdPenult      :: !EpochSlottingData
+    -- ^ 'EpochSlottingData' for penult (i. e. 'sdPenultEpoch') epoch.
+    , sdLast        :: !EpochSlottingData
+    -- ^ 'EpochSlottingData' for last epoch.
+    -- Last means 'sdPenultEpoch + 1'.
+    , sdPenultEpoch :: !EpochIndex
+    -- ^ Penult epoch for which 'EpochSlottingData' is known.
     }

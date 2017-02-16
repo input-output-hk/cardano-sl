cbf91fa7ced2ae53a1a11c65762de1005eed0294
Ante Kegalj <akegalj@gmail.com>
2017-02-16 14:52:13 +0100

    [DAE-87] Launch daedalus after before the update

--- scripts/updateScenario.sh
+++ scripts/updateScenario.sh
@@ -67,14 +67,6 @@ sleep 5
 sleep 20
 tmux select-window -t 0
 
-if $runNode; then
-  # Launcher launching
-  echo "Launching launcher"
-  sleep 1
-  rm -rf update-node-tmp.log
-  stack exec cardano-launcher -- --node binaries_v000/cardano-node --node-log-config scripts/update-log-config.yaml -n "--update-server"  -n "http://localhost:$serverPort" -n "--update-latest-path" -n "updateDownloaded.tar" -n "--listen" -n "127.0.0.1:3004" -n "--peer" -n "127.0.0.1:3000/a_P8zb6fNP7I2H54FtGuhqxaMDAwMDAwMDAwMDAwMDA=" -n "--flat-distr" -n "(3,100000)" -n "--rebuild-db" -n "--wallet" -n "--web-port" -n 8090 --updater $updater -u "dir" -u "binaries_v000" --node-timeout 5 --report-server http://localhost:8555/ --update-archive updateDownloaded.tar $wallet_cli &
-  echo "Luncher started"
-fi
 
 if $build; then
   # Building updater
@@ -95,6 +87,17 @@ if $build; then
   cp -v $csl_bin/* binaries_v000/
   beforeBumpMd5=$(md5sum binaries_v000/cardano-node)
   echo "$beforeBumpMd5"
+
+  if $runNode; then
+    # Launcher launching
+    echo "Launching launcher"
+    sleep 1
+    rm -rf update-node-tmp.log
+    stack exec cardano-launcher -- --node binaries_v000/cardano-node --node-log-config scripts/update-log-config.yaml -n "--update-server"  -n "http://localhost:$serverPort" -n "--update-latest-path" -n "updateDownloaded.tar" -n "--listen" -n "127.0.0.1:3004" -n "--peer" -n "127.0.0.1:3000/a_P8zb6fNP7I2H54FtGuhqxaMDAwMDAwMDAwMDAwMDA=" -n "--flat-distr" -n "(3,100000)" -n "--rebuild-db" -n "--wallet" -n "--web-port" -n 8090 --updater $updater -u "dir" -u "binaries_v000" --node-timeout 5 --report-server http://localhost:8555/ --update-archive updateDownloaded.tar $wallet_cli &
+    echo "Luncher started"
+  fi
+
+  echo "Waiting 10 secs before updating to new version"
   
   # Updating version in csl sources to v0.1.0
   sed -i.backup "s/BlockVersion 0 0 0/BlockVersion 0 1 0/" src/Pos/Constants.hs

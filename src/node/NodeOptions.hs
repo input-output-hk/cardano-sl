3b76ccd29ea08cf0fbc1a7b3717eddc9dd8493b5
Denis Shevchenko <denis.shevchenko@iohk.io>
2017-02-20 23:36:23 +0400

    New peer id in examples.

--- src/node/NodeOptions.hs
+++ src/node/NodeOptions.hs
@@ -153,7 +153,7 @@ argsParser =
   where
     peerHelpMsg =
         "Peer to connect to for initial peer discovery. Format\
-        \ example: \"localhost:1234/MHdtsP-oPf7UWly7QuXnLK5RDB8=\""
+        \ example: \"localhost:1234/dYGuDj0BrJxCsTC9ntJE7ePT7wUoVdQMH3sKLzQD8bo=\""
 
 getNodeOptions :: IO Args
 getNodeOptions = do

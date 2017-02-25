9bb6b355f679e4a7826d7adf682b8db06a457fca
Tim Dysinger <tim@dysinger.net>
2017-02-25 02:07:00 -0800

    remove unneeded LANGUAGE extension DataKinds

--- src/Pos/Util.hs
+++ src/Pos/Util.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE CPP                 #-}
-{-# LANGUAGE DataKinds           #-}
 {-# LANGUAGE DeriveTraversable   #-}
 {-# LANGUAGE GADTs               #-}
 {-# LANGUAGE RankNTypes          #-}

60e9d45e187ee3d864c8d059c0ed5300a5982b54
Alexandre Rodrigues <alexandrer_b@outlook.com>
2017-03-02 04:02:31 +0000

    [CSL-728] Fix and add new tests for 'computeSharesDistr'
    Due to tests in 'master' being broken over [CSL-728], they were previously
    disabled. In the meantime, a fix was developed, along with more complete
    tests for 'computeSharesDistr'.

--- src/Pos/Lrc.hs
+++ src/Pos/Lrc.hs
@@ -2,13 +2,15 @@
 
 module Pos.Lrc
        (
-         module Pos.Lrc.Consumer
+         module Pos.Lrc.Arbitrary
+       , module Pos.Lrc.Consumer
        , module Pos.Lrc.Consumers
        , module Pos.Lrc.Logic
        , module Pos.Lrc.FollowTheSatoshi
        , module Pos.Lrc.Types
        ) where
 
+import           Pos.Lrc.Arbitrary
 import           Pos.Lrc.Consumer
 import           Pos.Lrc.Consumers
 import           Pos.Lrc.FollowTheSatoshi

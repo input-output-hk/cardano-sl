73cedf8a8a9964b6635ba8ce763c24ff749ab1ab
Artyom <yom@artyom.me>
2017-03-03 00:24:20 +0300

    [CSL-779] Uncomment and fix the Bi LimitedLengthExt instance

--- infra/Pos/Binary/Infra/Communication.hs
+++ infra/Pos/Binary/Infra/Communication.hs
@@ -5,13 +5,12 @@ module Pos.Binary.Infra.Communication () where
 import           Data.Reflection                (Reifies (..))
 import           Universum
 
-import           Pos.Binary.Class               (Bi (..), limitGet)
-import           Pos.Communication.Limits.Types (LimitedLength (..),
-                                                 LimitedLengthExt (..), Limiter)
+import           Pos.Binary.Class               (Bi (..))
+import           Pos.Communication.Limits.Types (LimitedLengthExt (..), Limiter, limitGet)
 
-instance (Bi a, Reifies s l, Limiter l) => Bi (LimitedLengthExt s l a) where
+instance forall s l a. (Bi a, Reifies s l, Limiter l) =>
+         Bi (LimitedLengthExt s l a) where
     put (LimitedLength a) = put a
-    get = undefined
-    -- get = do
-    --     let maxBlockSize = reflect (Proxy @s)
-    --     limitGet maxBlockSize $ LimitedLength <$> get
+    get = do
+        let maxSize = reflect (Proxy @s)
+        limitGet maxSize $ LimitedLength <$> get

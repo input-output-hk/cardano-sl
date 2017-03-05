56a52d29f16414f5429f26329ddc89bfae043727
Domen Kožar <domen@dev.si>
2017-03-05 11:50:52 +0100

    Prepare travis-ci for daedalus MacOS builds

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-with import (fetchTarball https://github.com/NixOS/nixpkgs/archive/6a8790429692280998801c96660dcc85e30fb759.tar.gz) { };
+with import (fetchTarball https://github.com/NixOS/nixpkgs/archive/8bed8fb53227932886ab23e5f5f9eabe139f8e9f.tar.gz) { };
 
 let
   hsPkgs = haskell.packages.ghc802;

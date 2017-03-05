585d8d226e04326cf8c5666ebc4bbd453640e935
Alexandre Rodrigues <alexandrer_b@outlook.com>
2017-03-05 06:14:24 +0000

    [CSL-854] Add tests to unsuccessful verification
    Tests that verify successfuly verification of correct 'Commitment's
    and 'Opening's already existed. This commit now adds tests to verify
    incorrect values of these types will not be validated.

--- test/Test/Pos/Ssc/GodTossing/Types/BaseSpec.hs
+++ test/Test/Pos/Ssc/GodTossing/Types/BaseSpec.hs
@@ -6,7 +6,9 @@ module Test.Pos.Ssc.GodTossing.Types.BaseSpec
 
 import           Pos.Binary            ()
 import           Pos.Crypto            (SecretKey, sign, toPublic)
-import           Pos.Ssc.GodTossing    (Commitment, CommitmentOpening (..),
+import           Pos.Ssc.GodTossing    (BadCommAndOpening (..), BadCommitment (..),
+                                        BadSignedCommitment (..), Commitment,
+                                        CommitmentOpening (..), SignedCommitment,
                                         verifyCommitment, verifyCommitmentSignature,
                                         verifyOpening)
 import           Pos.Types             (EpochIndex)
@@ -19,29 +21,49 @@ spec :: Spec
 spec = describe "Ssc.GodTossing.Base" $ do
     describe "verifyCommitment" $ do
         prop description_verifiesOkComm verifiesOkComm
+        prop description_notVerifiesBadComm notVerifiesBadComm
     describe "verifyCommitmentSignature" $ do
         prop description_verifiesOkCommSig verifiesOkCommSig
+        prop description_notVerifiesBadSig notVerifiesBadCommSig
     describe "verifyOpening" $ do
         prop description_verifiesOkOpening verifiesOkOpening
+        prop description_notVerifiesBadOpening notVerifiesBadOpening
   where
     description_verifiesOkComm =
-        "successfully verifies a correct commitment, and fails to verify an incorrect\
-        \ commitment"
+        "successfully verifies a correct commitment commitment"
+    description_notVerifiesBadComm =
+        "unsuccessfully verifies an incorrect commitment"
     description_verifiesOkCommSig =
         "successfully verifies a signed commitment for a given epoch and secret key"
+    description_notVerifiesBadSig =
+        "unsuccessfully verifies a commitment signature and a mismatching epoch"
     description_verifiesOkOpening =
         "successfully verifies that an opening corresponding to the given commitment\
-        \ does indeed belong to it"
+\ does indeed belong to it"
+    description_notVerifiesBadOpening =
+        "unsuccessfully verifies a mismatching commitment and opening"
 
 verifiesOkComm :: CommitmentOpening -> Bool
 verifiesOkComm CommitmentOpening{..} =
     verifyCommitment coCommitment
+
+notVerifiesBadComm :: BadCommitment -> Bool
+notVerifiesBadComm (getBadComm -> badComm) =
+    not . verifyCommitment $ badComm
 
 verifiesOkCommSig :: SecretKey -> Commitment -> EpochIndex -> Bool
 verifiesOkCommSig sk comm epoch =
     let commSig = (toPublic sk, comm, sign sk (epoch, comm))
     in verifyCommitmentSignature epoch commSig
 
+notVerifiesBadCommSig :: BadSignedCommitment -> EpochIndex -> Bool
+notVerifiesBadCommSig (getBadSignedC -> badSignedComm) epoch =
+    not $ verifyCommitmentSignature epoch badSignedComm
+
 verifiesOkOpening :: CommitmentOpening -> Bool
 verifiesOkOpening CommitmentOpening{..} =
     verifyOpening coCommitment coOpening
+
+notVerifiesBadOpening :: BadCommAndOpening -> Bool
+notVerifiesBadOpening (getBadCAndO -> badCommsAndOp) =
+    not . uncurry verifyOpening $ badCommsAndOp

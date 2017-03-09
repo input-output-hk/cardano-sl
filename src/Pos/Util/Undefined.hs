ac8cfbdb2b4a7452ccabfc3eb667f666303a2741
Dmitry Kovanikov <kovanikov@gmail.com>
2017-03-09 22:28:48 +0300

    [SRK-53] Remove some -Wno-warning's

--- src/Pos/Util/Undefined.hs
+++ src/Pos/Util/Undefined.hs
@@ -1,8 +1,7 @@
 {-# LANGUAGE TemplateHaskell #-}
-
 {-# OPTIONS_GHC -Wno-deprecations #-}
 
--- | Instances and other stuff for 'NotImplemented'. We don't want GHC to
+-- | Instances and other stuff for 'Undefined'. We don't want GHC to
 -- emit warnings about them, so we have to put them into a separate module.
 module Pos.Util.Undefined () where
 
--- src/Pos/Wallet/Web/Doc.hs
+++ src/Pos/Wallet/Web/Doc.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE DataKinds #-}
-{-# OPTIONS_GHC -fno-warn-deprecations #-}
 
 -- | Documentation of wallet web API.
 

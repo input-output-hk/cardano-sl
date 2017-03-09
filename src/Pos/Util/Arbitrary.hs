164db5267777d855f0f0fe67aafbd96ad0e568c2
Ivan Gromakovskii <gie1994@gmail.com>
2017-03-09 16:54:11 +0300

    Comment and slightly prettify 'makeSmall' function

--- src/Pos/Util/Arbitrary.hs
+++ src/Pos/Util/Arbitrary.hs
@@ -27,6 +27,9 @@ import           Universum
 makeSmall :: Gen a -> Gen a
 makeSmall = scale f
   where
+    -- This function is “Golden function of testing”. It is perfect
+    -- for making tested values small. There was a profound research
+    -- in this area. `f 4` is 3, yes.
     f 0 = 0
     f 1 = 1
     f 2 = 2
@@ -35,8 +38,7 @@ makeSmall = scale f
     f n
       | n < 0 = n
       | otherwise =
-          (round . (sqrt :: Double -> Double) . realToFrac . (`div` 3)) n
-
+          (round . (sqrt @Double) . realToFrac . (`div` 3)) n
 
 -- | Choose a random (shuffled) subset of length n. Throws an error if
 -- there's not enough elements.

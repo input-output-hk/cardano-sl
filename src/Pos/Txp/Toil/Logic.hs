39e8d0c1a8558d2da42658f2b35a011a05444d12
Ivan Gromakovskii <gie1994@gmail.com>
2017-03-12 02:25:32 +0300

    [CSL-869] Slightly refactor and fix Toil.Logic

--- src/Pos/Txp/Toil/Logic.hs
+++ src/Pos/Txp/Toil/Logic.hs
@@ -26,13 +26,13 @@ import           Pos.Txp.Toil.Class    (MonadBalances (..), MonadTxPool (..),
 import           Pos.Txp.Toil.Failure  (ToilVerFailure (..))
 import qualified Pos.Txp.Toil.Utxo     as Utxo
 
+----------------------------------------------------------------------------
+-- Global
+----------------------------------------------------------------------------
+
 type GlobalTxpMode m = ( MonadUtxo m
                        , MonadBalances m
                        , WithLogger m)
-
-type LocalTxpMode m = ( MonadUtxo m
-                      , MonadTxPool m
-                      , MonadError ToilVerFailure m)
 
 -- CHECK: @verifyTxp
 -- | Verify transactions correctness with respect to Utxo applying
@@ -40,10 +40,13 @@ type LocalTxpMode m = ( MonadUtxo m
 -- Note: transactions must be topsorted to pass check.
 -- Warning: this function may apply some transactions and fail
 -- eventually. Use it only on temporary data.
-verifyTxp :: (GlobalTxpMode m, MonadError ToilVerFailure m) => [TxAux] -> m TxpUndo
-verifyTxp = mapM (processTxWithPureChecks True . withTxId)
+verifyTxp
+    :: (GlobalTxpMode m, MonadError ToilVerFailure m)
+    => [TxAux] -> m TxpUndo
+verifyTxp = mapM (verifyAndApplyTx False . withTxId)
 
--- | Apply transactions from one block.
+-- | Apply transactions from one block. They must be valid (for
+-- example, it implies topological sort).
 applyTxp :: GlobalTxpMode m => [(TxAux, TxUndo)] -> m ()
 applyTxp txun = do
     applyTxsToBalances txun
@@ -55,37 +58,46 @@ rollbackTxp txun = do
     rollbackTxsBalances txun
     mapM_ Utxo.rollbackTxUtxo $ reverse txun
 
+----------------------------------------------------------------------------
+-- Local
+----------------------------------------------------------------------------
+
+type LocalTxpMode m = ( MonadUtxo m
+                      , MonadTxPool m
+                      )
+
+-- CHECK: @processTx
+-- #processWithPureChecks
+-- | Verify one transaction and also add it to mem pool and apply to utxo
+-- if transaction is valid.
+processTx
+    :: (LocalTxpMode m, MonadError ToilVerFailure m)
+    => (TxId, TxAux) -> m ()
+processTx tx@(id, aux) = do
+    whenM (hasTx id) $ throwError ToilKnown
+    whenM ((>= maxLocalTxs) <$> poolSize) $ throwError ToilOverwhelmed
+    undo <- verifyAndApplyTx True tx
+    putTxWithUndo id aux undo
+
 -- | Get rid of invalid transactions.
 -- All valid transactions will be added to mem pool and applied to utxo.
 normalizeTxp
-    :: (MonadUtxo m, MonadTxPool m)
+    :: (LocalTxpMode m)
     => [(TxId, TxAux)] -> m ()
 normalizeTxp txs = mapM_ (runExceptT . processTx) ordered
   where
     ordered = fromMaybe txs $ topsortTxs wHash txs
     wHash (i, (t, _, _)) = WithHash t i
 
--- CHECK: @processTx
--- #processWithPureChecks
--- Validate one transaction and also add it to mem pool and apply to utxo
--- if transaction is valid.
-processTx
-    :: LocalTxpMode m => (TxId, TxAux) -> m ()
-processTx tx@(id, aux) = do
-    whenM (hasTx id) $ throwError ToilKnown
-    whenM ((>= maxLocalTxs) <$> poolSize) $ throwError ToilOverwhelmed
-    undo <- processTxWithPureChecks True tx
-    putTxWithUndo id aux undo
-
 ----------------------------------------------------------------------------
 -- Helpers
 ----------------------------------------------------------------------------
 
-processTxWithPureChecks
+verifyAndApplyTx
     :: (MonadUtxo m, MonadError ToilVerFailure m)
     => Bool -> (TxId, TxAux) -> m TxUndo
-processTxWithPureChecks verifyVersions tx@(_, aux) =
-    Utxo.verifyTxUtxo ctx aux <* applyTxToUtxo' tx
+verifyAndApplyTx verifyVersions tx@(_, txAux) =
+    Utxo.verifyTxUtxo ctx txAux <* applyTxToUtxo' tx
   where
     ctx = Utxo.VTxContext verifyVersions
 

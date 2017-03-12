146c9a04c190322bd21f07c28c6f174d450b25f5
ksaric <kristijan.saric@iohk.io>
2017-03-12 20:29:47 +0100

    [CSE-56] added  folder and added generation script for 'travis'

--- src/documentation/Main.hs
+++ src/documentation/Main.hs
@@ -6,4 +6,4 @@ import Pos.Explorer.Web.Doc (walletDocsText)
 main :: IO ()
 main = writeFile fp walletDocsText >> putStrLn ("See " <> fp)
   where
-    fp = "cardano-explorer-web-api.md"
+    fp = "docs/cardano-explorer-web-api.md"

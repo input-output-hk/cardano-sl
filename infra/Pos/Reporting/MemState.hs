9e8e383d6e8ba77608e6ca39b7f129026c8422ac
Mikhail Volkhov <volhovm.cs@gmail.com>
2017-03-13 20:37:30 +0300

    Refactor Reporting.MemState a little bit

--- infra/Pos/Reporting/MemState.hs
+++ infra/Pos/Reporting/MemState.hs
@@ -1,6 +1,33 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies    #-}
+
+-- | Reporting functionality abstracted.
 module Pos.Reporting.MemState
-       ( module X
+       ( ReportingContext (..)
+       , rcReportServers
+       , MonadReportingMem (..)
        ) where
 
-import           Pos.Reporting.MemState.Class as X
-import           Pos.Reporting.MemState.Types as X
+import           Control.Lens        (makeLenses)
+import           Control.Monad.Trans (MonadTrans)
+import           Universum
+
+-- | Context needed to provide remote reporting capabilities.
+data ReportingContext = ReportingContext
+    { _rcReportServers :: ![Text] -- ^ Report servers list (urls)
+    }
+
+makeLenses ''ReportingContext
+
+-- | Monads are able to do remote error reporting. IO for making http
+-- requests, context for saving reporting-related data.
+class (Monad m) => MonadReportingMem m where
+    askReportingContext :: m ReportingContext
+
+    default askReportingContext :: (MonadTrans t, MonadReportingMem m', t m' ~ m) =>
+       m ReportingContext
+    askReportingContext = lift askReportingContext
+
+instance MonadReportingMem m => MonadReportingMem (ReaderT s m) where
+instance MonadReportingMem m => MonadReportingMem (ExceptT s m) where
+instance MonadReportingMem m => MonadReportingMem (StateT s m) where

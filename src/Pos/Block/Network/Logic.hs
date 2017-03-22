3b41ec06370af09cd361874fded17a6eb6827fea
Philipp Kant <philipp.kant@iohk.io>
2017-03-22 17:13:39 +0100

    Do not enter recovery when receiving new block.
    Previously, addToBlockRequestQueue would set ncRecoveryHeader if it was
    not previously set, causing the node to enter recovery mode.  In
    particular, this would happen each time that handleUnsolicitedHeader got
    a header that continued the current chain.
    
    This commit fixes that behaviour.

--- src/Pos/Block/Network/Logic.hs
+++ src/Pos/Block/Network/Logic.hs
@@ -163,12 +163,12 @@ handleUnsolicitedHeader header peerId conv = do
     case classificationRes of
         CHContinues -> do
             logDebug $ sformat continuesFormat hHash
-            addToBlockRequestQueue (one header) peerId header
+            addToBlockRequestQueue (one header) peerId Nothing
         CHAlternative -> do
             logInfo $ sformat alternativeFormat hHash
             mghM <- mkHeadersRequest (Just hHash)
             whenJust mghM $ \mgh ->
-                requestHeaders mgh peerId header Proxy conv
+                requestHeaders mgh peerId (Just header) Proxy conv
         CHUseless reason -> logDebug $ sformat uselessFormat hHash reason
         CHInvalid _ -> do
             logDebug $ sformat ("handleUnsolicited: header "%shortHashF%
@@ -222,7 +222,7 @@ requestHeaders
        (SscWorkersClass ssc, WorkMode ssc m)
     => MsgGetHeaders
     -> NodeId
-    -> BlockHeader ssc
+    -> Maybe (BlockHeader ssc)
     -> Proxy s
     -> ConversationActions MsgGetHeaders (LimitedLength s (MsgHeaders ssc)) m
     -> m ()
@@ -255,7 +255,7 @@ handleRequestedHeaders
        (WorkMode ssc m)
     => NewestFirst NE (BlockHeader ssc)
     -> NodeId
-    -> BlockHeader ssc
+    -> Maybe (BlockHeader ssc)
     -> m ()
 handleRequestedHeaders headers peerId origTip = do
     logDebug "handleRequestedHeaders: headers were requested, will process"
@@ -299,28 +299,29 @@ addToBlockRequestQueue
        (WorkMode ssc m)
     => NewestFirst NE (BlockHeader ssc)
     -> NodeId
-    -> BlockHeader ssc
+    -> Maybe (BlockHeader ssc)
     -> m ()
-addToBlockRequestQueue headers peerId origTip = do
+addToBlockRequestQueue headers peerId mrecoveryTip = do
     wasInRecoveryMode <- isRecoveryMode
     logDebug $ sformat ("At start of addToBlockRequestQueue, isRecoveryMode="%shown) wasInRecoveryMode
     queue <- ncBlockRetrievalQueue <$> getNodeContext
     recHeaderVar <- ncRecoveryHeader <$> getNodeContext
     lastKnownH <- ncLastKnownHeader <$> getNodeContext
-    atomically $ do
-        oldV <- readTVar lastKnownH
-        when (maybe True (origTip `isMoreDifficult`) oldV) $
-            writeTVar lastKnownH (Just origTip)
-    atomically $ do
-        let replace = tryTakeTMVar recHeaderVar >>= \case
-                Just (_, header')
-                    | not (origTip `isMoreDifficult` header') -> pass
-                _ -> putTMVar recHeaderVar (peerId, origTip)
-        tryReadTMVar recHeaderVar >>= \case
-            Nothing -> replace
-            Just (_,curRecHeader) ->
-                when (origTip `isMoreDifficult` curRecHeader) replace
+    let updateRecoveryHeader (Just recoveryTip) = do
+            oldV <- readTVar lastKnownH
+            when (maybe True (recoveryTip `isMoreDifficult`) oldV) $
+                writeTVar lastKnownH (Just recoveryTip)
+            let replace = tryTakeTMVar recHeaderVar >>= \case
+                    Just (_, header')
+                        | not (recoveryTip `isMoreDifficult` header') -> pass
+                    _ -> putTMVar recHeaderVar (peerId, recoveryTip)
+            tryReadTMVar recHeaderVar >>= \case
+                Nothing -> replace
+                Just (_,curRecHeader) ->
+                    when (recoveryTip `isMoreDifficult` curRecHeader) replace
+        updateRecoveryHeader Nothing = pass
     added <- atomically $ do
+        updateRecoveryHeader mrecoveryTip
         ifM (isFullTBQueue queue)
             (pure False)
             (True <$ writeTBQueue queue (peerId, headers))

29110b1b229ef2a8c17c0b0acff937190a069406
Ilya Peresadin <pva701@gmail.com>
2017-03-25 16:18:40 +0300

    [CSL-880] 'discoverHDAddress' implemented

--- src/Pos/Crypto/HDDiscovery.hs
+++ src/Pos/Crypto/HDDiscovery.hs
@@ -1,6 +1,30 @@
--- | 
+-- |
 
 module Pos.Crypto.HDDiscovery
-       ( discoveryHDAddress
+       ( discoverHDAddress
        ) where
 
+import           Universum
+
+import           Pos.Core.Types      (Address (..), addrPkDerivationPath)
+import           Pos.Crypto.HD       (HDAddressPayload, HDPassphrase, unpackHDAddressAttr)
+import           Pos.Data.Attributes (attrData)
+import           Pos.DB.Class        (MonadDB)
+import           Pos.Txp.Core        (toaOut, txOutAddress)
+import           Pos.Txp.DB          (UtxoIter, runUtxoMapIterator)
+import           Pos.Util.Iterator   (MonadIterator (..))
+
+discoverHDAddress :: MonadDB m => HDPassphrase -> m [(Address, [Word32])]
+discoverHDAddress walletPassphrase =
+    runUtxoMapIterator @UtxoIter (step []) (txOutAddress . toaOut . snd)
+  where
+    hdPayload :: Address -> Maybe HDAddressPayload
+    hdPayload (PubKeyAddress _ p) =
+        addrPkDerivationPath . attrData $ p
+    hdPayload _ = Nothing
+
+    onItem res address
+        | Just path <- hdPayload address >>= unpackHDAddressAttr walletPassphrase =
+            step ((address, path):res)
+        | otherwise = step res
+    step res = nextItem >>= maybe (pure res) (onItem res)

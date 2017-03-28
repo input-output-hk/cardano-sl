2ce745a7742fa14a871f2b3b81f35f1d3e236562
Jens Krause <email@jkrause.io>
2017-03-28 18:31:42 +0200

    [CSE-20] Fix import of `Subscription`
    for generating client side lenses

--- frontend/scripts/generate-frontend-lenses.sh
+++ frontend/scripts/generate-frontend-lenses.sh
@@ -14,6 +14,7 @@ mkdir -p $DIR_TYPES_LENSES
 purescript-derive-lenses \
   < $DIR_TYPES/State.purs \
   --moduleName Explorer.Lenses.State \
+  --moduleImports "import Pos.Explorer.Socket.Methods (Subscription)" \
   > $DIR_TYPES_LENSES/State.purs
 
 # - - - - - - - - - - -

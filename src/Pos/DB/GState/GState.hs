63888aa581574d3560d7b173ee64e75bcaa4d35f
Ivan Gromakovskii <gie1994@gmail.com>
2017-04-12 22:02:00 +0300

    [CSL-993] Sort out work with balances (aka stakes)
    Now there are two versions of functions which get balances from DB with
    different names: with `effective` word in name and with `real` word in name.
    Real corresponds to stake which is modified by transactions and is stored
    in DB.
    Effective is same is real if we are not in bootstrap era, otherwise it's taken
    from predefined constants.
    This commit also fixes a bug where effective stake was used is 'MonadBalances'
    implementation.

--- src/Pos/DB/GState/GState.hs
+++ src/Pos/DB/GState/GState.hs
@@ -17,7 +17,7 @@ import           Pos.Context.Class      (WithNodeContext (getNodeContext))
 import           Pos.Context.Context    (ncSystemStart)
 import           Pos.Context.Functions  (genesisUtxoM)
 import           Pos.DB.Class           (MonadDB (getNodeDBs, usingReadOptions))
-import           Pos.DB.GState.Balances (getTotalFtsStake)
+import           Pos.DB.GState.Balances (getRealTotalStake)
 import           Pos.DB.GState.Common   (prepareGStateCommon)
 import           Pos.DB.Types           (DB (..), NodeDBs (..), Snapshot (..), gStateDB,
                                          usingSnapshot)
@@ -46,7 +46,7 @@ sanityCheckGStateDB
     => m ()
 sanityCheckGStateDB = do
     sanityCheckBalances
-    sanityCheckUtxo =<< getTotalFtsStake
+    sanityCheckUtxo =<< getRealTotalStake
 
 usingGStateSnapshot :: (MonadDB m, MonadMask m) => m a -> m a
 usingGStateSnapshot action = do

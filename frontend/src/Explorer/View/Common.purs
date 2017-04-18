6c6191f9e097a557c7c956660254d30a782aba85
Jens Krause <email@jkrause.io>
2017-04-18 17:52:22 +0200

    [CSE-35] Responsive table of transactions
    at Dashboard

--- frontend/src/Explorer/View/Common.purs
+++ frontend/src/Explorer/View/Common.purs
@@ -295,8 +295,8 @@ noData = "--"
 currencyCSSClass :: Maybe CCurrency -> String
 currencyCSSClass mCurrency =
   case mCurrency of
-      Just ADA -> " ada bg-ada-dark"
-      Just USD -> " usd bg-usd-dark"
+      Just ADA -> " currency ada bg-ada-dark"
+      Just USD -> " currency usd bg-usd-dark"
       _ -> ""
 
 -- TODO (jk) Remove placeholderView if all views are implemented

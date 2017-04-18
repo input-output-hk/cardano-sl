7f5f95d3cf23e8750f38224c94ba75ec18fafc46
Alex <alexandrer_b@outlook.com>
2017-04-18 05:39:02 +0200

    [CSL-979] Refactor 'PollState' and its 'MonadPoll[Read]' instances This commit is the result of the PR's first review. 'PollState' was refined to avoid using useless fields,and some were refactored to better make use of all the data the record type already carries. There was no need for 'MapModifier's, so they were reverted to 'HashMap's.
    The 'PollState' module was also moved to the 'update' folder.

--- src/Pos/Update/Poll/Pure.hs
+++ src/Pos/Update/Poll/Pure.hs
@@ -4,74 +4,77 @@ module Pos.Update.Poll.Pure
        ( PurePoll (..)
        ) where
 
-import           Universum                  hiding (toList)
-import qualified Universum                  (toList)
+import           Universum
 
 import           Control.Lens               (to, (%=), (.=))
 import qualified Data.HashMap.Strict        as HM
 import           System.Wlog                (CanLog, HasLoggerName, NamedPureLogger)
 
+import           Pos.Crypto                 (hash)
 import           Pos.Update.Poll.Class      (MonadPoll (..), MonadPollRead (..))
 import qualified Pos.Update.Poll.PollState  as Poll
-import           Pos.Update.Poll.Types      (DecidedProposalState (dpsDifficulty),
-                                             ProposalState (..),
-                                             UndecidedProposalState (upsSlot))
-import           Pos.Util.Modifier          (delete, insert, lookup, keys, toList, values)
+import           Pos.Update.Poll.Types      (DecidedProposalState (..),
+                                             UndecidedProposalState (..),
+                                             propStateToEither)
 
 newtype PurePoll a = PurePoll
     { getPurePoll :: NamedPureLogger (State Poll.PollState) a
     } deriving (Functor, Applicative, Monad, CanLog, HasLoggerName)
 
 instance MonadPollRead PurePoll where
-    getBVState bv = PurePoll $ use $ Poll.psBlockVersions . to (lookup (const Nothing) bv)
-    getProposedBVs = PurePoll $ use $ Poll.psBlockVersions . to (keys [])
+    getBVState bv = PurePoll $ use $ Poll.psBlockVersions . to (HM.lookup bv)
+    getProposedBVs = PurePoll $ use $ Poll.psBlockVersions . to HM.keys
     getEpochProposers = PurePoll $ use $ Poll.psEpochProposers
-    getConfirmedBVStates = PurePoll $ use $ Poll.psBlockVersions . to (toList [])
+    getConfirmedBVStates = PurePoll $ use $ Poll.psBlockVersions . to HM.toList
     getAdoptedBVFull = PurePoll $ use $ Poll.psAdoptedBV
-    getLastConfirmedSV an =
-        PurePoll $ use $ Poll.psConfirmedBVs . to (lookup (const Nothing) an)
-    getProposal ui =
-        PurePoll $ use $ Poll.psActiveProposals . to (lookup (const Nothing) ui)
+    getLastConfirmedSV an = PurePoll $ use $ Poll.psConfirmedBVs . to (HM.lookup an)
+    getProposal ui = PurePoll $ use $ Poll.psActiveProposals . to (HM.lookup ui)
     getProposalsByApp an = PurePoll $ do
         delActiveProposalsIndices <- use Poll.psDelActivePropsIdx
         activeProposals           <- use Poll.psActiveProposals
         pure $ propGetByApp delActiveProposalsIndices activeProposals
       where propGetByApp appHashmap upIdHashmap = fromMaybe [] $
                 do hashset <- HM.lookup an appHashmap
-                   let uidList = Universum.toList hashset
-                       propStateList =
-                           fmap (flip (lookup (const Nothing)) upIdHashmap) uidList
+                   let uidList = toList hashset
+                       propStateList = fmap (flip HM.lookup upIdHashmap) uidList
                    sequence propStateList
-    getConfirmedProposals = PurePoll $ use $ Poll.psConfirmedProposals . to (values [])
-    getEpochTotalStake ei = PurePoll $ use $ Poll.psStakePerEpoch . to (HM.lookup ei)
+    getConfirmedProposals = PurePoll $ use $ Poll.psConfirmedProposals . to HM.elems
+    getEpochTotalStake ei =
+        PurePoll $ use $ Poll.psFullRichmenData . to ((Just . fst) <=< HM.lookup ei)
     getRichmanStake ei si =
-        PurePoll $ use $ Poll.psMultiRichmenStake . to (HM.lookup ei >=> HM.lookup si)
+        PurePoll $ use $ Poll.psFullRichmenData .
+            to ((HM.lookup si . snd) <=< HM.lookup ei)
     getOldProposals si =
-        PurePoll $ use $ Poll.psUndecidedProposals . to (filter ((<= si) . upsSlot))
+        PurePoll $ use $ Poll.psActiveProposals . to (filter ((<= si) . upsSlot) .
+                                                      lefts .
+                                                      map propStateToEither .
+                                                      HM.elems)
     getDeepProposals cd =
-        PurePoll $ use $ Poll.psDecidedProposals .
-            to (filter ((> (Just cd)) . dpsDifficulty))
+        PurePoll $ use $ Poll.psActiveProposals .
+            to (filter ((> (Just cd)) . dpsDifficulty) .
+                rights .
+                map propStateToEither .
+                HM.elems)
     getBlockIssuerStake ei si =
         PurePoll $ use $ Poll.psIssuersStakes . to (HM.lookup ei >=> HM.lookup si)
     getSlottingData = PurePoll $ use Poll.psSlottingData
 
 instance MonadPoll PurePoll where
-    putBVState bv bvs = PurePoll $ Poll.psBlockVersions %= insert bv bvs
-    delBVState bv = PurePoll $ Poll.psBlockVersions %= delete bv
+    putBVState bv bvs = PurePoll $ Poll.psBlockVersions %= HM.insert bv bvs
+    delBVState bv = PurePoll $ Poll.psBlockVersions %= HM.delete bv
     setAdoptedBV bv = PurePoll $ Poll.psAdoptedBV %= (_1 .~ bv)
     setLastConfirmedSV sv = PurePoll $ Poll.psSoftwareVersion .= sv
-    delConfirmedSV an = PurePoll $ Poll.psConfirmedBVs %= delete an
+    delConfirmedSV an = PurePoll $ Poll.psConfirmedBVs %= HM.delete an
     addConfirmedProposal cps = PurePoll $ void $ do
         addProp <$> use Poll.psSoftwareVersion <*> use Poll.psConfirmedProposals
       where
-        addProp sv svMap = insert sv cps svMap
-    delConfirmedProposal sv = PurePoll $ Poll.psConfirmedProposals %= delete sv
-    insertActiveProposal p = PurePoll $ void $ do
-        decideProp <$> use Poll.psDecidedProposals
+        addProp sv svMap = HM.insert sv cps svMap
+    delConfirmedProposal sv = PurePoll $ Poll.psConfirmedProposals %= HM.delete sv
+    insertActiveProposal p = PurePoll $ Poll.psActiveProposals %= decideProp p
       where
-          decideProp = case p of
-              PSDecided ps -> (ps :)
-              _ -> identity
-    deactivateProposal ui = PurePoll $ Poll.psActiveProposals %= delete ui
+          decideProp ps = HM.insert (getUpId ps) ps
+          getUpId = either hashUProp (hashUProp . dpsUndecided) . propStateToEither
+          hashUProp = hash . upsProposal
+    deactivateProposal ui = PurePoll $ Poll.psActiveProposals %= HM.delete ui
     setSlottingData sd = PurePoll $ Poll.psSlottingData .= sd
     setEpochProposers hs = PurePoll $ Poll.psEpochProposers .= hs
--- update/Pos/Update/Poll/PollState.hs
+++ update/Pos/Update/Poll/PollState.hs
@@ -18,10 +18,7 @@ module Pos.Update.Poll.PollState
        , psActiveProposals
        , psDelActivePropsIdx
        , psSlottingData
-       , psUndecidedProposals
-       , psDecidedProposals
-       , psStakePerEpoch
-       , psMultiRichmenStake
+       , psFullRichmenData
        , psIssuersStakes
        ) where
 
@@ -30,23 +27,20 @@ import           Data.HashSet                 (HashSet)
 import           Data.HashMap.Strict          (HashMap)
 import           Universum
 
-import           Pos.Core.Types               (BlockVersionData, Coin)
+import           Pos.Core.Types               (BlockVersionData)
 import           Pos.Lrc.DB.Issuers           (IssuersStakes)
+import           Pos.Lrc.Types                (FullRichmenData)
 import           Pos.Slotting.Types           (SlottingData)
-import           Pos.Ssc.GodTossing.Toss.Pure (MultiRichmenStake)
 import           Pos.Types                    (ApplicationName, BlockVersion,
                                                EpochIndex, NumSoftwareVersion,
                                                SoftwareVersion, StakeholderId)
 import           Pos.Update.Core              (UpId)
-import           Pos.Update.Poll              (BlockVersionState,
-                                               ConfirmedProposalState,
-                                               DecidedProposalState, ProposalState,
-                                               UndecidedProposalState)
-import           Pos.Util.Modifier            (MapModifier)
+import           Pos.Update.Poll              (BlockVersionState, ConfirmedProposalState,
+                                               ProposalState)
 
 data PollState = PollState
-    { -- | All proposed block versions with their states
-      _psBlockVersions      :: !(MapModifier BlockVersion BlockVersionState)
+    { -- | All competing block versions with their states
+      _psBlockVersions      :: !(HashMap BlockVersion BlockVersionState)
       -- | Presently adopted block version with its data
     , _psAdoptedBV          :: !(BlockVersion, BlockVersionData)
       -- | Software version of this node
@@ -54,22 +48,17 @@ data PollState = PollState
       -- | All stakeholders who made proposals in the current epoch
     , _psEpochProposers     :: !(HashSet StakeholderId)
       -- | All applications in use and their latest (confirmed) versions
-    , _psConfirmedBVs       :: !(MapModifier ApplicationName NumSoftwareVersion)
+    , _psConfirmedBVs       :: !(HashMap ApplicationName NumSoftwareVersion)
       -- | All proposed software versions and their state
-    , _psConfirmedProposals :: !(MapModifier SoftwareVersion ConfirmedProposalState)
+    , _psConfirmedProposals :: !(HashMap SoftwareVersion ConfirmedProposalState)
       -- | All update proposals and their states
-    , _psActiveProposals    :: !(MapModifier UpId ProposalState)
+    , _psActiveProposals    :: !(HashMap UpId ProposalState)
       -- | Update proposals for each application
     , _psDelActivePropsIdx  :: !(HashMap ApplicationName (HashSet UpId))
       -- | Slotting data for this node
     , _psSlottingData       :: !SlottingData
-      -- | State of undecided proposals
-    , _psUndecidedProposals :: ![UndecidedProposalState]
-      -- | State of decided proposals
-    , _psDecidedProposals   :: ![DecidedProposalState]
-      -- | Mapping between epochs and stake to them allotted
-    , _psStakePerEpoch      :: !(HashMap EpochIndex Coin)
-    , _psMultiRichmenStake  :: !MultiRichmenStake
+      -- | Mapping between epochs and their richmen stake distribution
+    , _psFullRichmenData    :: !(HashMap EpochIndex FullRichmenData)
       -- | Mapping between epochs and stake of each of the epoch's slot's block issuer
     , _psIssuersStakes      :: !(HashMap EpochIndex IssuersStakes)
     } deriving (Show, Eq)

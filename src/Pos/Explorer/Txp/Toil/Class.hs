ff9bd6fce97e2037d4b46e2ee65c87c07be68ed2
Lionel Miller <delonorm@gmail.com>
2017-04-27 19:45:02 +0300

    [CSM-224] Store address balances for Explorer

--- src/Pos/Explorer/Txp/Toil/Class.hs
+++ src/Pos/Explorer/Txp/Toil/Class.hs
@@ -12,12 +12,12 @@ import           Universum
 import           Control.Lens                (at, (%=), (.=))
 import           Control.Monad.Trans.Class   (MonadTrans)
 
-import           Pos.Core                    (Address)
+import           Pos.Core                    (Address, Coin)
 import           Pos.DB.Class                (MonadDB)
 import           Pos.Explorer.Core           (AddrHistory, TxExtra)
 import qualified Pos.Explorer.DB             as DB
-import           Pos.Explorer.Txp.Toil.Types (ExplorerExtra, eeAddrHistories,
-                                              eeLocalTxsExtra)
+import           Pos.Explorer.Txp.Toil.Types (ExplorerExtra, eeAddrBalances,
+                                              eeAddrHistories, eeLocalTxsExtra)
 import           Pos.Txp.Core                (TxId)
 import           Pos.Txp.Toil                (DBTxp, ToilT, UtxoReaderT, tmExtra)
 import           Pos.Util                    (ether)
@@ -26,6 +26,7 @@ import qualified Pos.Util.Modifier           as MM
 class Monad m => MonadTxExtraRead m where
     getTxExtra :: TxId -> m (Maybe TxExtra)
     getAddrHistory :: Address -> m AddrHistory
+    getAddrBalance :: Address -> m (Maybe Coin)
 
     default getTxExtra
         :: (MonadTrans t, MonadTxExtraRead m', t m' ~ m) => TxId -> m (Maybe TxExtra)
@@ -34,6 +35,10 @@ class Monad m => MonadTxExtraRead m where
     default getAddrHistory
         :: (MonadTrans t, MonadTxExtraRead m', t m' ~ m) => Address -> m AddrHistory
     getAddrHistory = lift . getAddrHistory
+
+    default getAddrBalance
+        :: (MonadTrans t, MonadTxExtraRead m', t m' ~ m) => Address -> m (Maybe Coin)
+    getAddrBalance = lift . getAddrBalance
 
 instance MonadTxExtraRead m => MonadTxExtraRead (ReaderT s m)
 instance MonadTxExtraRead m => MonadTxExtraRead (StateT s m)
@@ -44,6 +49,8 @@ class MonadTxExtraRead m => MonadTxExtra m where
     putTxExtra :: TxId -> TxExtra -> m ()
     delTxExtra :: TxId -> m ()
     updateAddrHistory :: Address -> AddrHistory -> m ()
+    putAddrBalance :: Address -> Coin -> m ()
+    delAddrBalance :: Address -> m ()
 
     default putTxExtra
         :: (MonadTrans t, MonadTxExtra m', t m' ~ m) => TxId -> TxExtra -> m ()
@@ -57,6 +64,14 @@ class MonadTxExtraRead m => MonadTxExtra m where
         :: (MonadTrans t, MonadTxExtra m', t m' ~ m) => Address -> AddrHistory -> m ()
     updateAddrHistory addr = lift . updateAddrHistory addr
 
+    default putAddrBalance
+        :: (MonadTrans t, MonadTxExtra m', t m' ~ m) => Address -> Coin -> m ()
+    putAddrBalance addr = lift . putAddrBalance addr
+
+    default delAddrBalance
+        :: (MonadTrans t, MonadTxExtra m', t m' ~ m) => Address -> m ()
+    delAddrBalance = lift . delAddrBalance
+
 instance MonadTxExtra m => MonadTxExtra (ReaderT s m)
 instance MonadTxExtra m => MonadTxExtra (StateT s m)
 instance MonadTxExtra m => MonadTxExtra (ExceptT s m)
@@ -67,10 +82,12 @@ instance MonadTxExtra m => MonadTxExtra (UtxoReaderT m)
 ----------------------------------------------------------------------------
 
 instance MonadTxExtraRead m => MonadTxExtraRead (ToilT ExplorerExtra m) where
-    getTxExtra id = ether $
-        MM.lookupM getTxExtra id =<< use (tmExtra . eeLocalTxsExtra)
+    getTxExtra txId = ether $
+        MM.lookupM getTxExtra txId =<< use (tmExtra . eeLocalTxsExtra)
     getAddrHistory addr = ether $
         maybe (getAddrHistory addr) pure =<< use (tmExtra . eeAddrHistories . at addr)
+    getAddrBalance addr = ether $
+        MM.lookupM getAddrBalance addr =<< use (tmExtra . eeAddrBalances)
 
 instance MonadTxExtraRead m => MonadTxExtra (ToilT ExplorerExtra m) where
     putTxExtra id extra = ether $
@@ -79,6 +96,10 @@ instance MonadTxExtraRead m => MonadTxExtra (ToilT ExplorerExtra m) where
         tmExtra . eeLocalTxsExtra %= MM.delete id
     updateAddrHistory addr hist = ether $
         tmExtra . eeAddrHistories . at addr .= Just hist
+    putAddrBalance addr coin = ether $
+        tmExtra . eeAddrBalances %= MM.insert addr coin
+    delAddrBalance addr = ether $
+        tmExtra . eeAddrBalances %= MM.delete addr
 
 ----------------------------------------------------------------------------
 -- DBTxp instances
@@ -87,3 +108,4 @@ instance MonadTxExtraRead m => MonadTxExtra (ToilT ExplorerExtra m) where
 instance (Monad m, MonadDB m) => MonadTxExtraRead (DBTxp m) where
     getTxExtra = DB.getTxExtra
     getAddrHistory = DB.getAddrHistory
+    getAddrBalance = DB.getAddrBalance

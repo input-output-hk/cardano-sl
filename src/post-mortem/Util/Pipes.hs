1ca54ba8558f5d15bf57a64a3b4fe44d1f2ea6eb
Lars Bruenjes <lars.bruenjes@iohk.io>
2017-05-15 18:19:34 +0200

    'post-mortem' utility: parsing and merging json log files

---
+++ src/post-mortem/Util/Pipes.hs
@@ -0,0 +1,20 @@
+module Util.Pipes
+    ( parseP
+    ) where
+
+import Data.Attoparsec.ByteString (Parser, parse, Result, IResult (..))
+import Pipes
+
+import Universum
+
+parseP :: forall m a b. Monad m => Parser a -> Pipe ByteString a m b
+parseP p = go (parse p)
+  where
+    go :: (ByteString -> Result a) -> Pipe ByteString a m b
+    go p' = await >>= consume p'
+
+    consume :: (ByteString -> Result a) -> ByteString -> Pipe ByteString a m b
+    consume p' chunk = case p' chunk of
+        Fail _ _ e    -> error $ toText e
+        Done chunk' a -> yield a >> consume (parse p) chunk'
+        Partial p''   -> go p''

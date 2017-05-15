dc8c72b031955f710662f5abead8115d101d6457
Jens Krause <email@jkrause.io>
2017-05-15 09:51:48 +0200

    [CSE-108] Update `mkEmptyCAddressSummary`
    to latest changes.

--- frontend/src/Explorer/Util/Factory.purs
+++ frontend/src/Explorer/Util/Factory.purs
@@ -1,13 +1,9 @@
 module Explorer.Util.Factory where
 
 import Prelude
-import Data.Foldable (sum)
-import Data.Lens ((^.))
 import Data.Time.NominalDiffTime (mkTime)
-import Data.Tuple (Tuple(..))
 import Pos.Core.Types (EpochIndex(..), LocalSlotIndex(..))
-import Pos.Explorer.Web.ClientTypes (CCoin(..), CAddress(..), CAddressSummary(..), CHash(..), CTxEntry(..), CTxId(..))
-import Pos.Explorer.Web.Lenses.ClientTypes (_CCoin, getCoin)
+import Pos.Explorer.Web.ClientTypes (CAddress(..), CAddressSummary(..), CAddressType(..), CCoin(..), CHash(..), CTxEntry(..), CTxId(..))
 
 
 mkCHash :: String -> CHash
@@ -45,6 +41,7 @@ mkEmptyCTxEntry = CTxEntry
 mkEmptyCAddressSummary :: CAddressSummary
 mkEmptyCAddressSummary = CAddressSummary
     { caAddress: mkCAddress "--"
+    , caType: CUnknownAddress
     , caTxNum: 0
     , caBalance: mkCoin 0
     , caTxList: []

feedf23ebed26453e14b52907dd36b63539026f5
Lars Bruenjes <lars.bruenjes@iohk.io>
2017-05-16 17:23:51 +0200

    'post-mortem' utility: proper command line parameters

--- src/post-mortem/Main.hs
+++ src/post-mortem/Main.hs
@@ -2,16 +2,17 @@ import qualified Data.GraphViz.Commands.IO as G
 import qualified Data.Map.Strict           as M
 import           System.IO                 (hPutStrLn, IOMode (WriteMode))
 
+import           Options
 import           Statistics
 import           Universum
 
 
 main :: IO ()
 main = do
-    [logDir, timesFile, graphFile] <- getArgs
+    Options{..} <- parseOptions
     err $ "logs directory: " ++ show logDir
-    err $ "times file: " ++ show timesFile
-    err $ "graph file: " ++ show graphFile
+    err $ "times csv file: " ++ show timesCSV
+    err $ "graph dot file: " ++ show graphDOT
     (rc, g) <- runJSONFold logDir $ (,) <$> receivedCreatedF <*> graphF
     let total    = M.size rc
         included = sort $ mapMaybe snd $ M.toList rc
@@ -19,11 +20,11 @@ main = do
     err $ "total number of received transactions: " ++ show total
     err $ "included in blockchain: " ++ show (length included)
     err $ "lost transactions: " ++ show lost
-    withFile timesFile WriteMode $ \h -> do
+    withFile timesCSV WriteMode $ \h -> do
         hPutStrLn h "tx,time"
         for_ (M.toList rc) $ \(tx, mts) ->
             hPutStrLn h $ toString tx ++ "," ++ maybe "" show mts
-    void $ G.writeDotFile graphFile g
+    void $ G.writeDotFile graphDOT g
   where
     err :: String -> IO ()
     err = hPutStrLn stderr
---
+++ src/post-mortem/Options.hs
@@ -0,0 +1,38 @@
+module Options
+    ( Options (..)
+    , parseOptions
+    ) where
+
+import Options.Applicative
+
+import Universum
+
+data Options = Options
+    { logDir   :: FilePath
+    , timesCSV :: FilePath
+    , graphDOT :: FilePath
+    } deriving Show
+
+options :: Parser Options
+options = Options <$> strOption (  long "logdir"
+                                <> short 'd'
+                                <> metavar "LOGDIR"
+                                <> help "directory containing the log files"
+                                )
+                  <*> strOption (  long "times"
+                                <> short 't'
+                                <> metavar "TIMES"
+                                <> help "target for the times csv file"
+                                )
+                  <*> strOption (  long "graph"
+                                <> short 'g'
+                                <> metavar "GRAPH"
+                                <> help "target for the graph dot file"
+                                )
+
+parseOptions :: IO Options
+parseOptions = execParser $ info (options <**> helper)
+    (  fullDesc
+    <> progDesc "Analyzes the json logs from LOGDIR and writes the analysis to TIMES and GRAPH."
+    <> header "cardano-post-mortem"
+    )

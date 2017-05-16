09c8320a18ed99f107e4e908343a58ed41092450
Lars Bruenjes <lars.bruenjes@iohk.io>
2017-05-16 16:58:31 +0200

    'post-mortem' utility: proper CSV output

--- src/post-mortem/Main.hs
+++ src/post-mortem/Main.hs
@@ -1,6 +1,6 @@
 import qualified Data.GraphViz.Commands.IO as G
 import qualified Data.Map.Strict           as M
-import           System.IO                 (hPutStrLn)
+import           System.IO                 (hPutStrLn, IOMode (WriteMode))
 
 import           Statistics
 import           Universum
@@ -8,16 +8,21 @@ import           Universum
 
 main :: IO ()
 main = do
-    [logDir, graphFile] <- getArgs
+    [logDir, timesFile, graphFile] <- getArgs
     err $ "logs directory: " ++ show logDir
+    err $ "times file: " ++ show timesFile
+    err $ "graph file: " ++ show graphFile
     (rc, g) <- runJSONFold logDir $ (,) <$> receivedCreatedF <*> graphF
-    let total = M.size rc
-    let included = sort $ mapMaybe snd $ M.toList rc
+    let total    = M.size rc
+        included = sort $ mapMaybe snd $ M.toList rc
+        lost     = total - length included
     err $ "total number of received transactions: " ++ show total
     err $ "included in blockchain: " ++ show (length included)
-    err $ "lost transactions: " ++ show (total - length included)
-    for_ included print 
-    err $ "writing graph to: " ++ show graphFile
+    err $ "lost transactions: " ++ show lost
+    withFile timesFile WriteMode $ \h -> do
+        hPutStrLn h "tx,time"
+        for_ (M.toList rc) $ \(tx, mts) ->
+            hPutStrLn h $ toString tx ++ "," ++ maybe "" show mts
     void $ G.writeDotFile graphFile g
   where
     err :: String -> IO ()

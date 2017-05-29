b982be1b7b9ac88b54eea18d9a030c704d0e546d
Jens Krause <email@jkrause.io>
2017-05-29 19:45:52 +0200

    [CSE-95] Fix signal issues of `actionChannel`
    `actionChannel` is shared by all configurations now

--- frontend/src/Explorer/Types/Actions.purs
+++ frontend/src/Explorer/Types/Actions.purs
@@ -12,7 +12,7 @@ import Explorer.Routes (Route)
 import Explorer.Types.State (CBlockEntries, CTxBriefs, CTxEntries, DashboardAPICode, Search, SocketSubscriptionItem)
 import Pos.Core.Types (EpochIndex, LocalSlotIndex)
 import Pos.Explorer.Web.ClientTypes (CAddress, CAddressSummary, CBlockSummary, CHash, CTxId, CTxSummary)
-import Pux.Html.Events (Target, MouseEvent)
+import Pux.Html.Events (Target)
 import Signal.Channel (Channel)
 
 data Action

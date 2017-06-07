1a86b8ceb48b816cf129631794c5711a761753d3
Lionel Miller <delonorm@gmail.com>
2017-06-07 14:18:44 +0300

    Add shebangs to start-* scripts

--- util-scripts/start-qa.sh
+++ util-scripts/start-qa.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 stack exec -- cardano-node --system-start 1496075885 --kademlia-explicit-initial --log-config util-scripts/log-config-abc.yaml --logs-prefix "logs/qanet" --db-path db-qanet --kademlia-peer 52.57.159.95:3000 --wallet --wallet-db-path wdb-qanet --kademlia-dump-path kademlia-qanet.dump --static-peers $@

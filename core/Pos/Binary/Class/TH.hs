14bd4b4eab95f1c0030d28d8fc8a82e62628d3a7
Ilya Peresadin <pva701@gmail.com>
2017-06-13 15:07:18 +0300

    [CSL-1212] Implement Bi.get for one constructor

--- core/Pos/Binary/Class/TH.hs
+++ core/Pos/Binary/Class/TH.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
 
 -- | TH helpers for Bi
 module Pos.Binary.Class.TH
@@ -6,12 +7,18 @@ module Pos.Binary.Class.TH
        , Cons (..)
        , Field (..)
        ) where
+
 import           Universum
 
-import qualified Data.Text           as T
-import           Formatting          (build, sformat, shown, (%))
+import           Data.Default          (def)
+import qualified Data.Text             as T
+import           Formatting            (sformat, shown, (%))
 import           Language.Haskell.TH
-import           TH.ReifySimple      (DataCon (..), DataType (..), reifyDataType)
+import           TH.ReifySimple        (DataCon (..), DataType (..), reifyDataType)
+import           TH.Utilities          (plainInstanceD)
+
+import           Pos.Binary.Class.Core (Bi (..))
+import qualified Pos.Binary.Class.Core as Bi
 
 data Cons
     = Cons {
@@ -20,7 +27,10 @@ data Cons
     }
 
 data Field
-    = Field Name Name
+    = Field {
+      fName :: Name
+    , fType :: Name
+    }
     -- ^ Field name and field type
     | Unused Name
     -- ^ Name of unused field
@@ -45,6 +55,38 @@ deriveSimpleBi dataName constrs = do
                                      %shown%"' is passed to deriveSimpleBi")
                             field cName
     fail "not implemented yet"
+  where
+    isUsed :: Field -> Bool
+    isUsed (Unused _) = False
+    isUsed _          = True
+
+    biGetForConstr :: Cons -> Q Exp
+    biGetForConstr (Cons name []) = appE (varE 'pure) (conE name)
+    biGetForConstr Cons{..} = do
+        let usedFields = filter isUsed cFields
+        let unusedFields = filter (not . isUsed) cFields
+
+        varNames :: [Name] <- mapM (newName . show . fName) usedFields
+        varPs :: [Pat] <- mapM varP varNames
+        biGets :: [Exp] <- replicateM (length varPs) (varE 'Bi.get)
+        bindExprs :: [Stmt] <- mapM (uncurry bindS . bimap pure pure) (zip varPs biGets)
+        let recWildUsedVars = map (\(f, ex) -> (fName f,) <$> varE ex) $ zip usedFields varNames
+        let recWildUnusedVars = map (\f -> (fName f,) <$> varE 'def) unusedFields
+        recordWildCardReturn <- noBindS $
+                                appE (varE 'pure) $
+                                recConE cName $
+                                recWildUsedVars ++ recWildUnusedVars
+        doE $ map pure bindExprs ++ [pure recordWildCardReturn]
+
+makeBiInstanceTH :: Type -> Exp -> Exp -> Exp -> Dec
+makeBiInstanceTH ty sizeE putE getE =
+  plainInstanceD
+        [] -- empty context
+        (AppT (ConT ''Bi) ty)
+        [ ValD (VarP 'size) (NormalB sizeE) []
+        , ValD (VarP 'put) (NormalB putE) []
+        , ValD (VarP 'get) (NormalB getE) []
+        ]
 
 data ConsFail
     = MatchedCons [DataCon]

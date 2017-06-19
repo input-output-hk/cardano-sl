740f230609eed888cd4c1dd31fedf3acdd762485
Vladislav Zavialov <vlad.z.4096@gmail.com>
2017-06-19 09:20:38 +0300

    [CSL-1154] JsonLogConfig type

--- src/JsonLog/JsonLogT.hs
+++ src/JsonLog/JsonLogT.hs
@@ -24,18 +24,20 @@ module JsonLog.JsonLogT
     , runJsonLogT'
     , runWithJsonLogT
     , runWithJsonLogT'
+    , JsonLogConfig(..)
+    , jsonLogDefault
     ) where
 
 import Control.Concurrent.MVar        (MVar, withMVar)
 import Control.Monad.Base             (MonadBase)
 import Control.Monad.Fix              (MonadFix)
 import Control.Monad.IO.Class         (MonadIO (..))
-import Control.Monad.Morph            (MFunctor (..)) 
+import Control.Monad.Morph            (MFunctor (..))
 import Control.Monad.Trans.Class      (MonadTrans)
 import Control.Monad.Trans.Control    (MonadBaseControl (..))
 import Control.Monad.Trans.Lift.Local (LiftLocal)
 import Control.Monad.Trans.Reader     (ReaderT (..))
-import Data.Aeson                     (encode)
+import Data.Aeson                     (ToJSON, encode)
 import Data.ByteString.Lazy           (hPut)
 import Formatting                     (sformat, shown, (%))
 import Serokell.Util.Lens             (WrappedM (..))
@@ -56,12 +58,14 @@ import Mockable.Metrics               (Gauge, Counter, Distribution, Metrics)
 import Mockable.SharedAtomic          (SharedAtomicT, SharedAtomic)
 import Mockable.SharedExclusive       (SharedExclusiveT, SharedExclusive)
 
-type R = Maybe (MVar Handle, JLTimedEvent -> IO Bool)
+data JsonLogConfig
+    = JsonLogDisabled
+    | JsonLogConfig (MVar Handle) (JLTimedEvent -> IO Bool)
 
 -- | Monad transformer @'JsonLogT'@ adds support for JSON logging
 -- to a monad transformer stack.
-newtype JsonLogT m a = JsonLogT (ReaderT R m a)
-    deriving (Functor, Applicative, Monad, MonadTrans, MonadIO, MFunctor, 
+newtype JsonLogT m a = JsonLogT (ReaderT JsonLogConfig m a)
+    deriving (Functor, Applicative, Monad, MonadTrans, MonadIO, MFunctor,
               MonadThrow, MonadCatch, MonadMask, MonadFix, MonadBase b, LiftLocal)
 
 instance MonadBaseControl b m => MonadBaseControl b (JsonLogT m) where
@@ -82,7 +86,7 @@ instance WithLogger m => HasLoggerName (JsonLogT m) where
 
 instance Monad m => WrappedM (JsonLogT m) where
 
-    type UnwrappedM (JsonLogT m) = ReaderT R m
+    type UnwrappedM (JsonLogT m) = ReaderT JsonLogConfig m
 
     unpackM = JsonLogT
 
@@ -146,65 +150,70 @@ instance Mockable Metrics m => Mockable Metrics (JsonLogT m) where
 
     liftMockable = liftMockableWrappedM
 
+jsonLogDefault
+    :: (ToJSON a, Mockable Catch m, MonadIO m, WithLogger m)
+    => JsonLogConfig
+    -> a -> m ()
+jsonLogDefault jlc x =
+    case jlc of
+        JsonLogDisabled -> return ()
+        JsonLogConfig v decide -> do
+            event <- toEvent <$> timedIO x
+            b     <- liftIO (decide event)
+                `catchAll` \e -> do
+                    logWarning $ sformat ("error in deciding whether to json log: "%shown) e
+                    return False
+            when b $ liftIO (withMVar v $ flip hPut $ encode event)
+                `catchAll` \e ->
+                    logWarning $ sformat ("can't write json log: "%shown) e
+
 instance ( MonadIO m
          , WithLogger m
          , Mockable Catch m) => CanJsonLog (JsonLogT m) where
 
-    jsonLog x = JsonLogT $ do
-        mv <- ask
-        case mv of
-            Nothing -> return ()
-            Just (v, decide) -> do
-                event <- toEvent <$> timedIO x
-                b     <- liftIO (decide event)
-                    `catchAll` \e -> do
-                        logWarning $ sformat ("error in deciding whether to json log: "%shown) e
-                        return False
-                when b $ liftIO (withMVar v $ flip hPut $ encode event)
-                    `catchAll` \e ->
-                        logWarning $ sformat ("can't write json log: "%shown) e
+    jsonLog x = JsonLogT (ReaderT $ \jlc -> jsonLogDefault jlc x)
 
 -- | This function simply discards all JSON log messages.
 runWithoutJsonLogT :: JsonLogT m a -> m a
-runWithoutJsonLogT (JsonLogT m) = runReaderT m Nothing
+runWithoutJsonLogT (JsonLogT m) = runReaderT m JsonLogDisabled
 
 -- | Runs a computation containing JSON log messages,
 -- either discarding all messages or writing
 -- some of them to a handle.
-runJsonLogT :: MonadIO m 
+runJsonLogT :: MonadIO m
             => Maybe (Handle, JLTimedEvent -> IO Bool) -- ^ If @'Nothing'@, JSON log messages are discarded, if @'Just' (h, f)@,
                                                        -- log messages @e@ are written to handle @h@ if @f e@ returns @True@,
                                                        -- and are otherwise discarded.
-            -> JsonLogT m a                            -- ^ A monadic computation containing JSON log messages. 
+            -> JsonLogT m a                            -- ^ A monadic computation containing JSON log messages.
             -> m a
 runJsonLogT Nothing            m            = runWithoutJsonLogT m
 runJsonLogT (Just (h, decide)) (JsonLogT m) = do
     v <- newMVar h
-    runReaderT m $ Just (v, decide)
+    runReaderT m $ JsonLogConfig v decide
 
 -- | Runs a computation containing JSON log messages,
 -- either discarding all messages or writing them to a handle.
-runJsonLogT' :: MonadIO m 
+runJsonLogT' :: MonadIO m
              => Maybe Handle -- ^ If @'Nothing'@, JSON log messages are discarded, if @'Just' h@,
                              -- log messages are written to handle @h@.
-             -> JsonLogT m a -- ^ A monadic computation containing JSON log messages. 
+             -> JsonLogT m a -- ^ A monadic computation containing JSON log messages.
              -> m a
 runJsonLogT' mh = runJsonLogT $ fmap (\h -> (h, const $ return True)) mh
 
 -- | Runs a computation containing JSON log messages,
 -- writing some of them to a handle.
-runWithJsonLogT :: MonadIO m 
-                => Handle                    -- ^ The handle to write log messages to. 
+runWithJsonLogT :: MonadIO m
+                => Handle                    -- ^ The handle to write log messages to.
                 -> (JLTimedEvent -> IO Bool) -- ^ Monadic predicate to decide whether a given log message
                                              -- should be written to the handle or be discarded.
-                -> JsonLogT m a              -- ^ A monadic computation containing JSON log messages. 
+                -> JsonLogT m a              -- ^ A monadic computation containing JSON log messages.
                 -> m a
 runWithJsonLogT h decide = runJsonLogT $ Just (h, decide)
 
 -- | Runs a computation containing JSON log messages,
 -- writing them to a handle.
-runWithJsonLogT' :: MonadIO m 
-                 => Handle       -- ^ The handle to write log messages to. 
-                 -> JsonLogT m a -- ^ A monadic computation containing JSON log messages. 
+runWithJsonLogT' :: MonadIO m
+                 => Handle       -- ^ The handle to write log messages to.
+                 -> JsonLogT m a -- ^ A monadic computation containing JSON log messages.
                  -> m a
 runWithJsonLogT' = runJsonLogT' . Just

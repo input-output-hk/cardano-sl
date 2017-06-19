581c89b46882ed194bb8aae8630b4e4b79148511
Denis Shevchenko <denis.shevchenko@iohk.io>
2017-06-19 17:58:17 +0400

    [CSLD-101] Docs for Bash scripts, README.

--- scripts/launch/qa.sh
+++ scripts/launch/qa.sh
@@ -3,7 +3,7 @@
 stack exec -- cardano-node \
     --system-start 1497640619 \
     --kademlia-explicit-initial \
-    --log-config scripts/log-templates/log-config-abc.yaml \
+    --log-config scripts/log-templates/log-config-qa.yaml \
     --logs-prefix "logs/qanet" \
     --db-path db-qanet \
     --kademlia-peer 52.57.159.95:3000 \

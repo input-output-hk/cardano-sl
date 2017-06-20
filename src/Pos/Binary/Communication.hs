a298d11eb193eec5f3ca410519c172e93362e1c6
Artyom <yom@artyom.me>
2017-06-20 13:57:37 +0300

    [CSL-1122] Make Bi HandlerSpec a bit clearer

--- src/Pos/Binary/Communication.hs
+++ src/Pos/Binary/Communication.hs
@@ -1,4 +1,6 @@
+{-# LANGUAGE BinaryLiterals      #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+
 -- | Communication-related serialization -- messages mostly.
 
 module Pos.Binary.Communication () where
@@ -51,32 +53,40 @@ instance SscHelpersClass ssc => Bi (MsgBlock ssc) where
 -- Protocol version info and related
 ----------------------------------------------------------------------------
 
+--
 -- Encoding of HandlerSpec is as follow:
 --
--- | Type                                        | Size     | Value     | Following data |
--- |---------------------------------------------|----------|-----------|----------------|
--- | <reserved for future usage>                 | Fixed    | 0000 0000 | none           |
--- | ConvHandler m where m : UnsignedVarInt < 64 | Fixed    | 01xx xxxx | none           |
--- | ConvHandler m where m : Unknown             | Variable | 0000 0001 | EncodeString   |
--- | UnknownHandler w8 bs                        | Variable | w8        | bs             |
-
+--  | Type                                 | Size     | Value     | Following data   |
+--  |--------------------------------------|----------|-----------|------------------|
+--  | <reserved for future usage>          | Fixed    | 0000 0000 | <none>           |
+--  | ConvHandler m, m:UnsignedVarInt < 64 | Fixed    | 01xx xxxx | <none>           |
+--  | ConvHandler m, m:Unknown             | Variable | 0000 0001 | len,MessageName  |
+--  | UnknownHandler w8 bs                 | Variable | w8        | len,bs           |
+--
 instance Bi HandlerSpec where
     sizeNPut = labelS "HandlerSpec" $ convertToSizeNPut f
       where
+        -- CSL-1122: finish with binary literals
         f (ConvHandler (MessageName m)) =
             case decodeFull m of
                 Right (UnsignedVarInt a)
                     | a < 64 -> putWord8S (0x40 .|. (fromIntegral (a :: Word) .&. 0x3f))
                 _ -> putWord8S 1 <> putSmallWithLengthS (putS m)
+        -- CSL-1122: putSmallWithLengthS shouldn't be used on bytestrings
+        -- because bytestring serialization already puts length
         f (UnknownHandler t b) = putWord8S t <> putSmallWithLengthS (putS b)
+
     get = label "HandlerSpec" $ getWord8 >>= \case
-        0                        -> pure $ UnknownHandler 0 mempty
-        1                        -> getSmallWithLength (ConvHandler <$> get)
-        t | (t .&. 0xc0) == 0x40 ->
+        -- 0000 0000: reserved
+        0 -> pure $ UnknownHandler 0 mempty
+        -- 0000 0001: ConvHandler with a message name
+        1 -> getSmallWithLength (ConvHandler <$> get)
+        -- 01xx xxxx: ConvHandler (MessageName xxxxxx)
+        t | (t .&. 0b11000000) == 0b01000000 ->
             pure . ConvHandler . MessageName . encode $
-            UnsignedVarInt (fromIntegral (t .&. 0x3f) :: Word)
-          | otherwise            ->
-            UnknownHandler t <$> getSmallWithLength get
+            UnsignedVarInt (fromIntegral (t .&. 0b00111111) :: Word)
+        -- none of the above: unknown handler
+          | otherwise -> UnknownHandler t <$> getSmallWithLength get
 
 instance Bi VerInfo where
     sizeNPut = labelS "VerInfo" $

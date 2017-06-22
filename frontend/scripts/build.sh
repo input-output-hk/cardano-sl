c737d3a3f648d00fb632a1868f3ea1005f191efe
Jens Krause <email@jkrause.io>
2017-06-22 19:45:04 +0200

    [CSE-68] Buuuuuump nixpkgs revisions everywhere

--- frontend/scripts/build.sh
+++ frontend/scripts/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env nix-shell
 #! nix-shell -j 4 -i bash -p stack git
-#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/763e21e982370f67c126f92a1113ea949db3b6e0.tar.gz
+#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/cb90e6a0361554d01b7a576af6c6fae4c28d7513.tar.gz
 
 set -xe
 

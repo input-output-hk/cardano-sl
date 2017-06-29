c6686923dd6c381b0c29f2ad88c0f6cb0df67435
Dmitry Mukhutdinov <flyingleafe@gmail.com>
2017-06-29 22:04:48 +0300

    WIP

--- src/Pos/Wallet/Web/BListener.hs
+++ src/Pos/Wallet/Web/BListener.hs
@@ -60,8 +60,9 @@ onApplyTracking
     )
     => OldestFirst NE (Blund ssc) -> m SomeBatchOp
 onApplyTracking blunds = do
-    let txs = concatMap (gbTxs . fst) $ getOldestFirst blunds
-    let newTip = headerHash $ NE.last $ getOldestFirst blunds
+    let oldestFirst = getOldestFirst blunds
+        txs = concatMap (gbTxs . fst) oldestFirst
+        newTip = headerHash $ NE.last oldestFirst
     mapM_ (syncWalletSet newTip txs) =<< WS.getWalletAddresses
     
     -- It's silly, but when the wallet is migrated to RocksDB, we can write

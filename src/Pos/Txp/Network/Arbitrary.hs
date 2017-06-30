3728a518680dbef88e3064ee5ee3176e5fefbb3b
Alex <alexandrer_b@outlook.com>
2017-06-30 03:41:38 +0200

    [CSL-1162] Add missing serialization tests to 'txp/' types

--- src/Pos/Txp/Network/Arbitrary.hs
+++ src/Pos/Txp/Network/Arbitrary.hs
@@ -9,13 +9,8 @@ import           Universum
 import           Data.DeriveTH                 (derive, makeArbitrary)
 import           Test.QuickCheck               (Arbitrary (..))
 
-import           Pos.Binary.Update             ()
-import           Pos.Communication.Types.Relay (DataMsg (..))
-import           Pos.Core.Arbitrary            ()
+--import           Pos.Core.Arbitrary            ()
 import           Pos.Txp.Arbitrary             ()
 import           Pos.Txp.Network.Types         (TxMsgContents (..))
 
 derive makeArbitrary ''TxMsgContents
-
-instance Arbitrary (DataMsg TxMsgContents) where
-    arbitrary = DataMsg <$> arbitrary

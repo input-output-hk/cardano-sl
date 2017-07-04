86c30c071227667ed6c8d2fcaae7c145592e78cc
michael bishop <cleverca22@gmail.com>
2017-07-04 02:59:38 -0300

    fix the loading of constants.yaml the TH was embeding a /tmp/nix-build- path into the binary i have modified it to also embed the contents for parsing, but the path the original error can return may be wrong for nix based builds

---
+++ core/Pos/Util/Config/Contents.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE TypeApplications #-}
+
+-- | This is a separate module due to the TH stage restriction
+module Pos.Util.Config.Contents
+       ( cslConfigFile
+       ) where
+
+import           Pos.Util.Config.Path
+import           Data.Text                  (unpack)
+import           Universum                  hiding (lift)
+import           Language.Haskell.TH.Syntax (lift, runIO)
+
+-- | config that should be used by all parts of Cardano SL.
+--
+-- The contents of the config is @constants.yaml@.
+cslConfigFile :: String
+cslConfigFile = $(do
+    contents <- runIO (readFile cslConfigFilePath)
+    lift $ toString contents
+  )

5325de4648c72b4f5bb4f85724bcb33a6ab1c1da
Ante Kegalj <akegalj@gmail.com>
2017-07-06 16:34:25 +0200

    [CSM-175] Update staging cluster starttime

--- scripts/launch/staging.sh
+++ scripts/launch/staging.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 stack exec -- cardano-explorer \
-    --system-start 1498743070 \
+    --system-start 1499246772 \
     --log-config log-config.yaml \
     --logs-prefix "logs/testnet" \
     --db-path db-testnet \

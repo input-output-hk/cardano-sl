278a47352fb9824df4b4992c4450d9729177f7bc
Andrzej Rybczak <andrzej@rybczak.net>
2017-07-07 14:06:41 +0200

    Prototype of Address instance

--- core/Pos/Binary/Cbor/Test.hs
+++ core/Pos/Binary/Cbor/Test.hs
@@ -2,9 +2,13 @@
 {-# OPTIONS_GHC -ddump-splices #-}
 module Pos.Binary.Cbor.Test where
 
-import Pos.Binary.Cbor.TH
-import Pos.Binary.Cbor.Serialization
-import Universum
+import           Pos.Binary.Cbor.Class
+import           Pos.Binary.Cbor.TH
+import           Pos.Binary.Cbor.Serialization
+import           Universum
+
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BSL
 
 data User
     = Login {
@@ -30,3 +34,20 @@ deriveSimpleBi ''User [
 
 u1 :: User
 u1 = deserialize $ serialize $ Login "asd" 34
+
+data T = T1 Int | T2 Int Int | Unknown Word8 BS.ByteString
+    deriving Show
+
+instance Bi T where
+    encode = \case
+        T1 a         -> encode (0::Word8)
+                     <> (encode . BSL.toStrict $ serialize a)
+        T2 a b       -> encode (1::Word8)
+                     <> (encode . BSL.toStrict $ serialize (a, b))
+        Unknown n bs -> encode n
+                     <> encode bs
+
+    decode = decode @Word8 >>= \case
+        0 ->         T1 . deserialize . BSL.fromStrict <$> decode
+        1 -> uncurry T2 . deserialize . BSL.fromStrict <$> decode
+        t -> Unknown t                                 <$> decode

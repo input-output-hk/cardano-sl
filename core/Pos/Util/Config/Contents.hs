a75a0adccac9ce351f5fa13e37e283d8dcef1fe5
michael bishop <cleverca22@gmail.com>
2017-07-07 21:56:33 -0300

    fix all of the issues @nh2 pointed out

--- core/Pos/Util/Config/Contents.hs
+++ core/Pos/Util/Config/Contents.hs
@@ -7,13 +7,14 @@ module Pos.Util.Config.Contents
 
 import           Pos.Util.Config.Path
 import           Universum                  hiding (lift)
-import           Language.Haskell.TH.Syntax (lift, runIO)
+import           Language.Haskell.TH.Syntax (lift, runIO, addDependentFile)
 
 -- | config that should be used by all parts of Cardano SL.
 --
 -- The contents of the config is @constants.yaml@.
 cslConfigFile :: String
 cslConfigFile = $(do
+    addDependentFile cslConfigFilePath
     contents <- runIO (readFile cslConfigFilePath)
     lift $ toString contents
   )
--- pkgs/generate.sh
+++ pkgs/generate.sh
@@ -1,5 +1,6 @@
+#!/usr/bin/env bash
 
-#!/bin/sh
+# regenerate the `pkgs/default.nix` file based on the current contents of cardano-sl.cabal and stack.yaml
 
 function runInShell {
   nix-shell -j 4 -p cabal2nix nix-prefetch-scripts coreutils cabal-install stack --run "$*"
@@ -9,22 +10,22 @@ set -xe
 set -v
 
 # Get relative path to script directory
-scriptDir=$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")
+scriptDir="$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")"
 
-source ${scriptDir}/../scripts/set_nixpath.sh
+source "${scriptDir}/../scripts/set_nixpath.sh"
 
 # Generate stack2nix Nix package
 runInShell cabal2nix \
   --no-check \
-  --revision $(jq .rev <  ${scriptDir}/../stack2nix-src.json -r) \
-  https://github.com/input-output-hk/stack2nix.git > $scriptDir/stack2nix.nix
+  --revision $(jq .rev <  "${scriptDir}/../stack2nix-src.json" -r) \
+  https://github.com/input-output-hk/stack2nix.git > "$scriptDir/stack2nix.nix"
 
 # Build stack2nix Nix package
-nix-build ${scriptDir}/.. -A stack2nix -o $scriptDir/stack2nix -Q
+nix-build "${scriptDir}/.." -A stack2nix -o "$scriptDir/stack2nix" -Q
 
-pushd ${scriptDir}
+pushd "${scriptDir}"
 # Generate cardano-sl package set
-runInShell $scriptDir/stack2nix/bin/stack2nix ./.. > $scriptDir/default.nix.new
-mv $scriptDir/default.nix.new $scriptDir/default.nix
+runInShell ./stack2nix/bin/stack2nix ./.. > default.nix.new
+mv default.nix.new default.nix
 
 popd

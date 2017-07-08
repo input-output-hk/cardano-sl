8aef1f2bc493ed1dd2adabd87688bc9a4f7038a9
michael bishop <cleverca22@gmail.com>
2017-07-07 21:56:33 -0300

    fix all of the issues @nh2 pointed out

--- scripts/check-stack2nix.sh
+++ scripts/check-stack2nix.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+# check and warn if `pkgs/default.nix` is out of date
+
 set -xe
 
 fail_stack2nix_check() {
-  echo "ERROR: you need to run ./pkgs/generate.sh and commit the changes"
+  echo "ERROR: you need to run ./pkgs/generate.sh and commit the changes" >&2
   exit 1
 }
 
--- scripts/set_nixpath.sh
+++ scripts/set_nixpath.sh
@@ -1,6 +1,7 @@
 update_NIX_PATH() {
-  local scriptDir=$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")
-  export NIX_PATH=nixpkgs=https://github.com/NixOS/nixpkgs/archive/$(jq .rev < ${scriptDir}/../nixpkgs-src.json -r).tar.gz
+  local scriptDir="$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")"
+  export NIX_PATH=nixpkgs=https://github.com/NixOS/nixpkgs/archive/$(nix-shell -p jq --run "jq .rev < \"${scriptDir}/../nixpkgs-src.json\" -r").tar.gz
+  # allows the nix expressions to detect that the path is already fixed
   export NIX_PATH_LOCKED=1
 }
 update_NIX_PATH

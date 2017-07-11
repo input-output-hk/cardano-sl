d54e6dbdeb039bfc2df79295c6df40124d55ab33
Alfredo Di Napoli <alfredo.dinapoli@iohk.io>
2017-07-11 17:46:38 +0200

    Complete Pos.Binary.Infra.DHTModel

--- infra/Pos/Binary/Infra/DHTModel.hs
+++ infra/Pos/Binary/Infra/DHTModel.hs
@@ -10,6 +10,7 @@ import qualified Data.Store.Internal         as Store
 import           Network.Kademlia.HashNodeId (HashId (..))
 
 import           Pos.Binary.Class            (Bi (..), Size (..), getSize, label, labelP)
+import qualified Pos.Binary.Cbor             as Cbor
 import           Pos.DHT.Model.Types         (DHTData (..), DHTKey (..))
 
 instance Bi DHTKey where
@@ -18,10 +19,18 @@ instance Bi DHTKey where
     put (DHTKey (HashId bs)) = labelP "DHTKey" $ put bs
     get = label "DHTKey" $ DHTKey . HashId <$> get
 
+instance Cbor.Bi DHTKey where
+  encode (DHTKey (HashId bs)) = Cbor.encode bs
+  decode = DHTKey . HashId <$> Cbor.decode
+
 instance Bi DHTData where
     size = ConstSize 0
     put (DHTData ()) = labelP "DHTData" $ pure ()
     get = label "DHTData" $ pure $ DHTData ()
+
+instance Cbor.Bi DHTData where
+  encode (DHTData unit) = Cbor.encode unit
+  decode = DHTData <$> Cbor.decode
 
 -- Kademlia uses Store, so we define a Store instance here as well.
 instance Store.Store DHTKey where

26e0bea18e76ca25f2ca895b511bbf79ff97a752
Alfredo Di Napoli <alfredo.dinapoli@iohk.io>
2017-07-12 15:58:03 +0200

    Add instances for Pos.Binary.GodTossing.Toss

--- godtossing/Pos/Binary/GodTossing/Toss.hs
+++ godtossing/Pos/Binary/GodTossing/Toss.hs
@@ -5,6 +5,7 @@ module Pos.Binary.GodTossing.Toss
        ) where
 
 import           Pos.Binary.Class              (Cons (..), Field (..), deriveSimpleBi)
+import qualified Pos.Binary.Cbor               as Cbor
 import           Pos.Ssc.GodTossing.Core       (CommitmentsMap, OpeningsMap, SharesMap,
                                                 VssCertificatesMap)
 import           Pos.Ssc.GodTossing.Toss.Types (GtTag (..), TossModifier (..))
@@ -15,10 +16,24 @@ deriveSimpleBi ''GtTag [
     Cons 'SharesMsg [],
     Cons 'VssCertificateMsg []]
 
+Cbor.deriveSimpleBi ''GtTag [
+    Cbor.Cons 'CommitmentMsg [],
+    Cbor.Cons 'OpeningMsg [],
+    Cbor.Cons 'SharesMsg [],
+    Cbor.Cons 'VssCertificateMsg []]
+
 deriveSimpleBi ''TossModifier [
     Cons 'TossModifier [
         Field [| _tmCommitments  :: CommitmentsMap     |],
         Field [| _tmOpenings     :: OpeningsMap        |],
         Field [| _tmShares       :: SharesMap          |],
         Field [| _tmCertificates :: VssCertificatesMap |]
+    ]]
+
+Cbor.deriveSimpleBi ''TossModifier [
+    Cbor.Cons 'TossModifier [
+        Cbor.Field [| _tmCommitments  :: CommitmentsMap     |],
+        Cbor.Field [| _tmOpenings     :: OpeningsMap        |],
+        Cbor.Field [| _tmShares       :: SharesMap          |],
+        Cbor.Field [| _tmCertificates :: VssCertificatesMap |]
     ]]

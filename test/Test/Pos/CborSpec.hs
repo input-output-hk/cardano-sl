d89c4a9683d55f2c5f8595acdc47a87e6f5b2a82
Alfredo Di Napoli <alfredo.dinapoli@iohk.io>
2017-07-12 18:01:02 +0200

    Add inital Test.Post.CborSpec module

--- test/Test/Pos/CborSpec.hs
+++ test/Test/Pos/CborSpec.hs
@@ -1,5 +1,11 @@
 
--- | Pos.Crypto specification
+-- | Test.Pos.CborSpec specification
+
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Test.Pos.CborSpec
        ( spec
@@ -14,11 +20,84 @@ import           Pos.Core.Arbitrary ()
 import           Pos.Core.Fee
 import           Pos.Core.Genesis.Types
 import           Pos.Core.Types
+import           Test.Hspec (Expectation, Spec, describe)
 import           Test.QuickCheck
 import           Universum
 
+import qualified Codec.CBOR.FlatTerm as CBOR
+import           Pos.Binary.Cbor
+import           Test.Hspec.QuickCheck (prop)
+
+import           Universum
+import           Test.QuickCheck
+import           Test.QuickCheck.Arbitrary.Generic (genericArbitrary, genericShrink)
+import           Pos.Core.Fee
+import           Pos.Binary.Core.Fee ()
+import           Pos.Core.Arbitrary ()
+import           Pos.Binary.Core.Script ()
+import           Pos.Core.Types
+import           Pos.Core.Genesis.Types
+import           Pos.Binary.Class.Numbers
+import           Pos.Data.Attributes
+
 import qualified Data.ByteString as BS
-import qualified Data.ByteString.Lazy as BSL
+import qualified Data.Map as M
+
+data MyScript = MyScript
+    { version :: ScriptVersion -- ^ Version
+    , script  :: LByteString   -- ^ Serialized script
+    } deriving (Eq, Show, Generic, Typeable)
+
+instance Arbitrary MyScript where
+  arbitrary = MyScript <$> arbitrary <*> arbitrary
+
+deriveSimpleBi ''MyScript [
+    Cons 'MyScript [
+        Field [| version :: ScriptVersion |],
+        Field [| script  :: LByteString   |]
+    ]]
+
+-- Type to be used to simulate a breaking change in the serialisation
+-- schema, so we can test instances which uses the `UnknownXX` pattern
+-- for extensibility.
+data U = U Word8 BS.ByteString
+
+instance Bi U where
+  encode (U word8 bs) = encodeListLen 2 <> encode (word8 :: Word8) <> encode bs
+  decode = do
+    decodeListLenOf 2
+    U <$> decode <*> decode
+
+----------------------------------------
+
+data X1 = X1 { x1A :: Int }
+    deriving (Eq, Ord, Show, Generic)
+
+data X2 = X2 { x2A :: Int, x2B :: String }
+    deriving (Eq, Ord, Show, Generic)
+
+instance Arbitrary X1 where
+    arbitrary = genericArbitrary
+    shrink = genericShrink
+
+instance Arbitrary X2 where
+    arbitrary = genericArbitrary
+    shrink = genericShrink
+
+instance Bi (Attributes X1) where
+    encode = encodeAttributes [(0, serialize' . x1A)]
+    decode = decodeAttributes (X1 0) $ \n v acc -> case n of
+        0 -> Just $ acc { x1A = deserialize' v }
+        _ -> Nothing
+
+instance Bi (Attributes X2) where
+    encode = encodeAttributes [(0, serialize' . x2A), (1, serialize' . x2B)]
+    decode = decodeAttributes (X2 0 []) $ \n v acc -> case n of
+        0 -> Just $ acc { x2A = deserialize' v }
+        1 -> Just $ acc { x2B = deserialize' v }
+        _ -> Nothing
+
+----------------------------------------
 
 -- Machinery to test we perform "flat" encoding.
 hasValidFlatTerm :: Bi a => a -> Bool
@@ -40,41 +119,119 @@ extensionProperty (Proxy :: Proxy a) = forAll (arbitrary :: Gen a) $ \input ->
       (encoded :: a)  = deserialize (serialize u)
   in encoded === input
 
+soundSerializationAttributesOfAsProperty
+    :: forall a b aa ab. (aa ~ Attributes a, ab ~ Attributes b,
+                          Bi aa, Bi ab, Eq aa, Arbitrary a, Show aa)
+    => Proxy a
+    -> Proxy b
+    -> Property
+soundSerializationAttributesOfAsProperty _ _ = forAll arbitraryAttrs $ \input ->
+    let serialized      = serialize input
+        (middle  :: ab) = deserialize serialized
+        (encoded :: aa) = deserialize $ serialize middle
+    in encoded === input
+    where
+      arbitraryAttrs :: Gen aa
+      arbitraryAttrs = Attributes <$> arbitrary <*> arbitraryUnparsedFields
+
+-- TODO: Use as a main Arbitrary instance for UnparsedFields after transition to
+-- CBOR.
+arbitraryUnparsedFields :: Gen UnparsedFields
+arbitraryUnparsedFields = sized $ go M.empty
+  where
+    go !acc 0 = pure $ UnparsedFields acc
+    go !acc n = do
+        -- Assume that data type doesn't have more than 100 constructors.
+        k <- choose (100, maxBound)
+        v <- arbitrary
+        go (M.insert k v acc) (n - 1)
+
 soundInstanceProperty :: (Arbitrary a, Eq a, Show a, Bi a) => Proxy a -> Property
 soundInstanceProperty (Proxy :: Proxy a) = forAll (arbitrary :: Gen a) $ \input ->
   let itRoundtrips = roundtripProperty input
       isFlat       = hasValidFlatTerm input === True
   in itRoundtrips .&&. isFlat
 
-spec = describe "Cbor Specs" $ do
-         prop "Cbor.Bi" (soundInstanceProperty @(UnsignedVarInt Int) Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @(SignedVarInt Int) Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @(FixedSizeInt Int) Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @(UnsignedVarInt Int64) Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @(SignedVarInt Int64) Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @(FixedSizeInt Int64) Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @(UnsignedVarInt Word) Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @(FixedSizeInt Word) Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @(UnsignedVarInt Word16) Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @(UnsignedVarInt Word32) Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @(UnsignedVarInt Word64) Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @TinyVarInt Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @Int64 Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @MyScript Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @Coeff Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @TxSizeLinear Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @TxFeePolicy Proxy .&&. extensionProperty @TxFeePolicy Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @Script Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @Timestamp Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @EpochIndex Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @Coin Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @CoinPortion Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @LocalSlotIndex Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @SlotId Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @EpochOrSlot Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @SharedSeed Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @ChainDifficulty Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @StakeDistribution Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @ApplicationName Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @SoftwareVersion Proxy)
-         prop "Cbor.Bi" (soundInstanceProperty @BlockVersion Proxy)
+spec :: Spec
+spec = describe "Cbor.Bi instances" $ do
+    prop "UnsignedVarInt" (soundInstanceProperty @(UnsignedVarInt Int) Proxy)
+    prop "SignedVarInt" (soundInstanceProperty @(SignedVarInt Int) Proxy)
+    prop "FixedSizeInt" (soundInstanceProperty @(FixedSizeInt Int) Proxy)
+    prop "UnsignedVarInt" (soundInstanceProperty @(UnsignedVarInt Int64) Proxy)
+    prop "SignedVarInt" (soundInstanceProperty @(SignedVarInt Int64) Proxy)
+    prop "FixedSizeInt" (soundInstanceProperty @(FixedSizeInt Int64) Proxy)
+    prop "UnsignedVarInt" (soundInstanceProperty @(UnsignedVarInt Word) Proxy)
+    prop "FixedSizeInt" (soundInstanceProperty @(FixedSizeInt Word) Proxy)
+    prop "UnsignedVarInt" (soundInstanceProperty @(UnsignedVarInt Word16) Proxy)
+    prop "UnsignedVarInt" (soundInstanceProperty @(UnsignedVarInt Word32) Proxy)
+    prop "UnsignedVarInt" (soundInstanceProperty @(UnsignedVarInt Word64) Proxy)
+    prop "TinyVarInt" (soundInstanceProperty @TinyVarInt Proxy)
+    prop "Int64" (soundInstanceProperty @Int64 Proxy)
+    prop "MyScript" (soundInstanceProperty @MyScript Proxy)
+    prop "Coeff" (soundInstanceProperty @Coeff Proxy)
+    prop "TxSizeLinear" (soundInstanceProperty @TxSizeLinear Proxy)
+    prop "TxFeePolicy" (soundInstanceProperty @TxFeePolicy Proxy .&&. extensionProperty @TxFeePolicy Proxy)
+    prop "Script" (soundInstanceProperty @Script Proxy)
+    prop "Timestamp" (soundInstanceProperty @Timestamp Proxy)
+    prop "EpochIndex" (soundInstanceProperty @EpochIndex Proxy)
+    prop "Attributes" (soundInstanceProperty @(Attributes ()) Proxy)
+    prop "Coin" (soundInstanceProperty @Coin Proxy)
+    prop "CoinPortion" (soundInstanceProperty @CoinPortion Proxy)
+    prop "LocalSlotIndex" (soundInstanceProperty @LocalSlotIndex Proxy)
+    prop "SlotId" (soundInstanceProperty @SlotId Proxy)
+    prop "EpochOrSlot" (soundInstanceProperty @EpochOrSlot Proxy)
+    prop "SharedSeed" (soundInstanceProperty @SharedSeed Proxy)
+    prop "ChainDifficulty" (soundInstanceProperty @ChainDifficulty Proxy)
+    prop "StakeDistribution" (soundInstanceProperty @StakeDistribution Proxy)
+    prop "ApplicationName" (soundInstanceProperty @ApplicationName Proxy)
+    prop "SoftwareVersion" (soundInstanceProperty @SoftwareVersion Proxy)
+    prop "BlockVersion" (soundInstanceProperty @BlockVersion Proxy)
+    prop "Attributes" (soundInstanceProperty @(Attributes X1) Proxy)
+    prop "Attributes" (soundInstanceProperty @(Attributes X2) Proxy)
+    prop "X2" (soundSerializationAttributesOfAsProperty @X2 @X1 Proxy Proxy)
+
+----------------------------------------
+
+data User
+    = Login {
+      login :: String
+    , age   :: Int
+    }
+    | FullName {
+      firstName  :: String
+    , lastName   :: String
+    , sex        :: Bool
+    } deriving Show
+
+deriveSimpleBi ''User [
+    Cons 'Login [
+        Field [| login :: String |],
+        Field [| age   :: Int    |]
+    ],
+    Cons 'FullName [
+        Field [| firstName :: String |],
+        Field [| lastName  :: String |],
+        Field [| sex       :: Bool   |]
+    ]]
+
+u1 :: User
+u1 = deserialize $ serialize $ Login "asd" 34
+
+----------------------------------------
+
+data T = T1 Int | T2 Int Int | Unknown Word8 BS.ByteString
+    deriving Show
+
+instance Bi T where
+    encode = \case
+        T1 a         -> encode (0::Word8)
+                     <> encode (serialize' a)
+        T2 a b       -> encode (1::Word8)
+                     <> encode (serialize' (a, b))
+        Unknown n bs -> encode n
+                     <> encode bs
+
+    decode = decode @Word8 >>= \case
+        0 ->         T1 . deserialize' <$> decode
+        1 -> uncurry T2 . deserialize' <$> decode
+        t -> Unknown t                 <$> decode

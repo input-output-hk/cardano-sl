1be4e75bc87212163ad796bef382626048be809c
Alfredo Di Napoli <alfredo.dinapoli@iohk.io>
2017-07-12 09:09:27 +0200

    Add instance for DlgPayload

--- src/Pos/Binary/Delegation.hs
+++ src/Pos/Binary/Delegation.hs
@@ -11,7 +11,12 @@ import           Pos.Binary.Core      ()
 import           Pos.Binary.Crypto    ()
 import           Pos.Delegation.Types (DlgPayload (getDlgPayload), mkDlgPayload)
 import           Pos.Util.Util        (eitherToFail)
+import qualified Pos.Binary.Cbor      as Cbor
 
 instance Bi DlgPayload where
     sizeNPut = labelS "DlgPayload" $ putField getDlgPayload
     get = label "DlgPayload" $ eitherToFail . mkDlgPayload =<< get
+
+instance Cbor.Bi DlgPayload where
+  encode = Cbor.encode . getDlgPayload
+  decode = Cbor.decode >>= eitherToFail . mkDlgPayload

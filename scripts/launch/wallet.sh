70a40c3485daf79a317db73e4c62981f5adad7cc
Alexander Suhoverhov <naryl.pandora@gmail.com>
2017-07-13 21:49:18 +0300

    [CSL-1203] Update launch scripts

--- scripts/launch/wallet.sh
+++ scripts/launch/wallet.sh
@@ -9,7 +9,11 @@ if [[ "$SSC_ALGO" != "" ]]; then
     ssc_algo=" --ssc-algo $SSC_ALGO "
 fi
 
-$(find_binary cardano-wallet) $(peer_config 0) $(logs smartwallet$i.log) \
+$(find_binary cardano-wallet) --system-start $(cat "$base/../../run/system_start") \
+                              --log-config log-config-prod.yaml --logs-prefix "run/wallet-logs" \
                               --db-path "run/wallet-db" --rebuild-db \
-                              --flat-distr "(3, 100000)" $ssc_algo "$@" \
-                              --system-start 100500  # random value, not used, but mandatory
+                              --flat-distr "(4, 100000)" $ssc_algo "$@" \
+                              --peer 127.0.0.1:3000 \
+                              --peer 127.0.0.1:3001 \
+                              --peer 127.0.0.1:3002 \
+                              repl

e66f098a0ad734c5f9513e12346cabe4b6511af6
Vladislav Zavialov <vlad.z.4096@gmail.com>
2017-07-24 23:35:19 +0300

    [CSL-1414] Override current slot

--- test/Test/Pos/Block/Logic/Event.hs
+++ test/Test/Pos/Block/Logic/Event.hs
@@ -8,19 +8,23 @@ module Test.Pos.Block.Logic.Event
 
 import           Universum
 
-import           Control.Monad.Catch      (catch)
+import           Control.Lens              (_Right)
+import           Control.Monad.Catch       (catch)
+import qualified Data.List.NonEmpty        as NE
 
-import           Pos.Block.Logic.VAR      (BlockLrcMode, rollbackBlocks,
-                                           verifyAndApplyBlocks)
-import           Pos.Block.Types          (Blund)
-import           Pos.Core                 (HeaderHash)
-import           Pos.DB.Pure              (DBPureDiff, MonadPureDB, dbPureDiff,
-                                           dbPureDump)
-import           Pos.Generator.BlockEvent (BlockEvent, BlockEvent' (..),
-                                           IsBlockEventFailure (..), beaInput, berInput)
-import           Pos.Ssc.GodTossing.Type  (SscGodTossing)
-import           Pos.Util.Chrono          (NE, OldestFirst)
-import           Pos.Util.Util            (eitherToThrow)
+import           Pos.Block.Logic.VAR       (BlockLrcMode, rollbackBlocks,
+                                            verifyAndApplyBlocks)
+import           Pos.Block.Types           (Blund)
+import           Pos.Core                  (HeaderHash, getEpochOrSlot, unEpochOrSlot)
+import           Pos.DB.Pure               (DBPureDiff, MonadPureDB, dbPureDiff,
+                                            dbPureDump)
+import           Pos.Generator.BlockEvent  (BlockEvent, BlockEvent' (..),
+                                            IsBlockEventFailure (..), beaInput, berInput)
+import           Pos.Ssc.GodTossing.Type   (SscGodTossing)
+import           Pos.Util.Chrono           (NE, OldestFirst, getOldestFirst)
+import           Pos.Util.Util             (eitherToThrow)
+import           Test.Pos.Block.Logic.Mode (BlockTestContext)
+import           Test.Pos.Block.Logic.Util (withCurrentSlot)
 
 newtype IsExpected = IsExpected Bool
 
@@ -37,17 +41,19 @@ mkBlockEventFailure ev =
     BlockEventFailure (IsExpected (isBlockEventFailure ev))
 
 verifyAndApplyBlocks' ::
-       BlockLrcMode ssc ctx m
-    => OldestFirst NE (Blund ssc)
+       (BlockLrcMode SscGodTossing ctx m, MonadReader BlockTestContext m)
+    => OldestFirst NE (Blund SscGodTossing)
     -> m ()
 verifyAndApplyBlocks' bs = do
-    (_ :: HeaderHash) <- eitherToThrow identity =<<
-        verifyAndApplyBlocks True (fst <$> bs)
-    return ()
+    let mSlot = (unEpochOrSlot . getEpochOrSlot . fst . NE.last . getOldestFirst) bs ^? _Right
+    maybe identity withCurrentSlot mSlot $ do
+        (_ :: HeaderHash) <- eitherToThrow identity =<<
+            verifyAndApplyBlocks True (fst <$> bs)
+        return ()
 
 -- | Execute a single block event.
 runBlockEvent ::
-       BlockLrcMode SscGodTossing ctx m
+       (BlockLrcMode SscGodTossing ctx m, MonadReader BlockTestContext m)
     => BlockEvent
     -> m BlockEventResult
 runBlockEvent (BlkEvApply ev) =
@@ -65,7 +71,7 @@ data BlockScenarioResult
 -- | Execute a block scenario: a sequence of block events that either ends with
 -- an expected failure or with a rollback to the initial state.
 runBlockScenario ::
-       (BlockLrcMode SscGodTossing ctx m, MonadPureDB ctx m)
+       (BlockLrcMode SscGodTossing ctx m, MonadPureDB ctx m, MonadReader BlockTestContext m)
     => [BlockEvent]
     -> m BlockScenarioResult
 runBlockScenario events = do

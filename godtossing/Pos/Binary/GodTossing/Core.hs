ee291cb6e76c7dff8e8330a71645ec9e731c47ad
Artyom <yom@artyom.me>
2017-07-24 20:41:56 +0300

    [CSL-1296] Fix canonicity issues in some maps' encoding

--- godtossing/Pos/Binary/GodTossing/Core.hs
+++ godtossing/Pos/Binary/GodTossing/Core.hs
@@ -4,17 +4,20 @@ module Pos.Binary.GodTossing.Core
        (
        ) where
 
-import qualified Data.HashMap.Strict           as HM
+import qualified Data.HashSet                  as HS
 import           Universum
 
-import           Pos.Core.Address              (addressHash)
-import           Pos.Binary.Class              (Bi (..), enforceSize, encodeListLen, decodeListLen, matchSize)
+import           Pos.Binary.Class              (Bi (..), Decoder, Encoding, decodeListLen,
+                                                encodeListLen, enforceSize, matchSize)
 import           Pos.Binary.Crypto             ()
-import           Pos.Ssc.GodTossing.Core.Types (Commitment (..), Commitment (..),
-                                                CommitmentsMap (..), GtPayload (..),
-                                                GtProof (..), Opening (..),
-                                                VssCertificate (..), mkCommitmentsMap,
+import           Pos.Crypto                    (PublicKey)
+import           Pos.Ssc.GodTossing.Core.Types (Commitment (..), CommitmentsMap (..),
+                                                GtPayload (..), GtProof (..),
+                                                Opening (..), SignedCommitment,
+                                                VssCertificate (..), VssCertificatesMap,
+                                                mkCommitmentsMap, mkVssCertificatesMap,
                                                 recreateVssCertificate)
+import           Serokell.Util                 (allDistinct)
 
 instance Bi Commitment where
   encode Commitment{..} = encodeListLen 3 <> encode commShares
@@ -29,8 +32,8 @@ instance Bi Commitment where
     return $ Commitment commExtra commProof commShares
 
 instance Bi CommitmentsMap where
-  encode = encode . toList . getCommitmentsMap
-  decode = mkCommitmentsMap <$> decode
+  encode = encodeCommitments
+  decode = decodeCommitments
 
 instance Bi VssCertificate where
   encode vssCert = encodeListLen 4 <> encode (vcVssKey vssCert)
@@ -53,34 +56,42 @@ instance Bi Opening where
 
 instance Bi GtPayload where
   encode input = case input of
-    CommitmentsPayload  cmap vss -> encodeListLen 3 <> encode (0 :: Word8)
-                                                    <> encode cmap <> encode (toList vss)
-    OpeningsPayload     omap vss -> encodeListLen 3 <> encode (1 :: Word8)
-                                                    <> encode omap <> encode (toList vss)
-    SharesPayload       smap vss -> encodeListLen 3 <> encode (2 :: Word8)
-                                                    <> encode smap <> encode (toList vss)
-    CertificatesPayload vss      -> encodeListLen 2 <> encode (3 :: Word8)
-                                                    <> encode (toList vss)
+    CommitmentsPayload cmap vss ->
+        encodeListLen 3
+            <> encode (0 :: Word8)
+            <> encode cmap
+            <> encodeVssCertificates vss
+    OpeningsPayload omap vss ->
+        encodeListLen 3
+            <> encode (1 :: Word8)
+            <> encode omap
+            <> encodeVssCertificates vss
+    SharesPayload smap vss ->
+        encodeListLen 3
+            <> encode (2 :: Word8)
+            <> encode smap
+            <> encodeVssCertificates vss
+    CertificatesPayload vss ->
+        encodeListLen 2
+            <> encode (3 :: Word8)
+            <> encodeVssCertificates vss
   decode = do
     len <- decodeListLen
     tag <- decode @Word8
     case tag of
       0 -> do
         matchSize len "GtPayload.CommitmentsPayload" 3
-        liftM2 CommitmentsPayload decode getVssCerts
+        liftM2 CommitmentsPayload decode decodeVssCertificates
       1 -> do
         matchSize len "GtPayload.OpeningsPayload" 3
-        liftM2 OpeningsPayload decode getVssCerts
+        liftM2 OpeningsPayload decode decodeVssCertificates
       2 -> do
         matchSize len "GtPayload.SharesPayload" 3
-        liftM2 SharesPayload decode getVssCerts
+        liftM2 SharesPayload decode decodeVssCertificates
       3 -> do
         matchSize len "GtPayload.CertificatesPayload" 2
-        CertificatesPayload <$> getVssCerts
+        CertificatesPayload <$> decodeVssCertificates
       _ -> fail ("decode@GtPayload: invalid tag: " <> show tag)
-    where
-      getVssCerts = HM.fromList . map toCertPair <$> decode
-      toCertPair vc = (addressHash $ vcSigningKey vc, vc)
 
 instance Bi GtProof where
   encode input = case input of
@@ -109,3 +120,45 @@ instance Bi GtProof where
         matchSize len "GtProof.CertificatesProof" 2
         CertificatesProof <$> decode
       _ -> fail ("decode@GtProof: invalid tag: " ++ show tag)
+
+----------------------------------------------------------------------------
+-- Maps encoding/decoding
+----------------------------------------------------------------------------
+
+{-
+'VssCertificatesMap' and 'CommitmentsMap' are simply sets of values, indexed
+by stakeholder id *for performance only*; the invariant is that the key
+(stakeholder id) corresponds to the key stored in the value. This means that
+the keys are redundant and putting them into encoded data is bad for two
+reasons:
+
+  * it takes more space
+  * we have to do an extra invariant check after decoding
+
+Instead, we serialize those maps as sets, and we make sure to check that
+there are no values with duplicate stakeholder ids.
+-}
+
+encodeVssCertificates :: VssCertificatesMap -> Encoding
+encodeVssCertificates = encode . HS.fromList . toList
+
+decodeVssCertificates :: Decoder s VssCertificatesMap
+decodeVssCertificates = do
+    certs <- toList <$> decode @(HashSet VssCertificate)
+    -- if the attacker creates two certs that are different but have the same
+    -- 'vcSigningKey', it's bad because then we lose canonicity (only one
+    -- cert will be present in resulting map and the attacker can set the
+    -- other cert to be anything at all)
+    unless (allDistinct (map vcSigningKey certs)) $
+        fail "decodeVssCertificates: two certs have the same signing key"
+    pure (mkVssCertificatesMap certs)
+
+encodeCommitments :: CommitmentsMap -> Encoding
+encodeCommitments = encode . HS.fromList . toList
+
+decodeCommitments :: Decoder s CommitmentsMap
+decodeCommitments = do
+    comms <- toList <$> decode @(HashSet SignedCommitment)
+    unless (allDistinct (map (view _1) comms :: [PublicKey])) $
+        fail "decodeCommitments: two commitments have the same signing key"
+    pure (mkCommitmentsMap comms)

23ce728543ba08745c10b365ddb112c2fba11ddd
Ilya Peresadin <pva701@gmail.com>
2017-07-27 19:40:05 +0300

    [CSL-1419] Add tests for reasonable amount of shares

--- test/Test/Pos/Ssc/GodTossing/ComputeSharesSpec.hs
+++ test/Test/Pos/Ssc/GodTossing/ComputeSharesSpec.hs
@@ -13,9 +13,12 @@ import           Universum
 import           Pos.Arbitrary.Lrc     (GenesisMpcThd, InvalidRichmenStakes (..),
                                         ValidRichmenStakes (..))
 import           Pos.Constants         (genesisMpcThd)
-import           Pos.Core              (mkCoin, unsafeAddressHash,
-                                        unsafeCoinPortionFromDouble)
-import           Pos.Core.Coin         (coinPortionToDouble, sumCoins)
+import           Pos.Core              (Coin, mkCoin, unsafeAddressHash,
+                                        unsafeCoinPortionFromDouble,
+                                        unsafeCoinPortionFromDouble, unsafeGetCoin,
+                                        unsafeSubCoin)
+import           Pos.Core.Coin         (applyCoinPortionDown, coinPortionToDouble,
+                                        sumCoins)
 import           Pos.Lrc               (RichmenStakes, RichmenType (RTUsual),
                                         findRichmenPure)
 import           Pos.Ssc.GodTossing    (SharesDistribution, TossVerFailure,
@@ -29,6 +32,12 @@ spec = describe "computeSharesDistr" $ do
     prop totalStakeZeroDesc totalStakeIsZero
     prop validRichmenStakesWorksDesc validRichmenStakesWorks
     prop lrcConsistencyDesc lrcConsistency
+
+    describe "distributionNotBig" $ do
+        prop oneRichRichmanDesc oneRichRichman
+        prop twoRichRichmenDesc twoRichRichmen
+        prop onePoorOneRichDesc onePoorOneRich
+        prop severalPoorOneRichRichmenDesc severalPoorOneRichRichmen
   where
     emptyRichmenStakesDesc = "Fails to calculate a share distribution when the richmen\
     \ stake is empty."
@@ -44,8 +53,63 @@ spec = describe "computeSharesDistr" $ do
     lrcConsistencyDesc = "computeSharesDistr's definition of richmen is\
     \ consistent with one used by LRC."
 
+    oneRichRichmanDesc = "One richman with total stake, sum of distribution mustn't be big"
+    twoRichRichmenDesc = "Two richmen with almost half of stake, sum of distribution mustn't be big"
+    onePoorOneRichDesc = "Two richmen: first with mpcThd stake, second with remained stake,\
+    \ sum of distribution mustn't be big"
+    severalPoorOneRichRichmenDesc =
+        "Several richmen, one of them control most of stake, sum of distribution mustn't be big"
+
 computeShares' :: RichmenStakes -> Either TossVerFailure SharesDistribution
 computeShares' stake = computeSharesDistrPure stake genesisMpcThd
+
+isDistrReasonable :: Word16 -> Either TossVerFailure SharesDistribution -> Bool
+isDistrReasonable _ (Left _) = False
+isDistrReasonable mx (Right sd) =
+    -- (1) Sum of distribution is less than some reasonable number.
+    limitedBy mx sd
+
+-- Check that sum of distribtuion is less than passed value
+limitedBy :: Word16 -> SharesDistribution -> Bool
+limitedBy mx sd = sum (toList sd) <= mx
+
+maxCoin :: Coin
+maxCoin = maxBound @Coin
+
+(</>) :: Coin -> Word64 -> Coin
+(</>) c d = mkCoin $ unsafeGetCoin c `div` d
+
+(<->) :: Coin -> Word64 -> Coin
+(<->) c w = unsafeSubCoin c (mkCoin w)
+
+richmenStakesFromCoins :: [Coin] -> RichmenStakes
+richmenStakesFromCoins coins = do
+    let stakeholders = map unsafeAddressHash [0 :: Integer ..]
+    HM.fromList $ zip stakeholders coins
+
+richmenStakesFromFractions :: [Double] -> RichmenStakes
+richmenStakesFromFractions fracts0 = do
+    let sm = sum fracts0
+    let ports = map (unsafeCoinPortionFromDouble . (/ sm)) fracts0
+    let coins = map (flip applyCoinPortionDown maxCoin) ports
+    richmenStakesFromCoins coins
+
+oneRichRichman :: Bool
+oneRichRichman = isDistrReasonable 1 $
+    computeShares' $ richmenStakesFromCoins [maxCoin]
+
+twoRichRichmen :: Bool
+twoRichRichmen = isDistrReasonable 2 $
+    computeShares' $ richmenStakesFromCoins [maxCoin </> 2 <-> 1, maxCoin </> 2]
+
+onePoorOneRich :: Bool
+onePoorOneRich = isDistrReasonable 100 $
+    computeShares' $ richmenStakesFromFractions [0.01, 0.99]
+
+severalPoorOneRichRichmen :: Bool
+severalPoorOneRichRichmen = isDistrReasonable 100 $ do
+    let distr = [0.01, 0.051, 0.13]
+    computeShares' $ richmenStakesFromFractions $ 1 - sum distr : distr
 
 emptyRichmenStakes :: Expectation
 emptyRichmenStakes =

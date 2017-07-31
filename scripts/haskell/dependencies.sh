58f99bbc11a61e9b71bb180e8701555b440f7312
Alfredo Di Napoli <alfredo.dinapoli@iohk.io>
2017-07-31 16:11:51 +0200

    Initial slow (and probably buggy) revDep implementation

--- scripts/haskell/dependencies.sh
+++ scripts/haskell/dependencies.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env stack
--- stack runghc --package turtle --package algebraic-graphs
+-- stack runghc --package turtle --package algebraic-graphs-0.0.5 --package parallel
 
 {-# LANGUAGE BangPatterns        #-}
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TupleSections       #-}
 import           Algebra.Graph
+import           Algebra.Graph.Export.Dot    (Attribute (..), Style (..), export)
 import           Control.Monad
+import           Control.Parallel.Strategies (parMap, rpar)
 import           Data.Functor.Identity
 import           Data.List
-import qualified Data.Map.Strict       as M
-import qualified Data.Set              as Set
-import qualified Data.Text             as T
+import qualified Data.Map.Strict             as M
+import qualified Data.Set                    as Set
+import qualified Data.Text                   as T
 import           Text.Printf
 import           Text.Read
-import           Turtle                hiding (printf)
+import           Turtle                      hiding (export, printf)
 
 type PackageName = T.Text
 data Version = V [Int] deriving (Eq, Ord)
@@ -61,18 +63,19 @@ directDependenciesFor (name, ver) = do
             concatMap (map (mkPackage . normalisePackage) . T.splitOn " ") (takeWhile (/= "depends:") deps)
         _ -> mempty
 
-type DAG = Graph Package
+type DAG    = Graph Package
+type DepMap = M.Map Package [Package]
 
 --------------------------------------------------------------------------------
-buildDependencyContext :: [Package] -> IO (M.Map PackageName [Package], DAG)
+buildDependencyContext :: [Package] -> IO (DepMap, DAG)
 buildDependencyContext [] = return (M.empty, Algebra.Graph.empty)
 buildDependencyContext pkgs = go pkgs (M.empty, Set.empty)
   where
-    go :: [Package] -> (M.Map PackageName [Package], Set.Set (Package, Package)) -> IO (M.Map PackageName [Package], DAG)
+    go :: [Package] -> (DepMap, Set.Set (Package, Package)) -> IO (DepMap, DAG)
     go [] (depMap, dag) = return (depMap, edges (Set.toList dag))
-    go (pkg@(pkgName,_):xs) (depMap, dag) = do
+    go (pkg:xs) (depMap, dag) = do
       directDeps <- directDependenciesFor pkg
-      let !newMap = M.insert pkgName directDeps $! depMap
+      let !newMap = M.insert pkg directDeps $! depMap
       let !newDag = dag <> Set.fromList (map (pkg,) directDeps)
       go xs (newMap, newDag)
 
@@ -88,19 +91,66 @@ normalisePackage txt = case T.breakOnEnd "-" txt of
         Just _  -> txt
         Nothing -> if x == "" then error ("normalisePackage: " <> show txt) else T.init x
 
+--------------------------------------------------------------------------------
+reverseDependenciesFor :: Package -> [Package] -> DepMap -> DAG -> [Package]
+reverseDependenciesFor pkg allDeps directDeps dag = go allDeps mempty
+  where
+    go [] revDeps     = revDeps
+    go (x:xs) revDeps = case reachableFrom x pkg directDeps of
+        True  -> go xs (x : revDeps)
+        False -> go xs revDeps
+        -- For each package x, check the graph to see if there is a path going
+        -- from x to `pkg`. If there is, we found a reverse dep.
+
+reachableFrom :: Package -> Package -> DepMap -> Bool
+reachableFrom start end directDeps = go (M.findWithDefault mempty start directDeps)
+  where
+    go :: [Package] -> Bool
+    go [] = False
+    go (x:xs) = case x == end of
+        True  -> True
+        False -> any (\newStart -> reachableFrom newStart end directDeps) xs
+
+--------------------------------------------------------------------------------
+style :: Style Package String
+style = Style
+    { graphName               = ""
+    , preamble                = ""
+    , graphAttributes         = ["label" := "Example", "labelloc" := "top"]
+    , defaultVertexAttributes = ["shape" := "circle"]
+    , defaultEdgeAttributes   = mempty
+    , vertexName              = \(name,_)   -> T.unpack name
+    , vertexAttributes        = \_   -> ["color" := "blue"]
+    , edgeAttributes          = \_ _ -> ["style" := "dashed"]
+    }
+
+--------------------------------------------------------------------------------
+dottify :: DAG -> IO ()
+dottify dag = writeFile "dep_dot.graphviz" (export style dag)
+
+--------------------------------------------------------------------------------
 main :: IO ()
 main = do
     allDeps <- getTotalPackages
     putStrLn "Building direct dependency map..."
     (directDepMap, depDag) <- buildDependencyContext allDeps
 
-    let tableHeader         = printf "%-40s" ("Package" :: String) <> printf "%-10s" ("Direct dependencies" :: String)
-    let tableEntry pkg deps = printf "%-40s" (T.unpack pkg) <> printf "%-10s" (show deps)
+    let tableHeader         =  printf "%-40s" ("Package" :: String)
+                            <> printf "%-20s" ("Direct dependencies"  :: String)
+                            <> printf "%-20s" ("Reverse dependencies" :: String)
+    let tableEntry pkg deps revDeps =  printf "%-40s" (T.unpack pkg)
+                                    <> printf "%-20s" (show deps)
+                                    <> printf "%-20s\n" (show (revDeps :: Int))
     putStrLn tableHeader
 
     let depsMap = M.map length directDepMap
 
-    forM_ (sortOn snd $ M.toList depsMap) $ \(pkgName, deps) -> do
-        putStrLn (tableEntry pkgName deps)
+    let sortedDepList = reverse (sortOn snd $ M.toList depsMap)
+    let mkTableEntry  (pkg@(pkgName,_), deps) =
+            let revDeps = reverseDependenciesFor pkg allDeps directDepMap depDag
+            in tableEntry pkgName deps (length revDeps)
+    let table         = parMap rpar mkTableEntry sortedDepList
+
+    putStrLn $ mconcat table
     -- Display the total deps
-    putStrLn $ tableEntry "Total project deps" (length allDeps + length blacklistedPackages)
+    putStrLn $ tableEntry "Total project deps" (length allDeps + length blacklistedPackages) 0

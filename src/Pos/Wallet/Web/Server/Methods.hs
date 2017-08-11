9a6da22e8dbcaae8738e0ed15a68e46665f81081
Martoon <martoon.hsk@gmail.com>
2017-08-11 04:20:47 +0300

    [CSM-237] Remove restore from Methods

--- src/Pos/Wallet/Web/Server/Methods.hs
+++ src/Pos/Wallet/Web/Server/Methods.hs
@@ -420,45 +420,6 @@ selectSrcAddresses allAddrs outputCoins (TxFee fee) =
                          (find ((reqCoins ==) . snd) addresses)
 
 
--- | Which index to use to create initial account and address on new wallet
--- creation
-initialAccAddrIdxs :: Word32
-initialAccAddrIdxs = 0
-
-newWalletFromBackupPhrase
-    :: MonadWalletWebMode m
-    => PassPhrase -> CWalletInit -> m (EncryptedSecretKey, CId Wal)
-newWalletFromBackupPhrase passphrase CWalletInit {..} = do
-    let CWalletMeta {..} = cwInitMeta
-
-    skey <- genSaveRootKey passphrase cwBackupPhrase
-    let cAddr = encToCId skey
-
-    CWallet{..} <- L.createWalletSafe cAddr cwInitMeta
-    -- can't return this result, since balances can change
-
-    let accMeta = CAccountMeta { caName = "Initial account" }
-        accInit = CAccountInit { caInitWId = cwId, caInitMeta = accMeta }
-    () <$ L.newAccount (DeterminedSeed initialAccAddrIdxs) passphrase accInit
-
-    return (skey, cAddr)
-
-newWallet :: MonadWalletWebMode m => PassPhrase -> CWalletInit -> m CWallet
-newWallet passphrase cwInit = do
-    (_, wId) <- newWalletFromBackupPhrase passphrase cwInit
-    updateHistoryCache wId []
-    -- BListener checks current syncTip before applying update,
-    -- thus setting it up to date manually here
-    withBlkSemaphore_ $ \tip -> tip <$ setWalletSyncTip wId tip
-    L.getWallet wId
-
-restoreWallet :: MonadWalletWebMode m => PassPhrase -> CWalletInit -> m CWallet
-restoreWallet passphrase cwInit = do
-    (sk, wId) <- newWalletFromBackupPhrase passphrase cwInit
-    syncWalletOnImport sk
-    L.getWallet wId
-
-
 updateTransaction :: MonadWalletWebMode m => AccountId -> CTxId -> CTxMeta -> m ()
 updateTransaction accId txId txMeta = do
     setWalletTxMeta (aiWId accId) txId txMeta
@@ -565,64 +526,6 @@ reportingElectroncrash celcrash = do
     handler :: SomeException -> m ()
     handler e = logError $ sformat fmt celcrash e
 
-importWallet
-    :: MonadWalletWebMode m
-    => PassPhrase
-    -> Text
-    -> m CWallet
-importWallet passphrase (toString -> fp) = do
-    secret <-
-        rewrapToWalletError isDoesNotExistError noFile $
-        rewrapToWalletError (\UserSecretDecodingError{} -> True) decodeFailed $
-        readUserSecret fp
-    wSecret <- maybeThrow noWalletSecret (secret ^. usWalletSet)
-    wId <- cwId <$> importWalletSecret emptyPassphrase wSecret
-    L.changeWalletPassphrase wId emptyPassphrase passphrase
-    L.getWallet wId
-  where
-    noWalletSecret = RequestError "This key doesn't contain HD wallet info"
-    noFile _ = RequestError "File doesn't exist"
-    decodeFailed = RequestError . sformat ("Invalid secret file ("%build%")")
-
-importWalletSecret
-    :: MonadWalletWebMode m
-    => PassPhrase
-    -> WalletUserSecret
-    -> m CWallet
-importWalletSecret passphrase WalletUserSecret{..} = do
-    let key    = _wusRootKey
-        wid    = encToCId key
-        wMeta  = def { cwName = _wusWalletName }
-    addSecretKey key
-    importedWallet <- L.createWalletSafe wid wMeta
-
-    for_ _wusAccounts $ \(walletIndex, walletName) -> do
-        let accMeta = def{ caName = walletName }
-            seedGen = DeterminedSeed walletIndex
-        cAddr <- genUniqueAccountId seedGen wid
-        createAccount cAddr accMeta
-
-    for_ _wusAddrs $ \(walletIndex, accountIndex) -> do
-        let accId = AccountId wid walletIndex
-        L.newAddress (DeterminedSeed accountIndex) passphrase accId
-
-    void $ syncWalletOnImport key
-
-    return importedWallet
-
--- | Creates wallet with given genesis hd-wallet key.
-addInitialRichAccount :: MonadWalletWebMode m => Int -> m ()
-addInitialRichAccount keyId =
-    when isDevelopment . E.handleAll wSetExistsHandler $ do
-        key <- maybeThrow noKey (genesisDevHdwSecretKeys ^? ix keyId)
-        void $ importWalletSecret emptyPassphrase $
-            mkGenesisWalletUserSecret key
-                & wusWalletName .~ "Precreated wallet full of money"
-                & wusAccounts . traversed . _2 .~ "Initial account"
-  where
-    noKey = InternalError $ sformat ("No genesis key #" %build) keyId
-    wSetExistsHandler =
-        logDebug . sformat ("Creation of initial wallet was skipped (" %build % ")")
 
 syncProgress :: MonadWalletWebMode m => m SyncProgress
 syncProgress = do

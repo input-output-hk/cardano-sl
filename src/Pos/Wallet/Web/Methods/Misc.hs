06491b9245efbe96aace6946efe4db1ee3de785b
Martoon <martoon.hsk@gmail.com>
2017-08-11 05:17:16 +0300

    [CSM-237] Fix build

--- src/Pos/Wallet/Web/Methods/Misc.hs
+++ src/Pos/Wallet/Web/Methods/Misc.hs
@@ -1,8 +1,4 @@
-{-# LANGUAGE AllowAmbiguousTypes #-}
-{-# LANGUAGE RankNTypes          #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeFamilies        #-}
-{-# LANGUAGE TypeOperators       #-}
+{-# LANGUAGE TypeFamilies #-}
 
 -- | Various small endpoints
 
@@ -22,53 +18,18 @@ module Pos.Wallet.Web.Methods.Misc
 
 import           Universum
 
-import           Control.Lens                   (has)
-import qualified Data.List.NonEmpty             as NE
-import qualified Data.Set                       as S
-import           Formatting                     (build, sformat, (%))
-import qualified Formatting                     as F
-import           System.Wlog                    (logDebug, logInfo)
-
-import           Pos.Aeson.ClientTypes          ()
-import           Pos.Aeson.WalletBackup         ()
-import           Pos.Binary.Class               (biSize)
-import           Pos.Client.Txp.Balances        (getOwnUtxos)
-import           Pos.Client.Txp.History         (TxHistoryEntry (..))
-import           Pos.Client.Txp.Util            (TxError (..), createMTx,
-                                                 overrideTxDistrBoot,
-                                                 overrideTxOutDistrBoot)
-import           Pos.Communication              (SendActions (..), submitMTx)
-import           Pos.Core                       (Coin, TxFeePolicy (..),
-                                                 TxSizeLinear (..), addressF,
-                                                 bvdTxFeePolicy, calculateTxSizeLinear,
-                                                 decodeTextAddress, getCurrentTimestamp,
-                                                 integerToCoin, mkCoin, unsafeAddCoin,
-                                                 unsafeSubCoin, _RedeemAddress)
-import           Pos.Crypto                     (PassPhrase, fakeSigner, hash, keyGen,
-                                                 withSafeSigners)
-import           Pos.DB.Class                   (gsAdoptedBVData)
-import           Pos.Txp                        (TxFee (..))
-import           Pos.Txp.Core                   (TxAux (..), TxOut (..), TxOutAux (..),
-                                                 TxOutDistribution)
-import           Pos.Util                       (eitherToThrow, maybeThrow)
-import           Pos.Wallet.KeyStorage          (deleteSecretKey, getSecretKeys)
-import           Pos.Wallet.WalletMode          (applyLastUpdate, connectedPeers,
-                                                 localChainDifficulty,
-                                                 networkChainDifficulty)
-import           Pos.Wallet.Web.Account         (GenSeed (..), MonadKeySearch (..))
-import           Pos.Wallet.Web.ClientTypes     (AccountId (..), Addr, CAddress (..),
-                                                 CCoin, CId, CProfile, CProfile (..),
-                                                 CTx (..), CTxs (..), CUpdateInfo (..),
-                                                 CWAddressMeta (..), SyncProgress (..),
-                                                 Wal, addrMetaToAccount, mkCCoin)
-import           Pos.Wallet.Web.Error           (WalletError (..))
-import           Pos.Wallet.Web.Methods.History (addHistoryTx)
-import qualified Pos.Wallet.Web.Methods.Logic   as L
-import           Pos.Wallet.Web.Mode            (MonadWalletWebMode)
-import           Pos.Wallet.Web.State           (AddressLookupMode (Existing),
-                                                 getNextUpdate, getProfile,
-                                                 removeNextUpdate, setProfile, testReset)
-import           Pos.Wallet.Web.Util            (getWalletAccountIds, rewrapTxError)
+import           Pos.Aeson.ClientTypes      ()
+import           Pos.Core                   (decodeTextAddress)
+import           Pos.Util                   (maybeThrow)
+import           Pos.Wallet.KeyStorage      (deleteSecretKey, getSecretKeys)
+import           Pos.Wallet.WalletMode      (applyLastUpdate, connectedPeers,
+                                             localChainDifficulty, networkChainDifficulty)
+import           Pos.Wallet.Web.ClientTypes (CProfile, CProfile (..), CUpdateInfo (..),
+                                             SyncProgress (..))
+import           Pos.Wallet.Web.Error       (WalletError (..))
+import           Pos.Wallet.Web.Mode        (MonadWalletWebMode)
+import           Pos.Wallet.Web.State       (getNextUpdate, getProfile, removeNextUpdate,
+                                             setProfile, testReset)
 
 
 ----------------------------------------------------------------------------

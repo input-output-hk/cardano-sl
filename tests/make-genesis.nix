7085a4552c999f23aae56dd1fc19cba29c49369b
michael bishop <cleverca22@gmail.com>
2017-08-13 20:05:45 -0300

    fix genesis creation script

--- tests/make-genesis.nix
+++ tests/make-genesis.nix
@@ -2,5 +2,5 @@
 
 runCommand "genesis" { buildInputs = [ cardano-sl-tools ]; } ''
   mkdir $out
-  cardano-keygen --genesis-dir $out -m 5 -n 12000 --richmen-share 0.94 --testnet-stake 19072918462000000 --utxo-file ${../scripts/avvm-files/utxo-dump-last-new.json} --blacklisted ${../scripts/avvm-files/full_blacklist.js} --fake-avvm-entries 100 |& tee $out/genesisCreation.log
+  cardano-keygen generate-genesis --genesis-dir $out -m 5 -n 12000 --richmen-share 0.94 --testnet-stake 19072918462000000 --utxo-file ${../scripts/avvm-files/utxo-dump-last-new.json} --blacklisted ${../scripts/avvm-files/full_blacklist.js} --fake-avvm-entries 100 |& tee $out/genesisCreation.log
 ''

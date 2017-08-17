e687276cdc1efd80fac41698cebfc84031330e12
Philipp Kant <philipp.kant@iohk.io>
2017-08-17 11:56:54 +0200

    Increase delay to 45 seconds

--- scripts/launch/demo.sh
+++ scripts/launch/demo.sh
@@ -61,7 +61,7 @@ fi
 # and start processing the first slot.
 if [ -z "$system_start" ]
   then
-    system_start=$((`date +%s` + 30))
+    system_start=$((`date +%s` + 45))
 fi
 
 echo "Using system start time "$system_start

ab2a2c35e8926e8882823ed5e5a6775fae779e92
michael bishop <cleverca22@gmail.com>
2017-08-17 19:01:42 -0300

    it now creates blocks in the VM

--- tests/simple-node.nix
+++ tests/simple-node.nix
@@ -7,34 +7,42 @@ let
     sha256 = "03vyxxb608nds10c0vhjr1a42dqvsm8mip12dcfin0jgnwxl5ssc";
   };
   genesis = (import ../default.nix { inherit pkgs; }).make-genesis;
+  mkMachine = index: { config, pkgs, ... }: {
+    imports = [ ../nixos/cardano-node.nix ];
+    services.cardano-node = {
+      enable = true;
+      nodeIndex = index;
+      executable = "${(import ../. {}).testjob}/bin/cardano-node-simple";
+      autoStart = true;
+      #initialPeers = [];
+      initialKademliaPeers = [];
+      genesisN = 6;
+      enableP2P = true;
+      type = "core";
+      nodeName = "node${toString index}";
+      productionMode = true;
+      systemStart = 1501546200; # 2017-08-01 00:10:00
+    };
+    virtualisation.qemu.options = [ "-rtc base='2017-08-01'" ];
+    boot.kernelParams = [ "quiet" ];
+    systemd.services.cardano-node.preStart = ''
+      cp -v ${genesis}/keys-testnet/rich/testnet${toString index}.key /var/lib/cardano-node/key${toString index}.sk
+      ls -ltrh /var/lib/cardano-node/
+    '';
+  };
 in {
   name = "simple-node";
   nodes = {
-    machine = { config, pkgs, ... }: {
-      imports = [ ../nixos/cardano-node.nix ];
-      services.cardano-node = {
-        enable = true;
-        nodeIndex = 0;
-        executable = "${(import ../. {}).testjob}/bin/cardano-node-simple";
-        autoStart = true;
-        #initialPeers = [];
-        initialKademliaPeers = [];
-        genesisN = 6;
-        enableP2P = true;
-        type = "core";
-        nodeName = "node0";
-        productionMode = true;
-      };
-      systemd.services.cardano-node.preStart = ''
-        cp -vi ${genesis}/keys-testnet/rich/testnet1.key /var/lib/cardano-node/key0.sk
-        ls -ltrh /var/lib/cardano-node/
-      '';
-    };
+    node1 = mkMachine 1;
+    node2 = mkMachine 2;
+    node3 = mkMachine 3;
+    node4 = mkMachine 4;
+    node5 = mkMachine 5;
   };
   testScript = ''
     startAll
-    $machine->waitForUnit("cardano-node.service");
+    $node1->waitForUnit("cardano-node.service");
     # TODO, implement sd_notify?
-    $machine->waitForOpenPort(3000);
+    $node1->waitForOpenPort(3000);
   '';
 })

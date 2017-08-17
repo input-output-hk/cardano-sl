e83677d0d2f1a3c26caa8a28f38ac477cd08603e
michael bishop <cleverca22@gmail.com>
2017-08-16 22:17:38 -0300

    update the tests to handle cardano-node-simple

--- lib.nix
+++ lib.nix
@@ -7,4 +7,9 @@ in lib // (rec {
   fetchNixPkgs = if builtins.getEnv "NIX_PATH_LOCKED" == "1"
     then builtins.trace "using host nixpkgs" <nixpkgs>
     else builtins.trace "fetching nixpkgs"   fetchNixpkgsWithNixpkgs hostPkgs;
+    
+  # modulo operator
+  # mod 11 10 == 1
+  # mod 1 10 == 1
+  mod = base: int: base - (int * (builtins.div base int));
 })
--- tests/simple-node.nix
+++ tests/simple-node.nix
@@ -3,18 +3,26 @@ let
   iohk-nixops = pkgs.fetchFromGitHub {
     owner = "input-output-hk";
     repo = "iohk-nixops";
-    rev = "1777ae1edcfd64696e72e35de9c4b3bc947ba43e";
-    sha256 = "0xy6m1lgrwbn1vnnzgrn7fpccvfzf9l3fnrzc11bpvcag6rkyzbd";
+    rev = "88c2b0b05c993287988255d0f32c6e13aad74f1c";
+    sha256 = "03vyxxb608nds10c0vhjr1a42dqvsm8mip12dcfin0jgnwxl5ssc";
   };
 in {
   name = "simple-node";
   nodes = {
     machine = { config, pkgs, ... }: {
-      imports = [ (import (iohk-nixops + "/modules/cardano-node-config.nix") 0 "") ];
+      imports = [ ../nixos/cardano-node.nix ];
       services.cardano-node = {
-        executable = "${(import ../. {}).testjob}/bin/cardano-node";
+        enable = true;
+        nodeIndex = 0;
+        executable = "${(import ../. {}).testjob}/bin/cardano-node-simple";
         autoStart = true;
-        initialPeers = [];
+        #initialPeers = [];
+        initialKademliaPeers = [];
+        genesisN = 6;
+        enableP2P = true;
+        type = "core";
+        nodeName = "node0";
+        productionMode = true;
       };
     };
   };

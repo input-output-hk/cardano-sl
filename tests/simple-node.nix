2d9c3ba1ce8807139d00e81475a1f6b9261abb25
michael bishop <cleverca22@gmail.com>
2017-08-18 07:16:50 -0300

    the nodes can now start creating blocks

--- tests/simple-node.nix
+++ tests/simple-node.nix
@@ -6,6 +6,21 @@ let
     rev = "88c2b0b05c993287988255d0f32c6e13aad74f1c";
     sha256 = "03vyxxb608nds10c0vhjr1a42dqvsm8mip12dcfin0jgnwxl5ssc";
   };
+  mkNode = index: {
+    type = "core";
+    region = "";
+    static-routes = []; # list of lists of name pairs
+  };
+  topology = {
+    nodes = {
+      node1 = mkNode 1;
+      node2 = mkNode 2;
+      node3 = mkNode 3;
+      node4 = mkNode 4;
+      node5 = mkNode 5;
+    };
+  };
+  topologyFile = pkgs.writeText "topology.json" (builtins.toJSON topology);
   genesis = (import ../default.nix { inherit pkgs; }).make-genesis;
   mkMachine = index: { config, pkgs, ... }: {
     imports = [ ../nixos/cardano-node.nix ];
@@ -21,8 +36,10 @@ let
       type = "core";
       nodeName = "node${toString index}";
       productionMode = true;
-      systemStart = 1501546200; # 2017-08-01 00:10:00
+      systemStart = 1501545900; # 2017-08-01 00:05:00
+      topologyFile = "${topologyFile}";
     };
+    networking.firewall.enable = false;
     virtualisation.qemu.options = [ "-rtc base='2017-08-01'" ];
     boot.kernelParams = [ "quiet" ];
     systemd.services.cardano-node.preStart = ''
@@ -44,5 +61,13 @@ in {
     $node1->waitForUnit("cardano-node.service");
     # TODO, implement sd_notify?
     $node1->waitForOpenPort(3000);
+    $node2->waitForOpenPort(3000);
+    $node3->waitForOpenPort(3000);
+    $node4->waitForOpenPort(3000);
+    $node5->waitForOpenPort(3000);
+    #$node1->sleep(60);
+    #print $node1->execute("netstat -anp ; ps aux | grep cardano ");
+    $node1->sleep(600);
+    print $node1->execute("journalctl -u cardano-node | grep 'Created a new block' ");
   '';
 })

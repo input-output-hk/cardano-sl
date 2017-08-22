82aff1dd1db9bc16ac53c7c61d5ffc162389c987
Martoon <martoon.hsk@gmail.com>
2017-08-23 00:06:24 +0300

    Fix rewrapTxError

--- wallet/src/Pos/Wallet/Web/Methods/Txp.hs
+++ wallet/src/Pos/Wallet/Web/Methods/Txp.hs
@@ -30,9 +30,10 @@ rewrapTxError
     :: forall m a. MonadCatch m
     => Text -> m a -> m a
 rewrapTxError prefix =
-    rewrapToWalletError (const True) (InternalError . sbuild) .
+    rewrapToWalletError (\SomeException{} -> True) (InternalError . sbuild) .
     rewrapToWalletError (\TxError{} -> True) (RequestError . sbuild)
   where
+    sbuild :: Buildable e => e -> Text
     sbuild = sformat (stext%": "%build) prefix
 
 coinDistrToOutputs

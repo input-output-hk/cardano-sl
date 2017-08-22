a373af7760211ab2a593fe2c384531c4dd2ad62f
ksaric <kristijan.saric@iohk.io>
2017-08-22 12:20:49 +0200

    CSE-198 Fix script and readme.

--- scripts/launch/explorer-with-nodes.sh
+++ scripts/launch/explorer-with-nodes.sh
@@ -3,7 +3,7 @@
 system_start=$((`date +%s` + 1))
 
 WALLET_TEST=1 ./scripts/launch/demo-with-wallet-api.sh & PIDNODE=$!
-./scripts/launch/explorer-run.sh ./scripts/common-functions.sh & PIDEX=$!
+./scripts/launch/explorer.sh ./scripts/common-functions.sh & PIDEX=$!
 
 wait $PIDEX
 wait $PIDNODE

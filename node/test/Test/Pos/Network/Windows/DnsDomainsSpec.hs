d793fa987dd063b1da152b6a311b431b44b94b0b
Alfredo Di Napoli <alfredo.dinapoli@iohk.io>
2017-08-30 10:35:04 +0200

    Add multi-value lookup tests

--- node/test/Test/Pos/Network/Windows/DnsDomainsSpec.hs
+++ node/test/Test/Pos/Network/Windows/DnsDomainsSpec.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE CPP #-}
+{-# LANGUAGE CPP               #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 #if !defined(mingw32_HOST_OS)
 #define POSIX
@@ -14,23 +15,35 @@ module Test.Pos.Network.Windows.DnsDomainsSpec
 import           Universum
 
 import           Network.URI                    (isIPv4address)
-import           Test.Hspec                     (Expectation, Spec, describe,
-                                                 shouldSatisfy)
+import           Pos.Network.Types              (initDnsOnUse)
+import           Test.Hspec                     (Expectation, Spec, describe, it,
+                                                 shouldBe, shouldSatisfy)
 import           Test.Hspec.QuickCheck          (prop)
 
 #if !defined(POSIX)
 import qualified Pos.Network.Windows.DnsDomains as Win
 #endif
 
+
+testLookupFor :: ByteString -> Expectation
+testLookupFor dnsDomain = do
+    initDnsOnUse $ \resolve -> do
+      res <- resolve dnsDomain
+      isRight res `shouldBe` True
+
 spec :: Spec
 #if defined(POSIX)
 spec = return ()
 #else
-spec =
+spec = do
     describe "Default DNS server on Windows" $ do
         describe "getWindowsDefDnsServer" $ do
             prop "Successfully retrieves this Windows machine's DNS server"
                  prop_GetWindowsDefDnsServer
+    describe "Multi-value lookups" $ do
+        it "www.google.it resolves correctly" $ testLookupFor "www.google.it"
+        it "cardano-node-0.aws.iohkdev.io resolves correctly" $ testLookupFor "cardano-node-0.aws.iohkdev.io"
+        it "cardano-node-1.aws.iohkdev.io resolves correctly" $ testLookupFor "cardano-node-1.aws.iohkdev.io"
 
 prop_GetWindowsDefDnsServer :: Expectation
 prop_GetWindowsDefDnsServer = do

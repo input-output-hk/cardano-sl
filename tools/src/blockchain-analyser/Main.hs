31bbd588f84d12cbf4812421d17b8d1ff3f62587
Alfredo Di Napoli <alfredo.dinapoli@iohk.io>
2017-09-01 16:43:58 +0200

    Add unix du-like function

--- tools/src/blockchain-analyser/Main.hs
+++ tools/src/blockchain-analyser/Main.hs
@@ -1,7 +1,26 @@
-
+{-# LANGUAGE BangPatterns #-}
 module Main where
 
+import           System.Directory (canonicalizePath, doesDirectoryExist, getFileSize,
+                                   listDirectory, withCurrentDirectory)
 import           Universum
 
+-- | Like Unix's `du -s`, but works across all the major platforms and
+-- returns the total number of bytes the directory occupies on disk.
+-- Recurses inside each directory it finds.
+du_s :: FilePath -> IO Integer
+du_s root = go 0 [root]
+  where
+    go !acc [] = return acc
+    go !acc (f:fs) = do
+        sz        <- getFileSize f
+        isDir     <- doesDirectoryExist f
+        extraDirs <- if isDir then ls f else return mempty
+        go (acc + sz) (extraDirs <> fs)
+
+ls :: FilePath -> IO [FilePath]
+ls f = withCurrentDirectory f ((listDirectory >=> mapM canonicalizePath) f)
+
 main :: IO ()
-main = putText "May this be the beginning of a beautiful program."
+main = do
+    print =<< (canonicalizePath >=> du_s $ "run/node-db0")

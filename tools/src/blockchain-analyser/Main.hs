6291ded7267f90ba861b0be42428a2cb6efe1838
Alfredo Di Napoli <alfredo.dinapoli@iohk.io>
2017-09-03 18:02:42 +0200

    [CSL-1373] Print the size breakdown

--- tools/src/blockchain-analyser/Main.hs
+++ tools/src/blockchain-analyser/Main.hs
@@ -1,10 +1,14 @@
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BangPatterns  #-}
+{-# LANGUAGE TupleSections #-}
 module Main where
 
 import           Options          (CLIOptions (..), getOptions, renderBytes)
 import           System.Directory (canonicalizePath, doesDirectoryExist, getFileSize,
                                    listDirectory, withCurrentDirectory)
 import           Universum
+
+import           Text.Tabl        (Alignment (..), Decoration (..),
+                                   Environment (EnvAscii), tabl)
 
 -- | Like Unix's `du -s`, but works across all the major platforms and
 -- returns the total number of bytes the directory occupies on disk.
@@ -22,7 +26,18 @@ du_s root = go 0 [root]
 ls :: FilePath -> IO [FilePath]
 ls f = withCurrentDirectory f ((listDirectory >=> mapM canonicalizePath) f)
 
+dbSizes :: FilePath -> IO [(Text, Integer)]
+dbSizes root = do
+    parents <- ls root
+    forM (root : parents) $ \f -> (toText f,) <$> du_s f
+
 main :: IO ()
 main = do
     CLIOptions{..} <- getOptions
-    putText . renderBytes uom =<< (canonicalizePath >=> du_s $ dbPath)
+    sizes <- canonicalizePath dbPath >>= dbSizes
+    let rows = ["Directory", "Size"] : map (\(f,sz) -> [f, renderBytes uom sz]) sizes
+    putText $ tabl EnvAscii hdecor vdecor aligns rows
+  where
+    hdecor = DecorUnion [DecorOuter, DecorOnly [1]]
+    vdecor = DecorAll
+    aligns = [AlignLeft, AlignLeft]

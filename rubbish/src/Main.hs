8303100afa56432490fc8cdba5a423292f735bb5
Ivan Gromakovskii <gie1994@gmail.com>
2017-09-08 18:15:35 +0300

    [CSL-1621] Always use TopologyRubbish in Rubbish

--- rubbish/src/Main.hs
+++ rubbish/src/Main.hs
@@ -14,6 +14,7 @@ import           Pos.Core            (Timestamp (..))
 import           Pos.Core.Context    (HasCoreConstants, giveStaticConsts)
 import           Pos.Launcher        (NodeParams (..), bracketNodeResources,
                                       runRealBasedMode)
+import           Pos.Network.Types   (Topology (..), defaultNetworkConfig)
 import           Pos.Ssc.SscAlgo     (SscAlgo (GodTossingAlgo))
 import           Pos.Util.UserSecret (usVss)
 import           Pos.WorkMode        (RealMode)
@@ -23,14 +24,21 @@ import           Mode                (CmdCtx (..), RubbishContext (..), RubbishM
 import           Plugin              (rubbishPlugin)
 import           RubbishOptions      (RubbishOptions (..), getRubbishOptions)
 
+-- 'NodeParams' obtained using 'CLI.getNodeParams' are not perfect for
+-- Rubbish, so we need to adopt them slightly.
+correctNodeParams :: RubbishOptions -> NodeParams -> NodeParams
+correctNodeParams RubbishOptions {..} np =
+    np {npNetworkConfig = defaultNetworkConfig $ TopologyRubbish roPeers}
+
 action :: HasCoreConstants => RubbishOptions -> Production ()
 action opts@RubbishOptions {..} = do
     CLI.printFlags
     systemStart <- CLI.getNodeSystemStart $ CLI.sysStart commonArgs
-    logInfo $ sformat ("System start time is " % shown) systemStart
+    logInfo $ sformat ("System start time is "%shown) systemStart
     t <- currentTime
-    logInfo $ sformat ("Current time is " % shown) (Timestamp t)
-    nodeParams <- CLI.getNodeParams cArgs nArgs systemStart
+    logInfo $ sformat ("Current time is "%shown) (Timestamp t)
+    nodeParams <-
+        correctNodeParams opts <$> CLI.getNodeParams cArgs nArgs systemStart
     let vssSK = unsafeFromJust $ npUserSecret nodeParams ^. usVss
     let gtParams = CLI.gtSscParams cArgs vssSK (npBehaviorConfig nodeParams)
     bracketNodeResources @RubbishSscType nodeParams gtParams actionWithResources

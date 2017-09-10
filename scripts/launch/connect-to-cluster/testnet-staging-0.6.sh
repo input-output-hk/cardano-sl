d10146d3b5ea0e66ca5a1c907b433a95e38a85a6
Kosyrev Serge <serge.kosyrev@iohk.io>
2017-09-10 17:38:44 +0300

    connect-to-cluster/testnet-staging-0.6.sh:  bump system start, update valency, refactor

--- scripts/launch/connect-to-cluster/testnet-staging-0.6.sh
+++ scripts/launch/connect-to-cluster/testnet-staging-0.6.sh
@@ -1,26 +1,28 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "Launch a single node and connect it to 'testnet-staging-0.6' cluster..."
+readonly CLUSTER=testnet-staging-0.6
+readonly DOMAIN=aws.iohkdev.io
+readonly SYSTEM_START_TIME=1504807319
+
+echo "Launch a single node and connect it to '${CLUSTER}' cluster..."
 
 readonly TMP_TOPOLOGY_YAML=/tmp/topology.yaml
-readonly SYSTEM_START_TIME=1503504180
-
 printf "wallet:
     relays:
         [
             [
-                { host: cardano-node-0.aws.iohkdev.io, port: 3000 },
-                { host: cardano-node-1.aws.iohkdev.io, port: 3000 },
-                { host: cardano-node-2.aws.iohkdev.io, port: 3000 },
-                { host: cardano-node-3.aws.iohkdev.io, port: 3000 },
-                { host: cardano-node-4.aws.iohkdev.io, port: 3000 },
-                { host: cardano-node-5.aws.iohkdev.io, port: 3000 },
-                { host: cardano-node-6.aws.iohkdev.io, port: 3000 }
+                { host: cardano-node-0.${DOMAIN}, port: 3000 },
+                { host: cardano-node-1.${DOMAIN}, port: 3000 },
+                { host: cardano-node-2.${DOMAIN}, port: 3000 },
+                { host: cardano-node-3.${DOMAIN}, port: 3000 },
+                { host: cardano-node-4.${DOMAIN}, port: 3000 },
+                { host: cardano-node-5.${DOMAIN}, port: 3000 },
+                { host: cardano-node-6.${DOMAIN}, port: 3000 }
             ]
         ]
-    valency: 3
-    fallbacks: 2" > "${TMP_TOPOLOGY_YAML}"
+    valency: 1
+    fallbacks: 7" > "${TMP_TOPOLOGY_YAML}"
 
 stack exec -- cardano-node                                  \
     --tlscert ./scripts/tls-files/server.crt                \
@@ -29,7 +31,7 @@ stack exec -- cardano-node                                  \
     --no-ntp                                                \
     --topology "${TMP_TOPOLOGY_YAML}"                       \
     --log-config scripts/log-templates/log-config-qa.yaml   \
-    --logs-prefix "logs/testnet-staging-0.6"                \
-    --db-path db-testnet-staging-0.6                        \
-    --wallet-db-path wdb-testnet-staging-0.6                \
+    --logs-prefix "logs/${CLUSTER}"                         \
+    --db-path db-${CLUSTER}                                 \
+    --wallet-db-path wdb-${CLUSTER}                         \
     --system-start "${SYSTEM_START_TIME}"

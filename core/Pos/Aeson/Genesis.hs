57bff964edb5e60f447fab70f0b6b7787fbf28e0
Ivan Gromakovskii <gie1994@gmail.com>
2017-09-13 20:53:13 +0300

    [CSL-1647] Define FromJSON instance for GenesisData

--- core/Pos/Aeson/Genesis.hs
+++ core/Pos/Aeson/Genesis.hs
@@ -8,10 +8,7 @@ import           Universum
 
 import           Data.Aeson             (FromJSON (..), withObject, (.:))
 import           Data.Aeson.TH          (deriveFromJSON)
-import qualified Data.ByteString        as BS
-import qualified Data.Text              as T
 import           Serokell.Aeson.Options (defaultOptions)
-import qualified Serokell.Util.Base64   as B64
 
 import           Pos.Aeson.Core         ()
 import           Pos.Aeson.Crypto       ()
@@ -21,21 +18,7 @@ import           Pos.Core.Genesis.Types (AvvmData, AvvmEntry (..), FakeAvvmOptio
                                          GenesisWStakeholders, ProtocolConstants,
                                          TestnetBalanceOptions, TestnetDistribution,
                                          convertAvvmDataToBalances)
-import           Pos.Crypto             (RedeemPublicKey, redeemPkBuild)
--- | Read the text into a redeeming public key.
---
--- There's also a copy of this function in cardano-addr-convert.
-fromAvvmPk :: (MonadFail m, Monad m) => Text -> m RedeemPublicKey
-fromAvvmPk addrText = do
-    let base64rify = T.replace "-" "+" . T.replace "_" "/"
-    let parsedM = B64.decode $ base64rify addrText
-    addrParsed <-
-        maybe (fail $ "Address " <> toString addrText <> " is not base64(url) format")
-        pure
-        (rightToMaybe parsedM)
-    unless (BS.length addrParsed == 32) $
-        fail "Address' length is not equal to 32, can't be redeeming pk"
-    pure $ redeemPkBuild addrParsed
+import           Pos.Crypto             (fromAvvmPk)
 
 instance FromJSON AvvmEntry where
     parseJSON = withObject "avvmEntry" $ \o -> do

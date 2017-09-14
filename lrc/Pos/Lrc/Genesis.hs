5788af91046b7845e347c3044e8460c6015b9a8f
Ilya Peresadin <pva701@gmail.com>
2017-09-14 15:09:53 +0300

    [CSL-1617] Use genesisSeed from spec

--- lrc/Pos/Lrc/Genesis.hs
+++ lrc/Pos/Lrc/Genesis.hs
@@ -2,11 +2,13 @@ module Pos.Lrc.Genesis
        ( genesisSeed
        ) where
 
-import           Pos.Core.Types (SharedSeed (..))
+import           Pos.Core.Genesis.Parser (genesisSpec)
+import           Pos.Core.Genesis.Types  (gsFtsSeed)
+import           Pos.Core.Types          (SharedSeed (..))
 
 -- | Seed that will be used for the 0th epoch. We must hardcode a seed
 -- because we need to somehow determine leaders for the first ever epoch
 -- (stakes are hardcoded as well so we can run FTS on those stakes using this
 -- seed).
 genesisSeed :: SharedSeed
-genesisSeed = SharedSeed "vasa opasa skovoroda Ggurda boroda provoda"
+genesisSeed = gsFtsSeed genesisSpec

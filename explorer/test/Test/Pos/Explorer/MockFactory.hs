9db6d9e09fe5ef0c9c4324e96df0ce3550ce5137
Jens Krause <email@jkrause.io>
2017-09-14 19:29:46 +0200

    [CSE-214] Introduce `MockFactory`

---
+++ explorer/test/Test/Pos/Explorer/MockFactory.hs
@@ -0,0 +1,29 @@
+
+-- | Factory to create mock objects
+-- | It might be merged with other factories or test utilities of others modules
+-- | such as `Test.Pos.Client.Txp.UtilSpec`.
+
+module Test.Pos.Explorer.MockFactory
+       ( mkTxOut
+       , secretKeyToAddress
+       ) where
+
+import           Universum
+
+import           Pos.Types                        (Address)
+
+import           Pos.Core                          (makePubKeyAddressBoot, unsafeIntegerToCoin)
+import           Pos.Crypto                        (SecretKey, toPublic)
+import           Pos.Txp                          (TxOut(..))
+
+-- | Factory to create `TxOut`
+-- | It is mostly taken from `makeTxOutAux` in `Test.Pos.Client.Txp.UtilSpec`
+mkTxOut :: Integer -> Address -> TxOut
+mkTxOut amount addr =
+    let coin = unsafeIntegerToCoin amount in
+    TxOut addr coin
+
+-- | Factory to create an `Address`
+-- | Friendly borrowed from `Test.Pos.Client.Txp.UtilSpec`
+secretKeyToAddress :: SecretKey -> Address
+secretKeyToAddress = makePubKeyAddressBoot . toPublic

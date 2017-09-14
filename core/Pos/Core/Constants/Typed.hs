95f47203e8f2ef7f416f2fe4cbdbfc4c1f940df5
Ivan Gromakovskii <gie1994@gmail.com>
2017-09-14 17:47:29 +0300

    Remove genesis BlockVersionData from 'CoreConfig'
    They are obtained from 'GenesisSpec' now.

--- core/Pos/Core/Constants/Typed.hs
+++ core/Pos/Core/Constants/Typed.hs
@@ -5,38 +5,12 @@ module Pos.Core.Constants.Typed
        (
          staticSysStart
        , staticBlkSecurityParam
-
-       -- * Genesis constants
-       , genesisBlockVersionData
-       , genesisScriptVersion
-       , genesisSlotDuration
-       , genesisMaxBlockSize
-       , genesisMaxHeaderSize
-       , genesisMaxTxSize
-       , genesisMpcThd
-       , genesisHeavyDelThd
-       , genesisUpdateVoteThd
-       , genesisMaxUpdateProposalSize
-       , genesisUpdateProposalThd
-       , genesisUpdateImplicit
-       , genesisSoftforkRule
-       , genesisUnlockStakeEpoch
        ) where
 
 import           Universum
 
-import           Data.Time.Units            (Millisecond, convertUnit)
-import           Serokell.Data.Memory.Units (Byte)
-import           Serokell.Util              (sec)
-
-import           Pos.Core.Constants.Raw     (CoreConfig (..), coreConfig,
-                                             staticSysStartRaw)
-import           Pos.Core.Fee               (TxFeePolicy)
-import           Pos.Core.Fee.Config        (ConfigOf (..))
-import           Pos.Core.Types             (BlockCount, BlockVersionData (..),
-                                             CoinPortion, EpochIndex (..), ScriptVersion,
-                                             SoftforkRule (..), Timestamp (..),
-                                             unsafeCoinPortionFromDouble)
+import           Pos.Core.Constants.Raw (CoreConfig (..), coreConfig, staticSysStartRaw)
+import           Pos.Core.Types         (BlockCount, Timestamp (..))
 
 ----------------------------------------------------------------------------
 -- Constants taken from the config
@@ -52,97 +26,3 @@ staticSysStart = Timestamp staticSysStartRaw
 -- runtime context (it can differ from this one).
 staticBlkSecurityParam :: BlockCount
 staticBlkSecurityParam = fromIntegral $ ccK coreConfig
-
-----------------------------------------------------------------------------
--- Genesis
-----------------------------------------------------------------------------
-
--- | 'BlockVersionData' for genesis 'BlockVersion'.
-genesisBlockVersionData :: BlockVersionData
-genesisBlockVersionData =
-    BlockVersionData
-    { bvdScriptVersion = genesisScriptVersion
-    , bvdSlotDuration = genesisSlotDuration
-    , bvdMaxBlockSize = genesisMaxBlockSize
-    , bvdMaxHeaderSize = genesisMaxHeaderSize
-    , bvdMaxTxSize = genesisMaxTxSize
-    , bvdMaxProposalSize = genesisMaxUpdateProposalSize
-    , bvdMpcThd = genesisMpcThd
-    , bvdHeavyDelThd = genesisHeavyDelThd
-    , bvdUpdateVoteThd = genesisUpdateVoteThd
-    , bvdUpdateProposalThd = genesisUpdateProposalThd
-    , bvdUpdateImplicit = genesisUpdateImplicit
-    , bvdSoftforkRule = genesisSoftforkRule
-    , bvdTxFeePolicy = genesisTxFeePolicy
-    , bvdUnlockStakeEpoch = genesisUnlockStakeEpoch
-    }
-
--- | ScriptVersion used at the very beginning
-genesisScriptVersion :: ScriptVersion
-genesisScriptVersion = 0
-
--- | Initial length of slot.
-genesisSlotDuration :: Millisecond
-genesisSlotDuration = convertUnit . sec $
-    ccGenesisSlotDurationSec coreConfig
-
--- | Initial block size limit.
-genesisMaxBlockSize :: Byte
-genesisMaxBlockSize = ccGenesisMaxBlockSize coreConfig
-
--- | Maximum size of a block header (in bytes)
-genesisMaxHeaderSize :: Byte
-genesisMaxHeaderSize = ccGenesisMaxHeaderSize coreConfig
-
--- | See 'Pos.CompileConfig.ccGenesisMaxTxSize'.
-genesisMaxTxSize :: Byte
-genesisMaxTxSize = ccGenesisMaxTxSize coreConfig
-
--- | See 'ccGenesisMaxUpdateProposalSize'.
-genesisMaxUpdateProposalSize :: Byte
-genesisMaxUpdateProposalSize =
-    ccGenesisMaxUpdateProposalSize coreConfig
-
--- | See 'Pos.CompileConfig.ccGenesisMpcThd'.
-genesisMpcThd :: CoinPortion
-genesisMpcThd = unsafeCoinPortionFromDouble $
-    ccGenesisMpcThd coreConfig
-
--- | See 'Pos.CompileConfig.ccGenesisHeavyDelThd'.
-genesisHeavyDelThd :: CoinPortion
-genesisHeavyDelThd = unsafeCoinPortionFromDouble $
-    ccGenesisHeavyDelThd coreConfig
-
--- | See 'ccGenesisUpdateVoteThd'.
-genesisUpdateVoteThd :: CoinPortion
-genesisUpdateVoteThd = unsafeCoinPortionFromDouble $
-    ccGenesisUpdateVoteThd coreConfig
-
--- | See 'ccGenesisUpdateProposalThd'.
-genesisUpdateProposalThd :: CoinPortion
-genesisUpdateProposalThd = unsafeCoinPortionFromDouble $
-    ccGenesisUpdateProposalThd coreConfig
-
--- | See 'ccGenesisUpdateImplicit'.
-genesisUpdateImplicit :: Integral i => i
-genesisUpdateImplicit = fromIntegral $
-    ccGenesisUpdateImplicit coreConfig
-
--- | Genesis softfork resolution rule.
-genesisSoftforkRule :: SoftforkRule
-genesisSoftforkRule =
-    SoftforkRule
-    { srMinThd =
-          unsafeCoinPortionFromDouble $ ccGenesisSoftforkMin coreConfig
-    , srInitThd =
-          unsafeCoinPortionFromDouble $ ccGenesisSoftforkInit coreConfig
-    , srThdDecrement =
-          unsafeCoinPortionFromDouble $ ccGenesisSoftforkDec coreConfig
-    }
-
-genesisTxFeePolicy :: TxFeePolicy
-genesisTxFeePolicy = getConfigOf (ccGenesisTxFeePolicy coreConfig)
-
-genesisUnlockStakeEpoch :: EpochIndex
-genesisUnlockStakeEpoch = EpochIndex $
-    ccGenesisUnlockStakeEpoch coreConfig
--- core/Pos/Core/Genesis.hs
+++ core/Pos/Core/Genesis.hs
@@ -1,45 +1,91 @@
 module Pos.Core.Genesis
        (
-       -- * Constants/devmode
-         genesisDevKeyPairs
+       -- * Data from 'GenesisSpec'
+         genesisProdAvvmBalances
+       , genesisProdInitializer
+       , genesisProdDelegation
+
+       -- ** Genesis BlockVersionData
+       , genesisBlockVersionData
+       , genesisMpcThd
+       , genesisHeavyDelThd
+       , genesisUpdateVoteThd
+       , genesisSlotDuration
+       , genesisMaxBlockSize
+
+       -- * Obsolete constants for dev mode
+       , genesisDevKeyPairs
        , genesisDevPublicKeys
        , genesisDevSecretKeys
        , genesisDevHdwSecretKeys
        , genesisDevFlatDistr
 
-       -- * /genesis-core.bin/
-       , module Pos.Core.Genesis.Types
-       , module Pos.Core.Genesis.Parser
-
-       -- ** Derived data
-       , genesisProdAvvmBalances
-       , genesisProdInitializer
-       , genesisProdDelegation
-
        -- * Utils
        , generateGenesisKeyPair
        , generateHdwGenesisSecretKey
+
+       -- * Re-exports
+       , module Pos.Core.Genesis.Types
+       , module Pos.Core.Genesis.Parser
        ) where
 
 import           Universum
 
-import qualified Data.Text               as T
-import           Formatting              (int, sformat, (%))
+import qualified Data.Text                  as T
+import           Data.Time.Units            (Millisecond)
+import           Formatting                 (int, sformat, (%))
+import           Serokell.Data.Memory.Units (Byte)
 
-import           Pos.Binary.Crypto       ()
-import           Pos.Core.Coin           (unsafeMulCoin)
-import           Pos.Core.Constants      (genesisKeysN)
-import           Pos.Core.Types          (mkCoin)
-import           Pos.Crypto.SafeSigning  (EncryptedSecretKey, emptyPassphrase,
-                                          safeDeterministicKeyGen)
-import           Pos.Crypto.Signing      (PublicKey, SecretKey, deterministicKeyGen)
+import           Pos.Binary.Crypto          ()
+import           Pos.Core.Coin              (unsafeMulCoin)
+import           Pos.Core.Constants         (genesisKeysN)
+import           Pos.Core.Types             (BlockVersionData (..), CoinPortion, mkCoin)
+import           Pos.Crypto.SafeSigning     (EncryptedSecretKey, emptyPassphrase,
+                                             safeDeterministicKeyGen)
+import           Pos.Crypto.Signing         (PublicKey, SecretKey, deterministicKeyGen)
 
 -- reexports
 import           Pos.Core.Genesis.Parser
 import           Pos.Core.Genesis.Types
 
 ----------------------------------------------------------------------------
--- Constants/development
+-- Data taken from 'GenesisSpec'
+----------------------------------------------------------------------------
+
+-- | Genesis avvm balances.
+genesisProdAvvmBalances :: GenesisAvvmBalances
+genesisProdAvvmBalances = gsAvvmDistr genesisSpec
+
+-- | Genesis initializer determines way of initialization
+-- utxo, bootstrap stakeholders, etc.
+genesisProdInitializer :: GenesisInitializer
+genesisProdInitializer = gsInitializer genesisSpec
+
+-- | 'GenesisDelegation' for production mode.
+genesisProdDelegation :: GenesisDelegation
+genesisProdDelegation = gsHeavyDelegation genesisSpec
+
+-- | Genesis 'BlockVersionData'.
+genesisBlockVersionData :: BlockVersionData
+genesisBlockVersionData = gsBlockVersionData genesisSpec
+
+genesisMpcThd :: CoinPortion
+genesisMpcThd = bvdMpcThd genesisBlockVersionData
+
+genesisHeavyDelThd :: CoinPortion
+genesisHeavyDelThd = bvdHeavyDelThd genesisBlockVersionData
+
+genesisUpdateVoteThd :: CoinPortion
+genesisUpdateVoteThd = bvdUpdateVoteThd genesisBlockVersionData
+
+genesisSlotDuration :: Millisecond
+genesisSlotDuration = bvdSlotDuration genesisBlockVersionData
+
+genesisMaxBlockSize :: Byte
+genesisMaxBlockSize = bvdMaxBlockSize genesisBlockVersionData
+
+----------------------------------------------------------------------------
+-- Obsolete constants for dev mode
 ----------------------------------------------------------------------------
 
 -- | List of pairs from 'SecretKey' with corresponding 'PublicKey'.
@@ -64,23 +110,6 @@ genesisDevFlatDistr :: BalanceDistribution
 genesisDevFlatDistr =
     FlatBalances genesisKeysN $
     mkCoin 10000 `unsafeMulCoin` (genesisKeysN :: Int)
-
-----------------------------------------------------------------------------
--- GenesisCore derived data, production
-----------------------------------------------------------------------------
-
--- | Genesis avvm balances.
-genesisProdAvvmBalances :: GenesisAvvmBalances
-genesisProdAvvmBalances = gsAvvmDistr genesisSpec
-
--- | Genesis initializer determines way of initialization
--- utxo, bootstrap stakeholders, etc.
-genesisProdInitializer :: GenesisInitializer
-genesisProdInitializer = gsInitializer genesisSpec
-
--- | 'GenesisDelegation' for production mode.
-genesisProdDelegation :: GenesisDelegation
-genesisProdDelegation = gsHeavyDelegation genesisSpec
 
 ----------------------------------------------------------------------------
 -- Utils

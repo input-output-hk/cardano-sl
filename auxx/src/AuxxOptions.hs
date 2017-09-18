2a44700c6200cf43f2e7d4fb5e47c3cb237dbc44
Ilya Peresadin <pva701@gmail.com>
2017-09-18 22:25:39 +0300

    [CSL-1617] Remove lwallet and add auxx

---
+++ auxx/src/AuxxOptions.hs
@@ -0,0 +1,96 @@
+{-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE QuasiQuotes   #-}
+
+-- | Command line options of cardano-auxx
+
+module AuxxOptions
+       ( AuxxOptions (..)
+       , AuxxAction (..)
+       , getAuxxOptions
+       ) where
+
+import           Universum
+
+import           Data.Version                 (showVersion)
+import           NeatInterpolation            (text)
+import           Options.Applicative          (CommandFields, Mod, Parser, command,
+                                               execParser, footerDoc, fullDesc, header,
+                                               help, helper, info, infoOption, long,
+                                               metavar, progDesc, subparser)
+import           Pos.Communication            (NodeId)
+import           Serokell.Util.OptParse       (strOption)
+import           Text.PrettyPrint.ANSI.Leijen (Doc)
+
+import           Paths_cardano_sl             (version)
+import qualified Pos.Client.CLI               as CLI
+
+----------------------------------------------------------------------------
+-- Types
+----------------------------------------------------------------------------
+
+data AuxxOptions = AuxxOptions
+    { aoAction         :: !AuxxAction
+    , aoCommonNodeArgs :: !CLI.CommonNodeArgs  -- ^ Common CLI args for nodes
+    , aoPeers          :: ![NodeId]
+    -- ^ Peers with which we want to communicate
+    --   TODO: we also have topology, so it can be redundant.
+    }
+
+data AuxxAction
+    = Repl
+    | Cmd { cmd :: !Text }
+
+----------------------------------------------------------------------------
+-- Parse action
+----------------------------------------------------------------------------
+
+actionParser :: Parser AuxxAction
+actionParser = subparser $ replParser <> cmdParser
+
+replParser :: Mod CommandFields AuxxAction
+replParser = command "repl" $ info (pure Repl) $
+             progDesc "Run REPL in console to evaluate the commands."
+
+cmdParser :: Mod CommandFields AuxxAction
+cmdParser = command "cmd" $ info opts desc
+  where opts = Cmd <$> strOption (long "commands"
+                               <> metavar "CMD"
+                               <> help "Commands to execute, comma-separated.")
+        desc = progDesc "Execute a list of predefined commands."
+
+----------------------------------------------------------------------------
+-- Parse everything
+----------------------------------------------------------------------------
+
+auxxOptionsParser :: Parser AuxxOptions
+auxxOptionsParser = do
+    aoAction <- actionParser
+    aoCommonNodeArgs <- CLI.commonNodeArgsParser
+    aoPeers <- many $ CLI.nodeIdOption "peer" "Address of a peer."
+    pure AuxxOptions {..}
+
+getAuxxOptions :: IO AuxxOptions
+getAuxxOptions = execParser programInfo
+  where
+    programInfo = info (helper <*> versionOption <*> auxxOptionsParser) $
+        fullDesc <> progDesc "Cardano SL CLI utilities."
+                 <> header "CLI-based utilities (auxx)."
+                 <> footerDoc usageExample
+
+    versionOption = infoOption
+        ("cardano-auxx-" <> showVersion version)
+        (long "version" <> help "Show version.")
+
+usageExample :: Maybe Doc
+usageExample = (Just . fromString @Doc . toString @Text) [text|
+Command example:
+
+  stack exec -- cardano-auxx                                     \
+    --db-path node-db0                                           \
+    --rebuild-db                                                 \
+    --json-log=/tmp/logs/2017-05-22_181224/node0.json            \
+    --logs-prefix /tmp/logs/2017-05-22_181224                    \
+    --log-config /tmp/logs/2017-05-22_181224/conf/node0.log.yaml \
+    --system-start 1495462345                                    \
+    --peer 127.0.0.1:3001                                        \
+    repl|]

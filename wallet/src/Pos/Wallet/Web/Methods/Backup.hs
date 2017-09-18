91763f604821d99132967a3153accbc1201a26db
Anupam Jain <ajnsit@gmail.com>
2017-09-19 02:27:14 +0530

    Fix indentation

--- wallet/src/Pos/Wallet/Web/Methods/Backup.hs
+++ wallet/src/Pos/Wallet/Web/Methods/Backup.hs
@@ -49,18 +49,17 @@ restoreWalletFromBackup WalletBackup {..} = do
 
             addSecretKey wbSecretKey
             if length accList > 0
-              then
-                for_ accList $ \(idx, meta) -> do
+                then for_ accList $ \(idx, meta) -> do
                     let aIdx = fromInteger $ fromIntegral idx
                         seedGen = DeterminedSeed aIdx
                     accId <- genUniqueAccountId seedGen wId
                     createAccount accId meta
-              -- If there are no existing accounts, then create one
-              else do
-                let idx = DeterminedSeed firstHardened
-                    accMeta = CAccountMeta { caName = "Initial account" }
-                    accInit = CAccountInit { caInitWId = wId, caInitMeta = accMeta }
-                () <$ L.newAccountIncludeUnready True idx emptyPassphrase accInit
+                -- If there are no existing accounts, then create one
+                else do
+                    let idx = DeterminedSeed firstHardened
+                        accMeta = CAccountMeta { caName = "Initial account" }
+                        accInit = CAccountInit { caInitWId = wId, caInitMeta = accMeta }
+                    () <$ L.newAccountIncludeUnready True idx emptyPassphrase accInit
 
             -- Restoring a wallet from backup may take a long time.
             -- Hence we mark the wallet as "not ready" until `syncWalletOnImport` completes.

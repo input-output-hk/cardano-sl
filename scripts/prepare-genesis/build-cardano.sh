1ac2584799ca21c32088ef726231fe6ad0c357d3
Jacob Mitchell <jacob.mitchell@iohk.io>
2017-09-20 16:01:36 -0700

    Set new target cardano branch for genesis

--- scripts/prepare-genesis/build-cardano.sh
+++ scripts/prepare-genesis/build-cardano.sh
@@ -3,7 +3,7 @@ set -ex
 
 REPO_URL="https://github.com/input-output-hk/cardano-sl.git"
 REPO_PATH="$HOME/cardano-sl"
-BRANCH="cardano-sl-1.0-launch-tools"
+BRANCH="cardano-sl-1.0-genesis-json2"
 
 START_TIME="1505930400"    # 18:00 UTC on 2017-09-20
 AVVM_SEED_COUNT="3"

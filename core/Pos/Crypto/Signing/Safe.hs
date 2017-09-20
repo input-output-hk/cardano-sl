79ffb55a013fec98af4a87706e1a93f05a319230
Ilya Peresadin <pva701@gmail.com>
2017-09-20 20:49:43 +0300

    [CSL-1617] Fix reexports warnings

--- core/Pos/Crypto/Signing/Safe.hs
+++ core/Pos/Crypto/Signing/Safe.hs
@@ -1,11 +1,7 @@
 -- | Module for safe (zero-memory) signing
 
 module Pos.Crypto.Signing.Safe
-       ( noPassEncrypt
-       , checkPassMatches
-       , changeEncPassphrase
-       , mkEncSecret
-       , mkEncSecretWithSalt
+       ( changeEncPassphrase
        , safeSign
        , safeToPublic
        , safeKeyGen
@@ -20,21 +16,21 @@ module Pos.Crypto.Signing.Safe
        , module Pos.Crypto.Signing.Types.Safe
        ) where
 
-import qualified Cardano.Crypto.Wallet as CC
-import           Crypto.Random         (MonadRandom, getRandomBytes)
-import qualified Data.ByteString       as BS
-import           Data.Coerce           (coerce)
+import qualified Cardano.Crypto.Wallet           as CC
+import           Crypto.Random                   (MonadRandom, getRandomBytes)
+import qualified Data.ByteString                 as BS
+import           Data.Coerce                     (coerce)
 import           Universum
 
-import           Pos.Binary.Class      (Bi, Raw)
-import qualified Pos.Binary.Class      as Bi
+import           Pos.Binary.Class                (Bi, Raw)
+import qualified Pos.Binary.Class                as Bi
 import           Pos.Core.Configuration.Protocol (HasProtocolConstants)
-import           Pos.Crypto.Hashing    (Hash, hash)
-import qualified Pos.Crypto.Scrypt     as S
-import           Pos.Crypto.Signing.Signing (ProxyCert (..), ProxySecretKey (..),
-                                             PublicKey (..), SecretKey (..), Signature (..),
-                                             sign, toPublic)
-import           Pos.Crypto.Signing.Tag (SignTag (SignProxySK), signTag)
+import           Pos.Crypto.Hashing              (Hash, hash)
+import qualified Pos.Crypto.Scrypt               as S
+import           Pos.Crypto.Signing.Signing      (ProxyCert (..), ProxySecretKey (..),
+                                                  PublicKey (..), SecretKey (..),
+                                                  Signature (..), sign, toPublic)
+import           Pos.Crypto.Signing.Tag          (SignTag (SignProxySK), signTag)
 import           Pos.Crypto.Signing.Types.Safe
 
 -- | Regerates secret key with new passphrase.

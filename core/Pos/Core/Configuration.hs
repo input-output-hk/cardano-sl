b1dfc5fbb6a140edaefe84ed4c75e5db4862c781
Ivan Gromakovskii <gie1994@gmail.com>
2017-09-20 16:11:13 +0300

    [CSL-1678] Make genesis hash part of configuration
    Previously it was hardcoded as 'genesisDataDigest', but it wasn't convenient
    for some people. Now this hash is taken from configuration file if 'genesis'
    is specified as 'src', not as 'spec'.
    For format see changes in 'configuration.mainnet.yaml' file.

--- core/Pos/Core/Configuration.hs
+++ core/Pos/Core/Configuration.hs
@@ -13,13 +13,13 @@ module Pos.Core.Configuration
 
 import           Universum
 
-import qualified Crypto.Hash                             as Hash
 import qualified Data.ByteString                         as BS
 import qualified Data.ByteString.Lazy                    as BSL
 import           Formatting                              (sformat, shown, (%))
 import           System.Wlog                             (WithLogger, logInfo)
 import qualified Text.JSON.Canonical                     as Canonical
 
+import           Pos.Binary.Class                        (Raw)
 import           Pos.Core.Coin                           (coinToInteger)
 import           Pos.Core.Configuration.BlockVersionData as E
 import           Pos.Core.Configuration.Core             as E
@@ -27,7 +27,6 @@ import           Pos.Core.Configuration.GeneratedSecrets as E
 import           Pos.Core.Configuration.GenesisData      as E
 import           Pos.Core.Configuration.GenesisHash      as E
 import           Pos.Core.Configuration.Protocol         as E
-import           Pos.Core.Constants                      (genesisDataDigest)
 import           Pos.Core.Genesis.Canonical              ()
 import           Pos.Core.Genesis.Generate               (GeneratedGenesisData (..),
                                                           generateGenesisData)
@@ -36,7 +35,7 @@ import           Pos.Core.Genesis.Types                  (GenesisData (..),
                                                           GenesisSpec (..),
                                                           getGenesisAvvmBalances)
 import           Pos.Core.Types                          (Coin, Timestamp)
-import           Pos.Crypto.Hashing                      (AbstractHash (..), unsafeHash)
+import           Pos.Crypto.Hashing                      (Hash, hashRaw, unsafeHash)
 
 -- | Coarse catch-all configuration constraint for use by depending modules.
 type HasConfiguration =
@@ -48,10 +47,10 @@ type HasConfiguration =
     , HasProtocolConstants
     )
 
-canonicalGenesisJson :: GenesisData -> (BSL.ByteString, Hash.Digest Hash.Blake2b_256)
+canonicalGenesisJson :: GenesisData -> (BSL.ByteString, Hash Raw)
 canonicalGenesisJson theGenesisData = (canonicalJsonBytes, jsonHash)
   where
-    jsonHash = Hash.hash $ BSL.toStrict canonicalJsonBytes
+    jsonHash = hashRaw $ BSL.toStrict canonicalJsonBytes
     canonicalJsonBytes = Canonical.renderCanonicalJSON $ runIdentity $ Canonical.toJSON theGenesisData
 
 -- | Come up with a HasConfiguration constraint using a Configuration.
@@ -60,7 +59,7 @@ canonicalGenesisJson theGenesisData = (canonicalJsonBytes, jsonHash)
 -- the canonical JSON encoding of a mainnet genesis.
 --
 -- If the canonical JSON source is given, then it will be hashed and checked
--- against the constant expected genesisDataDigest.
+-- against expected hash (which is also part of configuration in this case).
 --
 -- If the configuration gives a testnet genesis spec, then a start time must
 -- be provided, probably sourced from command line arguments.
@@ -78,9 +77,9 @@ withCoreConfigurations
     -> m r
 withCoreConfigurations conf@CoreConfiguration{..} mSystemStart act = case ccGenesis of
     -- If a 'GenesisData' source file is given, we check its hash against the
-    -- built-in constant, parse it, and use the GenesisData to fill in all of]
+    -- given expected hash, parse it, and use the GenesisData to fill in all of
     -- the obligations.
-    GCSrc fp -> do
+    GCSrc fp expectedHash -> do
         !bytes <- liftIO $ BS.readFile fp
 
         gdataJSON <- case Canonical.parseCanonicalJSON (BSL.fromStrict bytes) of
@@ -92,17 +91,15 @@ withCoreConfigurations conf@CoreConfiguration{..} mSystemStart act = case ccGene
             Right it -> return it
 
         let (_, theGenesisHash) = canonicalGenesisJson theGenesisData
-        when (theGenesisHash /= genesisDataDigest) $
-            throwM $ GenesisHashMismatch (show theGenesisHash) (show genesisDataDigest)
-        -- See Pos.Core.Constants. The genesisHash is conceptually distinct
-        -- from the genesisDataDigest, they just happen to coincide for
-        -- mainnet.
+        when (theGenesisHash /= expectedHash) $
+            throwM $ GenesisHashMismatch
+                     (show theGenesisHash) (show expectedHash)
 
         withCoreConfiguration conf $
             withProtocolConstants (gdProtocolConsts theGenesisData) $
             withGenesisBlockVersionData (gdBlockVersionData theGenesisData) $
             withGenesisData theGenesisData $
-            withGenesisHash (AbstractHash theGenesisHash) $
+            withGenesisHash theGenesisHash $
             withGeneratedSecrets Nothing $
             act
 
@@ -130,7 +127,7 @@ withGenesisSpec
     -> (HasConfiguration => r)
     -> r
 withGenesisSpec theSystemStart conf@CoreConfiguration{..} val = case ccGenesis of
-    GCSrc _ -> error "withGenesisSpec called with GCSrc"
+    GCSrc {} -> error "withGenesisSpec called with GCSrc"
     GCSpec spec ->
         withProtocolConstants (gsProtocolConstants spec) $
         withGenesisBlockVersionData (gsBlockVersionData spec) $

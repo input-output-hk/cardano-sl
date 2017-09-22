2856ee028171d9032c489dfebeab1b17a22fc009
George Agapov <george.agapov@gmail.com>
2017-09-22 14:25:16 +0300

    [CSL-1666] Finally get rid of dev-mode flag in cardano-sl-core

--- scripts/bench/buildbench.sh
+++ scripts/bench/buildbench.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-# dev-mode : allow to generate custom genesis block
-# CONFIG=.. selects section from config file (core/constants.yaml)
-
-stack build --flag cardano-sl-core:dev-mode --flag cardano-sl-core:-asserts cardano-sl cardano-sl-auxx cardano-sl-wallet cardano-sl-explorer
+stack build --flag cardano-sl-core:-asserts cardano-sl cardano-sl-auxx cardano-sl-wallet cardano-sl-explorer

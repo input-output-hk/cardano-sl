c505858e205e802bb003007c0d5e129ad15f70a8
Jens Krause <email@jkrause.io>
2017-09-25 11:24:09 +0200

    [CSE-2214] Add test for `regroupBySnd`

--- explorer/test/Test/Pos/Explorer/Socket/UtilSpec.hs
+++ explorer/test/Test/Pos/Explorer/Socket/UtilSpec.hs
@@ -7,7 +7,11 @@ module Test.Pos.Explorer.Socket.UtilSpec
 
 import           Universum
 
-import           Test.Hspec (Spec, describe, it, shouldBe)
+import qualified Data.Map                   as M
+import qualified Data.IntSet                as IS
+import           Test.Hspec                 (Spec, describe, it, shouldBe)
+import           Pos.Explorer.Socket.Util   (regroupBySnd)
+
 
 
 ----------------------------------------------------------------------------
@@ -20,5 +24,17 @@ spec :: Spec
 spec =
     describe "Util" $
         describe "regroupBySnd" $
-            it "has to be tested" $
-                True `shouldBe` True
+            it "re-grouped a list by second value of its elements" $ do
+                let result = regroupBySnd [ (IS.singleton 1, "1" :: Text)
+                                          , (IS.singleton 2, "1" :: Text)
+                                          , (IS.singleton 3, "2" :: Text)
+                                          , (IS.singleton 4, "3" :: Text)
+                                          , (IS.singleton 5, "3" :: Text)
+                                          ]
+                    expected =  M.fromList  [ ('1', [ IS.singleton 2
+                                                    , IS.singleton 1 ])
+                                            , ('2', [ IS.singleton 3 ])
+                                            , ('3', [ IS.singleton 5
+                                                    , IS.singleton 4 ])
+                                            ]
+                result `shouldBe` expected

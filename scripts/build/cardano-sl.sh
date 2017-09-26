14b95dce612c7ac429aaf8df2219db0d975bec11
Mikhail Volkhov <volhovm.cs@gmail.com>
2017-09-26 18:23:01 +0300

    [CSL-1563] Improve cardano-sl.sh build targets dispatching

--- scripts/build/cardano-sl.sh
+++ scripts/build/cardano-sl.sh
@@ -124,10 +124,10 @@ do
     explorer=false
   # project name = build only the project
   # (for “godtossing” we allow “gt” as an alias)
-  elif [[ $var == "sl" ]]; then
-    spec_prj="sl"
-  elif [[ $var == "sl+" ]]; then
-    spec_prj="sl+"
+  elif [[ $var == "sl" ]] || [[ $var == "sl+" ]] || [[ $var == "all" ]]; then
+    spec_prj="all"
+  elif [[ $var == "lib" ]]; then
+    spec_prj="lib"
   elif [[ $var == "gt" ]]; then
     spec_prj="godtossing"
   elif [[ $var == "auxx" ]]; then
@@ -236,18 +236,20 @@ if [[ $spec_prj == "" ]]; then
     to_build="$to_build cardano-sl-$prj"
   done
 
-  to_build="$to_build cardano-sl cardano-sl-auxx cardano-sl-tools cardano-sl-wallet cardano-sl-explorer"
+  to_build="$to_build cardano-sl cardano-sl-auxx cardano-sl-tools cardano-sl-wallet cardano-sl-explorer cardano-sl-node"
 
-elif [[ $spec_prj == "sl" ]]; then
+elif [[ $spec_prj == "lib" ]]; then
   to_build="cardano-sl"
+elif [[ $spec_prj == "node" ]]; then
+  to_build="cardano-sl-node"
 elif [[ $spec_prj == "auxx" ]]; then
   to_build="cardano-sl-auxx"
 elif [[ $spec_prj == "wallet" ]]; then
   to_build="cardano-sl-wallet"
 elif [[ $spec_prj == "explorer" ]]; then
   to_build="cardano-sl-explorer"
-elif [[ $spec_prj == "sl+" ]]; then
-  to_build="cardano-sl cardano-sl-auxx cardano-sl-tools cardano-sl-explorer cardano-sl-wallet "
+elif [[ $spec_prj == "all" ]]; then
+  to_build="" # build everything concurrently
 else
   to_build="cardano-sl-$spec_prj"
 fi
@@ -264,7 +266,11 @@ if [[ $to_build == *"explorer"* && $explorer == false ]]; then
   exit
 fi
 
-echo "Going to build: $to_build"
+if [[ $to_build == "" ]]; then
+  echo "Going to build: everything, concurrently"
+else
+  echo "Going to build: $to_build"
+fi
 echo "'wallet' flag: $wallet"
 echo "'explorer' flag: $explorer"
 
@@ -290,6 +296,14 @@ for prj in $to_build; do
     | perl -pe "$xperl"                     \
     | { grep -E --color "$xgrep" || true; }
 done
+
+if [[ $to_build == "" ]]; then
+  sbuild="stack build --ghc-options=\"$ghc_opts\" $commonargs $norun $fast $args"
+  echo -e "$sbuild\n"
+  eval $sbuild 2>&1                         \
+    | perl -pe "$xperl"                     \
+    | { grep -E --color "$xgrep" || true; }
+fi
 
 if [[ $test == true ]]; then
   stack build                               \

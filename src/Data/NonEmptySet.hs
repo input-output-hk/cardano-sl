a02f823cb8ce9eb552e896394b73d8cfea01fa39
Andrzej Rybczak <andrzej@rybczak.net>
2017-10-17 21:38:07 +0200

    Make NonEmptySet stricter so it doesn't leak space

--- src/Data/NonEmptySet.hs
+++ src/Data/NonEmptySet.hs
@@ -15,28 +15,28 @@ import           Data.List.NonEmpty (NonEmpty((:|)))
 import           Data.Foldable (foldrM)
 
 -- | A set (no duplicates) with at least one element.
-newtype NonEmptySet t = NonEmptySet (t, Set t)
+data NonEmptySet t = NonEmptySet !t !(Set t)
 
 instance Show t => Show (NonEmptySet t) where
     show = show . toList
 
 -- | Construct a 'NonEmptySet' by giving one element.
 singleton :: t -> NonEmptySet t
-singleton t = NonEmptySet (t, S.empty)
+singleton t = NonEmptySet t S.empty
 
 -- | Extend a 'NonEmptySet' by adding one element.
 insert :: Ord t => t -> NonEmptySet t -> NonEmptySet t
-insert t (NonEmptySet (t', set)) = case t `compare` t' of
-    LT -> NonEmptySet (t, S.insert t' set)
-    _ -> NonEmptySet (t', S.insert t set)
+insert t (NonEmptySet t' set) = case t `compare` t' of
+    LT -> NonEmptySet t (S.insert t' set)
+    _  -> NonEmptySet t' (S.insert t set)
 
 -- | Remove an element from a 'NonEmptySet'. If it's the only element of the
 --   set, 'Nothing' is given.
 delete :: Ord t => t -> NonEmptySet t -> Maybe (NonEmptySet t)
-delete t (NonEmptySet (t', set)) = case (t == t', S.minView set) of
-    (False, _) -> Just $ NonEmptySet (t', S.delete t set)
+delete t (NonEmptySet t' set) = case (t == t', S.minView set) of
+    (False, _) -> Just $ NonEmptySet t' (S.delete t set)
     (True, Nothing) -> Nothing
-    (True, Just (t'', set')) -> Just (NonEmptySet (t'', set'))
+    (True, Just (t'', set')) -> Just $ NonEmptySet t'' set'
 
 deleteMany :: Ord t => [t] -> NonEmptySet t -> Maybe (NonEmptySet t)
 deleteMany ts neset = foldrM delete neset ts
@@ -44,4 +44,4 @@ deleteMany ts neset = foldrM delete neset ts
 -- | Forget the fact that a 'NonEmptySet' and construct a 'NonEmpty' list, in
 --   which duplicates *are* allowed.
 toList :: NonEmptySet t -> NonEmpty t
-toList (NonEmptySet (t, set)) = t :| S.toList set
+toList (NonEmptySet t set) = t :| S.toList set

0de408b4c871db49a26c1be845a9d542d0fac14d
Ilya Peresadin <pva701@gmail.com>
2017-10-21 19:59:24 +0300

    Remove outdated Pos.Wallet from lib

--- lib/src/Pos/Wallet.hs
+++
@@ -1,7 +0,0 @@
-module Pos.Wallet
-       ( module Pos.Wallet.KeyStorage
-       , module Pos.Wallet.WalletMode
-       ) where
-
-import           Pos.Wallet.KeyStorage
-import           Pos.Wallet.WalletMode

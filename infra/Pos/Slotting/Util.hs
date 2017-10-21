885b4e4665bbf9ebde850ba4fa354b1f07cb5734
Martoon <martoon.hsk@gmail.com>
2017-10-21 17:45:52 +0300

    [CSM-491] Add timeout reports in blistener

--- infra/Pos/Slotting/Util.hs
+++ infra/Pos/Slotting/Util.hs
@@ -7,6 +7,7 @@ module Pos.Slotting.Util
        , getSlotStart
        , getSlotStartPure
        , getSlotStartEmpatically
+       , getCurrentEpochSlotDuration
        , getNextEpochSlotDuration
        , slotFromTimestamp
 
@@ -79,6 +80,13 @@ getSlotStartEmpatically
     -> m Timestamp
 getSlotStartEmpatically slot =
     getSlotStart slot >>= maybeThrow (SEUnknownSlotStart slot)
+
+-- | Get current slot duration.
+getCurrentEpochSlotDuration
+    :: (MonadSlotsData ctx m)
+    => m Millisecond
+getCurrentEpochSlotDuration =
+    esdSlotDuration . fst <$> getCurrentNextEpochSlottingDataM
 
 -- | Get last known slot duration.
 getNextEpochSlotDuration

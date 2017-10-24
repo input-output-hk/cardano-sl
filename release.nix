c2d383c96d33ceb31550ad74c9428481165a1a12
Michael Bishop <cleverca22@gmail.com>
2017-10-24 10:51:36 -0300

    [DEVOPS-404] cardano-1.0 docker image (#1716)
    * [DEVOPS-404] cardano-1.0 docker image
    
    * change the network to staging
    
    * store secrets in /wallet/, expose wallet port, document how to start
    
    * auto-generate the ssl certificate on first startup, and document how to
    extract it
    
    * un-prefix the state directories

--- release.nix
+++ release.nix
@@ -25,5 +25,6 @@ let
     cardano-report-server-static = [ "x86_64-linux" ];
     stack2nix = supportedSystems;
     purescript = supportedSystems;
+    dockerImage = [ "x86_64-linux" ];
   };
 in mapTestOn platforms

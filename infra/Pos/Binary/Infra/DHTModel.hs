3858a891fa19b3b5f6a431299456d11870fb801d
Mikhail Volkhov <volhovm.cs@gmail.com>
2017-10-26 11:16:54 +0300

    [CSL-1399] Update binary infra/txp

--- infra/Pos/Binary/Infra/DHTModel.hs
+++ infra/Pos/Binary/Infra/DHTModel.hs
@@ -5,51 +5,52 @@ module Pos.Binary.Infra.DHTModel () where
 
 import           Universum
 
-import           Network.Kademlia.HashNodeId (HashId (..))
 import           Network.Kademlia            as K
+import           Network.Kademlia.HashNodeId (HashId (..))
+import           Network.Kademlia.Instance   (BanState)
 import           Network.Kademlia.Tree       as K
 import           Network.Kademlia.Types      as K
-import           Network.Kademlia.Instance   (BanState)
 
-import           Pos.Binary.Class            (Bi (..), encodeListLen, enforceSize, genericEncode, genericDecode)
+import           Pos.Binary.Class            (Bi (..), encodeListLen, enforceSize,
+                                              genericDecode, genericEncode)
 import           Pos.DHT.Model.Types         (DHTData (..), DHTKey (..))
 
 instance Bi DHTKey where
-  encode (DHTKey (HashId bs)) = encode bs
-  decode = DHTKey . HashId <$> decode
+    encode (DHTKey (HashId bs)) = encode bs
+    decode = DHTKey . HashId <$> decode
 
 instance Bi DHTData where
-  encode (DHTData unit) = encode unit
-  decode = DHTData <$> decode
+    encode (DHTData unit) = encode unit
+    decode = DHTData <$> decode
 
 -- CSL-1296: Orphan (inefficient) Kademlia instances.
 
 instance Bi PingInfo where
-  encode = genericEncode
-  decode = genericDecode
+    encode = genericEncode
+    decode = genericDecode
 
 instance Bi i => Bi (K.Node i) where
-  encode = genericEncode
-  decode = genericDecode
+    encode = genericEncode
+    decode = genericDecode
 
 instance Bi BanState where
-  encode = genericEncode
-  decode = genericDecode
+    encode = genericEncode
+    decode = genericDecode
 
 instance Bi K.Peer where
-  encode p = encodeListLen 2 <> encode (K.peerHost p) <> encode (K.unwrapPort . K.peerPort $ p)
-  decode   = do
-    enforceSize "Kademlia.Peer" 2
-    K.Peer <$> decode <*> (K.wrapPort <$> decode)
+    encode p = encodeListLen 2 <> encode (K.peerHost p) <> encode (K.unwrapPort . K.peerPort $ p)
+    decode   = do
+      enforceSize "Kademlia.Peer" 2
+      K.Peer <$> decode <*> (K.wrapPort <$> decode)
 
 instance Bi i => (Bi (K.NodeTreeElem i)) where
-  encode = genericEncode
-  decode = genericDecode
+    encode = genericEncode
+    decode = genericDecode
 
 instance Bi i => Bi (K.NodeTree i) where
-  encode = genericEncode
-  decode = genericDecode
+    encode = genericEncode
+    decode = genericDecode
 
 instance Bi i => Bi (K.KademliaSnapshot i) where
-  encode = genericEncode
-  decode = genericDecode
+    encode = genericEncode
+    decode = genericDecode
--- infra/Pos/Binary/Infra/Relay.hs
+++ infra/Pos/Binary/Infra/Relay.hs
@@ -5,26 +5,26 @@ module Pos.Binary.Infra.Relay
 import           Universum
 
 import           Pos.Binary.Class              (Bi (..))
-import           Pos.Communication.Types.Relay (InvMsg (..), MempoolMsg (..),
-                                                ReqMsg (..), ResMsg (..))
+import           Pos.Communication.Types.Relay (InvMsg (..), MempoolMsg (..), ReqMsg (..),
+                                                ResMsg (..))
 
 instance Bi key => Bi (InvMsg key) where
-  encode = encode . imKey
-  decode = InvMsg <$> decode
+    encode = encode . imKey
+    decode = InvMsg <$> decode
 
 instance Bi key => Bi (ReqMsg key) where
-  encode = encode . rmKey
-  decode = ReqMsg <$> decode
+    encode = encode . rmKey
+    decode = ReqMsg <$> decode
 
 instance Bi key => Bi (ResMsg key) where
-  encode (ResMsg {..}) = encode (resKey, resOk)
-  decode = uncurry ResMsg <$> decode
+    encode (ResMsg {..}) = encode (resKey, resOk)
+    decode = uncurry ResMsg <$> decode
 
 instance Typeable tag => Bi (MempoolMsg tag) where
-  -- The extra byte is needed because time-warp doesn't work with
-  -- possibly-empty messages. 228 was chosen as homage to @pva701
-  encode MempoolMsg = encode (228 :: Word8)
-  decode = do
-    x <- decode @Word8
-    when (x /= 228) $ fail "wrong byte"
-    pure MempoolMsg
+    -- The extra byte is needed because time-warp doesn't work with
+    -- possibly-empty messages. 228 was chosen as homage to @pva701
+    encode MempoolMsg = encode (228 :: Word8)
+    decode = do
+      x <- decode @Word8
+      when (x /= 228) $ fail "wrong byte"
+      pure MempoolMsg

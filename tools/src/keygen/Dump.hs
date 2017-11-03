df5acc7f2a49b53d6df362050515381209a3e81c
Ivan Gromakovskii <gie1994@gmail.com>
2017-11-03 22:19:10 +0300

    [CSL-1807] Extend cardano-keygen to dump delegation issuers' keys
    I also changed numbering of generated secrets to start from 0, not from 1.
    The similar change is going to be merged to cardano-sl-1.0 also.

--- tools/src/keygen/Dump.hs
+++ tools/src/keygen/Dump.hs
@@ -9,8 +9,8 @@ module Dump
 import           Universum
 
 import           Control.Lens           ((?~))
-import           Crypto.Random          (MonadRandom)
 import qualified Data.Text              as T
+import           Serokell.Util          (enumerate)
 import qualified Serokell.Util.Base64   as B64
 import           System.Directory       (createDirectoryIfMissing)
 import           System.FilePath        ((</>))
@@ -18,7 +18,7 @@ import           System.Wlog            (WithLogger, logInfo)
 
 import           Pos.Core.Configuration (HasGeneratedSecrets, generatedSecrets)
 import           Pos.Core.Genesis       (GeneratedSecrets (..), RichSecrets (..))
-import           Pos.Crypto             (EncryptedSecretKey)
+import           Pos.Crypto             (EncryptedSecretKey, SecretKey)
 import           Pos.Util.UserSecret    (UserSecret, initializeUserSecret,
                                          mkGenesisWalletUserSecret, takeUserSecret,
                                          usKeys, usPrimKey, usVss, usWallet,
@@ -28,8 +28,15 @@ import           Pos.Util.UserSecret    (UserSecret, initializeUserSecret,
 -- Dump individual secrets
 ----------------------------------------------------------------------------
 
+dumpDlgIssuerSecret
+    :: (MonadIO m, MonadThrow m, WithLogger m)
+    => FilePath
+    -> SecretKey
+    -> m ()
+dumpDlgIssuerSecret fp sk = dumpUserSecret fp $ usPrimKey .~ Just sk
+
 dumpRichSecrets
-    :: (MonadIO m, MonadThrow m, WithLogger m, MonadRandom m)
+    :: (MonadIO m, MonadThrow m, WithLogger m)
     => FilePath
     -> RichSecrets
     -> m ()
@@ -40,7 +47,7 @@ dumpRichSecrets fp RichSecrets {..} =
                         ]
 
 dumpPoorSecret
-    :: (MonadIO m, MonadThrow m, WithLogger m, MonadRandom m)
+    :: (MonadIO m, MonadThrow m, WithLogger m)
     => FilePath
     -> EncryptedSecretKey
     -> m ()
@@ -58,35 +65,42 @@ dumpFakeAvvmSeed fp seed = writeFile fp (B64.encode seed)
 ----------------------------------------------------------------------------
 
 dumpGeneratedGenesisData
-    :: (MonadIO m, WithLogger m, MonadThrow m, MonadRandom m, HasGeneratedSecrets)
+    :: (MonadIO m, WithLogger m, MonadThrow m, HasGeneratedSecrets)
     => (FilePath, FilePath)
     -> m ()
 dumpGeneratedGenesisData (dir, pat) = do
     let GeneratedSecrets {..} =
             fromMaybe (error "GeneratedSecrets are unknown") generatedSecrets
-    dumpKeyfiles (dir, pat) gsRichSecrets gsPoorSecrets
+    dumpKeyfiles (dir, pat) gsDlgIssuersSecrets gsRichSecrets gsPoorSecrets
     dumpFakeAvvmSeeds dir gsFakeAvvmSeeds
 
 dumpKeyfiles
-    :: (MonadIO m, MonadThrow m, WithLogger m, MonadRandom m)
+    :: (MonadIO m, MonadThrow m, WithLogger m)
     => (FilePath, FilePath) -- directory and key-file pattern
+    -> [SecretKey]
     -> [RichSecrets]
     -> [EncryptedSecretKey]
     -> m ()
-dumpKeyfiles (dir, pat) richs poors = do
+dumpKeyfiles (dir, pat) dlgIssuers richs poors = do
     let keysDir = dir </> "generated-keys"
+    let dlgIssuersDir = keysDir </> "dlg-issuers"
     let richDir = keysDir </> "rich"
     let poorDir = keysDir </> "poor"
     logInfo $ "Dumping generated genesis secrets into " <> fromString keysDir
-    liftIO $ createDirectoryIfMissing True richDir
-    liftIO $ createDirectoryIfMissing True poorDir
+    mapM_ (liftIO . createDirectoryIfMissing True)
+        [ dlgIssuersDir
+        , richDir
+        , poorDir
+        ]
 
-    let totalSecrets = length richs + length poors
+    let totalSecrets = length dlgIssuers + length richs + length poors
 
     let patternize = applyPattern @Int pat
-    forM_ (zip richs [1 .. ]) $ \(richSecrets, i) ->
+    forM_ (enumerate dlgIssuers) $ \(i, sk) ->
+        dumpDlgIssuerSecret (dlgIssuersDir </> patternize i) sk
+    forM_ (enumerate richs) $ \(i, richSecrets) ->
         dumpRichSecrets (richDir </> patternize i) richSecrets
-    forM_ (zip poors [1 .. ]) $ \(hdwSk, i) ->
+    forM_ (enumerate poors) $ \(i, hdwSk) ->
         dumpPoorSecret (poorDir </> patternize i) hdwSk
 
     logInfo $ show totalSecrets <> " keyfiles are generated"
@@ -102,7 +116,7 @@ dumpFakeAvvmSeeds dir seeds = do
     liftIO $ createDirectoryIfMissing True keysDir
     let faoCount = length seeds
 
-    forM_ (zip seeds [1 .. faoCount]) $ \(seed, i) ->
+    forM_ (enumerate seeds) $ \(i :: Int, seed) ->
         dumpFakeAvvmSeed (keysDir </> ("fake-" <> show i <> ".seed")) seed
 
     logInfo (show faoCount <> " fake avvm seeds are generated")
@@ -112,7 +126,7 @@ dumpFakeAvvmSeeds dir seeds = do
 ----------------------------------------------------------------------------
 
 dumpUserSecret
-    :: (MonadIO m, MonadThrow m, WithLogger m, MonadRandom m)
+    :: (MonadIO m, MonadThrow m, WithLogger m)
     => FilePath
     -> (UserSecret -> UserSecret)
     -> m ()

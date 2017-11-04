bcdecc2dbbeeedc3931599ea427b8ca65b1c07be
Mikhail Volkhov <volhovm.cs@gmail.com>
2017-11-04 16:53:41 +0300

    [CSL-1732] Restructure Reporting/Methods

--- infra/Pos/Reporting/Methods.hs
+++ infra/Pos/Reporting/Methods.hs
@@ -61,170 +61,9 @@ import           Pos.Reporting.MemState (HasLoggerConfig (..), HasReportServers 
 import           Pos.Util.CompileInfo (HasCompileInfo, compileInfo)
 import           Pos.Util.Util (maybeThrow, withSystemTempFile, (<//>))
 
-type MonadReporting ctx m =
-       ( MonadIO m
-       , MonadMask m
-       , MonadReader ctx m
-       , MonadFormatPeers m
-       , HasReportingContext ctx
-       , HasNodeType ctx
-       , WithLogger m
-       , HasConfiguration
-       , HasCompileInfo
-       )
 
 ----------------------------------------------------------------------------
--- Node-specific
-----------------------------------------------------------------------------
-
--- | Sends node's logs, taking 'LoggerConfig' from 'NodeContext',
--- retrieving all logger files from it. List of servers is also taken
--- from node's configuration.
-sendReportNode
-    :: (MonadReporting ctx m)
-    => ReportType -> m ()
-sendReportNode reportType = do
-    noServers <- null <$> view (reportingContext . reportServers)
-    if noServers
-        then onNoServers
-        else do
-            logConfig <- view (reportingContext . loggerConfig)
-            let allFiles = map snd $ retrieveLogFiles logConfig
-            logFile <-
-                maybeThrow
-                    NoPubFiles
-                    (head $ filter (".pub" `isSuffixOf`) allFiles)
-            logContent <-
-                takeGlobalSize charsConst <$>
-                retrieveLogContent logFile (Just 5000)
-            sendReportNodeImpl (reverse logContent) reportType
-  where
-    -- 2 megabytes, assuming we use chars which are ASCII mostly
-    charsConst :: Int
-    charsConst = 1024 * 1024 * 2
-    takeGlobalSize :: Int -> [Text] -> [Text]
-    takeGlobalSize _ [] = []
-    takeGlobalSize curLimit (t:xs) =
-        let delta = curLimit - length t
-        in bool [] (t : (takeGlobalSize delta xs)) (delta > 0)
-    onNoServers =
-        logInfo $
-        "sendReportNode: not sending report " <>
-        "because no reporting servers are specified"
-
--- | Same as 'sendReportNode', but doesn't attach any logs.
-sendReportNodeNologs :: (MonadReporting ctx m) => ReportType -> m ()
-sendReportNodeNologs = sendReportNodeImpl []
-
-sendReportNodeImpl
-    :: (MonadReporting ctx m)
-    => [Text] -> ReportType -> m ()
-sendReportNodeImpl rawLogs reportType = do
-    servers <- view (reportingContext . reportServers)
-    when (null servers) onNoServers
-    errors <- fmap lefts $ forM servers $ try .
-        sendReport [] rawLogs reportType "cardano-node" . toString
-    whenNotNull errors $ throwSE . NE.head
-  where
-    onNoServers =
-        logInfo $ "sendReportNodeImpl: not sending report " <>
-                  "because no reporting servers are specified"
-    throwSE (e :: SomeException) = throwM e
-
--- checks if ipv4 is from local range
-ipv4Local :: Word32 -> Bool
-ipv4Local w =
-    or [b1 == 10, b1 == 172 && b2 >= 16 && b2 <= 31, b1 == 192 && b2 == 168]
-  where
-    b1 = w .&. 0xff
-    b2 = (w `shiftR` 8) .&. 0xff
-
--- | Retrieves node info that we would like to know when analyzing
--- malicious behavior of node.
-getNodeInfo :: (MonadIO m, MonadFormatPeers m) => m Text
-getNodeInfo = do
-    peersText <- fromMaybe "unknown" <$> formatKnownPeers sformat
-    (ips :: [Text]) <-
-        map show . filter ipExternal . map ipv4 <$>
-        liftIO getNetworkInterfaces
-    pure $ sformat outputF (pretty $ listBuilderJSON ips) peersText
-  where
-    ipExternal (IPv4 w) =
-        not $ ipv4Local w || w == 0 || w == 16777343 -- the last is 127.0.0.1
-    outputF = ("{ nodeIps: '"%stext%"', peers: '"%stext%"' }")
-
-logReportType :: WithLogger m => ReportType -> m ()
-logReportType (RCrash i) = logError $ "Reporting crash with code " <> show i
-logReportType (RError reason) =
-    logError $ "Reporting error with reason \"" <> reason <> "\""
-logReportType (RMisbehavior True reason) =
-    logError $ "Reporting critical misbehavior with reason \"" <> reason <> "\""
-logReportType (RMisbehavior False reason) =
-    logWarning $ "Reporting non-critical misbehavior with reason \"" <> reason <> "\""
-logReportType (RInfo text) =
-    logInfo $ "Reporting info with text \"" <> text <> "\""
-
-extendRTDesc :: Text -> ReportType -> ReportType
-extendRTDesc text (RError reason)                  = RError $ reason <> text
-extendRTDesc text (RMisbehavior isCritical reason) = RMisbehavior isCritical $ reason <> text
-extendRTDesc text' (RInfo text)                    = RInfo $ text <> text'
-extendRTDesc _ x                                   = x
-
--- Note that we are catching all synchronous exceptions, but don't
--- catch async ones. If reporting is broken, we don't want it to
--- affect anything else.
-reportNode
-    :: forall ctx m . (MonadReporting ctx m)
-    => Bool -> Bool -> ReportType -> m ()
-reportNode sendLogs extendWithNodeInfo reportType =
-    reportNodeDo `catchAny` handler
-  where
-    send' = if sendLogs then sendReportNode else sendReportNodeNologs
-    reportNodeDo = do
-        logReportType reportType
-        if extendWithNodeInfo
-           then do
-              nodeInfo <- getNodeInfo
-              send' $ extendRTDesc (", nodeInfo: " <> nodeInfo) reportType
-           else send' reportType
-    handler :: SomeException -> m ()
-    handler e =
-        logError $
-        sformat ("Didn't manage to report "%shown%
-                 " because of exception '"%string%"' raised while sending")
-        reportType (displayException e)
-
--- | Report «misbehavior», i. e. a situation when something is globally
--- wrong, not only with our node. 'Bool' argument determines whether
--- misbehavior is critical and requires immediate response.
---
--- Misbehaviour is reported only by core nodes, because
--- a) core nodes usually have the most actual vision of the current state;
--- b) reporting misbehaviour from many nodes is redundant.
-reportMisbehaviour
-    :: forall ctx m . (MonadReporting ctx m)
-    => Bool -> Text -> m ()
-reportMisbehaviour isCritical message = do
-    nodeType <- getNodeType <$> ask
-    case nodeType of
-        NodeCore -> reportNode True True (RMisbehavior isCritical message)
-        _        -> pass
-
--- | Report some general information.
-reportInfo
-    :: forall ctx m . (MonadReporting ctx m)
-    => Bool -> Text -> m ()
-reportInfo sendLogs = reportNode sendLogs True . RInfo
-
--- | Report «error», i. e. a situation when something is wrong with our
--- node, e. g. an assertion failed.
-reportError
-    :: forall ctx m . (MonadReporting ctx m)
-    => Text -> m ()
-reportError = reportNode True True . RError
-
-----------------------------------------------------------------------------
--- General purpose
+-- General purpose/low level
 ----------------------------------------------------------------------------
 
 -- | Given logs files list and report type, sends reports to URI
@@ -297,6 +136,171 @@ retrieveLogFiles lconfig = fromLogTree $ lconfig ^. lcTree
         let curElems = map ([],) (lt ^.. ltFiles . each . hwFilePath)
             addFoo (part, node) = map (first (part :)) $ fromLogTree node
         in curElems ++ concatMap addFoo (lt ^. ltSubloggers . to HM.toList)
+
+----------------------------------------------------------------------------
+-- Node-specific
+----------------------------------------------------------------------------
+
+type MonadReporting ctx m =
+       ( MonadIO m
+       , MonadMask m
+       , MonadReader ctx m
+       , MonadFormatPeers m
+       , HasReportingContext ctx
+       , HasNodeType ctx
+       , WithLogger m
+       , HasConfiguration
+       , HasCompileInfo
+       )
+
+
+-- Common code across node sending: tries to send logs to at least one
+-- reporting server.
+sendReportNodeImpl
+    :: (MonadReporting ctx m)
+    => [Text] -> ReportType -> m ()
+sendReportNodeImpl rawLogs reportType = do
+    servers <- view (reportingContext . reportServers)
+    when (null servers) onNoServers
+    errors <- fmap lefts $ forM servers $ try .
+        sendReport [] rawLogs reportType "cardano-node" . toString
+    whenNotNull errors $ throwSE . NE.head
+  where
+    onNoServers =
+        logInfo $ "sendReportNodeImpl: not sending report " <>
+                  "because no reporting servers are specified"
+    throwSE (e :: SomeException) = throwM e
+
+-- | Sends node's logs, taking 'LoggerConfig' from 'NodeContext',
+-- retrieving all logger files from it. List of servers is also taken
+-- from node's configuration.
+sendReportNode
+    :: (MonadReporting ctx m)
+    => ReportType -> m ()
+sendReportNode reportType = do
+    noServers <- null <$> view (reportingContext . reportServers)
+    if noServers
+        then onNoServers
+        else do
+            logConfig <- view (reportingContext . loggerConfig)
+            let allFiles = map snd $ retrieveLogFiles logConfig
+            logFile <-
+                maybeThrow
+                    NoPubFiles
+                    (head $ filter (".pub" `isSuffixOf`) allFiles)
+            logContent <-
+                takeGlobalSize charsConst <$>
+                retrieveLogContent logFile (Just 5000)
+            sendReportNodeImpl (reverse logContent) reportType
+  where
+    -- 2 megabytes, assuming we use chars which are ASCII mostly
+    charsConst :: Int
+    charsConst = 1024 * 1024 * 2
+    takeGlobalSize :: Int -> [Text] -> [Text]
+    takeGlobalSize _ [] = []
+    takeGlobalSize curLimit (t:xs) =
+        let delta = curLimit - length t
+        in bool [] (t : (takeGlobalSize delta xs)) (delta > 0)
+    onNoServers =
+        logInfo $
+        "sendReportNode: not sending report " <>
+        "because no reporting servers are specified"
+
+-- | Same as 'sendReportNode', but doesn't attach any logs.
+sendReportNodeNologs :: (MonadReporting ctx m) => ReportType -> m ()
+sendReportNodeNologs = sendReportNodeImpl []
+
+-- Note that we are catching all synchronous exceptions, but don't
+-- catch async ones. If reporting is broken, we don't want it to
+-- affect anything else.
+reportNode
+    :: forall ctx m . (MonadReporting ctx m)
+    => Bool -> Bool -> ReportType -> m ()
+reportNode sendLogs extendWithNodeInfo reportType =
+    reportNodeDo `catchAny` handler
+  where
+    send' = if sendLogs then sendReportNode else sendReportNodeNologs
+    reportNodeDo = do
+        logReportType reportType
+        if extendWithNodeInfo
+           then do
+              nodeInfo <- getNodeInfo
+              send' $ extendRTDesc (", nodeInfo: " <> nodeInfo) reportType
+           else send' reportType
+    handler :: SomeException -> m ()
+    handler e =
+        logError $
+        sformat ("Didn't manage to report "%shown%
+                 " because of exception '"%string%"' raised while sending")
+        reportType (displayException e)
+
+    extendRTDesc :: Text -> ReportType -> ReportType
+    extendRTDesc text (RError reason)                  = RError $ reason <> text
+    extendRTDesc text (RMisbehavior isCritical reason) = RMisbehavior isCritical $ reason <> text
+    extendRTDesc text' (RInfo text)                    = RInfo $ text <> text'
+    extendRTDesc _ x                                   = x
+
+    logReportType :: WithLogger m => ReportType -> m ()
+    logReportType (RCrash i) = logError $ "Reporting crash with code " <> show i
+    logReportType (RError reason) =
+        logError $ "Reporting error with reason \"" <> reason <> "\""
+    logReportType (RMisbehavior True reason) =
+        logError $ "Reporting critical misbehavior with reason \"" <> reason <> "\""
+    logReportType (RMisbehavior False reason) =
+        logWarning $ "Reporting non-critical misbehavior with reason \"" <> reason <> "\""
+    logReportType (RInfo text) =
+        logInfo $ "Reporting info with text \"" <> text <> "\""
+
+    -- Retrieves node info that we would like to know when analyzing
+    -- malicious behavior of node.
+    getNodeInfo :: (MonadIO m, MonadFormatPeers m) => m Text
+    getNodeInfo = do
+        peersText <- fromMaybe "unknown" <$> formatKnownPeers sformat
+        (ips :: [Text]) <-
+            map show . filter ipExternal . map ipv4 <$>
+            liftIO getNetworkInterfaces
+        pure $ sformat outputF (pretty $ listBuilderJSON ips) peersText
+      where
+        ipExternal (IPv4 w) =
+            not $ ipv4Local w || w == 0 || w == 16777343 -- the last is 127.0.0.1
+        outputF = ("{ nodeIps: '"%stext%"', peers: '"%stext%"' }")
+
+    -- checks if ipv4 is from local range
+    ipv4Local :: Word32 -> Bool
+    ipv4Local w =
+        or [b1 == 10, b1 == 172 && b2 >= 16 && b2 <= 31, b1 == 192 && b2 == 168]
+      where
+        b1 = w .&. 0xff
+        b2 = (w `shiftR` 8) .&. 0xff
+
+-- | Report «misbehavior», i. e. a situation when something is globally
+-- wrong, not only with our node. 'Bool' argument determines whether
+-- misbehavior is critical and requires immediate response.
+--
+-- Misbehaviour is reported only by core nodes, because
+-- a) core nodes usually have the most actual vision of the current state;
+-- b) reporting misbehaviour from many nodes is redundant.
+reportMisbehaviour
+    :: forall ctx m . (MonadReporting ctx m)
+    => Bool -> Text -> m ()
+reportMisbehaviour isCritical message = do
+    nodeType <- getNodeType <$> ask
+    case nodeType of
+        NodeCore -> reportNode True True (RMisbehavior isCritical message)
+        _        -> pass
+
+-- | Report some general information.
+reportInfo
+    :: forall ctx m . (MonadReporting ctx m)
+    => Bool -> Text -> m ()
+reportInfo sendLogs = reportNode sendLogs True . RInfo
+
+-- | Report «error», i. e. a situation when something is wrong with our
+-- node, e. g. an assertion failed.
+reportError
+    :: forall ctx m . (MonadReporting ctx m)
+    => Text -> m ()
+reportError = reportNode True True . RError
 
 ----------------------------------------------------------------------------
 -- Exception handling

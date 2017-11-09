6370c2c24473a4c5b335b667d7acf95a91f24ad0
Jens Krause <email@jkrause.io>
2017-11-09 19:26:50 +0100

    [CSE-253] Bump `default.nix`

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5954,8 +5954,8 @@ self: {
           pname = "servant-blaze";
           version = "0.7.1";
           sha256 = "90ed1c7a22b83bee344ef3896203f3699b7633bf986ffa064752c3596c072646";
-          revision = "5";
-          editedCabalFile = "05zz0kvnmai230palf44f72gm1vadqyssk9hl4h0qq5263frbsli";
+          revision = "6";
+          editedCabalFile = "051m44rqmxkl30n96qcbz1xwwsw2n7l7laflnc0xydc40ws0bj96";
           libraryHaskellDepends = [
             base
             blaze-html

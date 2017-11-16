6b8b5458adfe830ecbd956baf871e49ec5bcec95
Alexander Vieth <aovieth@gmail.com>
2017-11-16 16:38:39 -0500

    [CSL-2031] runDiffusionLayer for full implemented.

--- lib/src/Pos/Diffusion/Full.hs
+++ lib/src/Pos/Diffusion/Full.hs
@@ -1,32 +1,52 @@
+{-# LANGUAGE CPP                 #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE RecordWildCards     #-}
+{-# LANGUAGE RankNTypes          #-}
 
 module Pos.Diffusion.Full
     ( diffusionLayerFull
     ) where
 
-import           Universum
+import           Universum hiding (bracket)
 
+import           Control.Monad.Fix (MonadFix)
 import qualified Data.Map as M
-import           Mockable (throw)
+import           Formatting (sformat, shown, (%))
+import           Mockable (Mockable, Bracket, bracket, Catch, Throw, throw, withAsync)
 import           Mockable.Production (Production)
 import qualified Network.Broadcast.OutboundQueue as OQ
 import           Network.Broadcast.OutboundQueue.Types (MsgType (..), Origin (..))
+import           Network.QDisc.Fair (fairQDisc)
+import qualified Network.Transport as NT (closeTransport)
+import           Network.Transport.Abstract (Transport)
+import           Network.Transport.Concrete (concrete)
+import qualified Network.Transport.TCP as TCP
+import           Node (Node, NodeAction (..), simpleNodeEndPoint, NodeEnvironment (..), defaultNodeEnvironment, node)
+import           Node.Conversation (Converse, converseWith)
 import qualified System.Metrics as Monitoring
+import           System.Random (newStdGen)
+import           System.Wlog (WithLogger, CanLog, logError, getLoggerName, usingLoggerName)
+import qualified System.Wlog as Logger
+#ifdef linux_HOST_OS
+import qualified System.Systemd.Daemon as Systemd
+#endif
 
-import           Pos.Communication          (NodeId, VerInfo, EnqueueMsg, makeEnqueueMsg)
+import           Pos.Communication (NodeId, VerInfo, PeerData, PackingType, EnqueueMsg, makeEnqueueMsg, bipPacking, Listener, MkListeners (..))
 import           Pos.Communication.Relay.Logic (invReqDataFlowTK)
+import           Pos.Communication.Server (allListeners)
+import           Pos.Configuration (HasNodeConfiguration, conversationEstablishTimeout, networkConnectionTimeout)
 import           Pos.Core.Block (Block, MainBlockHeader, BlockHeader)
 import           Pos.Core.Types (HeaderHash)
 import           Pos.Core.Txp (TxAux)
 import           Pos.Core.Update (UpId, UpdateVote, UpdateProposal)
+import           Pos.DHT.Real (KademliaDHTInstance, KademliaParams (..), startDHTInstance, stopDHTInstance)
 import qualified Pos.Diffusion.Full.Block  as Diffusion.Block
 import qualified Pos.Diffusion.Full.Txp    as Diffusion.Txp
 import qualified Pos.Diffusion.Full.Update as Diffusion.Update
 import           Pos.Diffusion.Types (Diffusion (..), DiffusionLayer (..), GetBlocksError (..))
 import           Pos.Logic.Types (Logic (..))
 import           Pos.Network.CLI (NetworkConfigOpts (..), intNetworkConfigOpts)
-import           Pos.Network.Types (Bucket, initQueue)
+import           Pos.Network.Types (NetworkConfig (..), Topology (..), Bucket, initQueue)
 import           Pos.Ssc.Message (MCOpening, MCShares, MCCommitment, MCVssCertificate)
 import           Pos.Util.OutboundQueue (EnqueuedConversation (..))
 import           Pos.WorkMode (WorkMode)
@@ -45,7 +65,7 @@ import           Pos.WorkMode (WorkMode)
 -- work to juggle the instances.
 diffusionLayerFull
     :: forall ctx d x .
-       ( WorkMode ctx d )
+       ( WorkMode ctx d, MonadFix d )
     => NetworkConfigOpts
     -> Maybe Monitoring.Store
     -> VerInfo
@@ -107,10 +127,13 @@ diffusionLayerFull networkConfigOpts mEkgStore ourVerInfo expectLogic =
             sendSscCommitment :: MCCommitment -> d ()
             sendSscCommitment = void . invReqDataFlowTK "ssc" enqueue (MsgMPC OriginSender) (ourStakeholderId logic)
 
+            listeners :: VerInfo -> [Listener d]
+            listeners = mkListeners (allListeners oq (ncTopology networkConfig) enqueue) ourVerInfo
+
             -- Bracket kademlia and network-transport, create a node. This
             -- will be very involved. Should make it top-level I think.
             runDiffusionLayer :: d ()
-            runDiffusionLayer = runDiffusionLayerFull oq
+            runDiffusionLayer = runDiffusionLayerFull networkConfig ourVerInfo oq listeners
 
             diffusion :: Diffusion d
             diffusion = Diffusion {..}
@@ -122,7 +145,164 @@ diffusionLayerFull networkConfigOpts mEkgStore ourVerInfo expectLogic =
     acquire = pure ()
     release = \_ -> pure ()
 
+-- FIXME TBD move the remainder into a separate module?
+
 -- | Create kademlia, network-transport, and run the outbound queue's
 -- dequeue thread.
-runDiffusionLayerFull :: Applicative d => OQ.OutboundQ (EnqueuedConversation d) NodeId Bucket -> d ()
-runDiffusionLayerFull _ = pure ()
+runDiffusionLayerFull
+    :: forall ctx d .
+       ( WorkMode ctx d, MonadFix d )
+    => NetworkConfig KademliaParams
+    -> VerInfo
+    -> OQ.OutboundQ (EnqueuedConversation d) NodeId Bucket
+    -> (VerInfo -> [Listener d])
+    -> d ()
+runDiffusionLayerFull networkConfig ourVerInfo oq listeners =
+    bracketTransport (ncTcpAddr networkConfig) $ \(transport :: Transport d) ->
+        bracketKademlia networkConfig $ \_ ->
+            timeWarpNode transport ourVerInfo listeners $ \__theNode converse ->
+                withAsync (OQ.dequeueThread oq (sendMsgFromConverse converse)) $ \_ ->
+                    notifyReady
+
+sendMsgFromConverse
+    :: Converse PackingType PeerData d
+    -> OQ.SendMsg d (EnqueuedConversation d) NodeId
+sendMsgFromConverse converse (EnqueuedConversation (_, k)) nodeId =
+    converseWith converse nodeId (k nodeId)
+
+-- | Bring up a time-warp node. It will come down when the continuation ends.
+timeWarpNode
+    :: forall ctx d t .
+       ( WorkMode ctx d, MonadFix d )
+    => Transport d
+    -> VerInfo
+    -> (VerInfo -> [Listener d])
+    -> (Node d -> Converse PackingType PeerData d -> d t)
+    -> d t
+timeWarpNode transport ourVerInfo listeners k = do
+    stdGen <- liftIO newStdGen
+    node mkTransport mkReceiveDelay mkConnectDelay stdGen bipPacking ourVerInfo nodeEnv $ \theNode ->
+        NodeAction listeners $ k theNode
+  where
+    mkTransport = simpleNodeEndPoint transport
+    mkReceiveDelay = const (pure Nothing)
+    mkConnectDelay = const (pure Nothing)
+    nodeEnv = defaultNodeEnvironment { nodeAckTimeout = conversationEstablishTimeout }
+
+----------------------------------------------------------------------------
+-- Kademlia
+----------------------------------------------------------------------------
+
+createKademliaInstance ::
+       (HasNodeConfiguration, MonadIO m, Mockable Catch m, Mockable Throw m, CanLog m)
+    => KademliaParams
+    -> Word16 -- ^ Default port to bind to.
+    -> m KademliaDHTInstance
+createKademliaInstance kp defaultPort =
+    usingLoggerName "kademlia" (startDHTInstance instConfig defaultBindAddress)
+  where
+    instConfig = kp {kpPeers = ordNub $ kpPeers kp}
+    defaultBindAddress = ("0.0.0.0", defaultPort)
+
+-- | RAII for 'KademliaDHTInstance'.
+bracketKademliaInstance
+    :: (HasNodeConfiguration, MonadIO m, Mockable Catch m, Mockable Throw m, Mockable Bracket m, CanLog m)
+    => KademliaParams
+    -> Word16 -- ^ Default port to bind to.
+    -> (KademliaDHTInstance -> m a)
+    -> m a
+bracketKademliaInstance kp defaultPort action =
+    bracket (createKademliaInstance kp defaultPort) stopDHTInstance action
+
+-- | The 'NodeParams' contain enough information to determine whether a Kademlia
+-- instance should be brought up. Use this to safely acquire/release one.
+bracketKademlia
+    :: (HasNodeConfiguration, MonadIO m, Mockable Catch m, Mockable Throw m, Mockable Bracket m, CanLog m)
+    => NetworkConfig KademliaParams
+    -> (NetworkConfig KademliaDHTInstance -> m a)
+    -> m a
+bracketKademlia nc@NetworkConfig {..} action = case ncTopology of
+    -- cases that need Kademlia
+    TopologyP2P{topologyKademlia = kp, ..} ->
+      bracketKademliaInstance kp ncDefaultPort $ \kinst ->
+        k $ TopologyP2P{topologyKademlia = kinst, ..}
+    TopologyTraditional{topologyKademlia = kp, ..} ->
+      bracketKademliaInstance kp ncDefaultPort $ \kinst ->
+        k $ TopologyTraditional{topologyKademlia = kinst, ..}
+    TopologyRelay{topologyOptKademlia = Just kp, ..} ->
+      bracketKademliaInstance kp ncDefaultPort $ \kinst ->
+        k $ TopologyRelay{topologyOptKademlia = Just kinst, ..}
+    TopologyCore{topologyOptKademlia = Just kp, ..} ->
+      bracketKademliaInstance kp ncDefaultPort $ \kinst ->
+        k $ TopologyCore{topologyOptKademlia = Just kinst, ..}
+
+    -- cases that don't
+    TopologyRelay{topologyOptKademlia = Nothing, ..} ->
+        k $ TopologyRelay{topologyOptKademlia = Nothing, ..}
+    TopologyCore{topologyOptKademlia = Nothing, ..} ->
+        k $ TopologyCore{topologyOptKademlia = Nothing, ..}
+    TopologyBehindNAT{..} ->
+        k $ TopologyBehindNAT{..}
+    TopologyAuxx{..} ->
+        k $ TopologyAuxx{..}
+  where
+    k topology = action (nc { ncTopology = topology })
+
+data MissingKademliaParams = MissingKademliaParams
+    deriving (Show)
+
+instance Exception MissingKademliaParams
+
+----------------------------------------------------------------------------
+-- Transport
+----------------------------------------------------------------------------
+
+createTransportTCP
+    :: (HasNodeConfiguration, MonadIO n, MonadIO m, WithLogger m, Mockable Throw m)
+    => TCP.TCPAddr
+    -> m (Transport n, m ())
+createTransportTCP addrInfo = do
+    loggerName <- getLoggerName
+    let tcpParams =
+            (TCP.defaultTCPParameters
+             { TCP.transportConnectTimeout =
+                   Just $ fromIntegral networkConnectionTimeout
+             , TCP.tcpNewQDisc = fairQDisc $ \_ -> return Nothing
+             -- Will check the peer's claimed host against the observed host
+             -- when new connections are made. This prevents an easy denial
+             -- of service attack.
+             , TCP.tcpCheckPeerHost = True
+             , TCP.tcpServerExceptionHandler = \e ->
+                     usingLoggerName (loggerName <> "transport") $
+                         logError $ sformat ("Exception in tcp server: " % shown) e
+             })
+    transportE <-
+        liftIO $ TCP.createTransport addrInfo tcpParams
+    case transportE of
+        Left e -> do
+            logError $ sformat ("Error creating TCP transport: " % shown) e
+            throw e
+        Right transport -> return (concrete transport, liftIO $ NT.closeTransport transport)
+
+-- | RAII for 'Transport'.
+bracketTransport
+    :: (HasNodeConfiguration, MonadIO m, MonadIO n, Mockable Throw m, Mockable Bracket m, WithLogger m)
+    => TCP.TCPAddr
+    -> (Transport n -> m a)
+    -> m a
+bracketTransport tcpAddr k =
+    bracket (createTransportTCP tcpAddr) snd (k . fst)
+
+-- | Notify process manager tools like systemd the node is ready.
+-- Available only on Linux for systems where `libsystemd-dev` is installed.
+-- It defaults to a noop for all the other platforms.
+notifyReady :: (MonadIO m, WithLogger m) => m ()
+#ifdef linux_HOST_OS
+notifyReady = do
+    res <- liftIO Systemd.notifyReady
+    case res of
+        Just () -> return ()
+        Nothing -> Logger.logWarning "notifyReady failed to notify systemd."
+#else
+notifyReady = return ()
+#endif

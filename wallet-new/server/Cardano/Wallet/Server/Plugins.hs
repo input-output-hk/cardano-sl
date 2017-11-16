d7d2649b306c12b5e8178b3698a27a9d9ca0334a
Martoon <martoon.hsk@gmail.com>
2017-11-16 17:46:37 +0300

    Allow to disable TLS on --no-tls

--- wallet-new/server/Cardano/Wallet/Server/Plugins.hs
+++ wallet-new/server/Cardano/Wallet/Server/Plugins.hs
@@ -13,42 +13,39 @@ module Cardano.Wallet.Server.Plugins (
 import qualified Prelude
 import           Universum
 
-import           Cardano.Wallet.API                   as API
-import           Cardano.Wallet.Server                as API
-import           Cardano.Wallet.Server.CLI            (RunMode, WalletBackendParams (..),
-                                                       isDebugMode, walletAcidInterval,
-                                                       walletDbOptions)
+import           Cardano.Wallet.API as API
+import           Cardano.Wallet.Server as API
+import           Cardano.Wallet.Server.CLI (RunMode, WalletBackendParams (..), isDebugMode,
+                                            walletAcidInterval, walletDbOptions)
 
-import qualified Control.Concurrent.STM               as STM
-import           Network.Wai                          (Application, Middleware)
-import           Network.Wai.Middleware.Cors          (cors, corsMethods, corsRequestHeaders,
-                                                       simpleCorsResourcePolicy, simpleMethods)
+import qualified Control.Concurrent.STM as STM
+import           Network.Wai (Application, Middleware)
+import           Network.Wai.Middleware.Cors (cors, corsMethods, corsRequestHeaders,
+                                              simpleCorsResourcePolicy, simpleMethods)
 import           Network.Wai.Middleware.RequestLogger (logStdoutDev)
-import           Pos.Wallet.Web                       (cleanupAcidStatePeriodically,
-                                                       syncWalletsWithGState, wwmcSendActions)
-import           Pos.Wallet.Web.Account               (findKey, myRootAddresses)
-import           Pos.Wallet.Web.Methods.Restore       (addInitialRichAccount)
-import           Pos.Wallet.Web.Pending.Worker        (startPendingTxsResubmitter)
-import qualified Pos.Wallet.Web.Server.Runner         as V0
-import           Pos.Wallet.Web.Sockets               (getWalletWebSockets, launchNotifier,
-                                                       upgradeApplicationWS)
-import           Servant                              (Handler, Server, serve)
-import           System.Wlog                          (logInfo, modifyLoggerName)
+import           Pos.Wallet.Web (cleanupAcidStatePeriodically, syncWalletsWithGState,
+                                 wwmcSendActions)
+import           Pos.Wallet.Web.Account (findKey, myRootAddresses)
+import           Pos.Wallet.Web.Methods.Restore (addInitialRichAccount)
+import           Pos.Wallet.Web.Pending.Worker (startPendingTxsResubmitter)
+import qualified Pos.Wallet.Web.Server.Runner as V0
+import           Pos.Wallet.Web.Sockets (getWalletWebSockets, launchNotifier, upgradeApplicationWS)
+import           Servant (Handler, Server, serve)
+import           System.Wlog (logInfo, modifyLoggerName)
 
-import qualified Data.ByteString.Char8                as BS8
-import           Pos.Communication                    (ActionSpec (..), OutSpecs, SendActions,
-                                                       WorkerSpec, sendTxOuts, worker)
-import           Pos.Context                          (HasNodeContext)
+import qualified Data.ByteString.Char8 as BS8
+import           Pos.Communication (ActionSpec (..), OutSpecs, SendActions, WorkerSpec, sendTxOuts,
+                                    worker)
+import           Pos.Context (HasNodeContext)
 
-import           Pos.Launcher.Configuration           (HasConfigurations)
-import           Pos.Util.CompileInfo                 (HasCompileInfo)
-import           Pos.Wallet.Web.Mode                  (MonadFullWalletWebMode, MonadWalletWebMode,
-                                                       MonadWalletWebSockets, WalletWebMode,
-                                                       WalletWebModeContext,
-                                                       WalletWebModeContextTag)
-import           Pos.Wallet.Web.Server.Launcher       (walletServeImpl, walletServerOuts)
-import           Pos.Web                              (TlsParams, serveImpl, serveWeb)
-import           Pos.WorkMode                         (WorkMode)
+import           Pos.Launcher.Configuration (HasConfigurations)
+import           Pos.Util.CompileInfo (HasCompileInfo)
+import           Pos.Wallet.Web.Mode (MonadFullWalletWebMode, MonadWalletWebMode,
+                                      MonadWalletWebSockets, WalletWebMode, WalletWebModeContext,
+                                      WalletWebModeContextTag)
+import           Pos.Wallet.Web.Server.Launcher (walletServeImpl, walletServerOuts)
+import           Pos.Web (TlsParams, serveImpl, serveWeb)
+import           Pos.WorkMode (WorkMode)
 
 -- A @Plugin@ running in the monad @m@.
 type Plugin m = ([WorkerSpec m], OutSpecs)
@@ -70,7 +67,7 @@ conversation wArgs = (, mempty) $ map (\act -> ActionSpec $ \__vI __sA -> act) (
                          => WalletBackendParams
                          -> [m ()]
     pluginsMonitoringApi WalletBackendParams {..}
-        | enableMonitoringApi = [serveWeb monitoringApiPort (Just walletTLSParams)]
+        | enableMonitoringApi = [serveWeb monitoringApiPort walletTLSParams]
         | otherwise = []
 
 -- | A @Plugin@ to start the wallet backend API.
@@ -83,7 +80,7 @@ walletBackend WalletBackendParams {..} =
         (getApplication sendActions)
         walletAddress
         -- Disable TLS if in debug mode.
-        (if (isDebugMode walletRunMode) then Nothing else (Just walletTLSParams))
+        (if (isDebugMode walletRunMode) then Nothing else walletTLSParams)
   where
     -- Gets the Wai `Application` to run.
     getApplication :: SendActions WalletWebMode -> WalletWebMode Application

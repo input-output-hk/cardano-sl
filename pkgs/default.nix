2b7ea2fe42e996cb71b4e15815b8642602315ac4
Alfredo Di Napoli <alfredo.dinapoli@iohk.io>
2017-11-17 11:22:26 +0100

    Update pkgs/default.nix

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2366,7 +2366,7 @@ self: {
           description = "Cardano SL - wallet";
           license = stdenv.lib.licenses.mit;
         }) {};
-      cardano-sl-wallet-new = callPackage ({ QuickCheck, aeson, aeson-pretty, base, bytestring, cardano-sl, cardano-sl-core, cardano-sl-infra, cardano-sl-ssc, cardano-sl-txp, cardano-sl-update, cardano-sl-wallet, containers, data-default, exceptions, formatting, hspec, http-api-data, http-client, http-types, insert-ordered-containers, lens, log-warper, memory, mkDerivation, mtl, neat-interpolation, network-uri, node-sketch, optparse-applicative, quickcheck-instances, serokell-util, servant, servant-client, servant-quickcheck, servant-server, servant-swagger, stdenv, stm, string-conv, swagger2, text, text-format, time-units, transformers, universum, unordered-containers, wai, wai-cors, wai-extra, warp }:
+      cardano-sl-wallet-new = callPackage ({ QuickCheck, aeson, aeson-pretty, base, bytestring, cardano-sl, cardano-sl-core, cardano-sl-infra, cardano-sl-ssc, cardano-sl-txp, cardano-sl-update, cardano-sl-util, cardano-sl-wallet, containers, data-default, exceptions, formatting, hspec, http-api-data, http-client, http-types, insert-ordered-containers, lens, log-warper, memory, mkDerivation, mtl, neat-interpolation, network-uri, node-sketch, optparse-applicative, quickcheck-instances, serokell-util, servant, servant-client, servant-quickcheck, servant-server, servant-swagger, stdenv, stm, string-conv, swagger2, text, text-format, time-units, transformers, universum, unordered-containers, wai, wai-cors, wai-extra, warp }:
       mkDerivation {
           pname = "cardano-sl-wallet-new";
           version = "0.1.0.0";
@@ -2382,6 +2382,7 @@ self: {
             cardano-sl-infra
             cardano-sl-ssc
             cardano-sl-update
+            cardano-sl-util
             cardano-sl-wallet
             containers
             data-default
@@ -2416,6 +2417,7 @@ self: {
             cardano-sl-infra
             cardano-sl-ssc
             cardano-sl-txp
+            cardano-sl-util
             cardano-sl-wallet
             containers
             data-default
@@ -2459,6 +2461,7 @@ self: {
             cardano-sl-infra
             cardano-sl-ssc
             cardano-sl-txp
+            cardano-sl-util
             cardano-sl-wallet
             containers
             data-default

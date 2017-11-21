b205451f114107cbcf5b807fe71f17f9bdba14e2
Alexander Vieth <aovieth@gmail.com>
2017-11-21 10:37:59 -0500

    [CSL-2031] Txp listeners.

--- lib/src/Pos/Diffusion/Full/Txp.hs
+++ lib/src/Pos/Diffusion/Full/Txp.hs
@@ -3,9 +3,12 @@
 
 module Pos.Diffusion.Full.Txp
        ( sendTx
+       , txListeners
        ) where
 
 import           Universum
+import           Data.Tagged (Tagged)
+import qualified Network.Broadcast.OutboundQueue as OQ
 
 import           Pos.Binary.Communication ()
 import           Pos.Binary.Core ()
@@ -13,10 +16,17 @@ import           Pos.Binary.Relay ()
 import           Pos.Binary.Txp ()
 import           Pos.Communication.Message ()
 import           Pos.Communication.Limits (HasTxpLimits)
-import           Pos.Communication.Protocol (EnqueueMsg, MsgType (..), Origin (..))
-import           Pos.Communication.Relay (invReqDataFlowTK, resOk, MinRelayWorkMode)
-import           Pos.Core.Txp (TxAux (..))
+import           Pos.Communication.Protocol (EnqueueMsg, MsgType (..), Origin (..), NodeId,
+                                             MkListeners)
+import           Pos.Communication.Relay (invReqDataFlowTK, resOk, MinRelayWorkMode,
+                                          InvReqDataParams (..), invReqMsgType, Relay (..),
+                                          relayListeners, MempoolParams (..))
+import           Pos.Core.Txp (TxAux (..), TxId)
 import           Pos.Crypto (hash)
+import           Pos.Diffusion.Full.Types (DiffusionWorkMode)
+import           Pos.Logic.Types (Logic (..))
+import qualified Pos.Logic.Types as KV (KeyVal (..))
+import           Pos.Network.Types (Bucket)
 import           Pos.Txp.Network.Types (TxMsgContents (..))
 
 -- | Send Tx to given addresses.
@@ -42,3 +52,33 @@ sendTx enqueue txAux = do
         | Right (Just peerResponse) <- toList outcome
         , resOk peerResponse
         ]
+
+txListeners
+    :: DiffusionWorkMode m
+    => Logic m
+    -> OQ.OutboundQ pack NodeId Bucket
+    -> EnqueueMsg m
+    -> MkListeners m
+txListeners logic oq enqueue = relayListeners oq enqueue (txRelays logic)
+
+txInvReqDataParams
+    :: DiffusionWorkMode m
+    => Logic m
+    -> InvReqDataParams (Tagged TxMsgContents TxId) TxMsgContents m
+txInvReqDataParams logic =
+    InvReqDataParams
+       { invReqMsgType = MsgTransaction
+       , contentsToKey = KV.toKey (postTx logic)
+       , handleInv = \_ -> KV.handleInv (postTx logic)
+       , handleReq = \_ -> KV.handleReq (postTx logic)
+       , handleData = \_ -> KV.handleData (postTx logic)
+       }
+
+txRelays
+    :: DiffusionWorkMode m
+    => Logic m
+    -> [Relay m]
+txRelays logic = pure $
+    -- Previous implementation had KeyMempool, but mempool messages are never
+    -- used so we drop it.
+    InvReqData NoMempool (txInvReqDataParams logic)

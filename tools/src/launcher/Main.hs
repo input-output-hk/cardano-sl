ba9d1167a356f25e12ec4f0ec207df00523037e1
Mikhail Volkhov <volhovm.cs@gmail.com>
2017-11-23 23:02:53 +0300

    [CSL-1909] More debug

--- tools/src/launcher/Main.hs
+++ tools/src/launcher/Main.hs
@@ -11,62 +11,75 @@
 {-# LANGUAGE TemplateHaskell       #-}
 {-# LANGUAGE TypeSynonymInstances  #-}
 
-import Universum
+import           Universum
 
-import Control.Concurrent (modifyMVar_)
-import Control.Concurrent.Async.Lifted.Safe (Async, async, cancel, poll, wait, waitAny,
-                                             withAsyncWithUnmask)
-import Control.Exception.Safe (handleAny, tryAny)
-import Control.Lens (makeLensesWith)
-import qualified Data.ByteString.Lazy as BS.L
-import Data.List (isSuffixOf)
-import qualified Data.Text.IO as T
-import Data.Time.Units (Second, convertUnit)
-import Data.Version (showVersion)
-import Formatting (int, sformat, shown, stext, (%))
-import qualified NeatInterpolation as Q (text)
-import Options.Applicative (Mod, OptionFields, Parser, auto, execParser, footerDoc, fullDesc,
-                            header, help, helper, info, infoOption, long, metavar, option, progDesc,
-                            short, strOption)
-import System.Directory (createDirectoryIfMissing, doesFileExist, getTemporaryDirectory, removeFile)
-import System.Environment (getExecutablePath)
-import System.Exit (ExitCode (..))
-import System.FilePath (normalise, (</>))
-import qualified System.IO as IO
-import System.Process (ProcessHandle, readProcessWithExitCode)
-import qualified System.Process as Process
-import System.Timeout (timeout)
-import System.Wlog (logError, logInfo, logNotice, logWarning)
-import qualified System.Wlog as Log
-import Text.PrettyPrint.ANSI.Leijen (Doc)
+import           Control.Concurrent                   (modifyMVar_)
+import           Control.Concurrent.Async.Lifted.Safe (Async, async, cancel, poll, wait,
+                                                       waitAny, withAsyncWithUnmask)
+import           Control.Exception.Safe               (handleAny, tryAny)
+import           Control.Lens                         (makeLensesWith)
+import qualified Data.ByteString.Lazy                 as BS.L
+import           Data.List                            (isSuffixOf)
+import qualified Data.Text.IO                         as T
+import           Data.Time.Units                      (Second, convertUnit)
+import           Data.Version                         (showVersion)
+import           Formatting                           (int, sformat, shown, stext, (%))
+import qualified NeatInterpolation                    as Q (text)
+import           Options.Applicative                  (Mod, OptionFields, Parser,
+                                                       ParserInfo, ParserResult (..),
+                                                       auto, defaultPrefs, execParserPure,
+                                                       footerDoc, fullDesc, header, help,
+                                                       helper, info, infoOption, long,
+                                                       metavar, option, progDesc,
+                                                       renderFailure, short, strOption)
+import           System.Directory                     (createDirectoryIfMissing,
+                                                       doesFileExist,
+                                                       getTemporaryDirectory, removeFile)
+import           System.Environment                   (getExecutablePath)
+import           System.Exit                          (ExitCode (..))
+import           System.FilePath                      (normalise, (</>))
+import qualified System.IO                            as IO
+import           System.Process                       (ProcessHandle,
+                                                       readProcessWithExitCode)
+import qualified System.Process                       as Process
+import           System.Timeout                       (timeout)
+import           System.Wlog                          (logError, logInfo, logNotice,
+                                                       logWarning)
+import qualified System.Wlog                          as Log
+import           Text.PrettyPrint.ANSI.Leijen         (Doc)
 
 #ifdef mingw32_HOST_OS
-import qualified System.IO.Silently as Silently
+import qualified System.IO.Silently                   as Silently
 #endif
 
 #ifndef mingw32_HOST_OS
-import System.Posix.Signals (sigKILL, signalProcess)
-import qualified System.Process.Internals as Process
+import           System.Posix.Signals                 (sigKILL, signalProcess)
+import qualified System.Process.Internals             as Process
 #endif
 
 -- Modules needed for system'
-import Control.Exception (handle, mask_, throwIO)
-import Foreign.C.Error (Errno (..), ePIPE)
-import GHC.IO.Exception (IOErrorType (..), IOException (..))
+import           Control.Exception                    (handle, mask_, throwIO)
+import           Foreign.C.Error                      (Errno (..), ePIPE)
+import           GHC.IO.Exception                     (IOErrorType (..), IOException (..))
 
-import Paths_cardano_sl (version)
-import Pos.Client.CLI (configurationOptionsParser, readLoggerConfig)
-import Pos.Core (HasConfiguration, Timestamp (..))
-import Pos.DB.Class (MonadDB (..), MonadDBRead (..))
-import Pos.DB.Misc (affirmUpdateInstalled)
-import Pos.DB.Rocks (NodeDBs, closeNodeDBs, dbDeleteDefault, dbGetDefault, dbIterSourceDefault,
-                     dbPutDefault, dbWriteBatchDefault, openNodeDBs)
-import Pos.Launcher (HasConfigurations, withConfigurations)
-import Pos.Launcher.Configuration (ConfigurationOptions (..))
-import Pos.Reporting.Methods (retrieveLogFiles, sendReport)
-import Pos.ReportServer.Report (ReportType (..))
-import Pos.Update (installerHash)
-import Pos.Util (HasLens (..), directory, postfixLFields, sleep)
+import           Paths_cardano_sl                     (version)
+import           Pos.Client.CLI                       (configurationOptionsParser,
+                                                       readLoggerConfig)
+import           Pos.Core                             (HasConfiguration, Timestamp (..))
+import           Pos.DB.Class                         (MonadDB (..), MonadDBRead (..))
+import           Pos.DB.Misc                          (affirmUpdateInstalled)
+import           Pos.DB.Rocks                         (NodeDBs, closeNodeDBs,
+                                                       dbDeleteDefault, dbGetDefault,
+                                                       dbIterSourceDefault, dbPutDefault,
+                                                       dbWriteBatchDefault, openNodeDBs)
+import           Pos.Launcher                         (HasConfigurations,
+                                                       withConfigurations)
+import           Pos.Launcher.Configuration           (ConfigurationOptions (..))
+import           Pos.Reporting.Methods                (retrieveLogFiles, sendReport)
+import           Pos.ReportServer.Report              (ReportType (..))
+import           Pos.Update                           (installerHash)
+import           Pos.Util                             (HasLens (..), directory,
+                                                       postfixLFields, sleep)
 
 data LauncherOptions = LO
     { loNodePath            :: !FilePath
@@ -87,7 +100,7 @@ data LauncherOptions = LO
     -- console, except on Windows where we don't output anything to console
     -- because it crashes).
     , loLauncherLogsPrefix  :: !(Maybe FilePath)
-    }
+    } deriving (Show)
 
 -- | The concrete monad where everything happens
 type M a = HasConfigurations => Log.LoggerNameBox IO a
@@ -167,8 +180,17 @@ optionsParser = do
 
     pure LO{..}
 
+execParserCustom :: (Show a) => ParserInfo a -> IO a
+execParserCustom x = (execParserPure defaultPrefs x <$> getArgs) >>= \case
+    Success a -> pure a
+    (Failure f) -> do
+        let (msg,_) = renderFailure f "launcher"
+        appendFile "patak.log" $ "\n" <> (fromString msg)
+        exitFailure
+    _ -> error "bababa, completion shouldn't be invoked"
+
 getLauncherOptions :: IO LauncherOptions
-getLauncherOptions = execParser programInfo
+getLauncherOptions = execParserCustom programInfo
   where
     programInfo = info (helper <*> versionOption <*> optionsParser) $
         fullDesc <> progDesc ""

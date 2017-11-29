a0c2771247d0a31ffc3adb37fb2fdafb535f93f4
Mikhail Volkhov <volhovm.cs@gmail.com>
2017-11-29 09:53:33 +0300

    Bump pkgs/default.nix

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4347,8 +4347,8 @@ self: {
       hscolour = callPackage ({ base, containers, mkDerivation, stdenv }:
       mkDerivation {
           pname = "hscolour";
-          version = "1.24.2";
-          sha256 = "55fb86bafdcad9613c25910b1cbca4b071c1ddc6365538c3b3d4e350cb30cf22";
+          version = "1.24.3";
+          sha256 = "4a2d34800318de57950041b71e7c571f6baf5c71d4a4a9987b07a32dbfdfb119";
           isLibrary = true;
           isExecutable = true;
           enableSeparateDataOutput = true;

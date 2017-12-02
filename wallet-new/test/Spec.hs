ad60213caa84506d6ec7a3200b055ece376c24f7
Alfredo Di Napoli <alfredo.dinapoli@iohk.io>
2017-12-02 11:23:26 +0100

    [CSL-2002] Resort to CPP to disable tests on Win.

--- wallet-new/test/Spec.hs
+++ wallet-new/test/Spec.hs
@@ -1,22 +1,23 @@
+{-# LANGUAGE CPP #-}
 module Main where
+
+#if defined(mingw32_HOST_OS)
+import Prelude
+main :: IO ()
+main = return ()
+#else
 
 import           Universum
 
 import qualified APISpec as API
-import qualified Data.Text as T
 import qualified MarshallingSpec as Marshalling
 import qualified SwaggerSpec as Swagger
-import           System.Info
 import           Test.Hspec
 
 -- | Tests whether or not some instances (JSON, Bi, etc) roundtrips.
--- Temporarily disable specs on AppVeyor: https://iohk.myjetbrains.com/youtrack/issue/CSL-2002
 main :: IO ()
-main = do
-    let platform = toText System.Info.os
-    case "darwin" `T.isInfixOf` platform || "linux" `T.isInfixOf` platform of
-        True -> hspec $ do
-            Marshalling.spec
-            API.spec
-            Swagger.spec
-        False -> return ()
+main = hspec $ do
+    Marshalling.spec
+    API.spec
+    Swagger.spec
+#endif

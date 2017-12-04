016ea42e14cf7e19c8a8a7ea687efd5ab9f23759
Cardano Staging <staging@cardano>
2017-12-04 11:13:41 +0000

    [CSL-1995] Update pkgs/default.nix

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3851,8 +3851,8 @@ self: {
       hscolour = callPackage ({ base, containers, mkDerivation, stdenv }:
       mkDerivation {
           pname = "hscolour";
-          version = "1.24.2";
-          sha256 = "55fb86bafdcad9613c25910b1cbca4b071c1ddc6365538c3b3d4e350cb30cf22";
+          version = "1.24.4";
+          sha256 = "243332b082294117f37b2c2c68079fa61af68b36223b3fc07594f245e0e5321d";
           isLibrary = true;
           isExecutable = true;
           enableSeparateDataOutput = true;

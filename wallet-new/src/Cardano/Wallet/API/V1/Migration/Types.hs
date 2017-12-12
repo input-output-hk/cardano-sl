f71dc80dd7317e15519e04923fcaf8b2a1355e17
Ante Kegalj <akegalj@gmail.com>
2017-12-12 16:02:38 +0100

    [CSL-2018] Add migration roundtrip specs for wallet-new (#2049)
    * [CSL-2018] Add migration roundtrip specs
    * [CSL-2018] Fix review points. Add coin migration roundtrip spec.

---
+++ wallet-new/src/Cardano/Wallet/API/V1/Migration/Types.hs
@@ -0,0 +1,196 @@
+{- | This is a temporary module to help migration @V0@ datatypes into @V1@ datatypes.
+-}
+{-# LANGUAGE TypeSynonymInstances #-}
+module Cardano.Wallet.API.V1.Migration.Types (
+      Migrate(..)
+    , migrate
+    ) where
+
+import           Universum
+
+import           Data.List.NonEmpty (fromList)
+import           Data.Map (elems)
+import           Data.Time.Clock.POSIX (POSIXTime)
+
+import           Cardano.Wallet.API.V1.Errors as Errors
+import qualified Cardano.Wallet.API.V1.Types as V1
+import qualified Pos.Core.Common as Core
+import qualified Pos.Core.Txp as Core
+import qualified Pos.Util.Servant as V0
+import qualified Pos.Wallet.Web.ClientTypes.Instances ()
+import qualified Pos.Wallet.Web.ClientTypes.Types as V0
+
+import qualified Control.Monad.Catch as Catch
+
+-- | 'Migrate' encapsulates migration between types, when possible.
+class Migrate from to where
+    eitherMigrate :: from -> Either Errors.WalletError to
+
+-- | "Run" the migration.
+migrate :: ( Catch.MonadThrow m, Migrate from to ) => from -> m to
+migrate from = case eitherMigrate from of
+    Left e   -> Catch.throwM e
+    Right to -> pure to
+
+--
+-- Instances
+--
+
+instance Migrate from to => Migrate [from] [to] where
+    eitherMigrate = mapM eitherMigrate
+
+-- | Migration from list to NonEmpty, as suggested by @akegalj.
+instance Migrate from to => Migrate [from] (NonEmpty to) where
+  eitherMigrate a = fromList <$> mapM eitherMigrate a
+
+instance Migrate V0.CWallet V1.Wallet where
+    eitherMigrate V0.CWallet{..} =
+        V1.Wallet <$> eitherMigrate cwId
+                  <*> pure (V0.cwName cwMeta)
+                  <*> eitherMigrate cwAmount
+
+-- NOTE: Migrate V1.Wallet V0.CWallet unable to do - not idempotent
+
+--
+instance Migrate V0.CWalletAssurance V1.AssuranceLevel where
+    eitherMigrate V0.CWAStrict = pure V1.StrictAssurance
+    eitherMigrate V0.CWANormal = pure V1.NormalAssurance
+
+instance Migrate V1.AssuranceLevel V0.CWalletAssurance where
+    eitherMigrate V1.StrictAssurance = pure V0.CWAStrict
+    eitherMigrate V1.NormalAssurance = pure V0.CWANormal
+
+--
+instance Migrate V0.CCoin Core.Coin where
+    eitherMigrate =
+        first (const $ Errors.MigrationFailed "error migrating V0.CCoin -> Core.Coin, mkCoin failed.") .
+        V0.decodeCType
+
+instance Migrate Core.Coin V0.CCoin where
+    eitherMigrate = pure . V0.encodeCType
+
+--
+instance Migrate (V0.CId V0.Wal) V1.WalletId where
+    eitherMigrate (V0.CId (V0.CHash h)) = pure (V1.WalletId h)
+
+instance Migrate V1.WalletId (V0.CId V0.Wal) where
+    eitherMigrate (V1.WalletId h) = pure (V0.CId (V0.CHash h))
+
+instance Migrate V1.AccountUpdate V0.CAccountMeta where
+    eitherMigrate V1.AccountUpdate{..} =
+        pure $ V0.CAccountMeta uaccName
+
+instance Migrate V1.NewAccount V0.CAccountMeta where
+    eitherMigrate V1.NewAccount{..} =
+        pure $ V0.CAccountMeta naccName
+
+instance Migrate (V1.WalletId, V1.NewAccount) V0.CAccountInit where
+    eitherMigrate (wId, nAcc) = do
+        newWId <- eitherMigrate wId
+        accMeta <- eitherMigrate nAcc
+        pure $ V0.CAccountInit accMeta newWId
+
+-- | Migrates to a V1 `SyncProgress` by computing the percentage as
+-- coded here: https://github.com/input-output-hk/daedalus/blob/master/app/stores/NetworkStatusStore.js#L108
+instance Migrate V0.SyncProgress V1.SyncProgress where
+    eitherMigrate V0.SyncProgress{..} =
+        let percentage = case _spNetworkCD of
+                Nothing -> (0 :: Word8)
+                Just nd | _spLocalCD >= nd -> 100
+                Just nd -> round @Double $ (fromIntegral _spLocalCD / fromIntegral nd) * 100.0
+        in pure $ V1.mkSyncProgress (fromIntegral percentage)
+
+-- NOTE: Migrate V1.SyncProgress V0.SyncProgress unable to do - not idempotent
+
+--
+instance Migrate V0.CAccount V1.Account where
+    eitherMigrate V0.CAccount{..} =
+        V1.Account <$> eitherMigrate caId
+                   -- ^ accId
+                   <*> mapM eitherMigrate caAddresses
+                   -- ^ accAddresses
+                   <*> eitherMigrate caAmount
+                   -- ^ accAmount
+                   <*> pure (V0.caName caMeta)
+                   -- ^ accName
+                   <*> eitherMigrate caId
+                   -- ^ accWalletId
+
+--
+-- Following instances are friendly borrowed from @martoon's PR https://github.com/input-output-hk/cardano-sl/pull/2008
+-- TODO (jk): Use instances of his PR when it has been merged
+
+-- in old API 'V0.AccountId' supposed to carry both wallet id and derivation index
+instance Migrate (V1.WalletId, V1.AccountId) V0.AccountId where
+    eitherMigrate (walId, accId) =
+        V0.AccountId <$> eitherMigrate walId <*> pure accId
+
+instance Migrate V0.AccountId (V1.WalletId, V1.AccountId) where
+    eitherMigrate accId =
+        (,) <$> eitherMigrate (V0.aiWId accId) <*> pure (V0.aiIndex accId)
+
+instance Migrate V0.CAccountId V0.AccountId where
+    eitherMigrate = first Errors.MigrationFailed . V0.decodeCType
+
+--
+-- #end of TODO (jk) ^
+--
+
+instance Migrate V0.CAccountId V1.AccountId where
+    eitherMigrate cAccId = do
+        oldAccountId :: V0.AccountId <- eitherMigrate cAccId
+        (_, newAccountId) :: (V1.WalletId, V1.AccountId) <- eitherMigrate oldAccountId
+        pure newAccountId
+
+instance Migrate V0.CAccountId V1.WalletId where
+    eitherMigrate cAccId = do
+        oldAccountId :: V0.AccountId <- eitherMigrate cAccId
+        (walletId, _) :: (V1.WalletId, V1.AccountId) <- eitherMigrate oldAccountId
+        pure walletId
+
+instance Migrate V0.CAddress Core.Address where
+       eitherMigrate V0.CAddress {..} =
+          first (const $ Errors.MigrationFailed "Error migrating V0.CAddress -> Core.Address failed.")
+              $ V0.decodeCType cadId
+
+----------------------------------------------------------------------------
+-- Transactions
+----------------------------------------------------------------------------
+
+instance Migrate (V0.CId V0.Addr) Core.Address where
+    eitherMigrate (V0.CId (V0.CHash h)) =
+        first (const $ Errors.MigrationFailed "Error migrating (V0.CId V0.Addr) -> Core.Address failed.")
+            . Core.decodeTextAddress $ h
+
+instance Migrate (V0.CId V0.Addr, V0.CCoin) V1.PaymentDistribution where
+    eitherMigrate (cIdAddr, cCoin) = do
+        pdAddress <- eitherMigrate cIdAddr
+        pdAmount  <- eitherMigrate cCoin
+        pure $ V1.PaymentDistribution {..}
+
+instance Migrate V0.CTxId V1.TxId where
+    eitherMigrate (V0.CTxId (V0.CHash h)) = pure $ V1.TxId h
+
+instance Migrate V0.CTx V1.Transaction where
+    eitherMigrate V0.CTx{..} = do
+        txId      <- eitherMigrate ctId
+        let txConfirmations = ctConfirmations
+        txAmount  <- eitherMigrate ctAmount
+        txInputs  <- eitherMigrate ctInputs
+        txOutputs <- eitherMigrate ctOutputs
+
+        let txType = if ctIsLocal
+            then V1.LocalTransaction
+            else V1.ForeignTransaction
+
+        let txDirection = if ctIsOutgoing
+            then V1.OutgoingTransaction
+            else V1.IncomingTransaction
+
+        pure V1.Transaction{..}
+
+-- | The migration instance for migrating history to a list of transactions
+instance Migrate (Map Core.TxId (V0.CTx, POSIXTime), Word) [V1.Transaction] where
+    eitherMigrate txsMapAndSize = do
+        let txsMapValues = elems . fst $ txsMapAndSize
+        mapM (eitherMigrate . fst) txsMapValues
--- wallet-new/test/MarshallingSpec.hs
+++ wallet-new/test/MarshallingSpec.hs
@@ -3,12 +3,13 @@ module MarshallingSpec where
 import           Universum
 
 import           Cardano.Wallet.API.V1.Errors (WalletError)
+import           Cardano.Wallet.API.V1.Migration.Types (Migrate (..))
 import           Cardano.Wallet.API.V1.Types
 import           Cardano.Wallet.Orphans ()
 import           Data.Aeson
 import           Data.Typeable (typeRep)
-import           Pos.Crypto (emptyPassphrase)
 import qualified Pos.Crypto as Crypto
+import qualified Pos.Wallet.Web.ClientTypes.Types as V0
 import           Test.Hspec
 import           Test.Hspec.QuickCheck
 import           Test.QuickCheck
@@ -22,6 +23,7 @@ import qualified Pos.Core as Core
 spec :: Spec
 spec = describe "Marshalling & Unmarshalling" $ do
     describe "Roundtrips" $ do
+        -- Aeson roundrips
         aesonRoundtripProp @BackupPhrase Proxy
         aesonRoundtripProp @Account Proxy
         aesonRoundtripProp @AssuranceLevel Proxy
@@ -44,20 +46,38 @@ spec = describe "Marshalling & Unmarshalling" $ do
         aesonRoundtripProp @NodeInfo Proxy
         aesonRoundtripProp @NodeSettings Proxy
 
+        -- Migrate roundrips
+        migrateRoundtripProp @Core.Coin @V0.CCoin Proxy Proxy
+        migrateRoundtripProp @AssuranceLevel @V0.CWalletAssurance Proxy Proxy
+        migrateRoundtripProp @WalletId @(V0.CId V0.Wal) Proxy Proxy
+        migrateRoundtripProp @(WalletId, AccountId) @V0.AccountId Proxy Proxy
+
     describe "Invariants" $ do
         describe "password" $ do
             it "empty string decodes to empty password" $
-                jsonString "" `decodesTo` (== emptyPassphrase)
+                jsonString "" `decodesTo` (== Crypto.emptyPassphrase)
             it "base-16 string of length 32 decodes to nonempty password" $
                 jsonString (fromString $ replicate 64 'a')
-                    `decodesTo` (/= emptyPassphrase)
+                    `decodesTo` (/= Crypto.emptyPassphrase)
             it "invalid length password decoding fails" $
                 -- currently passphrase should be either empty or of length 32
                 decodingFails @Crypto.PassPhrase "aabbcc" Proxy
 
+migrateRoundtrip :: (Arbitrary from, Migrate from to, Migrate to from, Eq from, Show from) => proxy from -> proxy to -> Property
+migrateRoundtrip (_ :: proxy from) (_ :: proxy to) = forAll arbitrary $ \(arbitraryFrom :: from) -> do
+    (eitherMigrate =<< migrateTo arbitraryFrom) === Right arbitraryFrom
+  where
+    migrateTo x = eitherMigrate x :: Either WalletError to
+
+migrateRoundtripProp
+    :: (Arbitrary from, Migrate from to, Migrate to from, Eq from, Show from, Typeable from, Typeable to)
+    => proxy from -> proxy to -> Spec
+migrateRoundtripProp proxyFrom proxyTo =
+    prop ("Migrate " <> show (typeRep proxyFrom) <> " <-> " <> show (typeRep proxyTo) <> " roundtrips") (migrateRoundtrip proxyFrom proxyTo)
+
 aesonRoundtrip :: (Arbitrary a, ToJSON a, FromJSON a, Eq a, Show a) => proxy a -> Property
 aesonRoundtrip (_ :: proxy a) = forAll arbitrary $ \(s :: a) -> do
-    eitherDecode (encode (toJSON s)) === Right s
+    eitherDecode (encode s) === Right s
 
 aesonRoundtripProp
     :: (Arbitrary a, ToJSON a, FromJSON a, Eq a, Show a, Typeable a)

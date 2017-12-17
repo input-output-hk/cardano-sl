d5cf9fcb43eeeb71cd1f4a5fa4d2c00a05abae22
Erik de Castro Lopo <erikd@mega-nerd.com>
2017-12-18 05:43:37 +1100

    Re-enable the FollowTheSatoshi benchmark

--- lib/bench/Local/Criterion.hs
+++ lib/bench/Local/Criterion.hs
@@ -6,11 +6,11 @@ import           Universum
 
 import           System.IO (hSetEncoding, stdout, utf8)
 
--- import qualified Bench.Pos.Criterion.FollowTheSatoshiBench as FTS
+import qualified Bench.Pos.Criterion.FollowTheSatoshiBench as FTS
 import qualified Bench.Pos.Criterion.TxSigningBench as TS
 
 main :: IO ()
 main = do
     hSetEncoding stdout utf8
-    -- FTS.runBenchmark
+    FTS.runBenchmark
     TS.runBenchmark

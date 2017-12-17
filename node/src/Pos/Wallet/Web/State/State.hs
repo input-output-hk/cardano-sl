c962ebea33098c3480246cd31c89aab8dd98a696
Duncan Coutts <duncan.coutts@iohk.io>
2017-12-17 11:07:50 +0000

    [CSL-2022] Adjust MonadWalletWebDB/WebWalletModeDB class constraints
    The queryDisk/updateDisk do not need HasConfiguration

--- node/src/Pos/Wallet/Web/State/State.hs
+++ node/src/Pos/Wallet/Web/State/State.hs
@@ -116,22 +116,28 @@ import           Pos.Wallet.Web.State.Storage (AddressInfo (..), AddressLookupMo
 type MonadWalletWebDB ctx m =
     ( MonadReader ctx m
     , HasLens WalletState ctx WalletState
-    , HasConfiguration
     )
 
 getWalletWebState :: MonadWalletWebDB ctx m => m WalletState
 getWalletWebState = view (lensOf @WalletState)
 
 -- | Constraint for working with web wallet DB
-type WebWalletModeDB ctx m = (MonadWalletWebDB ctx m, MonadIO m, MonadMockable m)
+type WebWalletModeDB ctx m =
+    ( MonadWalletWebDB ctx m
+    , MonadIO m
+    , MonadMockable m
+    , HasConfiguration
+    )
 
 queryDisk
-    :: (EventState event ~ WalletStorage, QueryEvent event, WebWalletModeDB ctx m)
+    :: (EventState event ~ WalletStorage, QueryEvent event,
+        MonadWalletWebDB ctx m, MonadIO m)
     => event -> m (EventResult event)
 queryDisk e = getWalletWebState >>= flip A.query e
 
 updateDisk
-    :: (EventState event ~ WalletStorage, UpdateEvent event, WebWalletModeDB ctx m)
+    :: (EventState event ~ WalletStorage, UpdateEvent event,
+        MonadWalletWebDB ctx m, MonadIO m)
     => event -> m (EventResult event)
 updateDisk e = getWalletWebState >>= flip A.update e
 

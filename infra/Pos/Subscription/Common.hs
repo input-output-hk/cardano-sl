12dae1c9c0047b34719d5b7e1ffe47bcbbc08db9
Andrzej Rybczak <andrzej@rybczak.net>
2017-12-19 04:10:49 +0100

    [CSL-1676] Use versioning mechanism for backwards compatibility

--- infra/Pos/Subscription/Common.hs
+++ infra/Pos/Subscription/Common.hs
@@ -5,7 +5,6 @@ module Pos.Subscription.Common
     ( SubscriptionMode
     , SubscriptionTerminationReason (..)
     , subscribeTo
-    , subscriptionListener
     , subscriptionListeners
     , subscriptionWorker
     ) where
@@ -13,6 +12,7 @@ module Pos.Subscription.Common
 import qualified Network.Broadcast.OutboundQueue as OQ
 import           Network.Broadcast.OutboundQueue.Types (removePeer, simplePeers)
 import           Universum hiding (bracket)
+import qualified Data.List.NonEmpty as NE
 
 import           Formatting (sformat, shown, (%))
 import           Mockable (Bracket, Catch, Mockable, Throw, bracket, try)
@@ -23,7 +23,8 @@ import           Pos.Binary.Class (Bi)
 import           Pos.Communication.Limits.Types (MessageLimited, recvLimited)
 import           Pos.Communication.Listener (listenerConv)
 import           Pos.Communication.Protocol (Conversation (..), ConversationActions (..),
-                                             ListenerSpec, MkListeners, MsgSubscribe (..), NodeId,
+                                             ListenerSpec, MkListeners, MsgSubscribe (..),
+                                             MsgSubscribe1 (..), NodeId,
                                              OutSpecs, SendActions, Worker, WorkerSpec,
                                              constantListeners, convH, toOutSpecs, withConnectionTo,
                                              worker)
@@ -39,8 +40,11 @@ type SubscriptionMode m =
     , Mockable Bracket m
     , MonadKnownPeers m
     , Message MsgSubscribe
+    , Message MsgSubscribe1
     , MessageLimited MsgSubscribe m
+    , MessageLimited MsgSubscribe1 m
     , Bi MsgSubscribe
+    , Bi MsgSubscribe1
     , Message Void
     )
 
@@ -58,19 +62,31 @@ subscribeTo
     => Timer -> SendActions m -> NodeId -> m SubscriptionTerminationReason
 subscribeTo keepAliveTimer sendActions peer = do
     logNotice $ msgSubscribingTo peer
-    outcome <- try $ withConnectionTo sendActions peer $ \_peerData -> do
-        pure $ Conversation $ \(conv :: ConversationActions MsgSubscribe Void m) -> do
-            send conv MsgSubscribe
-            forever $ do
-                startTimer keepAliveTimer
-                atomically $ waitTimer keepAliveTimer
-                logDebug $ sformat ("subscriptionWorker: sending keep-alive to "%shown)
-                                    peer
-                send conv MsgSubscribeKeepAlive
+    outcome <- try $ withConnectionTo sendActions peer $ \_peerData -> NE.fromList
+        -- Sort conversations in descending order based on their version so that
+        -- the highest available version of the conversation is picked.
+        [ Conversation convMsgSubscribe
+        , Conversation convMsgSubscribe1
+        ]
     let reason = either Exceptional (maybe Normal absurd) outcome
     logNotice $ msgSubscriptionTerminated peer reason
     return reason
   where
+    convMsgSubscribe :: ConversationActions MsgSubscribe Void m -> m t
+    convMsgSubscribe conv = do
+        send conv MsgSubscribe
+        forever $ do
+            startTimer keepAliveTimer
+            atomically $ waitTimer keepAliveTimer
+            logDebug $ sformat ("subscriptionWorker: sending keep-alive to "%shown)
+                                peer
+            send conv MsgSubscribeKeepAlive
+
+    convMsgSubscribe1 :: ConversationActions MsgSubscribe1 Void m -> m (Maybe Void)
+    convMsgSubscribe1 conv = do
+        send conv MsgSubscribe1
+        recv conv 0 -- Other side will never send
+
     msgSubscribingTo :: NodeId -> Text
     msgSubscribingTo = sformat $ "subscriptionWorker: subscribing to "%shown
 
@@ -86,28 +102,50 @@ subscriptionListener
     => OQ.OutboundQ pack NodeId Bucket
     -> NodeType
     -> (ListenerSpec m, OutSpecs)
-subscriptionListener oq nodeType = listenerConv @Void oq $ \__ourVerInfo nodeId conv -> do
+subscriptionListener oq nodeType = listenerConv @Void oq $ \_ourVerInfo nodeId conv -> do
     recvLimited conv >>= \case
         Just MsgSubscribe -> do
             let peers = simplePeers [(nodeType, nodeId)]
             bracket
               (updatePeersBucket BucketSubscriptionListener (<> peers))
-              (\added -> when added $
-                void $ updatePeersBucket BucketSubscriptionListener (removePeer nodeId))
-              (\added -> when added . fix $ \loop -> recvLimited conv >>= \case
-                  Just MsgSubscribeKeepAlive -> do
-                      logDebug $ sformat
-                          ("subscriptionListener: received keep-alive from "%shown)
-                          nodeId
-                      loop
-                  msg -> logNotice $ expectedMsgFromGot MsgSubscribeKeepAlive
-                                                        nodeId msg
-              ) -- if not added, close the conversation
+              (\added -> when added $ do
+                void $ updatePeersBucket BucketSubscriptionListener (removePeer nodeId)
+                logDebug $ sformat ("subscriptionListener: removed "%shown) nodeId)
+              (\added -> when added $ do -- if not added, close the conversation
+                  logDebug $ sformat ("subscriptionListener: added "%shown) nodeId
+                  fix $ \loop -> recvLimited conv >>= \case
+                      Just MsgSubscribeKeepAlive -> do
+                          logDebug $ sformat
+                              ("subscriptionListener: received keep-alive from "%shown)
+                              nodeId
+                          loop
+                      msg -> logNotice $ expectedMsgFromGot MsgSubscribeKeepAlive
+                                                            nodeId msg)
         msg -> logNotice $ expectedMsgFromGot MsgSubscribe nodeId msg
   where
     expectedMsgFromGot = sformat
             ("subscriptionListener: expected "%shown%" from "%shown%
              ", got "%shown%", closing the connection")
+
+-- | Version of subscriptionListener for MsgSubscribe1.
+subscriptionListener1
+    :: forall pack m.
+       (SubscriptionMode m)
+    => OQ.OutboundQ pack NodeId Bucket
+    -> NodeType
+    -> (ListenerSpec m, OutSpecs)
+subscriptionListener1 oq nodeType = listenerConv @Void oq $ \_ourVerInfo nodeId conv -> do
+    mbMsg <- recvLimited conv
+    whenJust mbMsg $ \MsgSubscribe1 -> do
+      let peers = simplePeers [(nodeType, nodeId)]
+      bracket
+          (updatePeersBucket BucketSubscriptionListener (<> peers))
+          (\added -> when added $ do
+              void $ updatePeersBucket BucketSubscriptionListener (removePeer nodeId)
+              logDebug $ sformat ("subscriptionListener1: removed "%shown) nodeId)
+          (\added -> when added $ do -- if not added, close the conversation
+              logDebug $ sformat ("subscriptionListener1: added "%shown) nodeId
+              void $ recvLimited conv)
 
 subscriptionListeners
     :: forall pack m.
@@ -115,7 +153,10 @@ subscriptionListeners
     => OQ.OutboundQ pack NodeId Bucket
     -> NodeType
     -> MkListeners m
-subscriptionListeners oq nodeType = constantListeners [subscriptionListener oq nodeType]
+subscriptionListeners oq nodeType = constantListeners
+    [ subscriptionListener  oq nodeType
+    , subscriptionListener1 oq nodeType
+    ]
 
 -- | Throw the standard subscription worker OutSpecs onto a given
 -- implementation of a single subscription worker.
@@ -125,4 +166,7 @@ subscriptionWorker
 subscriptionWorker theWorker = first (:[]) (worker subscriptionWorkerSpec theWorker)
   where
     subscriptionWorkerSpec :: OutSpecs
-    subscriptionWorkerSpec = toOutSpecs [ convH (Proxy @MsgSubscribe) (Proxy @Void) ]
+    subscriptionWorkerSpec = toOutSpecs
+        [ convH (Proxy @MsgSubscribe)  (Proxy @Void)
+        , convH (Proxy @MsgSubscribe1) (Proxy @Void)
+        ]

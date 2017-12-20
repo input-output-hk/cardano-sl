d3503c43485b5cf44a04ffc8def94c823360b561
Kosyrev Serge <serge.kosyrev@iohk.io>
2017-12-20 14:43:18 +0300

    [DEVOPS-497]  buildkite:  detect OS & derive profile path

--- scripts/ci/nix-shell.sh
+++ scripts/ci/nix-shell.sh
@@ -2,13 +2,19 @@
 
 set -x
 
+case $(uname -s | tr A-Z a-z) in
+        linux )  NIX_PROFILE_BINPATH=/run/current-system/sw/bin/;;
+        darwin ) NIX_PROFILE_BINPATH=/nix/var/nix/profiles/default/bin;;
+        * )      echo "Unsupported OS: $OS_NAME" >&2; exit 1;;
+esac
+
 # Bootstrap on OS X/NixOS:
 NIX_BUILD="$(type -P nix-build)"
-NIX_BUILD="${NIX_BUILD:-/run/current-system/sw/bin/nix-build}"
+NIX_BUILD="${NIX_BUILD:-$NIX_PROFILE_BINPATH/nix-build}"
 NIX_SHELL="$(type -P nix-shell)"
-NIX_SHELL="${NIX_SHELL:-/run/current-system/sw/bin/nix-shell}"
+NIX_SHELL="${NIX_SHELL:-$NIX_PROFILE_BINPATH/nix-shell}"
 NIX_BUILD_SHELL="$(type -P bash)"
-NIX_BUILD_SHELL="${NIX_BUILD_SHELL:-/run/current-system/sw/bin/bash}"
+NIX_BUILD_SHELL="${NIX_BUILD_SHELL:-$NIX_PROFILE_BINPATH/bash}"
 
 export NIX_REMOTE=daemon
 export NIX_PATH="nixpkgs=$(${NIX_BUILD} fetch-nixpkgs.nix -o nixpkgs)"

d03240528a02fcb1c03976b31d7c443cf5c4c68f
James Michael DuPont <jamesmikedupont+build@gmail.com>
2017-12-30 07:33:51 -0500

    webtypes

--- lib/src/Pos/Web/Types.hs
+++ lib/src/Pos/Web/Types.hs
@@ -9,12 +9,17 @@ module Pos.Web.Types
 import           Universum
 
 -- | Stages of SSC.
+-- also called GodTossing algorithm.
+-- GodTossing is a coin tossing with guaranteed output delivery. Nodes exchange commitments, openings, and shares, and in the end arrive at a shared seed.
+-- See https://eprint.iacr.org/2016/889.pdf (“A Provably Secure Proof-of-Stake Blockchain Protocol”), section 4 for more details.
+
 data SscStage
     = CommitmentStage
     | OpeningStage
     | SharesStage
     | OrdinaryStage
 
+-- | TLS Transport Layer Security file paths.
 data TlsParams = TlsParams
     { tpCertPath :: FilePath
     , tpCaPath   :: FilePath

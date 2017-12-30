aad3d95975ff724c1584c931f81768e65afb7c16
Lionel Miller <delonorm@gmail.com>
2017-12-30 16:41:37 +0300

    [CSL-2129] Fix script for blockchain dumping

--- scripts/auxx/dump_blockchain.sh
+++ scripts/auxx/dump_blockchain.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
-TOPOLOGY_FILE=/tmp/topology-mainnet.yaml
 CLUSTER=mainnet
-RELAY=relays.cardano-mainnet.iohk.io
+TOPOLOGY_FILE=/tmp/topology-$CLUSTER.yaml
 OUTPATH=/tmp/cardano-$CLUSTER-blockchain_$(date +%Y-%m-%d_%H-%M-%S)
 
+RELAY=relays.cardano-mainnet.iohk.io
+
+# Parse output of `nslookup` and extract IP addresses of the bootstrap nodes,
+# then format them into `--peer <IP>:3000` arguments.
 PEERS=$(\
     nslookup $RELAY | \
-    awk '/^Address: / { print "--peer", $2 }' | \
+    awk '/^Address: / { print "--peer", $2 ":3000" }' | \
     paste -sd ' ')
 
 echo "\
@@ -24,15 +27,18 @@ if [[ $1 == '--nowait' ]]; then
 	echo "Auxx does not support waiting for synchronization."
 	echo "The blockchain will be dumped without waiting until"
 	echo "it is downloaded completely."
+  echo ""
 elif [[ $1 == '--wait' ]]; then
-	EXECMODE="repl"
-	echo "Auxx does not support waiting for synchronization."
-	echo "As soon as the logs show that the node is synched,"
-	echo "execute the following command:"
-	echo ""
-	echo "    dump $OUTPATH"
+  EXECMODE="repl"
+  echo "Auxx does not support waiting for synchronization."
+  echo "As soon as the logs show that the node is synched,"
+  echo "execute the following command:"
+  echo ""
+  echo "    dump $OUTPATH"
+  echo ""
 else
 	echo "Usage: $0 [--wait|--nowait]"
+  exit
 fi;
 
 stack exec -- cardano-auxx --mode=with-node               \

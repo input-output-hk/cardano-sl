b65be9bf9a71334b08d1849d2bee20f8bbcf9886
Martoon <martoon.hsk@gmail.com>
2018-01-01 14:40:08 +0300

    [CSL-2135] Forget above peer approval of tx on manual cancellation

--- node/src/Pos/Wallet/Web/Pending/Updates.hs
+++ node/src/Pos/Wallet/Web/Pending/Updates.hs
@@ -60,6 +60,7 @@ cancelApplyingPtx :: HasConfiguration => PendingTx -> PendingTx
 cancelApplyingPtx ptx@PendingTx{..}
     | PtxApplying poolInfo <- _ptxCond =
           ptx { _ptxCond = PtxWontApply reason poolInfo
+              , _ptxPeerAck = False
               }
     | otherwise = ptx
   where

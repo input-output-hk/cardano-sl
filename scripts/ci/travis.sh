2d51632a678b053c0b7a8ead77a176adc5d24f44
Domen Kožar <domen@enlambda.com>
2018-01-03 17:35:10 +0000

    [DEVOPS-497] backport buildkite to exchanges (#2240)
    * [DEVOPS-497]  Make Domen the code owner for scripts/ci/
    
    * [DEVOPS-497]  Buildkite CI prototype
    
    * [DEVOPS-528]  buildkite:  remove travis-isms
    
    * [DEVOPS-528]  buildkite:  three phases from old CI
    
    * [DEVOPS-528]  travis.sh: drop
    
    * [DEVOPS-528]  buildkite WIP
    
    * [DEVOPS-497]  buildkite:  upload artifacts
    
    * [DEVOPS-497]  ci.sh:  mirror daedalus bridge removal
    
    * [DEVOPS-497]  buildkite:  re-enable disabled parts of the pipeline
    
    * [DEVOPS-497]  buildkite:  change output pipeline
    
    * [DEVOPS-497]  buildkite:  disable the waiting for hlint
    
    * [DEVOPS-589]  buildkite | cardano-sl-networking:  disable tests
    
    * [DEVOPS-497]  buildkite:  switch to Buildkite internal artifactory
    
    * [DEVOPS-497]  buildkite:  detect OS & derive profile path
    
    * [DEVOPS-497]  buildkite:  quite prebuild phase
    
    * Revert "[DEVOPS-497]  buildkite:  switch to Buildkite internal artifactory"
    
    This reverts commit 4fb3f6bc246a0547ca7ae8f4954e4a5f2beabb1f.
    
    * [DEVOPS-497]  buildkite:  massage artifact upload
    
    * [DEVOPS-497]  buildkite:  slightly more order in artifacts uploaded
    
    * [DEVOPS-497]  travis:  restore
    
    * [DEVOPS-497]  packaging:  node->lib
    
    * [DEVOPS-497]  buildkite:  disable the quiet pre-building
    
    * [DEVOPS-497]  Hydra: build cardano-sl-auxx
    
    * [DEVOPS-497]  cardano:  node/lib for configuration and genesis

--- scripts/ci/travis.sh
+++ scripts/ci/travis.sh
@@ -16,7 +16,7 @@ fi
 
 # TODO: CSL-1133: Add test coverage to CI. To be reenabled when build times
 # become smaller and allow coverage report to be built.
-#projects="core db lrc infra update ssc godtossing txp"
+#projects="core db lrc infra update ssc txp"
 #to_build=''
 
 #for prj in $projects; do
@@ -49,6 +49,7 @@ pushd daedalus
   nix-shell --run "npm install && npm run build:prod"
   echo $TRAVIS_BUILD_NUMBER > build-id
   echo $TRAVIS_COMMIT > commit-id
+  echo https://travis-ci.org/${TRAVIS_REPO_SLUG}/jobs/$TRAVIS_JOB_ID > ci-url
   cp ../log-config-prod.yaml .
   cp ../node/configuration.yaml .
   cp ../node/*genesis*.json .

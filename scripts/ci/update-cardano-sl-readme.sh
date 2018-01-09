d9ece339caa64ed9ea2147e4992c8105bcccba4c
Kosyrev Serge <deepfire@users.noreply.github.com>
2018-01-09 21:38:18 +0300

    [DEVOPS-528] Remove Travis integration (#2291)
    * [DEVOPS-528]  travis: remove CI integration
    
    * [DEVOPS-528]  README:  migrate status badge to Buildkite
    
    * [DEVOPS-528]  doc | ci.sh:  documentation auto-update depends on env vars no longer provided
    
    * [DEVOPS-528]  pkgs/default.nix:  regenerate

--- scripts/ci/update-cardano-sl-readme.sh
+++ scripts/ci/update-cardano-sl-readme.sh
@@ -31,7 +31,7 @@ echo "**** Building ${README} from documentation ****"
   echo ""
   echo "# Cardano SL";
   echo "";
-  echo "[![Build Status](https://travis-ci.org/input-output-hk/cardano-sl.svg)](https://travis-ci.org/input-output-hk/cardano-sl)";
+  echo "[![Build status](https://badge.buildkite.com/9c3141d21214ff3ea95d0a38a0e1dab59b206159d2841dee44.svg)](https://buildkite.com/input-output-hk/cardano-sl)";
   echo "[![Windows build status](https://ci.appveyor.com/api/projects/status/github/input-output-hk/cardano-sl?branch=master&svg=true)](https://ci.appveyor.com/project/jagajaga/cardano-sl)";
   echo "[![Release](https://img.shields.io/github/release/input-output-hk/cardano-sl.svg)](https://github.com/input-output-hk/cardano-sl/releases)";
   echo "";

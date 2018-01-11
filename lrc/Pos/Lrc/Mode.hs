8360826751eb7ae9fef5bb88b0dc54e95854c445
Vladislav Zavialov <vlad.z.4096@gmail.com>
2018-01-11 14:20:23 +0300

    [CSL-738] cardano-sl-lrc

--- lrc/Pos/Lrc/Mode.hs
+++ lrc/Pos/Lrc/Mode.hs
@@ -8,7 +8,7 @@ module Pos.Lrc.Mode
 
 import           Universum
 
-import           Mockable (Async, Bracket, Concurrently, Delay, Mockables)
+import           Mockable (Async, Concurrently, Delay, Mockables)
 import           System.Wlog (WithLogger)
 
 import           Pos.Core (HasConfiguration)
@@ -22,7 +22,7 @@ type LrcMode ctx m
        , MonadGState m
        , MonadDB m
        , MonadIO m
-       , Mockables m [Async, Bracket, Concurrently, Delay]
+       , Mockables m [Async, Concurrently, Delay]
                      -- ^ alphabet for the youngest haskellers
        , MonadReader ctx m
        , HasLrcContext ctx

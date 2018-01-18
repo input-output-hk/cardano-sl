c69c6188cf9498a52e1fc631543e8640b2d13157
Sasa Bogicevic <t4nt0r@protonmail.com>
2018-01-18 23:17:14 +0100

    [CSL-1838] Try to report DBError in reportOrLog fnc (#2229)

--- infra/Pos/Reporting/Methods.hs
+++ infra/Pos/Reporting/Methods.hs
@@ -63,6 +63,7 @@ import           System.Wlog (LoggerConfig (..), Severity (..), WithLogger, hwFi
 import           Paths_cardano_sl_infra (version)
 import           Pos.Core.Configuration (HasConfiguration, protocolMagic)
 import           Pos.Crypto (ProtocolMagic (..))
+import           Pos.DB.Error (DBError (..))
 import           Pos.Exception (CardanoFatalError)
 import           Pos.KnownPeers (MonadFormatPeers (..))
 import           Pos.Network.Types (HasNodeType (..), NodeType (..))
@@ -72,7 +73,6 @@ import           Pos.Reporting.MemState (HasLoggerConfig (..), HasReportServers 
 import           Pos.Util.CompileInfo (HasCompileInfo, compileInfo)
 import           Pos.Util.Filesystem (withSystemTempFile)
 import           Pos.Util.Util (maybeThrow, (<//>))
-
 
 ----------------------------------------------------------------------------
 -- General purpose/low level
@@ -380,7 +380,7 @@ reportOrLog
     :: forall ctx m . (MonadReporting ctx m)
     => Severity -> Text -> SomeException -> m ()
 reportOrLog severity prefix exc =
-    case tryCast @CardanoFatalError <|> tryCast @ErrorCall of
+    case tryCast @CardanoFatalError <|> tryCast @ErrorCall <|> tryCast @DBError of
         Just msg -> reportError $ prefix <> msg
         Nothing  -> logMessage severity $ prefix <> pretty exc
   where

27de172e6e48bc9bc55c14e6620eefefa075d646
Alexander Vieth <aovieth@gmail.com>
2018-01-22 11:22:12 -0500

    [CSL-2149] checkUpdateProposal, checkUpdateVote

--- core/Pos/Core/Update/Util.hs
+++ core/Pos/Core/Update/Util.hs
@@ -2,8 +2,10 @@
 
 module Pos.Core.Update.Util
        (
-         -- * Constructors
-         mkUpdateProposal
+         -- * Checkers/validators.
+         checkUpdateProposal
+       , checkUpdateVote
+
        , mkUpdateProposalWSign
        , mkVoteId
        , mkUpdateProof
@@ -31,41 +33,35 @@ import           Pos.Core.Update.Types (BlockVersion, BlockVersionModifier (..),
                                         SoftwareVersion, SystemTag, UpAttributes, UpdateData,
                                         UpdatePayload, UpdateProof, UpdateProposal (..),
                                         UpdateProposalToSign (..), UpdateVote (..), VoteId)
-import           Pos.Crypto (PublicKey, SafeSigner, SignTag (SignUSProposal), Signature, checkSig,
-                             hash, safeSign, safeToPublic)
+import           Pos.Crypto (PublicKey, SafeSigner, SignTag (SignUSProposal, SignUSVote),
+                             Signature, checkSig, hash, safeSign, safeToPublic)
 
 -- | 'SoftforkRule' formatter which restricts type.
 softforkRuleF :: Format r (SoftforkRule -> r)
 softforkRuleF = build
 
-mkUpdateProposal
-    :: (HasConfiguration, Bi UpdateProposalToSign)
-    => BlockVersion
-    -> BlockVersionModifier
-    -> SoftwareVersion
-    -> HM.HashMap SystemTag UpdateData
-    -> UpAttributes
-    -> PublicKey
-    -> Signature UpdateProposalToSign
-    -> Either Text UpdateProposal
-mkUpdateProposal
-    upBlockVersion
-    upBlockVersionMod
-    upSoftwareVersion
-    upData
-    upAttributes
-    upFrom
-    upSignature = do
-        let toSign =
-                UpdateProposalToSign
-                    upBlockVersion
-                    upBlockVersionMod
-                    upSoftwareVersion
-                    upData
-                    upAttributes
-        unless (checkSig SignUSProposal upFrom toSign upSignature) $
-            Left "UpdateProposal: signature is invalid"
-        pure UnsafeUpdateProposal{..}
+checkUpdateVote
+    :: (HasConfiguration, MonadFail m)
+    => UpdateVote
+    -> m UpdateVote
+checkUpdateVote it =
+    it <$ unless sigValid (fail "UpdateVote: invalid signature")
+  where
+    sigValid = checkSig SignUSVote (uvKey it) (uvProposalId it, uvDecision it) (uvSignature it)
+
+checkUpdateProposal
+    :: (HasConfiguration, MonadFail m, Bi UpdateProposalToSign)
+    => UpdateProposal
+    -> m UpdateProposal
+checkUpdateProposal it = do
+    let toSign = UpdateProposalToSign
+            (upBlockVersion it)
+            (upBlockVersionMod it)
+            (upSoftwareVersion it)
+            (upData it)
+            (upAttributes it)
+    it <$ unless (checkSig SignUSProposal (upFrom it) toSign (upSignature it))
+        (fail $ "UpdateProposal: invalid signature")
 
 mkUpdateProposalWSign
     :: (HasConfiguration, Bi UpdateProposalToSign)

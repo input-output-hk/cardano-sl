aaea81082faf11d92f681925043660159c14c3d4
Denis Shevchenko <denis.shevchenko@iohk.io>
2018-01-25 13:07:19 +0400

    [CBR-23] CLI-options.

--- wallet/bench/Main.hs
+++ wallet/bench/Main.hs
@@ -5,27 +5,38 @@ module Main
 import           Universum
 
 import           Control.Concurrent.Async       (async, wait)
+import           Control.Monad                  (void)
 import           System.IO                      (hSetEncoding, stdout, utf8)
 import           Data.Maybe                     (maybe)
 
-import           Bench.Pos.Wallet.Config        (extractConfigFor, getBenchConfig)
+import           Bench.Pos.Wallet.Config        (extractConfigFor, getBenchConfig,
+                                                 getOptions)
 import           Bench.Pos.Wallet.Run           (runBench)
-import           Bench.Pos.Wallet.Types         (BenchEndpoint (..))
+import           Bench.Pos.Wallet.Types         (BenchEndpoint (..), CLOptions (..))
 import           Client.Pos.Wallet.Web.Endpoint (getHistoryIO, getWalletsIO, newPaymentIO)
 
--- | Use `stack bench cardano-sl-wallet` to run this benchmark.
+-- | Example of benchmark command:
+-- $ stack bench cardano-sl-wallet --benchmark-arguments "--ep-conf=$PWD/wallet/bench/config/Endpoints.csv"
+--
 -- It's a client, so we assume that the node (with Wallet Web API enabled)
 -- is already running.
 main :: IO ()
 main = do
-    config <- getBenchConfig
+    CLOptions {..} <- getOptions
+    conf <- getBenchConfig pathToEndpointsConf
     hSetEncoding stdout utf8
-    putText "Wallet benchmarking begin..."
-    -- Only benchmark with config will be launched.
-    b1 <- async $ maybe (return ()) (runBench getHistoryIO) (extractConfigFor GetHistoryBench config)
-    b2 <- async $ maybe (return ()) (runBench getWalletsIO) (extractConfigFor GetWalletsBench config)
-    b3 <- async $ maybe (return ()) (runBench newPaymentIO) (extractConfigFor NewPaymentBench config)
-    _ <- wait b1
-    _ <- wait b2
-    _ <- wait b3
-    putText $ "Wallet benchmarking done, please see reports (paths are defined in configuration file)."
+    if runConcurrently then do
+        b1 <- async $ maybeRun getHistoryIO GetHistoryBench conf
+        b2 <- async $ maybeRun getWalletsIO GetWalletsBench conf
+        b3 <- async $ maybeRun newPaymentIO NewPaymentBench conf
+        void $ wait b1
+        void $ wait b2
+        void $ wait b3
+    else do
+        maybeRun getHistoryIO GetHistoryBench conf
+        maybeRun getWalletsIO GetWalletsBench conf
+        maybeRun newPaymentIO NewPaymentBench conf
+  where
+    -- | Run benchmark if corresponding config is defined.
+    maybeRun client endpoint configs =
+        maybe (return ()) (runBench client) (extractConfigFor endpoint configs)

5d40d189951c668f896382b0fb2521c971234165
Alexander Vieth <aovieth@gmail.com>
2018-01-29 12:29:45 -0500

    [CSL-2089] remove immedaiteConcurrentConversations

--- infra/Pos/Communication/Types/Protocol.hs
+++ infra/Pos/Communication/Types/Protocol.hs
@@ -25,7 +25,6 @@ module Pos.Communication.Types.Protocol
        , N.Converse (..)
        , SendActions (..)
        , EnqueueMsg
-       , immediateConcurrentConversations
        , enqueueMsg'
        , waitForConversations
        , toOutSpecs
@@ -44,15 +43,12 @@ import           Data.Aeson (FromJSON (..), ToJSON (..), Value)
 import           Data.Aeson.Types (Parser)
 import qualified Data.ByteString.Base64 as B64 (decode, encode)
 import qualified Data.HashMap.Strict as HM
-import qualified Data.Map as M
 import qualified Data.Text.Buildable as B
 import qualified Data.Text.Encoding as Text (decodeUtf8, encodeUtf8)
 import qualified Data.Text.Internal.Builder as B
 import           Formatting (bprint, build, hex, sformat, shown, (%))
 -- TODO should not have to import outboundqueue stuff here. MsgType and
 -- NodeType should be a cardano-sl notion.
-import           Mockable.Class (Mockable)
-import           Mockable.Concurrent (Async, async, wait)
 import           Network.Transport (EndPointAddress (..))
 import qualified Node as N
 import           Node.Message.Class (Message (..), MessageCode)
@@ -92,21 +88,6 @@ data SendActions m = SendActions {
           -> (NodeId -> PeerData -> NonEmpty (Conversation m t))
           -> m (Map NodeId (m t))
     }
-
--- | An 'EnqueueMsg m' which concurrently converses with a list of nodes.
--- This will spawn a conversation against each of them and then return.
---
--- You probably do not want to use this. Use 'enqueueMsg' instead.
-immediateConcurrentConversations
-    :: ( Mockable Async m )
-    => SendActions m
-    -> [NodeId]
-    -> EnqueueMsg m
-immediateConcurrentConversations sendActions peers _ k = do
-    lst <- forM peers $ \peer -> do
-        it <- async $ withConnectionTo sendActions peer $ \pd -> k peer pd
-        return (peer, wait it)
-    return $ M.fromList lst
 
 type EnqueueMsg m =
        forall t .

113e18b1ea7f71951bdb710e975d7d48178497cd
Denis Shevchenko <denis.shevchenko@iohk.io>
2018-01-30 12:32:59 +0400

    [CBR-23] Random delay in the Random module.

--- wallet/bench/Bench/Pos/Wallet/Random.hs
+++ wallet/bench/Bench/Pos/Wallet/Random.hs
@@ -2,15 +2,29 @@
 
 module Bench.Pos.Wallet.Random
     ( pickRandomElementFrom
+    , waitRandom
     ) where
 
 import           Universum
 
-import           Data.List     ((!!))
-import           System.Random (randomRIO)
+import           Control.Concurrent (threadDelay)
+import           Data.List          ((!!))
+import           System.Random      (randomRIO)
 
 -- | It is assumed that list isn't empty.
 pickRandomElementFrom :: MonadIO m => [a] -> m a
 pickRandomElementFrom aList = do
     someIndex <- liftIO $ randomRIO (0, length aList - 1)
     return $ aList !! someIndex
+
+-- | Waiting some random delay.
+-- Values of @from@ and @to@ are already checked:
+-- both are positive and @to@ is greater than @from@.
+waitRandom :: (Double, Double) -> IO ()
+waitRandom (from, to) =
+    randomRIO (fromInMicrosec, toInMicrosec) >>= threadDelay
+  where
+    fromInMicrosec, toInMicrosec :: Int
+    fromInMicrosec = truncate $ from * asMicrosec
+    toInMicrosec   = truncate $ to * asMicrosec
+    asMicrosec = 1000000
--- wallet/bench/Bench/Pos/Wallet/Run.hs
+++ wallet/bench/Bench/Pos/Wallet/Run.hs
@@ -6,13 +6,12 @@ module Bench.Pos.Wallet.Run
 
 import           Universum
 
-import           Gauge.Main             (bench, runMode, nfIO)
-import           Gauge.Main.Options     (Config (..), Mode (..), defaultConfig)
-import           System.Random          (randomRIO)
-import           Control.Concurrent     (threadDelay)
+import           Gauge.Main              (bench, runMode, nfIO)
+import           Gauge.Main.Options      (Config (..), Mode (..), defaultConfig)
 
-import           Bench.Pos.Wallet.Types (CompleteConfig (..), EndpointConfig (..),
-                                         BenchEndpoint (..), EndpointClient)
+import           Bench.Pos.Wallet.Random (waitRandom)
+import           Bench.Pos.Wallet.Types  (CompleteConfig (..), EndpointConfig (..),
+                                          BenchEndpoint (..), EndpointClient)
 
 -- | Runs benchmark using particular client.
 runBench
@@ -26,21 +25,10 @@ runBench endpointClient benchEp completeConfig EndpointConfig {..} =
             config
             [show benchEp]
             [bench benchName $
-                nfIO (endpointClient completeConfig >> wait (minDelayForCalls, maxDelayForCalls))]
+                nfIO (endpointClient completeConfig >>
+                        waitRandom (minDelayForCalls, maxDelayForCalls))]
   where
     config = defaultConfig {
         timeLimit  = Just benchDuration,
         reportFile = Just pathToReportFile
     }
-
--- | Waiting some random delay.
--- Values of @from@ and @to@ are already checked:
--- both are positive and @to@ is greater than @from@.
-wait :: (Double, Double) -> IO ()
-wait (from, to) =
-    randomRIO (fromInMicrosec, toInMicrosec) >>= threadDelay
-  where
-    fromInMicrosec, toInMicrosec :: Int
-    fromInMicrosec = truncate $ from * asMicrosec
-    toInMicrosec   = truncate $ to * asMicrosec
-    asMicrosec = 1000000

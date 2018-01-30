9d5bb5240ec4510cf786731d2553f9d028f73d60
Denis Shevchenko <denis.shevchenko@iohk.io>
2018-01-30 16:58:48 +0400

    [CBR-23] Pick random wallet id.

--- wallet/bench/Client/Pos/Wallet/Web/Endpoint/GetWallet.hs
+++ wallet/bench/Client/Pos/Wallet/Web/Endpoint/GetWallet.hs
@@ -8,17 +8,13 @@ import           Universum
 
 import           Client.Pos.Wallet.Web.Api  (getWallet)
 import           Client.Pos.Wallet.Web.Run  (runEndpointClient)
-import           Bench.Pos.Wallet.Types     (CompleteConfig (..), Wallet (..), WalletsConfig (..))
-import           Bench.Pos.Wallet.Random    (pickRandomElementFrom)
-
-import           Pos.Wallet.Web.ClientTypes (CId (..), CHash (..))
+import           Bench.Pos.Wallet.Types     (CompleteConfig (..))
+import           Bench.Pos.Wallet.Random    (pickRandomWalletIdFrom)
 
 -- | Run 'GetWallet' client. As a result we will get a particular wallet.
 getWalletIO :: CompleteConfig -> IO ()
 getWalletIO conf@CompleteConfig {..} = do
-    walletId <- pickRandomElementFrom walletsIds
+    walletId <- pickRandomWalletIdFrom walletsConfig
     runEndpointClient conf (getWallet walletId) >>= \case
         Left problem -> putText $ "Cannot obtain wallet information: " <> problem
-        Right wallet -> print wallet     -- :: CWallet
-  where
-      walletsIds = [CId (CHash anId) | Wallet anId _ <- toList (wallets walletsConfig)]
+        Right _ -> return ()

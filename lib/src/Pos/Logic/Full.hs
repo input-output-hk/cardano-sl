e4363e7e0f602ec28331d4b8d4e27378cc22f564
Alexander Vieth <aovieth@gmail.com>
2018-01-31 14:39:13 -0500

    [CSL-2031] remove bracket from full logic layer

--- lib/src/Pos/Logic/Full.hs
+++ lib/src/Pos/Logic/Full.hs
@@ -94,153 +94,148 @@ logicLayerFull
     => (JLTxR -> m ())
     -> (LogicLayer m -> m x)
     -> m x
-logicLayerFull jsonLogTx k =
-    bracket acquire release $ \_ -> do
+logicLayerFull jsonLogTx k = do
 
-        -- Delivered monadically but in fact is constant (comes from a
-        -- reader context).
-        ourStakeholderId <- getOurStakeholderId
-        securityParams <- view (lensOf @SecurityParams)
+    -- Delivered monadically but in fact is constant (comes from a
+    -- reader context).
+    ourStakeholderId <- getOurStakeholderId
+    securityParams <- view (lensOf @SecurityParams)
 
-        let
-            getBlock :: HeaderHash -> m (Maybe Block)
-            getBlock = DB.getBlock
+    let
+        getBlock :: HeaderHash -> m (Maybe Block)
+        getBlock = DB.getBlock
 
-            getTip :: m Block
-            getTip = DB.getTipBlock
+        getTip :: m Block
+        getTip = DB.getTipBlock
 
-            getTipHeader :: m BlockHeader
-            getTipHeader = DB.getTipHeader
+        getTipHeader :: m BlockHeader
+        getTipHeader = DB.getTipHeader
 
-            getAdoptedBVData :: m BlockVersionData
-            getAdoptedBVData = gsAdoptedBVData
+        getAdoptedBVData :: m BlockVersionData
+        getAdoptedBVData = gsAdoptedBVData
 
-            recoveryInProgress :: m Bool
-            recoveryInProgress = Recovery.recoveryInProgress
+        recoveryInProgress :: m Bool
+        recoveryInProgress = Recovery.recoveryInProgress
 
-            getBlockHeader :: HeaderHash -> m (Maybe BlockHeader)
-            getBlockHeader = DB.getHeader
+        getBlockHeader :: HeaderHash -> m (Maybe BlockHeader)
+        getBlockHeader = DB.getHeader
 
-            getBlockHeaders
-                :: NonEmpty HeaderHash
-                -> Maybe HeaderHash
-                -> m (Either GetBlockHeadersError (NewestFirst NE BlockHeader))
-            getBlockHeaders checkpoints start = do
-                result <- runExceptT (DB.getHeadersFromManyTo checkpoints start)
-                either (pure . Left . GetBlockHeadersError) (pure . Right) result
+        getBlockHeaders
+            :: NonEmpty HeaderHash
+            -> Maybe HeaderHash
+            -> m (Either GetBlockHeadersError (NewestFirst NE BlockHeader))
+        getBlockHeaders checkpoints start = do
+            result <- runExceptT (DB.getHeadersFromManyTo checkpoints start)
+            either (pure . Left . GetBlockHeadersError) (pure . Right) result
 
-            getBlockHeaders'
-                :: HeaderHash
-                -> HeaderHash
-                -> m (Either GetBlockHeadersError (OldestFirst NE HeaderHash))
-            getBlockHeaders' older newer = do
-                outcome <- DB.getHeadersRange Nothing older newer
-                case outcome of
-                    Left txt -> pure (Left (GetBlockHeadersError txt))
-                    Right it -> pure (Right it)
+        getBlockHeaders'
+            :: HeaderHash
+            -> HeaderHash
+            -> m (Either GetBlockHeadersError (OldestFirst NE HeaderHash))
+        getBlockHeaders' older newer = do
+            outcome <- DB.getHeadersRange Nothing older newer
+            case outcome of
+                Left txt -> pure (Left (GetBlockHeadersError txt))
+                Right it -> pure (Right it)
 
-            postBlockHeader :: BlockHeader -> NodeId -> m ()
-            postBlockHeader = Block.handleUnsolicitedHeader
+        postBlockHeader :: BlockHeader -> NodeId -> m ()
+        postBlockHeader = Block.handleUnsolicitedHeader
 
-            postPskHeavy :: ProxySKHeavy -> m Bool
-            postPskHeavy = Delegation.handlePsk
+        postPskHeavy :: ProxySKHeavy -> m Bool
+        postPskHeavy = Delegation.handlePsk
 
-            postTx = KeyVal
-                { toKey = pure . Tagged . hash . taTx . getTxMsgContents
-                , handleInv = \(Tagged txId) -> not . HM.member txId . _mpLocalTxs <$> getMemPool
-                , handleReq = \(Tagged txId) -> fmap TxMsgContents . HM.lookup txId . _mpLocalTxs <$> getMemPool
-                , handleData = \(TxMsgContents txAux) -> Txp.handleTxDo jsonLogTx txAux
-                }
+        postTx = KeyVal
+            { toKey = pure . Tagged . hash . taTx . getTxMsgContents
+            , handleInv = \(Tagged txId) -> not . HM.member txId . _mpLocalTxs <$> getMemPool
+            , handleReq = \(Tagged txId) -> fmap TxMsgContents . HM.lookup txId . _mpLocalTxs <$> getMemPool
+            , handleData = \(TxMsgContents txAux) -> Txp.handleTxDo jsonLogTx txAux
+            }
 
-            postUpdate = KeyVal
-                { toKey = \(up, _) -> pure . tag $ hash up
-                , handleInv = Update.isProposalNeeded . unTagged
-                , handleReq = Update.getLocalProposalNVotes . unTagged
-                , handleData = Update.handleProposal
-                }
-              where
-                tag = tagWith (Proxy :: Proxy (UpdateProposal, [UpdateVote]))
+        postUpdate = KeyVal
+            { toKey = \(up, _) -> pure . tag $ hash up
+            , handleInv = Update.isProposalNeeded . unTagged
+            , handleReq = Update.getLocalProposalNVotes . unTagged
+            , handleData = Update.handleProposal
+            }
+          where
+            tag = tagWith (Proxy :: Proxy (UpdateProposal, [UpdateVote]))
 
-            postVote = KeyVal
-                { toKey = \UnsafeUpdateVote{..} -> pure $ tag (uvProposalId, uvKey, uvDecision)
-                , handleInv = \(Tagged (id, pk, dec)) -> Update.isVoteNeeded id pk dec
-                , handleReq = \(Tagged (id, pk, dec)) -> Update.getLocalVote id pk dec
-                , handleData = Update.handleVote
-                }
-              where
-                tag = tagWith (Proxy :: Proxy UpdateVote)
+        postVote = KeyVal
+            { toKey = \UnsafeUpdateVote{..} -> pure $ tag (uvProposalId, uvKey, uvDecision)
+            , handleInv = \(Tagged (id, pk, dec)) -> Update.isVoteNeeded id pk dec
+            , handleReq = \(Tagged (id, pk, dec)) -> Update.getLocalVote id pk dec
+            , handleData = Update.handleVote
+            }
+          where
+            tag = tagWith (Proxy :: Proxy UpdateVote)
 
-            postSscCommitment = postSscCommon
-                CommitmentMsg
-                (\(MCCommitment (pk, _, _)) -> addressHash pk)
-                (\id tm -> MCCommitment <$> tm ^. tmCommitments . to getCommitmentsMap . at id)
-                (\(MCCommitment comm) -> sscProcessCommitment comm)
+        postSscCommitment = postSscCommon
+            CommitmentMsg
+            (\(MCCommitment (pk, _, _)) -> addressHash pk)
+            (\id tm -> MCCommitment <$> tm ^. tmCommitments . to getCommitmentsMap . at id)
+            (\(MCCommitment comm) -> sscProcessCommitment comm)
 
-            postSscOpening = postSscCommon
-                OpeningMsg
-                (\(MCOpening key _) -> key)
-                (\id tm -> MCOpening id <$> tm ^. tmOpenings . at id)
-                (\(MCOpening key open) -> sscProcessOpening key open)
+        postSscOpening = postSscCommon
+            OpeningMsg
+            (\(MCOpening key _) -> key)
+            (\id tm -> MCOpening id <$> tm ^. tmOpenings . at id)
+            (\(MCOpening key open) -> sscProcessOpening key open)
 
-            postSscShares = postSscCommon
-                SharesMsg
-                (\(MCShares key _) -> key)
-                (\id tm -> MCShares id <$> tm ^. tmShares . at id)
-                (\(MCShares key shares) -> sscProcessShares key shares)
+        postSscShares = postSscCommon
+            SharesMsg
+            (\(MCShares key _) -> key)
+            (\id tm -> MCShares id <$> tm ^. tmShares . at id)
+            (\(MCShares key shares) -> sscProcessShares key shares)
 
-            postSscVssCert = postSscCommon
-                VssCertificateMsg
-                (\(MCVssCertificate vc) -> getCertId vc)
-                (\id tm -> MCVssCertificate <$> lookupVss id (tm ^. tmCertificates))
-                (\(MCVssCertificate cert) -> sscProcessCertificate cert)
+        postSscVssCert = postSscCommon
+            VssCertificateMsg
+            (\(MCVssCertificate vc) -> getCertId vc)
+            (\id tm -> MCVssCertificate <$> lookupVss id (tm ^. tmCertificates))
+            (\(MCVssCertificate cert) -> sscProcessCertificate cert)
 
-            postSscCommon
-                :: ( Buildable err, Buildable contents )
-                => SscTag
-                -> (contents -> StakeholderId)
-                -> (StakeholderId -> TossModifier -> Maybe contents)
-                -> (contents -> ExceptT err m ())
-                -> KeyVal (Tagged contents StakeholderId) contents m
-            postSscCommon sscTag contentsToKey toContents processData = KeyVal
-                { toKey = pure . tagWith contentsProxy . contentsToKey
-                , handleInv = sscIsDataUseful sscTag . unTagged
-                , handleReq = \(Tagged addr) -> toContents addr . view ldModifier <$> sscRunLocalQuery ask
-                , handleData = \dat -> do
-                      let addr = contentsToKey dat
-                      -- [CSL-685] TODO: Add here malicious emulation for network
-                      -- addresses when TW will support getting peer address
-                      -- properly
-                      -- Stale comment?
-                      handleDataDo dat addr =<< shouldIgnorePkAddress addr
-                }
-              where
-                contentsProxy = (const Proxy :: (contents -> k) -> Proxy contents) contentsToKey
-                ignoreFmt =
-                    "Malicious emulation: data "%build%" for id "%build%" is ignored"
-                handleDataDo dat id shouldIgnore
-                    | shouldIgnore = False <$ logDebug (sformat ignoreFmt id dat)
-                    | otherwise = sscProcessMessage processData dat
-                sscProcessMessage sscProcessMessageDo dat =
-                    runExceptT (sscProcessMessageDo dat) >>= \case
-                        Left err -> False <$ logDebug (sformat ("Data is rejected, reason: "%build) err)
-                        Right () -> return True
+        postSscCommon
+            :: ( Buildable err, Buildable contents )
+            => SscTag
+            -> (contents -> StakeholderId)
+            -> (StakeholderId -> TossModifier -> Maybe contents)
+            -> (contents -> ExceptT err m ())
+            -> KeyVal (Tagged contents StakeholderId) contents m
+        postSscCommon sscTag contentsToKey toContents processData = KeyVal
+            { toKey = pure . tagWith contentsProxy . contentsToKey
+            , handleInv = sscIsDataUseful sscTag . unTagged
+            , handleReq = \(Tagged addr) -> toContents addr . view ldModifier <$> sscRunLocalQuery ask
+            , handleData = \dat -> do
+                  let addr = contentsToKey dat
+                  -- [CSL-685] TODO: Add here malicious emulation for network
+                  -- addresses when TW will support getting peer address
+                  -- properly
+                  -- Stale comment?
+                  handleDataDo dat addr =<< shouldIgnorePkAddress addr
+            }
+          where
+            contentsProxy = (const Proxy :: (contents -> k) -> Proxy contents) contentsToKey
+            ignoreFmt =
+                "Malicious emulation: data "%build%" for id "%build%" is ignored"
+            handleDataDo dat id shouldIgnore
+                | shouldIgnore = False <$ logDebug (sformat ignoreFmt id dat)
+                | otherwise = sscProcessMessage processData dat
+            sscProcessMessage sscProcessMessageDo dat =
+                runExceptT (sscProcessMessageDo dat) >>= \case
+                    Left err -> False <$ logDebug (sformat ("Data is rejected, reason: "%build) err)
+                    Right () -> return True
 
-            -- TODO implement these proper
-            -- I do not know of an existing program which will do what we want:
-            -- check the block (header) in isolation; a coarse-but-fast test
-            -- to eliminate obviously bogus things. False positive possible,
-            -- but no false negatives, like a bloom filter.
-            checkBlockHeader = \_ -> pure True
-            checkBlock = \_ -> pure True
+        -- TODO implement these proper
+        -- I do not know of an existing program which will do what we want:
+        -- check the block (header) in isolation; a coarse-but-fast test
+        -- to eliminate obviously bogus things. False positive possible,
+        -- but no false negatives, like a bloom filter.
+        checkBlockHeader = \_ -> pure True
+        checkBlock = \_ -> pure True
 
-            logic :: Logic m
-            logic = Logic {..}
+        logic :: Logic m
+        logic = Logic {..}
 
-            runLogicLayer :: forall y . m y -> m y
-            runLogicLayer = identity
+        runLogicLayer :: forall y . m y -> m y
+        runLogicLayer = identity
 
-        k (LogicLayer {..})
-
-  where
-    acquire = pure ()
-    release = \_ -> pure ()
+    k (LogicLayer {..})

36b42db50ea4e3017c8851571f7f97628bce9e0e
Marcin Szamotulski <marcin.szamotulski@iohk.io>
2018-02-05 10:34:29 -0400

    [CSL-2260] Use Millisecond type in Timer

--- util/Pos/Util/Timer.hs
+++ util/Pos/Util/Timer.hs
@@ -24,36 +24,37 @@ module Pos.Util.Timer
     , startTimer
     ) where
 
+import           Data.Time.Units (Microsecond)
 import           Control.Concurrent (modifyMVar_)
 import           Control.Concurrent.STM (readTVar, registerDelay, retry)
 import           Universum
 
 data Timer = Timer
-  { timerDuration   :: !(MVar Int)
-  , timerSempaphore :: !(TVar (TVar Bool))
+  { timerDuration   :: !(MVar Microsecond)
+  , timerSemaphore :: !(TVar (TVar Bool))
   }
 
 -- | Create a new, inactive Timer given the number of microseconds. In order to
 -- activate it 'startTimer' needs to be called.
-newTimer :: MonadIO m => Int -> m Timer
+newTimer :: MonadIO m => Microsecond -> m Timer
 newTimer n = Timer <$> newMVar n <*> (newTVarIO True >>= newTVarIO)
 
 -- | Set the duration of a timer to a specified number of microseconds. Note
 -- that if the timer is already started, calling this function wont't restart
 -- it.
-setTimerDuration :: MonadIO m => Timer -> Int -> m ()
+setTimerDuration :: MonadIO m => Timer -> Microsecond -> m ()
 setTimerDuration Timer{..} n = liftIO $ modifyMVar_ timerDuration $ \_ -> return $! n
 
 -- | Wait for the duration associated with the Timer that passed since the last
 -- time 'startTimer' was called.
 waitTimer :: Timer -> STM ()
 waitTimer Timer{..} = do
-  done <- readTVar =<< readTVar timerSempaphore
+  done <- readTVar =<< readTVar timerSemaphore
   unless done retry
 
 -- Start the timer. If the function is called before @t + n@, where @t@ is the
 -- time startTimer was called for the last time and @n@ is the number of
 -- microseconds associated with the Timer, the timer is restarted.
 startTimer :: MonadIO m => Timer -> m ()
-startTimer Timer{..} = liftIO (registerDelay =<< readMVar timerDuration)
-    >>= atomically . writeTVar timerSempaphore
+startTimer Timer{..} = liftIO (registerDelay . fromIntegral =<< readMVar timerDuration)
+    >>= atomically . writeTVar timerSemaphore

8848882931401031e5d33632eadbb493ca1f55b2
Alexander Vieth <aovieth@gmail.com>
2018-02-15 10:52:19 -0500

    [CSL-2149] remove dlg verify createMainBody
    If it's invalid, we have a bug.

--- block/src/Pos/Block/Logic/Creation.hs
+++ block/src/Pos/Block/Logic/Creation.hs
@@ -42,7 +42,7 @@ import           Pos.Crypto (SecretKey)
 import qualified Pos.DB.BlockIndex as DB
 import           Pos.DB.Class (MonadDBRead)
 import           Pos.Delegation (DelegationVar, DlgPayload (..), ProxySKBlockInfo,
-                                 clearDlgMemPool, getDlgMempool, checkDlgPayload)
+                                 clearDlgMemPool, getDlgMempool)
 import           Pos.Exception (assertionFailed, reportFatalError)
 import           Pos.Lrc (HasLrcContext, LrcModeFull, lrcSingleShot)
 import           Pos.Lrc.Context (lrcActionOnEpochReason)
@@ -449,10 +449,6 @@ createMainBody bodyLimit sId payload =
                 usPayload' <- includeUSPayload
                 return (psks', usPayload')
         let dlgPay' = UnsafeDlgPayload psks'
-        -- TBD: is it necessary to check here?
-        -- What if it fails? What will be the behaviour of cardano-sl at
-        -- large?
-        checkDlgPayload dlgPay'
         -- include transactions
         txs' <- takeSome txs
         -- return the resulting block

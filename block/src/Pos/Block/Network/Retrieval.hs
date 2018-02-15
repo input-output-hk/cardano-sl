ecea7cbfa50c85759b9ef7cee9526cc3b7fb173c
Alexander Vieth <aovieth@gmail.com>
2018-02-15 11:01:06 -0500

    [CSL-2149] remove import in block retrieval

--- block/src/Pos/Block/Network/Retrieval.hs
+++ block/src/Pos/Block/Network/Retrieval.hs
@@ -16,7 +16,7 @@ import           Control.Monad.STM (retry)
 import qualified Data.List.NonEmpty as NE
 import           Formatting (build, int, sformat, (%))
 import           Mockable (delay)
-import           Serokell.Util (listJson, sec)
+import           Serokell.Util (sec)
 import           System.Wlog (logDebug, logError, logInfo, logWarning)
 
 import           Pos.Block.BlockWorkMode (BlockWorkMode)

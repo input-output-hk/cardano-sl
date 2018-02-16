a7786ef46108253e220c61e6458094a0afe501ec
michael bishop <michael.bishop@iohk.io>
2018-02-16 10:32:13 -0400

    [DEVOPS-652] pin hydra to the version hydra.iohk.io uses

--- scripts/ci/check-hydra.sh
+++ scripts/ci/check-hydra.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-nix-build '<nixpkgs>' -A hydra
-echo '~~~ Evaluation release.nix'
+nix-build https://github.com/nixos/nixpkgs/archive/4fb198892d298452023ab176e7067da58d30772e.tar.gz -A hydra
+echo '~~~ Evaluating release.nix'
 ./result/bin/hydra-eval-jobs -I . release.nix

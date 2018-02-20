55ffa429635df6d1b635602a172c8089d07f080a
Mikhail Volkhov <volhovm.cs@gmail.com>
2018-02-20 15:06:38 +0300

    [CSL-2193] Move quality check config to block

--- core/Pos/Core/Configuration/Core.hs
+++ core/Pos/Core/Configuration/Core.hs
@@ -14,20 +14,13 @@ module Pos.Core.Configuration.Core
        , coreConfiguration
        , dbSerializeVersion
        , memPoolLimitTx
-       , nonCriticalCQBootstrap
-       , criticalCQBootstrap
-       , nonCriticalCQ
-       , criticalCQ
-       , criticalForkThreshold
-       , fixedTimeCQ
-       , fixedTimeCQSec
+
 
        ) where
 
 import           Universum
 
 import           Data.Reflection (Given (..), give)
-import           Data.Time.Units (Microsecond, Second, convertUnit)
 
 import           Pos.Binary.Class (Raw)
 import           Pos.Core.Genesis.Types (GenesisSpec (..))
@@ -48,34 +41,13 @@ data GenesisConfiguration
 data CoreConfiguration = CoreConfiguration
     {
       -- | Specifies the genesis
-      ccGenesis                :: !GenesisConfiguration
+      ccGenesis            :: !GenesisConfiguration
 
     , -- | Versioning for values in node's DB
-      ccDbSerializeVersion     :: !Word8
+      ccDbSerializeVersion :: !Word8
       -- | Limint on the number of transactions that can be stored in
       -- the mem pool.
-    , ccMemPoolLimitTx         :: !Int
-
-      -- Chain quality thresholds and other constants to detect
-      -- suspicious things.
-
-      -- | If chain quality in bootstrap era is less than this value,
-      -- non critical misbehavior will be reported.
-    , ccNonCriticalCQBootstrap :: !Double
-      -- | If chain quality in bootstrap era is less than this value,
-      -- critical misbehavior will be reported.
-    , ccCriticalCQBootstrap    :: !Double
-      -- | If chain quality after bootstrap era is less than this
-      -- value, non critical misbehavior will be reported.
-    , ccNonCriticalCQ          :: !Double
-      -- | If chain quality after bootstrap era is less than this
-      -- value, critical misbehavior will be reported.
-    , ccCriticalCQ             :: !Double
-      -- | Number of blocks such that if so many blocks are rolled
-      -- back, it requires immediate reaction.
-    , ccCriticalForkThreshold  :: !Int
-      -- | Chain quality will be also calculated for this amount of seconds.
-    , ccFixedTimeCQ            :: !Second
+    , ccMemPoolLimitTx     :: !Int
 
     }
     deriving (Show, Generic)
@@ -97,36 +69,3 @@ dbSerializeVersion = fromIntegral . ccDbSerializeVersion $ coreConfiguration
 -- the mem pool.
 memPoolLimitTx :: (HasCoreConfiguration, Integral i) => i
 memPoolLimitTx = fromIntegral . ccMemPoolLimitTx $ coreConfiguration
-
--- | If chain quality in bootstrap era is less than this value,
--- non critical misbehavior will be reported.
-nonCriticalCQBootstrap :: HasCoreConfiguration => Double
-nonCriticalCQBootstrap = ccNonCriticalCQBootstrap coreConfiguration
-
--- | If chain quality in bootstrap era is less than this value,
--- critical misbehavior will be reported.
-criticalCQBootstrap :: HasCoreConfiguration => Double
-criticalCQBootstrap = ccCriticalCQBootstrap coreConfiguration
-
--- | If chain quality after bootstrap era is less than this
--- value, non critical misbehavior will be reported.
-nonCriticalCQ :: HasCoreConfiguration => Double
-nonCriticalCQ = ccNonCriticalCQ coreConfiguration
-
--- | If chain quality after bootstrap era is less than this
--- value, critical misbehavior will be reported.
-criticalCQ :: HasCoreConfiguration => Double
-criticalCQ = ccCriticalCQ coreConfiguration
-
--- | If chain quality after bootstrap era is less than this
--- value, critical misbehavior will be reported.
-criticalForkThreshold :: (HasCoreConfiguration, Integral i) => i
-criticalForkThreshold = fromIntegral . ccCriticalForkThreshold $ coreConfiguration
-
--- | Chain quality will be also calculated for this amount of time.
-fixedTimeCQ :: HasCoreConfiguration => Microsecond
-fixedTimeCQ = convertUnit fixedTimeCQSec
-
--- | 'fixedTimeCQ' expressed as seconds.
-fixedTimeCQSec :: HasCoreConfiguration => Second
-fixedTimeCQSec = ccFixedTimeCQ coreConfiguration

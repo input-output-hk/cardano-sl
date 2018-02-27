c92c55138ccd2d427074533b6e4e78e9641e8eed
Alexander Vieth <aovieth@gmail.com>
2018-02-27 12:59:50 -0500

    [CSL-2328] cardano-sl-delegation

--- delegation/src/Pos/Arbitrary/Delegation.hs
+++ delegation/src/Pos/Arbitrary/Delegation.hs
@@ -12,21 +12,21 @@ import           Test.QuickCheck.Arbitrary.Generic (genericArbitrary, genericShr
 
 import           Pos.Arbitrary.Core ()
 import           Pos.Binary.Core ()
-import           Pos.Core (EpochIndex, HasConfiguration, HeavyDlgIndex (..))
-import           Pos.Crypto (ProxySecretKey (..), createPsk)
+import           Pos.Core (EpochIndex, HeavyDlgIndex (..))
+import           Pos.Crypto (ProtocolMagic, ProxySecretKey (..), createPsk)
 import           Pos.Delegation.Types (DlgPayload (..), DlgUndo (..))
 
-genDlgPayload :: HasConfiguration => EpochIndex -> Gen DlgPayload
+genDlgPayload :: Arbitrary ProtocolMagic => EpochIndex -> Gen DlgPayload
 genDlgPayload epoch =
     UnsafeDlgPayload . toList . HM.fromList . map convert <$> listOf genPSK
   where
     convert psk = (pskIssuerPk psk, psk)
-    genPSK = createPsk <$> arbitrary <*> arbitrary <*> pure (HeavyDlgIndex epoch)
+    genPSK = createPsk <$> arbitrary <*> arbitrary <*> arbitrary <*> pure (HeavyDlgIndex epoch)
 
-instance HasConfiguration => Arbitrary DlgPayload where
+instance Arbitrary ProtocolMagic => Arbitrary DlgPayload where
     arbitrary = arbitrary >>= genDlgPayload
     shrink = genericShrink
 
-instance HasConfiguration => Arbitrary DlgUndo where
+instance Arbitrary ProtocolMagic => Arbitrary DlgUndo where
     arbitrary = genericArbitrary
     shrink = genericShrink

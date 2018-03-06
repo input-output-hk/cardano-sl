410628b7bd4fb0c5f2180067c1ba2694cf5d50eb
Matt Parsons <parsonsmatt@gmail.com>
2018-03-06 14:06:35 -0700

    [CSL-2339] Vendor but use ValidJSON type

--- wallet-new/test/SwaggerSpec.hs
+++ wallet-new/test/SwaggerSpec.hs
@@ -6,19 +6,26 @@ import           Universum
 
 import qualified Prelude
 
-import qualified Cardano.Wallet.API.V1 as V1
-import           Cardano.Wallet.API.V1.Swagger ()
-import           Cardano.Wallet.Orphans.Aeson ()
-import           Cardano.Wallet.Orphans.Arbitrary ()
 import           Data.String.Conv
 import           Data.Swagger
 import           Pos.Wallet.Aeson.ClientTypes ()
 import           Servant.API.ContentTypes
-import           Servant.Swagger.Test
+import           Servant.Swagger.Test ()
 import           Test.Hspec
 import           Test.Hspec.QuickCheck
 import           Test.QuickCheck.Instances ()
 
+import           Cardano.Wallet.API.Response (ValidJSON)
+import qualified Cardano.Wallet.API.V1 as V1
+import           Cardano.Wallet.API.V1.Swagger ()
+import           Cardano.Wallet.Orphans.Aeson ()
+import           Cardano.Wallet.Orphans.Arbitrary ()
+
+-- for vendored code
+import           Data.Aeson (ToJSON)
+import           Servant.Swagger.Internal.Test (props)
+import           Servant.Swagger.Internal.TypeLevel (BodyTypes, Every, TMap)
+import           Test.QuickCheck (Arbitrary)
 
 -- Syntethic instances and orphans to be able to use `validateEveryToJSON`.
 -- In the future, hopefully, we will never need these.
@@ -32,4 +39,17 @@ instance ToSchema NoContent where
 spec :: Spec
 spec = modifyMaxSuccess (const 10) $
     describe "Swagger Integration" $ do
-        xdescribe "(V1) ToJSON matches ToSchema" $ validateEveryToJSON (Proxy @ V1.API)
+        describe "(V1) ToJSON matches ToSchema" $
+            validateEveryToJSON' (Proxy @ V1.API)
+
+-- vendored
+validateEveryToJSON'
+    :: forall proxy api
+    . TMap (Every [Typeable, Show, Arbitrary, ToJSON, ToSchema]) (BodyTypes ValidJSON api)
+    => proxy api   -- ^ Servant API.
+    -> Spec
+validateEveryToJSON' _ = props
+  (Proxy :: Proxy [ToJSON, ToSchema])
+  (null . validateToJSON)
+  (Proxy :: Proxy (BodyTypes ValidJSON api))
+

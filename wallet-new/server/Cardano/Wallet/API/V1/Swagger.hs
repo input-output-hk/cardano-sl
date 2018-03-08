4fcca715b31fa9d1511035e4fbb8fec83061b496
KtorZ <matthias.benkort@gmail.com>
2018-03-08 12:25:24 +0100

    [CSL-2277] Rework API side-documentation
    The first section has been re-organised to provide a better hierarchical division. A
    walkthough guide has also been adapted from an original cardano-sl-getting-started
    document. Note however that the new API guide doesn't give any details yet about how
    to get some test ADA to work on the integration / debug. There doesn't seem to be any
    equivalent to the /api/wallets/keys endpoint from the V0 API in the V1 API. Also,
    generating a wallet by this method currently returns a 200 OK with an error:
    
    'This key doesn't contain HD wallet info'

--- wallet-new/server/Cardano/Wallet/API/V1/Swagger.hs
+++ wallet-new/server/Cardano/Wallet/API/V1/Swagger.hs
@@ -180,33 +180,183 @@ A **SORT** operation on this $resource. Allowed keys: `$allowedKeys`.
 
 highLevelDescription :: DescriptionEnvironment -> T.Text
 highLevelDescription DescriptionEnvironment{..} = [text|
+This is the specification for the Cardano Wallet API, automatically generated as a [Swagger](https://swagger.io/)
+spec from the [Servant](http://haskell-servant.readthedocs.io/en/stable/) API of [Cardano](https://github.com/input-output-hk/cardano-sl).
 
-This is the specification for the Cardano Wallet API, automatically generated
-as a [Swagger](https://swagger.io/) spec from the [Servant](http://haskell-servant.readthedocs.io/en/stable/) API
-of [Cardano](https://github.com/input-output-hk/cardano-sl).
+Software Version | Git Revision
+-----------------|-------------------
+$deGitRevision   | $deSoftwareVersion
 
-Git revision: **$deGitRevision**
-Software version: **$deSoftwareVersion**
+## Getting Started
 
+In the following examples, we will use *curl* to illustrate request to an API running on the
+default port **8090**.
 
-## Request format (all versions)
+> Please note that wallet web API uses TLS for secure communication. Requests to the API need
+to send a client CA certificate that was used when launching the node and identifies the client as
+being permitted to invoke the server API.
 
-Issuing requests against this API is conceptually not very different from any other web service out there. The API
-is **versioned**, meaning that is possible to access different versions of the API by adding the _version number_
-in the URL.
+### Creating a New Wallet
 
-**For the sake of backward compatibility, we expose the legacy version of the API, available simply as
-unversioned endpoints.**
-
-This means that _omitting_ the version number would call the old version of the API. Examples:
+You can create your first wallet using the `POST /api/v1/wallets` endpoint as follow:
 
 ```
-/api/version
+curl -X POST https://localhost:8090/api/v1/wallets                     \
+     -H "Content-Type: application/json; charset=utf-8"                \
+     -H "Accept: application/json; charset=utf-8"                      \
+     --cacert ./scripts/tls-files/ca.crt                               \
+     -d '{                                                             \
+  "operation": "create",                                               \
+  "backupPhrase": ["squirrel", "material", "silly", "twice", "direct", \
+    "slush", "pistol", "razor", "become", "junk", "kingdom", "flee"],  \
+  "assuranceLevel": "normal",                                          \
+  "name": "MyFirstWallet"                                              \
+}'
 ```
 
-Compatibility between major versions is not _guaranteed_, i.e. the request & response formats might differ.
+As a response, the API provides you with a wallet `id` used in subsequent requests to uniquely
+identity the wallet. Make sure to store it / write it down.
 
-## Response format (V1 onwards)
+```json
+{
+    "status": "success",
+    "data": {
+        "id": "Ae2tdPwUPE...8V3AVTnqGZ",
+        "name": "MyFirstWallet",
+        "balance": 0
+    },
+    "meta": {
+        "pagination": {
+            "totalPages": 1,
+            "page": 1,
+            "perPage": 1,
+            "totalEntries": 1
+        }
+    }
+}
+```
+
+You have just created your first wallet. Information about this wallet can be retrieved using
+the `GET /api/v1/wallets/{walletId}` endpoint as follow:
+
+```
+curl -X GET https://localhost:8090/api/v1/wallets/{{walletId}} \
+     -H "Accept: application/json; charset=utf-8"              \
+     --cacert ./scripts/tls-files/ca.crt                       \
+```
+
+### Receiving Money
+
+To receive money from other user you should provide your address. This address can be obtained
+from an account. Each wallet contains at least one account, you can think of account as a
+pocket inside of your wallet. Besides, you can view all existing accounts of a wallet by using
+the `GET /api/v1/wallets/{{walletId}}/accounts` endpoint as follow:
+
+```
+curl -X GET https://localhost:8090/api/v1/wallets/{{walletId}}/accounts?page=1&per_page=10 \
+     -H "Accept: application/json; charset=utf-8"                                          \
+     --cacert ./scripts/tls-files/ca.crt                                                   \
+```
+
+Since you have, for now, only a single wallet, you'll see something like this:
+
+```json
+{
+    "status": "success",
+    "data": [
+        {
+            "index": 2147483648,
+            "addresses": [
+                "DdzFFzCqrh...fXSru1pdFE"
+            ],
+            "amount": 0,
+            "name": "Initial account",
+            "walletId": "Ae2tdPwUPE...8V3AVTnqGZ"
+        }
+    ],
+    "meta": {
+        "pagination": {
+            "totalPages": 1,
+            "page": 1,
+            "perPage": 10,
+            "totalEntries": 1
+        }
+    }
+}
+```
+
+Each account has at least one address, all listed under the `addresses` field. You can
+communicate one of these addresses to receive money on the associated account.
+
+
+### Sending Money
+
+In order to send money from one of your account to another address, you can create a new
+payment transaction using the `POST /api/v1/transactions` endpoint as follow:
+
+```
+curl -X POST https://localhost:8090/api/v1/transactions \
+     -H "Content-Type: application/json; charset=utf-8" \
+     -H "Accept: application/json; charset=utf-8"       \
+     --cacert ./scripts/tls-files/ca.crt                \
+     -d '{                                              \
+  "destinations": [{                                    \
+    "amount": 14,                                       \
+    "address": "A7k5bz1QR2...Tx561NNmfF"                \
+  }],                                                   \
+  "source": {                                           \
+    "accountIndex": 0,                                  \
+    "walletId": "Ae2tdPwUPE...8V3AVTnqGZ"               \
+  }                                                     \
+}'
+```
+
+Note that, in order to perform a transaction, you need to have some existing coins on the
+source account! When the transaction succeeds, funds are _moved_ from an address to another.
+Note that, you can at any time see the status of your wallets by using the
+`GET /api/v1/transactions/{{walletId}}` endpoint as follow:
+
+```
+curl -X GET https://localhost:8090/api/v1/wallets/{{walletId}}?account_index=0  \
+     -H "Accept: application/json; charset=utf-8"                               \
+     --cacert ./scripts/tls-files/ca.crt                                        \
+```
+
+We have here constrainted the request to a specific account, with our previous transaction the
+output should look roughly similar to this:
+
+```json
+{
+    "status": "success",
+    "data": [
+        {
+            "amount": 14,
+            "inputs": [{
+              "amount": 14,
+              "address": "DdzFFzCqrh...fXSru1pdFE"
+            }],
+            "direction": "outgoing",
+            "outputs": [{
+              "amount": 14,
+              "address": "A7k5bz1QR2...Tx561NNmfF"
+            }],
+            "confirmations": 42,
+            "id": "43zkUzCVi7...TT31uDfEF7",
+            "type": "local"
+        }
+    ],
+    "meta": {
+        "pagination": {
+            "totalPages": 1,
+            "page": 1,
+            "perPage": 10,
+            "totalEntries": 1
+        }
+    }
+}
+```
+
+## Pagination
 
 **All GET requests of the API are paginated by default**. Whilst this can be a source of surprise, is
 the best way of ensuring the performance of GET requests is not affected by the size of the data storage.
@@ -218,14 +368,10 @@ used to modify the shape of the response. In particular, those are:
 * `page`: (Default value: **1**).
 * `per_page`: (Default value: **$deDefaultPerPage**)
 
-For a more accurate description, see the section `Parameters` of each GET request, but as a brief overview
-the first two control how many results and which results to access in a paginated request.
+For a more accurate description, see the section `Parameters` of each GET request, but as a
+brief overview the first two control how many results and which results to access in a
+paginated request.
 
-This is an example of a typical (successful) response from the API:
-
-``` json
-$deWalletResponseExample
-```
 
 ## Filtering and sorting
 
@@ -237,7 +383,7 @@ in the swagger docs with the `FILTER` or `SORT` labels. The query format is quit
 | Operator | Description                                                               | Example                |
 |----------|---------------------------------------------------------------------------|------------------------|
 | -        | If **no operator** is passed, this is equivalent to `EQ` (see below).     | `balance=10`           |
-| `EQ`     | Retrieves the resources with index _equal to the one provided.            | `balance=EQ[10]`       |
+| `EQ`     | Retrieves the resources with index _equal_ to the one provided.           | `balance=EQ[10]`       |
 | `LT`     | Retrieves the resources with index _less than_ the one provided.          | `balance=LT[10]`       |
 | `LTE`    | Retrieves the resources with index _less than equal_ the one provided.    | `balance=LTE[10]`      |
 | `GT`     | Retrieves the resources with index _greater than_ the one provided.       | `balance=GT[10]`       |
@@ -252,7 +398,8 @@ in the swagger docs with the `FILTER` or `SORT` labels. The query format is quit
 | `DES`    | Sorts the resources with the given index in _descending_ order.           | `sort_by=DES[balance]` |
 | -        | If **no operator** is passed, this is equivalent to `DES` (see above).    | `sort_by=balance`      |
 
-## Dealing with errors (V1 onwards)
+
+## Errors
 
 In case a request cannot be served by the API, a non-2xx HTTP response will be issue, together with a
 [JSend-compliant](https://labs.omniti.com/labs/jsend) JSON Object describing the error in detail together
@@ -267,6 +414,27 @@ $deErrorExample
 
 $deWalletErrorTable
 
+
+## Mnemonic Codes
+
+The full list of accepted mnemonic codes to secure a wallet is defined by the BIP-39
+specifications and available [here](https://github.com/bitcoin/bips/blob/master/bip-0039/english.txt).
+
+
+## Versioning & Legacy
+
+The API is **versioned**, meaning that is possible to access different versions of the API by
+adding the _version number_ in the URL.
+
+**For the sake of backward compatibility, we expose the legacy version of the API, available
+simply as unversioned endpoints.**
+
+This means that _omitting_ the version number would call the old version of the API. Deprecated
+endpoints are currently grouped under an appropriate section; they would be removed in upcoming
+released, if you're starting a new integration with Cardano-SL, please ignore these.
+
+Note that Compatibility between major versions is not _guaranteed_, i.e. the request & response
+formats might differ.
 |]
 
 

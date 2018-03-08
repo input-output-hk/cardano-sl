fe8d0e1869e5e62d2ce323ac0ea424a8199f3436
Alexander Vieth <aovieth@gmail.com>
2018-03-08 13:17:44 -0500

    [CSL-2326] killNode on async exception

--- networking/src/Node.hs
+++ networking/src/Node.hs
@@ -306,11 +306,8 @@ node mkEndPoint mkReceiveDelay mkConnectDelay prng packing peerData nodeEnv k = 
               converse :: Converse packing peerData m
               converse = nodeConverse llnode packing
               unexceptional :: m t
-              unexceptional = do
-                  t <- act converse
-                  logNormalShutdown
-                  (LL.stopNode llnode `catch` logNodeException)
-                  return t
+              unexceptional =
+                  act converse <* (logNormalShutdown >> LL.stopNode llnode)
         ; llnode <- LL.startNode
               packing
               peerData
@@ -323,7 +320,7 @@ node mkEndPoint mkReceiveDelay mkConnectDelay prng packing peerData nodeEnv k = 
         }
     unexceptional
         `catch` logException
-        `onException` (LL.stopNode llnode `catch` logNodeException)
+        `onException` (LL.killNode llnode `catch` logNodeException)
   where
     logNormalShutdown :: m ()
     logNormalShutdown =

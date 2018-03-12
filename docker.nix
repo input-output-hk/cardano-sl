351690af058cf615896d7d2889bb09026639bcb3
Samuel Leathers <samuel.leathers@iohk.io>
2018-03-12 10:37:57 -0400

    [DEVOPS-719] docker: run cardano-node inside /wallet

--- docker.nix
+++ docker.nix
@@ -22,6 +22,7 @@ let
       echo '/wallet volume not mounted, you need to create one with `docker volume create` and pass the correct -v flag to `docker run`'
     exit 1
     fi
+    cd /wallet
     exec ${connectToCluster}
   '';
 in pkgs.dockerTools.buildImage {

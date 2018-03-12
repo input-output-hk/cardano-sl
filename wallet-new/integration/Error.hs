24b56fffb8c1c21de2e87f16c69928e2a1f9cd4a
ksaric <kristijan.saric@iohk.io>
2018-03-12 14:17:28 +0100

    [CSL-2341] Added typed exceptions.

--- wallet-new/integration/Error.hs
+++ wallet-new/integration/Error.hs
@@ -12,12 +12,22 @@ import           Universum
 import qualified Data.Text.Buildable
 import           Formatting (bprint, stext, (%))
 
+import           Cardano.Wallet.API.V1.Types (Account (..), Wallet (..))
+
 import           Cardano.Wallet.Client (ClientError)
 
+
 data WalletTestError
-    = Internal Text
-    | HttpClientError ClientError
-    | ServerConnectionFailed
+    = HttpClientError ClientError
+
+    | WalletBalanceNotZero Wallet
+    | LocalWalletDiffers Wallet
+    | LocalWalletsDiffers [Wallet]
+
+    | AccountBalanceNotZero Account
+    | LocalAccountDiffers Account
+    | LocalAccountsDiffers [Account]
+
     deriving (Show, Eq, Generic)
 
 
@@ -25,7 +35,14 @@ instance Exception WalletTestError
 
 
 instance Buildable WalletTestError where
-    build (Internal msg)           = bprint ("Wallet test error ("%stext%")") msg
-    build (ServerConnectionFailed) = bprint "Server connection failed"
-    -- TODO (ks): A proper instance
-    build (HttpClientError _     ) = bprint "Http client error"
+    build (HttpClientError _      )  = bprint "Http client error"
+    -- ^ TODO (ks): A proper instance
+    build (WalletBalanceNotZero  w)  = bprint ("Wallet balance is not zero. Wallet - ("%stext%")") (show w)
+    build (LocalWalletDiffers    w)  = bprint ("Local wallet differs. Wallet - ("%stext%")") (show w)
+    build (LocalWalletsDiffers   w)  = bprint ("Local wallets differs. Wallet - ("%stext%")") (show w)
+
+    build (AccountBalanceNotZero a)  = bprint ("Acccount balance is not zero. Account - ("%stext%")") (show a)
+    build (LocalAccountDiffers   a)  = bprint ("Local account differs. Account - ("%stext%")") (show a)
+    build (LocalAccountsDiffers  a)  = bprint ("Local accounts differs. Account - ("%stext%")") (show a)
+
+

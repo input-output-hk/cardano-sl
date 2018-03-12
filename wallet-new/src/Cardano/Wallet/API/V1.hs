6756ba8d6edbc3c40103bf6645dba3b4e8fe2b75
KtorZ <matthias.benkort@gmail.com>
2018-03-12 18:12:54 +0100

    [CSL-2277] Remove irrelevant / unfinished Updates section on V1

--- wallet-new/src/Cardano/Wallet/API/V1.hs
+++ wallet-new/src/Cardano/Wallet/API/V1.hs
@@ -7,12 +7,10 @@ import qualified Cardano.Wallet.API.V1.Addresses as Addresses
 import qualified Cardano.Wallet.API.V1.Info as Info
 import qualified Cardano.Wallet.API.V1.Settings as Settings
 import qualified Cardano.Wallet.API.V1.Transactions as Transactions
-import qualified Cardano.Wallet.API.V1.Updates as Updates
 import qualified Cardano.Wallet.API.V1.Wallets as Wallets
 
 type API =  Addresses.API
        :<|> Wallets.API
        :<|> Transactions.API
-       :<|> Updates.API
        :<|> Settings.API
        :<|> Info.API

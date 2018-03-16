1da9a2e7175d27df49418baf50bf77a533f65e0a
KtorZ <matthias.benkort@gmail.com>
2018-03-16 14:53:44 +0100

    [CSL-2209] Fix Server-side certificate checks
    The library we pulled in (Data.X509) seems to be thought from a client perspective (at least for the default validation) as I suppose it is the most common use-case. Hence, by default, the validate function tries to verify that the hostname we're trying to reach actually matches one of the alternative names in the target certificate (see #ValidationHooks). This is wrong in our server check for we can't really assume anything about the client's origin. What we can (ought to) do here however is to verify the client's name / identifier to match against a list of accepted client (provided the certificate is valid and was delivered by the approved server's authority).

--- lib/src/Pos/Web/Server.hs
+++ lib/src/Pos/Web/Server.hs
@@ -20,9 +20,12 @@ import qualified Control.Monad.Reader as Mtl
 import           Data.Aeson.TH (defaultOptions, deriveToJSON)
 import qualified Data.ByteString.Char8 as BSC
 import           Data.Default (Default, def)
-import           Data.X509 (HashALG (..))
+import           Data.List (intersect)
+import           Data.X509 (Certificate (..), HashALG (..))
+import qualified Data.X509 as X509
 import           Data.X509.CertificateStore (readCertificateStore)
-import           Data.X509.Validation (defaultChecks, defaultHooks, validate)
+import           Data.X509.Validation (ValidationHooks (..))
+import qualified Data.X509.Validation as X509
 import           Mockable (Async, Mockable, Production (runProduction), withAsync)
 import           Network.TLS (CertificateRejectReason (..), CertificateUsage (..), ServerHooks (..))
 import           Network.Wai (Application)
@@ -105,32 +108,54 @@ tlsParamsToWai
     :: String -> Word16 -> TlsParams -> TLSSettings
 tlsParamsToWai host port TlsParams{..} = tlsSettingsWithCertCheck
   where
-    tlsSettings = tlsSettingsChain tpCertPath [tpCaPath] tpKeyPath
-    tlsSettingsWithCertCheck = tlsSettings {
-        -- Demand a certificate from the client.
-        tlsWantClientCert = True,
-        -- We must handle received certificates in a server hook
-        -- or all connections will fail.
-        tlsServerHooks = hooksWithCertCheck
-    }
-    hooksWithCertCheck = def {
-        onClientCertificate = \certChain ->
-            readCertificateStore tpCaPath >>= \case
-                Nothing -> return $ CertificateUsageReject $
-                    CertificateRejectOther "Cannot init a store, unable to validate client certificates"
-                Just certsStore ->
-                    validate HashSHA256
-                             defaultHooks
-                             defaultChecks
-                             certsStore
-                             def
-                             (host, BSC.pack $ show port)
-                             certChain
-                    >>= \case
-                        []       -> return CertificateUsageAccept
-                        problems -> return $ CertificateUsageReject $
-                            CertificateRejectOther $ show problems
-    }
+    tlsSettings =
+      tlsSettingsChain tpCertPath [tpCaPath] tpKeyPath
+
+    serviceID =
+      (host, BSC.pack (show port))
+
+    -- By default, X509.Validation validates the certificate names against the host
+    -- which is irrelevant when checking the client certificate (but relevant for
+    -- the client when checking the server's certificate).
+    validationHooks = def
+      { hookValidateName = \_ -> validateClient tpClients . certSubjectDN }
+
+    -- Require and validate client certificate
+    tlsSettingsWithCertCheck = tlsSettings
+      { tlsWantClientCert = True
+      , tlsServerHooks    = def
+          { onClientCertificate = fmap acceptOrRejectCertificate . validateCertificate }
+      }
+
+    acceptOrRejectCertificate =
+      maybe CertificateUsageAccept (CertificateUsageReject . CertificateRejectOther)
+
+    -- This solely verify that the provided certificate is valid and was signed by authority we
+    -- recognize (tpCaPath)
+    validateCertificate cert = do
+      mstore <- readCertificateStore tpCaPath
+      maybe
+        (pure $ Just "Cannot init a store, unable to validate client certificates")
+        (fmap fromX509FailedReasons . (\store -> X509.validate HashSHA256 validationHooks X509.defaultChecks store def serviceID cert))
+        mstore
+
+    -- Once a valid certificate is presented, we simply check if the name matches one of the allowed
+    -- client's name. Not much use from an anthentication perspective, but could prevent people from
+    -- using the server certificate to authenticate client. We don't yet rely on the information
+    -- inside the certificate so it won't matter much now but perhaps more in the future.
+    validateClient rs ns =
+      let
+        acceptedNames = fmap fromString rs
+        clientNames   = fmap snd (X509.getDistinguishedElements ns)
+      in
+        case acceptedNames `intersect` clientNames of
+          [] -> [X509.InvalidName "Unrecognized client"]
+          _  -> []
+
+    fromX509FailedReasons reasons =
+      case reasons of
+        [] -> Nothing
+        _  -> Just (show reasons)
 
 ----------------------------------------------------------------------------
 -- Servant infrastructure

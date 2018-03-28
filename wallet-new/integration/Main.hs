a19c9e4bbcabfff2dd5325b8b2c122ca69a4b81a
Ante Kegalj <akegalj@gmail.com>
2018-03-28 08:16:47 +0200

    [CSL-2398] Wire up ClientWallet. Add initial integration test script

--- wallet-new/integration/Main.hs
+++ wallet-new/integration/Main.hs
@@ -2,13 +2,16 @@ module Main where
 
 import           Universum
 
-import           Cardano.Wallet.Client (WalletClient (..))
+import           Cardano.Wallet.Client (WalletClient (..), liftClient)
+import           Cardano.Wallet.Client.Http (mkHttpClient)
+import           Network.HTTP.Client (defaultManagerSettings, newManager)
+import           Servant.Client (BaseUrl (..), Scheme (Http))
 import           System.IO (hSetEncoding, stdout, utf8)
 
 import           CLI
+import           Error
 import           Functions
 import           Types
-import           Error
 
 -- | Here we want to run main when the (local) nodes
 -- have started.
@@ -22,14 +25,21 @@ main = do
     _privKey <- readFile tlsPrivKeyPath
     -- stateless
 
+    -- TODO (akegalj): run server cluster in haskell, instead of using shell scripts
+    -- serverThread <- async (runWalletServer options)
+
     printT "Starting the integration testing for wallet."
 
     when stateless $ do
         printT "The wallet test node is running in stateless mode."
         printT "Stateless mode not implemented currently!"
 
-    let walletClient :: forall m. WalletClient m
-        walletClient = error "Missing implementation for client!"
+    -- TODO (akegalj): move these to CLOptions
+    let baseUrl = BaseUrl Http "127.0.0.1" 8090 mempty
+    manager <- newManager defaultManagerSettings
+
+    let walletClient :: MonadIO m => WalletClient m
+        walletClient = liftClient $ mkHttpClient baseUrl manager
 
     let walletState = WalletState mempty mempty mempty mempty mempty 0
 

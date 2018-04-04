8925fd6e6bb78dda1c3c64adee150afd2f036ed7
Samuel Leathers <disasm@gmail.com>
2018-04-04 19:30:36 -0400

    [DEVOPS-670] Exchanges Documentation (#2673)
    * [DEVOPS-719] docker: run cardano-node inside /wallet
    
    * [DEVOPS-670] allow overriding the wallet port and any other params
    
    (cherry picked from commit 7b121da718f6212fb4ddb01ef491210748d83996)
    
    * [DEVOPS-670] improve documentation for exchanges
    
    * [DEVOPS-670] Proof-read API V1 Documentation
    
    * [DEVOPS-670] update sample wallet config to add note for docker users
    
    * [DEVOPS-670] allow overriding the confKey
    
    (cherry picked from commit 0540686989b3415a66eeb9a6651b4892e8b3986a)
    
    * [DEVOPS-670] cardano-node nixos tests
    
    * [DEVOPS-670] move nixos tests from buildkite to hydra
    
    * [DEVOPS-670] remove WIP from exchange onboarding docs

--- scripts/launch/connect-to-cluster/default.nix
+++ scripts/launch/connect-to-cluster/default.nix
@@ -11,6 +11,8 @@
 , ekgListen ? "127.0.0.1:8000"
 , ghcRuntimeArgs ? "-N2 -qg -A1m -I0 -T"
 , additionalNodeArgs ? ""
+, confKey ? null
+, relays ? null
 }:
 
 with localLib;
@@ -28,6 +30,9 @@ let
     mainnet-staging = {
       relays = "relays.awstest.iohkdev.io";
       confKey = "mainnet_dryrun_full";
+    };
+    override = {
+      inherit relays confKey;
     };
   };
   executables =  {

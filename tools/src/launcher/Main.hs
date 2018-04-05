95f21e514a45266fd41df11bf58f21e77e890c36
Kosyrev Serge <serge.kosyrev@iohk.io>
2018-04-05 16:02:20 +0300

    [DEVOPS-13]  launcher:  add docstrings to the environment substitution functions

--- tools/src/launcher/Main.hs
+++ tools/src/launcher/Main.hs
@@ -183,11 +183,17 @@ reportErrorDefault filename contents = do
     createDirectoryIfMissing True logDir
     writeFile (logDir </> filename) contents
 
+-- * Environment variable parsing and substitution for the launcher configuration file,
+--   typically launcher-config.yaml.
+
+-- | Represent an element of a parse of a string containing environment variable
+--   references: either a plain text piece, or a variable reference.
 data Chunk
   = EnvVar Text
   | Plain  Text
   deriving (Show)
 
+-- | A parse is a sequence of variable references & plain text pieces.
 parseEnvrefs :: Text -> P.Result [Chunk]
 parseEnvrefs text = P.parseString pEnvrefs mempty (toString text)
   where
@@ -210,12 +216,14 @@ parseEnvrefs text = P.parseString pEnvrefs mempty (toString text)
     pEnvvarName :: (Monad p, P.TokenParsing p) => p Text
     pEnvvarName = (P.some $ P.choice [P.alphaNum, P.char '_']) <&> toText
 
+-- | Substitute environment variable references. The 'desc' argument supplies
+-- human-oriented context description in case of error.
 substituteEnvVarsText :: Text -> Text -> IO Text
-substituteEnvVarsText ctx text = do
+substituteEnvVarsText desc text = do
   chunks <- case parseEnvrefs text of
     P.Success r  -> pure r
     P.Failure ei -> do
-      let err = ctx <> "\n" <> (show $ P._errDoc ei)
+      let err = desc <> "\n" <> (show $ P._errDoc ei)
       reportErrorDefault "config-parse-error.log" err
       error err
   substd <- forM chunks $
@@ -225,16 +233,18 @@ substituteEnvVarsText ctx text = do
             case val of
               Just x' -> pure $ toText x'
               Nothing -> do
-                let err = ctx <> "\n" <> "Reference to an undefined environment variable '"<> x <>"'"
+                let err = desc <> "\n" <> "Reference to an undefined environment variable '"<> x <>"'"
                 reportErrorDefault "config-parse-error.log" err
                 error err
   pure $ T.concat substd
 
+-- | Given an Aeson 'Value', parse and substitute environment variables in all
+--   'AE.String' objects.  The 'desc' argument supplies context in case of error.
 substituteEnvVars :: Text -> Value -> IO Value
-substituteEnvVars ctx (AE.String text) = AE.String <$> substituteEnvVarsText ctx text
-substituteEnvVars ctx (AE.Array xs)    = AE.Array  <$> traverse (substituteEnvVars ctx) xs
-substituteEnvVars ctx (AE.Object o)    = AE.Object <$> traverse (substituteEnvVars ctx) o
-substituteEnvVars _   x                = pure x
+substituteEnvVars desc (AE.String text) = AE.String <$> substituteEnvVarsText desc text
+substituteEnvVars desc (AE.Array xs)    = AE.Array  <$> traverse (substituteEnvVars desc) xs
+substituteEnvVars desc (AE.Object o)    = AE.Object <$> traverse (substituteEnvVars desc) o
+substituteEnvVars _    x                = pure x
 
 getLauncherOptions :: IO LauncherOptions
 getLauncherOptions = do

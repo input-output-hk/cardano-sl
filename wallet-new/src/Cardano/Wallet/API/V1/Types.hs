4337b724fe261b17c7a0339f6c1f045e7d77823f
KtorZ <matthias.benkort@gmail.com>
2018-04-24 15:22:09 +0200

    [CBR-190] Remove address balance from wallets' addresses
    Computing this balance is an expensive and innacurate operation that we most probably don't
    want to support in the future. If needed, it will be 'easy' to re-introduce than to remove
    in a non-backward compatible way. Let's therefore remove it before shipping API V1.

--- wallet-new/src/Cardano/Wallet/API/V1/Types.hs
+++ wallet-new/src/Cardano/Wallet/API/V1/Types.hs
@@ -827,7 +827,6 @@ instance BuildableSafeGen AddressValidity where
 -- | Summary about single address.
 data WalletAddress = WalletAddress
     { addrId            :: !(V1 Core.Address)
-    , addrBalance       :: !(V1 Core.Coin)
     , addrUsed          :: !Bool
     , addrChangeAddress :: !Bool
     } deriving (Show, Eq, Generic, Ord)
@@ -838,14 +837,12 @@ instance ToSchema WalletAddress where
     declareNamedSchema =
         genericSchemaDroppingPrefix "addr" (\(--^) props -> props
             & ("id"            --^ "Actual address.")
-            & ("balance"       --^ "Associated balance, in ADA.")
             & ("used"          --^ "True if this address has been used.")
             & ("changeAddress" --^ "True if this address stores change from a previous transaction.")
         )
 
 instance Arbitrary WalletAddress where
     arbitrary = WalletAddress <$> arbitrary
-                              <*> arbitrary
                               <*> arbitrary
                               <*> arbitrary
 
@@ -951,12 +948,10 @@ deriveSafeBuildable ''WalletAddress
 instance BuildableSafeGen WalletAddress where
     buildSafeGen sl WalletAddress{..} = bprint ("{"
         %" id="%buildSafe sl
-        %" balance="%buildSafe sl
         %" used="%build
         %" changeAddress="%build
         %" }")
         addrId
-        addrBalance
         addrUsed
         addrChangeAddress
 

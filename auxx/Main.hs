facb303ed26d671c64d5b66dfbc2b8e8045fbf3c
Alexander Vieth <aovieth@gmail.com>
2018-04-26 11:00:57 -0400

    review comments

--- auxx/Main.hs
+++ auxx/Main.hs
@@ -5,7 +5,7 @@ module Main
 import           Universum
 
 import           Control.Exception.Safe (handle)
-import           Data.Maybe (fromJust)
+import           Data.Maybe (fromMaybe)
 import           Formatting (sformat, shown, (%))
 import           Mockable (Production, runProduction)
 import           JsonLog (jsonLog)
@@ -119,7 +119,7 @@ action opts@AuxxOptions {..} command = do
                         { acRealModeContext = realModeContext
                         , acTempDbUsed = tempDbUsed }
                 lift $ runReaderT auxxAction auxxContext
-        let vssSK = fromJust $ npUserSecret nodeParams ^. usVss
+        let vssSK = fromMaybe (error "no user secret given") $ npUserSecret nodeParams ^. usVss
         let sscParams = CLI.gtSscParams cArgs vssSK (npBehaviorConfig nodeParams)
         bracketNodeResources nodeParams sscParams txpGlobalSettings initNodeDBs $ \nr ->
             elimRealMode nr $ toRealMode $

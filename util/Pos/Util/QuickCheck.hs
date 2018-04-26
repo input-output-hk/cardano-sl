7f7106e9f65725802855d6c008d6d38ccfd333a2
Jordan Millar <jordan.millar@iohk.io>
2018-04-26 10:58:24 -0400

    [CDEC-240] Remove use of autoexporter from util package

--- util/Pos/Util/QuickCheck.hs
+++ util/Pos/Util/QuickCheck.hs
@@ -1,4 +1,10 @@
--- Pos.Util.QuickCheck
-{-# OPTIONS_GHC -F -pgmF autoexporter #-}
 {-# OPTIONS_GHC -Wno-unused-imports   #-}
 {-# OPTIONS_GHC -Wno-dodgy-exports    #-}
+
+module Pos.Util.QuickCheck
+       ( module Pos.Util.QuickCheck.Arbitrary
+       , module Pos.Util.QuickCheck.Property
+       ) where
+
+import           Pos.Util.QuickCheck.Arbitrary
+import           Pos.Util.QuickCheck.Property

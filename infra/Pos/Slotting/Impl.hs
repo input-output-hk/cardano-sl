6e6eae0a14f0e1102bf8260732887411a588df36
Jordan Millar <jordan.millar@iohk.io>
2018-04-28 13:04:16 -0400

    [CDEC-241] Remove use of autoexporter in infra

--- infra/Pos/Slotting/Impl.hs
+++ infra/Pos/Slotting/Impl.hs
@@ -1,3 +1,9 @@
 -- | This module re-exports slotting implementations.
 
-{-# OPTIONS_GHC -F -pgmF autoexporter #-}
+module Pos.Slotting.Impl
+       ( module Pos.Slotting.Impl.Simple
+       , module Pos.Slotting.Impl.Util
+       ) where
+
+import           Pos.Slotting.Impl.Simple
+import           Pos.Slotting.Impl.Util

53f6f04018d4138bd7e87996d9ef50c11b87c1d4
Jordan Millar <jordan.millar@iohk.io>
2018-04-30 11:55:42 -0400

    [CDEC-273]: Removed use of autoexporter from ssc

--- ssc/Pos/Ssc/Error.hs
+++ ssc/Pos/Ssc/Error.hs
@@ -1,1 +1,7 @@
-{-# OPTIONS_GHC -F -pgmF autoexporter #-}
+module Pos.Ssc.Error
+       ( module Pos.Ssc.Error.Seed
+       , module Pos.Ssc.Error.Verify
+       ) where
+
+import           Pos.Ssc.Error.Seed
+import           Pos.Ssc.Error.Verify

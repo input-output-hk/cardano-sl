db4254f0ef44205f6f8e2d6e62faf76c1f9416d0
parsonsmatt <parsonsmatt@gmail.com>
2018-05-01 15:47:57 -0600

    [CBR-222] Clean even more stuff up

--- wallet-new/test/APISpec.hs
+++ wallet-new/test/APISpec.hs
@@ -9,88 +9,17 @@
 
 module APISpec (spec) where
 
-import qualified Prelude
 import           Universum
 
-import           Control.Concurrent (threadDelay)
-import           Data.Default (def)
-import           Data.List.NonEmpty (fromList)
-import           Data.Maybe (fromJust)
-
-import           Data.Time.Clock.POSIX (getPOSIXTime)
-import           Data.Time.Units (Microsecond, fromMicroseconds)
-import           Formatting (format, shown, string, (%))
-import           Network.HTTP.Client hiding (Proxy)
-import           Network.HTTP.Types
-import           System.Wlog (HasLoggerName (..), LoggerName (..))
-
-import           Pos.Block.Types (Blund)
-import           Pos.Client.CLI (CommonArgs (..), CommonNodeArgs (..), NodeArgs (..), getNodeParams,
-                                 gtSscParams)
-import           Pos.Core (GenesisBlock, MainBlock, ProtocolMagic, Timestamp (..), headerHash,
-                           protocolMagic)
-import           Pos.DB.DB (initNodeDBs)
-import           Pos.DB.Rocks.Functions (openNodeDBs)
-import           Pos.DB.Rocks.Types (NodeDBs)
-import qualified Pos.Diffusion.Types as D
-import           Pos.Launcher (ConfigurationOptions (..), HasConfigurations, NodeResources (..),
-                               allocateNodeResources, defaultConfigurationOptions, npBehaviorConfig,
-                               npUserSecret, withConfigurations)
-import           Pos.Network.CLI (NetworkConfigOpts (..))
-import           Pos.Txp (txpGlobalSettings)
-import           Pos.Util.CompileInfo (HasCompileInfo, retrieveCompileTimeInfo, withCompileInfo)
-
-import           Cardano.Wallet.API.V1.LegacyHandlers.Accounts (newAccount)
-import           Cardano.Wallet.API.V1.LegacyHandlers.Addresses (newAddress)
-import           Cardano.Wallet.API.V1.LegacyHandlers.Wallets (newWallet)
-
-import           Pos.DB.GState.Common (getTip, initGStateCommon, setInitialized)
-import           Pos.StateLock (StateLock (..))
-import           Pos.Wallet.Web.Mode (MonadWalletWebMode, WalletWebMode, WalletWebModeContext (..))
-import           Pos.Wallet.Web.State (WalletDB, openState)
-
-import qualified Control.Concurrent.STM as STM
 import qualified Data.ByteString as BS
+import qualified Data.List as List
 import qualified Data.Text.Encoding as Text
-import           Ntp.Client (withoutNtpClient)
-
-import           Pos.WorkMode (RealModeContext (..))
-
-import           Pos.DB.Block (prepareBlockDB, putBlunds)
-
-import           Pos.Util.Util (lensOf)
-import           System.Directory (createDirectoryIfMissing, doesPathExist, getCurrentDirectory,
-                                   removeDirectoryRecursive)
-
-import           TestUtil (BlockNumber, SlotsPerEpoch, createEmptyUndo,
-                           produceBlocksByBlockNumberAndSlots, produceSecretKeys,
-                           produceSlotLeaders)
-
-import           Mockable (Production, runProduction)
-import           Pos.Util.JsonLog (jsonLogConfigFromHandle)
-import           Pos.Util.UserSecret (usVss)
-
 import           Servant
-import           Servant.QuickCheck
-import           Servant.QuickCheck.Internal
-
-import           System.Directory
-
+import           System.Directory (getCurrentDirectory, listDirectory, makeAbsolute,
+                                   setCurrentDirectory)
 import           Test.Hspec
-import           Test.Pos.Configuration (withDefConfigurations)
-import           Test.QuickCheck
-import           Test.QuickCheck.Instances ()
 
-import           Cardano.Wallet.API.Request
-import           Cardano.Wallet.API.Response (WalletResponse (..))
-import           Cardano.Wallet.API.Types
-import qualified Cardano.Wallet.API.V1 as V0
 import qualified Cardano.Wallet.API.V1 as V1
-import qualified Cardano.Wallet.API.V1.LegacyHandlers as V0
-import qualified Cardano.Wallet.API.V1.LegacyHandlers as V1
-import qualified Cardano.Wallet.API.V1.Migration as Migration
-import           Cardano.Wallet.API.V1.Parameters
-import           Cardano.Wallet.API.V1.Types (Wallet (..))
 
 -- Our API apparently is returning JSON Arrays which is considered bad practice as very old
 -- browsers can be hacked: https://haacked.com/archive/2009/06/25/json-hijacking.aspx/
@@ -105,7 +34,7 @@ spec = do
             oldLayout <- BS.readFile layoutPath `catch` \(_err :: SomeException) -> pure ""
             when (oldLayout /= serverLayout) $ do
                 BS.writeFile newLayoutPath serverLayout
-                expectationFailure $ Prelude.unlines
+                expectationFailure $ List.unlines
                     [ "The API layout has changed!!! The new layout has been written to:"
                     , "    " <> newLayoutPath
                     , "If this was intentional and correct, move the new layout path to:"

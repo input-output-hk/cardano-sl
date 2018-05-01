2efbffb41d2e9597dde14f130be70f82d530d464
Jordan Millar <jordan.millar@iohk.io>
2018-05-01 10:00:35 -0400

    [CDEC-274] Remove autoexporter from wallet

--- wallet/src/Pos/Wallet/Web/Methods.hs
+++ wallet/src/Pos/Wallet/Web/Methods.hs
@@ -1,4 +1,27 @@
 -- | Wallet endpoints implementation
 
-{-# OPTIONS_GHC -F -pgmF autoexporter #-}
+module Pos.Wallet.Web.Methods
+       ( module Pos.Wallet.Web.Methods.Backup
+       , module Pos.Wallet.Web.Methods.History
+       , module Pos.Wallet.Web.Methods.Info
+       , module Pos.Wallet.Web.Methods.Logic
+       , module Pos.Wallet.Web.Methods.Misc
+       , module Pos.Wallet.Web.Methods.Payment
+       , module Pos.Wallet.Web.Methods.Redeem
+       , module Pos.Wallet.Web.Methods.Reporting
+       , module Pos.Wallet.Web.Methods.Restore
+       , module Pos.Wallet.Web.Methods.Txp
+       ) where
+
+import           Pos.Wallet.Web.Methods.Backup
+import           Pos.Wallet.Web.Methods.History
+import           Pos.Wallet.Web.Methods.Info
+import           Pos.Wallet.Web.Methods.Logic
+import           Pos.Wallet.Web.Methods.Misc
+import           Pos.Wallet.Web.Methods.Payment
+import           Pos.Wallet.Web.Methods.Redeem
+import           Pos.Wallet.Web.Methods.Reporting
+import           Pos.Wallet.Web.Methods.Restore
+import           Pos.Wallet.Web.Methods.Txp
+
 
--- wallet/src/Pos/Wallet/Web/Sockets.hs
+++ wallet/src/Pos/Wallet/Web/Sockets.hs
@@ -1,3 +1,13 @@
 -- | Wallet websockets notifier logic.
 
-{-# OPTIONS_GHC -F -pgmF autoexporter #-}
+module Pos.Wallet.Web.Sockets
+       ( module Pos.Wallet.Web.Sockets.Connection
+       , module Pos.Wallet.Web.Sockets.ConnSet
+       , module Pos.Wallet.Web.Sockets.Notifier
+       , module Pos.Wallet.Web.Sockets.Types
+       ) where
+
+import           Pos.Wallet.Web.Sockets.Connection
+import           Pos.Wallet.Web.Sockets.ConnSet
+import           Pos.Wallet.Web.Sockets.Notifier
+import           Pos.Wallet.Web.Sockets.Types

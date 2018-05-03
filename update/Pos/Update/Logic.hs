adc7fbd913f4a962036a2621ef87d592150bea5a
Jordan Millar <jordan.millar@iohk.io>
2018-05-03 09:50:42 -0400

    [CDEC-275] Remove autoexporter from update

--- update/Pos/Update/Logic.hs
+++ update/Pos/Update/Logic.hs
@@ -1,3 +1,9 @@
 -- | Major logic of Update System (US).
 
-{-# OPTIONS_GHC -F -pgmF autoexporter #-}
+module Pos.Update.Logic
+       ( module Pos.Update.Logic.Global
+       , module Pos.Update.Logic.Local
+       ) where
+
+import           Pos.Update.Logic.Global
+import           Pos.Update.Logic.Local

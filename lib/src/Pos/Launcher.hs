92f50572cefdc3b1153194bfb0aea4f0213470e1
Jordan Millar <jordan.millar@iohk.io>
2018-05-03 09:42:16 -0400

    [CDEC-275] Remove autoexporter from lib

--- lib/src/Pos/Launcher.hs
+++ lib/src/Pos/Launcher.hs
@@ -1,3 +1,19 @@
 -- | High-level code capable of running various scenarios in various modes.
 
-{-# OPTIONS_GHC -F -pgmF autoexporter #-}
+module Pos.Launcher
+       ( module Pos.Launcher.Configuration
+       , module Pos.Launcher.Launcher
+       , module Pos.Launcher.Mode
+       , module Pos.Launcher.Param
+       , module Pos.Launcher.Resource
+       , module Pos.Launcher.Runner
+       , module Pos.Launcher.Scenario
+       ) where
+
+import           Pos.Launcher.Configuration
+import           Pos.Launcher.Launcher
+import           Pos.Launcher.Mode
+import           Pos.Launcher.Param
+import           Pos.Launcher.Resource
+import           Pos.Launcher.Runner
+import           Pos.Launcher.Scenario

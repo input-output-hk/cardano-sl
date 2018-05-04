0f8a4dba82db21ba9f15a092468acd2cc2fdd1a6
KtorZ <matthias.benkort@gmail.com>
2018-05-04 17:22:43 +0200

    [CSL-2406] Generate certificates as part of cardano-sl installer

--- tools/src/launcher/Main.hs
+++ tools/src/launcher/Main.hs
@@ -38,7 +38,8 @@ import           Options.Applicative (Parser, ParserInfo, ParserResult (..), def
                                       header, help, helper, info, infoOption, long, metavar,
                                       progDesc, renderFailure, short, strOption)
 import           Serokell.Aeson.Options (defaultOptions)
-import           System.Directory (createDirectoryIfMissing, doesFileExist, removeFile)
+import           System.Directory (createDirectoryIfMissing, doesDirectoryExist, doesFileExist,
+                                   removeFile)
 import qualified System.Directory as Sys
 import           System.Environment (getExecutablePath, getProgName, setEnv)
 import           System.Exit (ExitCode (..))
@@ -72,13 +73,14 @@ import           Pos.DB.Rocks (NodeDBs, closeNodeDBs, dbDeleteDefault, dbGetDefa
                                dbIterSourceDefault, dbPutDefault, dbWriteBatchDefault, openNodeDBs)
 import           Pos.Launcher (HasConfigurations, withConfigurations)
 import           Pos.Launcher.Configuration (ConfigurationOptions (..))
-import           Pos.Reporting.Wlog (compressLogs, retrieveLogFiles)
 import           Pos.Reporting.Http (sendReport)
+import           Pos.Reporting.Wlog (compressLogs, retrieveLogFiles)
 import           Pos.ReportServer.Report (ReportType (..))
 import           Pos.Update (installerHash)
 import           Pos.Update.DB.Misc (affirmUpdateInstalled)
 import           Pos.Util (HasLens (..), directory, logException, postfixLFields)
-import           Pos.Util.CompileInfo (HasCompileInfo, retrieveCompileTimeInfo, withCompileInfo, compileInfo)
+import           Pos.Util.CompileInfo (HasCompileInfo, compileInfo, retrieveCompileTimeInfo,
+                                       withCompileInfo)
 
 import           Launcher.Environment (substituteEnvVarsValue)
 import           Launcher.Logging (reportErrorDefault)
@@ -94,6 +96,8 @@ data LauncherOptions = LO
     , loWalletArgs          :: ![Text]
     , loWalletLogging       :: !Bool
     , loWalletLogPath       :: !(Maybe FilePath)
+    , loTlsPath             :: !FilePath
+    , loX509ToolPath        :: !FilePath
     , loUpdaterPath         :: !FilePath
     , loUpdaterArgs         :: ![Text]
     , loUpdateArchive       :: !(Maybe FilePath)
@@ -127,8 +131,7 @@ instance FromJSON LauncherOptions where
 -- | The concrete monad where everything happens
 type M a = (HasConfigurations, HasCompileInfo) => Log.LoggerNameBox IO a
 
--- | Executable can be node, wallet or updater
-data Executable = EWallet | ENode | EUpdater
+data Executable = EWallet | ENode | EUpdater | ECertGen
 
 -- | This datatype holds values for either node or wallet
 --   Node/wallet path, args, log path
@@ -279,6 +282,7 @@ main =
     setEnv "LANG"   "en_GB.UTF-8"
 
     LO {..} <- getLauncherOptions
+
     -- Launcher logs should be in public directory
     let launcherLogsPrefix = (</> "pub") <$> loLogsPrefix
     -- Add options specified in loConfiguration but not in loNodeArgs to loNodeArgs.
@@ -297,7 +301,11 @@ main =
                       set Log.ltFiles [Log.HandlerWrap "launcher" Nothing] .
                       set Log.ltSeverity (Just Log.debugPlus)
     logException loggerName . Log.usingLoggerName loggerName $
-        withConfigurations loConfiguration $ \_ ->
+        withConfigurations loConfiguration $ \_ -> do
+
+        -- Generate TLS certificates as needed
+        generateTlsCertificates loConfiguration loTlsPath loX509ToolPath
+
         case (loWalletPath, loFrontendOnlyMode) of
             (Nothing, _) -> do
                 logNotice "LAUNCHER STARTED"
@@ -372,6 +380,26 @@ main =
 
     timestampToText (Timestamp ts) =
         pretty @Integer $ fromIntegral $ convertUnit @_ @Second ts
+
+
+generateTlsCertificates :: ConfigurationOptions -> FilePath -> FilePath -> M ()
+generateTlsCertificates ConfigurationOptions{..} tlsPath executable =
+    unlessM (liftIO $ doesDirectoryExist tlsPath) $ do
+        let process = createProc  Process.Inherit executable
+                [ "--server-out-dir"     , toText tlsPath
+                , "--clients-out-dir"    , toText tlsPath
+                , "--configuration-file" , toText cfoFilePath
+                , "--configuration-key"  , cfoKey
+                ]
+
+        exitCode <- liftIO $ do
+            createDirectoryIfMissing True tlsPath
+            phvar <- newEmptyMVar
+            system' phvar process mempty ECertGen
+
+        when (exitCode /= ExitSuccess) $
+            logError "Couldn't generate TLS certificates for Wallet"
+
 
 -- | If we are on server, we want the following algorithm:
 --
@@ -642,6 +670,7 @@ customLogger hndl loggerName logStr = do
             ENode    -> "[node] "
             EWallet  -> "[wallet] "
             EUpdater -> "[updater] "
+            ECertGen -> "[X509-certificates] "
 
 ----------------------------------------------------------------------------
 -- Working with the report server

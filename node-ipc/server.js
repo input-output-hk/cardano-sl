0c04b31dda4114c9faf911d11ea85c0d83661381
Michael Bishop <michael.bishop@iohk.io>
2018-05-06 03:17:30 -0300

    improve the demo server

--- node-ipc/server.js
+++ node-ipc/server.js
@@ -1,12 +1,12 @@
 var child_process = require("child_process");
 const fs = require('fs');
 
-console.log("in parent")
+console.log(new Date(), "in parent")
 
 logfile = fs.createWriteStream("cardano-node.log", { flags: "a" });
 
 logfile.on("open", function () {
-  console.log("log file opened");
+  console.log(new Date(), "log file opened");
 
   const subprocess = child_process.spawn("cardano-node", [
     "--wallet-address", "127.0.0.1:0",
@@ -17,24 +17,25 @@ logfile.on("open", function () {
     stdio: [ "inherit", logfile, logfile, "ipc" ]
   });
   subprocess.on("message", function (msg) {
-    console.log("got reply",msg);
+    console.log(new Date(), "got reply",msg);
   });
   subprocess.on("close", function(code, signal) {
     console.log("all stdio to child has been closed", code, signal);
   });
   subprocess.on("disconnect", function() {
-    console.log("all IPC handles closed");
+    console.log(new Date(), "all IPC handles closed");
   });
   subprocess.on("error", function (err) {
     console.log("error:", err);
   });
   subprocess.on("exit", function (code, signal) {
-    console.log("child exited", code, signal);
+    console.log(new Date(), "child exited", code, signal);
   });
 
   subprocess.send({ QueryPort:[]})
 
   setTimeout(function () {
-    process.exit();
+    //process.exit();
+    subprocess.disconnect();
   }, 30000);
 });

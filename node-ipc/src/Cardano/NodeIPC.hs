c44c3ec40efe10c76be67daab250729dcb7c6d56
Michael Bishop <michael.bishop@iohk.io>
2018-05-08 21:06:17 -0300

    ugh, windows

--- node-ipc/src/Cardano/NodeIPC.hs
+++ node-ipc/src/Cardano/NodeIPC.hs
@@ -13,8 +13,11 @@ import           Control.Monad.Reader (MonadReader)
 import           Control.Monad.IO.Class
 import           Data.Aeson
 import           Data.Aeson.Types          (Options, SumEncoding(ObjectWithSingleField), sumEncoding)
+import           Data.Binary.Get           (runGet, getWord64le, getWord32le)
+import           Data.Binary.Put           (runPut, putWord32le, putWord64le, putLazyByteString)
 import qualified Data.ByteString.Lazy      as BSL
 import qualified Data.ByteString.Lazy.Char8 as BSLC
+import           Distribution.System       (buildOS, OS(Windows))
 import           Foreign.C.Types           (CInt)
 import           GHC.IO.Handle.FD          (fdToHandle)
 import           GHC.Generics              (Generic)
@@ -61,25 +64,36 @@ ipcListener fd port = do
   let
     send :: Packet -> m ()
     send cmd = liftIO $ do
-      BSL.hPut handle $ (encode cmd) <> "\n"
-      hFlush handle
+      sendMessage $ encode cmd
     action :: Packet -> m ()
     action QueryPort = do
       send $ ReplyPort port
     action foo = logInfo $ "Unhandled IPC msg: " <> show foo
+    sendMessage :: BSL.ByteString -> IO ()
+    sendMessage blob = do
+      if buildOS == Windows
+        then sendWindowsMessage 1 0 (blob <> "\n")
+        else sendLinuxMessage blob
+      hFlush handle
+    sendWindowsMessage :: Word32 -> Word32 -> BSL.ByteString -> IO ()
+    sendWindowsMessage int1 int2 blob = do
+      let
+        message = runPut $ (putWord32le int1) <> (putWord32le int2) <> (putWord64le $ fromIntegral $ BSL.length blob) <> (putLazyByteString blob)
+      BSLC.hPut handle message
+    sendLinuxMessage :: BSL.ByteString -> IO ()
+    sendLinuxMessage blob = do
+      BSLC.hPutStrLn handle blob
   let
     loop :: m ()
     loop = do
       send Started
       forever $ do
-        line <- liftIO $ hGetLine handle
+        line <- readMessage
         let
-          packet :: Either String Packet
-          packet = eitherDecode $ BSLC.pack line
           handlePacket :: Either String Packet -> m ()
           handlePacket (Left err) = send $ ParseError $ toText err
           handlePacket (Right cmd) = action cmd
-        handlePacket packet
+        handlePacket $ eitherDecode line
     handler :: IOError -> m ()
     handler err = do
       logError $ "exception caught in NodeIPC: " <> (show err)
@@ -90,4 +104,31 @@ ipcListener fd port = do
     debugloop = forever $ do
         char <- liftIO $ hGetChar handle
         logInfo $ "got char '" <> (show char) <> "' (" <> (show $ ord char) <> ")"
-  catch debugloop handler
+    readMessage :: m BSL.ByteString
+    readMessage = do
+      if buildOS == Windows
+        then do
+          (int1, int2, blob) <- liftIO windowsReadMessage
+          logInfo $ "int is: " <> (show [int1, int2]) <> " and blob is: " <> (show blob)
+          return blob
+        else
+          liftIO linuxReadMessage
+    linuxReadMessage :: IO BSL.ByteString
+    linuxReadMessage = do
+      line <- hGetLine handle
+      return $ BSLC.pack line
+    windowsReadMessage :: IO (Word32, Word32, BSL.ByteString)
+    windowsReadMessage = do
+      int1 <- readInt32 handle
+      int2 <- readInt32 handle
+      size <- readInt64 handle
+      blob <- BSL.hGet handle $ fromIntegral size
+      return (int1, int2, blob)
+    readInt64 :: Handle -> IO Word64
+    readInt64 hnd = do
+      bs <- BSL.hGet hnd 8
+      pure $ runGet getWord64le bs
+    readInt32 hnd = do
+      bs <- BSL.hGet hnd 4
+      pure $ runGet getWord32le bs
+  catch loop handler

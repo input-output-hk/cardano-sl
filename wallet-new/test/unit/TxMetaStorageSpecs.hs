4cafcc055ec64df30ac1316ed2ae8d5623c0dfa5
Alfredo Di Napoli <alfredo.dinapoli@iohk.io>
2018-05-10 18:40:12 +0200

    [CBR-255] Fix the generators hopefully for the last time

--- wallet-new/test/unit/TxMetaStorageSpecs.hs
+++ wallet-new/test/unit/TxMetaStorageSpecs.hs
@@ -5,21 +5,24 @@ import           Universum
 
 import           Cardano.Wallet.Kernel.DB.TxMeta
 import           Control.Exception.Safe (bracket)
+import qualified Data.List as List
 import qualified Data.List.NonEmpty as NonEmpty
 import qualified Data.List.Split as Split
+import qualified Data.Set as Set
 import           Data.Text.Buildable (build)
 import qualified Prelude
 
 import qualified Pos.Core as Core
 
 import           Formatting (bprint)
-import           Serokell.Util.Text (listJsonIndent)
+import           Serokell.Util.Text (listJsonIndent, pairF)
 import           Test.Hspec (shouldThrow)
 import           Test.Hspec.QuickCheck (prop)
 import           Test.QuickCheck (Arbitrary, Gen, arbitrary, forAll, vectorOf)
-import           Test.QuickCheck.Monadic (monadicIO, pick, run)
+import           Test.QuickCheck.Monadic (assert, monadicIO, pick, run)
 import           Util.Buildable (ShowThroughBuild (..))
 import           Util.Buildable.Hspec
+
 
 -- | Handy combinator which yields a fresh database to work with on each spec.
 withTemporaryDb :: forall m a. (MonadIO m, MonadMask m) => (MetaDBHandle -> m a) -> m a
@@ -41,7 +44,7 @@ genSimilarTxMetas :: Gen (ShowThroughBuild TxMeta, ShowThroughBuild TxMeta)
 genSimilarTxMetas = do
     inputs  <- uniqueElements 5
     outputs <- uniqueElements 5
-    blueprint <- arbitrary
+    blueprint <- unSTB <$> genMeta
     let t1 = blueprint & over txMetaInputs  (const inputs)
                        . over txMetaOutputs (const outputs)
     let t2 = blueprint & over txMetaInputs  (const (NonEmpty.reverse inputs))
@@ -65,19 +68,42 @@ newtype TxEntry = TxEntry { getTxEntry :: (Core.Address, Core.Coin) }
 instance Eq TxEntry where
     (TxEntry (a1, _)) == (TxEntry (a2, _)) = a1 == a2
 
+-- | This is a totally bonkers 'Ord' instance (as it doesn't really make sense
+-- to order anything by an 'Address' value, but it's necessary for the sake
+-- of the input and output generation. In particular, writing the following
+-- will introduce a bug later on:
+--
+-- instance Ord TxEntry where
+--     compare (TxEntry (_, c1)) (TxEntry (_, c2)) = compare c1 c2
+--
+-- This will speed up the tests considerably, but it comes with a fatal flaw:
+-- later on, once generating unique inputs & outputs as part of 'uniqueElements',
+-- we rely on a 'Set' (and thus an 'Ord' instance) to generate unique elements.
+-- But the instance above will 'compare' the two 'Coin' values and in turn
+-- piggyback on equality for Coins, essentially trashing the invariant we
+-- describe above as the entire @raison d'etre@ for the 'TxEntry' type.
 instance Ord TxEntry where
-    compare (TxEntry (_, c1)) (TxEntry (_, c2)) = compare c1 c2
+    compare (TxEntry (a1, _)) (TxEntry (a2, _)) = compare a1 a2
 
 instance Arbitrary TxEntry where
     arbitrary = TxEntry  <$> arbitrary
+
+instance Buildable TxEntry where
+    build (TxEntry b) = bprint pairF b
+
+instance Buildable (Int, TxEntry) where
+    build b = bprint pairF b
+
+instance Buildable [TxEntry] where
+    build = bprint (listJsonIndent 4)
 
 -- | Handy generator which make sure we are generating 'TxMeta' which all
 -- have distinct inputs and outptus.
 genMetas :: Int -> Gen [ShowThroughBuild TxMeta]
 genMetas size = do
-    metas  <- vectorOf size arbitrary
-    inputs  <- Split.chunksOf 5 . toList <$> uniqueElements (length metas * 5)
-    outputs <- Split.chunksOf 5 . toList <$> uniqueElements (length metas * 5)
+    metas  <- map unSTB <$> vectorOf size genMeta
+    inputs  <- Split.chunksOf 3 . toList <$> uniqueElements (length metas * 3)
+    outputs <- Split.chunksOf 3 . toList <$> uniqueElements (length metas * 3)
     return $ map (STB . mkTx) (Prelude.zip3 metas inputs outputs)
 
     where
@@ -90,6 +116,23 @@ genMetas size = do
                      tMeta & over txMetaInputs  (const inputs)
                            . over txMetaOutputs (const outputs)
 
+-- | Generator for an arbitrary 'TxMeta' which uses 'TxEntry' underneath to
+-- generate the inputs and the outputs.
+genMeta :: Gen (ShowThroughBuild TxMeta)
+genMeta = do
+    meta <- TxMeta <$> arbitrary
+                   <*> arbitrary
+                   <*> (fmap getTxEntry <$> uniqueElements 10)
+                   <*> (fmap getTxEntry <$> uniqueElements 10)
+                   <*> arbitrary
+                   <*> arbitrary
+                   <*> arbitrary
+    pure (STB meta)
+
+newtype TxMetaWrapper = TxMetaWrapper (ShowThroughBuild TxMeta) deriving Show
+
+instance Arbitrary TxMetaWrapper where
+    arbitrary = TxMetaWrapper <$> genMeta
 
 -- | Handy wrapper to be able to compare things with the 'isomorphicTo'
 -- combinator, which ignores the different order of the inputs & outputs.
@@ -130,11 +173,20 @@ sortByCreationAt direction = sortBy sortFn
         sortFn (Isomorphic a) (Isomorphic b) =
             (withDir direction compare) (a ^. txMetaCreationAt) (b ^. txMetaCreationAt)
 
+
+hasDupes :: Ord a => [a] -> Bool
+hasDupes xs = length (Set.fromList xs) /= List.length xs
+
 -- | Specs which tests the persistent storage and API provided by 'TxMeta'.
 txMetaStorageSpecs :: Spec
 txMetaStorageSpecs = do
+    describe "uniqueElements generator" $ do
+        it "generates unique inputs" $ monadicIO $ do
+            (inputs :: NonEmpty (ShowThroughBuild TxEntry)) <- pick (uniqueElements 30)
+            assert (not $ hasDupes . map unSTB . toList $ inputs)
+
     describe "TxMeta equality" $ do
-        prop "should be reflexive" $ \blueprint -> do
+        prop "should be reflexive" $ \(TxMetaWrapper blueprint) -> do
             unSTB blueprint `exactlyEqualTo` unSTB (blueprint :: ShowThroughBuild TxMeta)
 
         it "should be strict when needed"
@@ -148,7 +200,7 @@ txMetaStorageSpecs = do
     describe "TxMeta storage" $ do
 
         it "can store a TxMeta and retrieve it back" $ monadicIO $ do
-            testMetaSTB <- pick arbitrary
+            testMetaSTB <- pick genMeta
             run $ withTemporaryDb $ \hdl -> do
                 let testMeta = unSTB testMetaSTB
                 void $ putTxMeta hdl testMeta
@@ -156,14 +208,14 @@ txMetaStorageSpecs = do
                 fmap DeepEqual mbTx `shouldBe` Just (DeepEqual testMeta)
 
         it "yields Nothing when calling getTxMeta, if a TxMeta is not there" $ monadicIO $ do
-            testMetaSTB <- pick arbitrary
+            testMetaSTB <- pick genMeta
             run $ withTemporaryDb $ \hdl -> do
                 let testMeta = unSTB testMetaSTB
                 mbTx <- getTxMeta hdl (testMeta ^. txMetaId)
                 fmap DeepEqual mbTx `shouldBe` Nothing
 
         it "inserting the same tx twice is a no-op" $ monadicIO $ do
-            testMetaSTB <- pick arbitrary
+            testMetaSTB <- pick genMeta
             run $ withTemporaryDb $ \hdl -> do
                 let testMeta = unSTB testMetaSTB
 
@@ -171,7 +223,7 @@ txMetaStorageSpecs = do
                 putTxMeta hdl testMeta `shouldReturn` ()
 
         it "inserting two tx with the same tx, but different content is an error" $ monadicIO $ do
-            testMetaSTB <- pick arbitrary
+            testMetaSTB <- pick genMeta
             run $ withTemporaryDb $ \hdl -> do
                 let meta1 = unSTB testMetaSTB
                 let meta2 = set txMetaIsOutgoing (not $ meta1 ^. txMetaIsOutgoing) meta1
@@ -181,7 +233,7 @@ txMetaStorageSpecs = do
                     (\(InvariantViolated (DuplicatedTransactionWithDifferentHash _)) -> True)
 
         it "inserting multiple txs and later retrieving all of them works" $ monadicIO $ do
-            testMetasSTB <- pick (genMetas 10)
+            testMetasSTB <- pick (genMetas 5)
             run $ withTemporaryDb $ \hdl -> do
                 let metas = map unSTB testMetasSTB
                 forM_ metas (putTxMeta hdl)
@@ -189,15 +241,15 @@ txMetaStorageSpecs = do
                 map Isomorphic result `shouldMatchList` map Isomorphic metas
 
         it "pagination correctly limit the results" $ monadicIO $ do
-            testMetasSTB <- pick (genMetas 15)
+            testMetasSTB <- pick (genMetas 10)
             run $ withTemporaryDb $ \hdl -> do
                 let metas = map unSTB testMetasSTB
                 forM_ metas (putTxMeta hdl)
-                result <- getTxMetas hdl (Offset 0) (Limit 10) Nothing
-                length result `shouldBe` 10
+                result <- getTxMetas hdl (Offset 0) (Limit 5) Nothing
+                length result `shouldBe` 5
 
         it "pagination correctly sorts (ascending) the results" $ monadicIO $ do
-            testMetasSTB <- pick (genMetas 10)
+            testMetasSTB <- pick (genMetas 5)
             run $ withTemporaryDb $ \hdl -> do
                 let metas = map unSTB testMetasSTB
                 forM_ metas (putTxMeta hdl)
@@ -205,7 +257,7 @@ txMetaStorageSpecs = do
                 map Isomorphic result `shouldBe` sortByAmount Ascending (map Isomorphic metas)
 
         it "pagination correctly sorts (descending) the results" $ monadicIO $ do
-            testMetasSTB <- pick (genMetas 10)
+            testMetasSTB <- pick (genMetas 5)
             run $ withTemporaryDb $ \hdl -> do
                 let metas = map unSTB testMetasSTB
                 forM_ metas (putTxMeta hdl)

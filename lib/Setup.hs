bd95661f4fe642e91954874d5055256b836717b1
Michael Bishop <michael.bishop@iohk.io>
2018-05-11 13:24:50 -0300

    print the PATH in lib

--- lib/Setup.hs
+++ lib/Setup.hs
@@ -1,2 +1,8 @@
 import           Distribution.Simple
-main = defaultMain
+import System.Environment
+import Data.Monoid
+main :: IO ()
+main = do
+    path <- getEnv "PATH"
+    print $ "path is: " <> path
+    defaultMain

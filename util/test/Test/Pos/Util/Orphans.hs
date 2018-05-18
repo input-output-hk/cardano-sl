d4d7c4c18ba758833342be33d584eaa755f6b8b4
Erik de Castro Lopo <erikd@mega-nerd.com>
2018-05-18 14:14:19 +1000

    Move some Arbitrary instances from core to util

--- util/test/Test/Pos/Util/Orphans.hs
+++ util/test/Test/Pos/Util/Orphans.hs
@@ -10,6 +10,9 @@ module Test.Pos.Util.Orphans where
 import           Control.Monad.Reader.Class (MonadReader (..))
 import qualified Crypto.Random as Rand
 
+import           Data.Time.Units (Microsecond, Millisecond, Second, TimeUnit (..), convertUnit)
+
+import           Test.QuickCheck (Arbitrary, Gen)
 import qualified Test.QuickCheck as QC
 import           Test.QuickCheck.Monadic (PropertyM (..))
 
@@ -27,7 +30,19 @@ data Five a = Five a a a a a
 five :: a -> Five a
 five a = Five a a a a a
 
-instance Rand.MonadRandom QC.Gen where
+instance Rand.MonadRandom Gen where
     getRandomBytes n = do
         Five a b c d e <- sequenceA . five $ QC.choose (minBound, maxBound)
         pure $ fst $ Rand.randomBytesGenerate n (Rand.drgNewTest (a,b,c,d,e))
+
+instance Arbitrary Millisecond where
+    arbitrary = fromMicroseconds <$> QC.choose (0, 600 * 1000 * 1000)
+    shrink = QC.shrinkIntegral
+
+instance Arbitrary Microsecond where
+    arbitrary = fromMicroseconds <$> QC.choose (0, 600 * 1000 * 1000)
+    shrink = QC.shrinkIntegral
+
+instance Arbitrary Second where
+    arbitrary = convertUnit @Microsecond <$> QC.arbitrary
+    shrink = QC.shrinkIntegral

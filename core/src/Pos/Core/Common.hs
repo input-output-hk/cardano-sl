66847b97b47d486d4aecfc005f97a40f830ebc8b
Rupert Horlick <rupert.horlick@iohk.io>
2018-05-30 17:33:49 -0700

    [CDEC-346] Refactor Pos.Core.Common module set

--- core/src/Pos/Core/Common.hs
+++ core/src/Pos/Core/Common.hs
@@ -1,11 +1,37 @@
 module Pos.Core.Common
-       ( module Pos.Core.Common.Address
+       ( module Pos.Core.Common.AddrAttributes
+       , module Pos.Core.Common.Address
+       , module Pos.Core.Common.AddressHash
+       , module Pos.Core.Common.AddrSpendingData
+       , module Pos.Core.Common.AddrStakeDistribution
+       , module Pos.Core.Common.BlockCount
+       , module Pos.Core.Common.ChainDifficulty
+       , module Pos.Core.Common.Coeff
        , module Pos.Core.Common.Coin
-       , module Pos.Core.Common.Fee
-       , module Pos.Core.Common.Types
+       , module Pos.Core.Common.CoinPortion
+       , module Pos.Core.Common.Script
+       , module Pos.Core.Common.SharedSeed
+       , module Pos.Core.Common.SlotLeaders
+       , module Pos.Core.Common.StakeholderId
+       , module Pos.Core.Common.Stakes
+       , module Pos.Core.Common.TxFeePolicy
+       , module Pos.Core.Common.TxSizeLinear
        ) where
 
+import           Pos.Core.Common.AddrAttributes
 import           Pos.Core.Common.Address
+import           Pos.Core.Common.AddressHash
+import           Pos.Core.Common.AddrSpendingData
+import           Pos.Core.Common.AddrStakeDistribution
+import           Pos.Core.Common.BlockCount
+import           Pos.Core.Common.ChainDifficulty
+import           Pos.Core.Common.Coeff
 import           Pos.Core.Common.Coin
-import           Pos.Core.Common.Fee
-import           Pos.Core.Common.Types
+import           Pos.Core.Common.CoinPortion
+import           Pos.Core.Common.Script
+import           Pos.Core.Common.SharedSeed
+import           Pos.Core.Common.SlotLeaders
+import           Pos.Core.Common.StakeholderId
+import           Pos.Core.Common.Stakes
+import           Pos.Core.Common.TxFeePolicy
+import           Pos.Core.Common.TxSizeLinear
---
+++ core/src/Pos/Core/Common/TxSizeLinear.hs
@@ -0,0 +1,37 @@
+module Pos.Core.Common.TxSizeLinear
+       ( TxSizeLinear (..)
+       , txSizeLinearMinValue
+       , calculateTxSizeLinear
+       ) where
+
+import           Universum
+
+import           Data.Fixed (Nano)
+import qualified Data.Text.Buildable as Buildable
+import           Formatting (bprint, build, (%))
+import           Serokell.Data.Memory.Units (Byte, toBytes)
+
+import           Pos.Core.Common.Coeff
+
+-- | A linear equation on the transaction size. Represents the @\s -> a + b*s@
+-- function where @s@ is the transaction size in bytes, @a@ and @b@ are
+-- constant coefficients.
+data TxSizeLinear = TxSizeLinear !Coeff !Coeff
+    deriving (Eq, Ord, Show, Generic)
+
+instance NFData TxSizeLinear
+
+instance Buildable TxSizeLinear where
+    build (TxSizeLinear a b) =
+        bprint (build%" + "%build%"*s") a b
+
+instance Hashable TxSizeLinear
+
+calculateTxSizeLinear :: TxSizeLinear -> Byte -> Nano
+calculateTxSizeLinear
+    (TxSizeLinear (Coeff a) (Coeff b))
+    (fromInteger . toBytes -> txSize) =
+        a + b * txSize
+
+txSizeLinearMinValue :: TxSizeLinear -> Nano
+txSizeLinearMinValue (TxSizeLinear (Coeff minVal) _) = minVal

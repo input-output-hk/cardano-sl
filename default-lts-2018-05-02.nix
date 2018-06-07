fb85dd4d99c177fa692198abc80a7a2c95d70e33
Moritz Angermann <moritz.angermann@gmail.com>
2018-06-07 14:59:09 +0800

    fix default

--- default-lts-2018-05-02.nix
+++ default-lts-2018-05-02.nix
@@ -1,6 +1,7 @@
 let
 
   stack-pkgs = import ./stack-pkgs.nix;
+  #haskell = import <haskell>;
 
   overlay = self: super: {
     haskellPackages = (import <stackage> { pkgs = super; }).lts-2018-05-02
@@ -15,10 +16,23 @@ let
 
     packageOverrides = ps: with ps; rec {
       haskell = lib.recursiveUpdate ps.haskell {
+        lib = ps.haskell.lib // (with ps.haskell.lib; {
+          # sanity
+          addExtraLibrary'  = ls: drv: addExtraLibrary drv ls;
+          addBuildDepends'  = ds: drv: addBuildDepends drv ds;
+          appendBuildFlags' = fs: drv: appendBuildFlags drv fs;
+          overrideCabal'    = os: drv: overrideCabal drv os;
+          addBuildTools'    = ts: drv: addBuildTools drv ts;
+          addPreBuild'      = x: drv:  overrideCabal drv (drv: { preBuild  = (drv.preBuild or  "") + x; });
+          addPostBuild'     = x: drv:  overrideCabal drv (drv: { postBuild = (drv.postBuild or "") + x; });
+          dropRevision'     = drv: overrideCabal drv (_: { revision = null; editedCabalFile = null; });
+        });
+
         compiler.ghc842 = (ps.haskell.compiler.ghc842.override {
           ghcCrossFlavour = "quick-cross-ncg";
           ghcFlavour = "quick";
           enableShared = false;
+          enableIntegerSimple = false;
         }).overrideAttrs (drv: {
           dontStrip = true;
           hardeningDisable = [ "stackprotector" ];
@@ -54,18 +68,64 @@ let
 
 in with pkgs.haskellPackages;
 # pkgs.lib.mapAttrs (_: x: callPackage x {})
-pkgs.haskellPackages.override {
-  overrides = self: super: builtins.trace super {
+pkgs.haskellPackages.override rec {
+  # note: we want `haskellPackages` here, as that is the one
+  #       we provide in the overly(!)
+  buildHaskellPackages = pkgs.buildPackages.haskellPackages;
+  overrides = self: super: rec {
+
+    # Logic to run TH via an external interpreter (64bit windows via wine64)
+  doTemplateHaskell = pkg: with pkgs.haskell.lib; let
+    buildTools = [ buildHaskellPackages.iserv-proxy pkgs.buildPackages.winePackages.minimal ];
+    buildFlags = map (opt: "--ghc-option=" + opt) [
+      "-fexternal-interpreter"
+      "-pgmi" "${buildHaskellPackages.iserv-proxy}/bin/iserv-proxy"
+      "-opti" "127.0.0.1" "-opti" "$PORT"
+      # TODO: this should be automatically injected based on the extraLibrary.
+      "-L${pkgs.windows.mingw_w64_pthreads}/lib"
+    ];
+    preBuild = ''
+      PORT=$((5000 + $RANDOM % 5000))
+      echo "---> Starting remote-iserv on port $PORT"
+      WINEPREFIX=$TMP ${pkgs.buildPackages.winePackages.minimal}/bin/wine64 ${self.remote-iserv}/bin/remote-iserv.exe tmp $PORT &
+      sleep 60 # wait for wine to fully boot up...
+      echo "---| remote-iserv should be started on $PORT"
+      RISERV_PID=$!
+    '';
+    postBuild = ''
+      echo "---> killing remote-iserv..."
+      kill $RISERV_PID
+    ''; in
+    appendBuildFlags' buildFlags
+     (addBuildDepends' [ self.remote-iserv ]
+      (addExtraLibrary' pkgs.windows.mingw_w64_pthreads
+       (addBuildTools' buildTools
+        (addPreBuild' preBuild
+         (addPostBuild' postBuild pkg)))));
+
     #fetch a package candidate from hackage and return the cabal2nix expression.
     hackageCandidate = name: ver: args: self.callCabal2nix name (fetchTarball "https://hackage.haskell.org/package/${name}-${ver}/candidate/${name}-${ver}.tar.gz") args;
-    #libiserv = with haskell.lib; addExtraLibrary (enableCabalFlag (self.hackageCandidate "libiserv" "8.5" {}) "network") self.network;
-    #iserv-proxy = self.hackageCandidate "iserv-proxy" "8.5" { libiserv = self.libiserv; };
+
     # TODO: Why is `network` not properly propagated from `libiserv`?
     remote-iserv = with pkgs.haskell.lib; let pkg = addExtraLibrary super.remote-iserv self.network; in
-      overrideCabal (addBuildDepends pkg [ windows.mingw_w64_pthreads ]) (drv: {
+      overrideCabal (addBuildDepends pkg [ pkgs.windows.mingw_w64_pthreads ]) (drv: {
         postInstall = ''
-          cp ${windows.mingw_w64_pthreads}/bin/libwinpthread-1.dll $out/bin/
+          cp ${pkgs.windows.mingw_w64_pthreads}/bin/libwinpthread-1.dll $out/bin/
+          cp ${pkgs.gcc7-ng-libssp}/lib/libssp-0.dll $out/bin/
         '';
       });
+    streaming-commons = pkgs.haskell.lib.appendPatch super.streaming-commons ./streaming-commons-0.2.0.0.patch;
+    cryptonite-openssl = pkgs.haskell.lib.appendPatch super.cryptonite-openssl ./cryptonite-openssl-0.7.patch;
+    x509-system = pkgs.haskell.lib.appendPatch super.x509-system ./x509-system-1.6.6.patch;
+    conduit = pkgs.haskell.lib.appendPatch super.conduit ./conduit-1.3.0.2.patch;
+    ether = doTemplateHaskell super.ether;
+    generics-sop = doTemplateHaskell super.generics-sop;
+    th-lift-instances = doTemplateHaskell super.th-lift-instances;
+    math-functions = doTemplateHaskell super.math-functions;
+    wreq = doTemplateHaskell super.wreq;
+    swagger2 = doTemplateHaskell super.swagger2;
+    log-warper = doTemplateHaskell super.log-warper;
+    th-orphans = doTemplateHaskell super.th-orphans;
+    wai-app-static = doTemplateHaskell super.wai-app-static;
   };
 } // { pkgs-x = pkgs; }

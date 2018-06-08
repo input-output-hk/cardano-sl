4dcfbb728b9c661eabff02c5e19c2ca7ee594ce8
Michael Hueschen <michael.hueschen@iohk.io>
2018-06-07 18:50:16 -0700

    [CDEC-328] Add deriveSimpleBi<->deriveIndexedBi equivalence test

--- binary/test/test.hs
+++ binary/test/test.hs
@@ -1,1 +1,20 @@
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+import           Universum
+
+import           Test.Hspec (hspec)
+
+import           Spec (spec)
+
+import qualified Test.Pos.Binary.BiSerialize
+
+main :: IO ()
+main = do
+    hspec spec
+    runTests
+        [ Test.Pos.Binary.BiSerialize.tests
+        ]
+
+runTests :: [IO Bool] -> IO ()
+runTests tests = do
+    result <- andM tests
+    unless result
+        exitFailure

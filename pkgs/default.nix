712cea0e2b8b19b71df4e67bf592f80de996fdcd
KtorZ <matthias.benkort@gmail.com>
2018-06-13 16:02:05 +0200

    [CBR-289] Re-generate pkgs/default.nix

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -13938,33 +13938,49 @@ license = stdenv.lib.licenses.bsd3;
 ({
   mkDerivation
 , base
+, basement
 , bytestring
 , cryptonite
 , cryptonite-openssl
 , deepseq
 , fetchgit
+, foundation
 , hashable
+, integer-gmp
 , memory
 , stdenv
 }:
 mkDerivation {
 
 pname = "cardano-crypto";
-version = "1.0.0";
+version = "1.1.0";
 src = fetchgit {
 
 url = "https://github.com/input-output-hk/cardano-crypto";
-sha256 = "05i1z01fzg0la4hk2ff0l89sk0a5ada81w60kwj9i8ix74jchp94";
-rev = "287cc575fafe86af9d24af9d012c47f9d3f04da0";
+sha256 = "1v165n4cmp4g272406vaaan7bwvrw2m55jwcbz8qqjxslxv1l63p";
+rev = "33c7ecc6e4bd71c3ea0195e9d796eeace7be22cf";
 
 };
+isLibrary = true;
+isExecutable = true;
 libraryHaskellDepends = [
 base
+basement
 bytestring
 cryptonite
 cryptonite-openssl
 deepseq
+foundation
 hashable
+integer-gmp
+memory
+];
+executableHaskellDepends = [
+base
+basement
+bytestring
+cryptonite
+foundation
 memory
 ];
 doHaddock = false;
@@ -16901,6 +16917,7 @@ license = stdenv.lib.licenses.mit;
 , async
 , base
 , base58-bytestring
+, basement
 , bytestring
 , cardano-crypto
 , cardano-sl
@@ -16987,7 +17004,9 @@ aeson
 async
 base
 base58-bytestring
+basement
 bytestring
+cardano-crypto
 cardano-sl
 cardano-sl-block
 cardano-sl-client

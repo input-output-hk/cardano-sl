52daffec4dcaa8337cad051389bd91b9a085e809
Erik de Castro Lopo <erikd@mega-nerd.com>
2018-06-26 08:04:34 +1000

    Fix minor wibbles

--- lib/src/Pos/Binary.hs
+++ lib/src/Pos/Binary.hs
@@ -9,7 +9,6 @@ import           Pos.Binary.Class
 import           Pos.Binary.Communication ()
 import           Pos.Binary.Core ()
 import           Pos.Binary.Delegation ()
-import           Pos.Binary.Merkle ()
 import           Pos.Binary.Ssc ()
 import           Pos.Binary.Txp ()
 import           Pos.Binary.Update ()

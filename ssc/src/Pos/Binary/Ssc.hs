4e39eae5c804edfd4f35f20333251124dd824c66
Jordan Millar <jordan.millar@iohk.io>
2018-06-28 09:31:12 -0400

    [CDEC-401] Relocate `Bi` instances from `Pos.Binary.Ssc.Toss`

--- ssc/src/Pos/Binary/Ssc.hs
+++ ssc/src/Pos/Binary/Ssc.hs
@@ -3,11 +3,9 @@
 
 module Pos.Binary.Ssc
        ( module Pos.Binary.Ssc.Relay
-       , module Pos.Binary.Ssc.Toss
        , module Pos.Binary.Ssc.Types
        ) where
 
 import           Pos.Binary.Ssc.Relay
-import           Pos.Binary.Ssc.Toss
 import           Pos.Binary.Ssc.Types ()
 

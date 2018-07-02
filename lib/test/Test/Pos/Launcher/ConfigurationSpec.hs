4723d7e6732a4e559076680d67faf1b8e6d0446d
Marcin Szamotulski <marcin.szamotulski@iohk.io>
2018-07-02 15:43:19 +0200

    [CDEC-405] Test for parsing lib/configuration.yaml

---
+++ lib/test/Test/Pos/Launcher/ConfigurationSpec.hs
@@ -0,0 +1,31 @@
+module Test.Pos.Launcher.ConfigurationSpec
+    ( spec
+    ) where
+
+import           Universum
+
+import           Data.Time.Clock.POSIX (getPOSIXTime)
+import           System.Wlog (LoggerName (..))
+import           Test.Hspec (Spec, describe, it, shouldSatisfy)
+
+import           Pos.Core.Slotting (Timestamp (..))
+import           Pos.Launcher.Configuration (ConfigurationOptions (..),
+                     defaultConfigurationOptions, withConfigurationsM)
+import           Pos.Util.Config (ConfigurationException)
+
+spec :: Spec
+spec = describe "Pos.Launcher.Configuration" $ do
+    describe "withConfigurationsM" $ do
+        it "should parse `lib/configuration.yaml` file" $ do
+            startTime <- Timestamp . round . (* 1000000) <$> liftIO getPOSIXTime
+            let cfo = defaultConfigurationOptions
+                        { cfoFilePath = "./configuration.yaml"
+                        , cfoSystemStart = Just startTime
+                        }
+            let catchFn :: ConfigurationException -> IO (Maybe ConfigurationException)
+                catchFn e = return $ Just e
+            res  <- liftIO $ catch
+                (withConfigurationsM (LoggerName "test") Nothing cfo (\_ _ -> return Nothing))
+                catchFn
+            res `shouldSatisfy` isNothing
+

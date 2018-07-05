bf26602648bafbd334690b750872ff20109a9ff7
parsonsmatt <parsonsmatt@gmail.com>
2018-07-05 13:46:00 -0600

    [CO-334] Rename to internal

--- wallet-new/src/Cardano/Wallet/API/V1/Internal.hs
+++ wallet-new/src/Cardano/Wallet/API/V1/Internal.hs
@@ -1,12 +1,12 @@
 -- | This module contains the top level API definition for frontend-related
 -- tasks.  The API endpoints presented here are intended for use with the
 -- Daedalus client, and aren't useful for wallets, exchanges, and other users.
-module Cardano.Wallet.API.V1.Daedalus where
+module Cardano.Wallet.API.V1.Internal where
 
 -- migrate everything except for import/export
 
 type API =
-    "daedalus"
+    "internal"
         :> ( "update"
             :> ( "apply"
                 :> Post '[ValidJSON] NoContent

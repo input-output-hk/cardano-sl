d24b22df504674ef0c49eedc4ed2f10253df292c
parsonsmatt <parsonsmatt@gmail.com>
2018-07-06 14:07:19 -0600

    [CO-336] Finish implementing handler

--- wallet-new/src/Cardano/Wallet/API/V1/Internal/Redemption.hs
+++ wallet-new/src/Cardano/Wallet/API/V1/Internal/Redemption.hs
@@ -10,10 +10,10 @@ import           Cardano.Wallet.API.V1.Migration (HasConfigurations, MonadV1,
                      migrate)
 import           Cardano.Wallet.API.V1.Types
 import           Pos.Core (TxAux)
-import           Pos.Core.Configuration (HasConfiguration)
 import           Pos.Crypto.Configuration (ProtocolMagic)
-import qualified Pos.Wallet.Web.ClientTypes.Types as V0
 import qualified Pos.Wallet.Web.Methods.Redeem as V0
+import qualified Pos.Wallet.Web.ClientTypes as V0
+import qualified Pos.Util.Servant as V0
 
 type API
     = Summary "TODO: Describe the redemption endpoint"
@@ -37,11 +37,21 @@ redeemAda
     -> MonadV1 (WalletResponse Transaction)
 redeemAda pm submitTx r = do
     let ShieldedRedemptionCode seed = redemptionRedemptionCode r
-        cwalletRedeem = error "TODO: conjure this up"
         passphrase = maybe def unV1 (redemptionPassphrase r)
+    accountId <- migrate (redemptionWalletId r, redemptionAccountIndex r)
+    let caccountId = V0.encodeCType accountId
     fmap single . migrate =<< case redemptionMnemonic r of
         Just mnemonic -> do
-            let cpaperRedeem = error "TODO: conjure this up"
+            phrase <- migrate mnemonic
+            let cpaperRedeem = V0.CPaperVendWalletRedeem
+                    { V0.pvWalletId = caccountId
+                    , V0.pvSeed = seed
+                    , V0.pvBackupPhrase = phrase
+                    }
             V0.redeemAdaPaperVend pm submitTx passphrase cpaperRedeem
-        Nothing ->
+        Nothing -> do
+            let cwalletRedeem = V0.CWalletRedeem
+                    { V0.crWalletId = caccountId
+                    , V0.crSeed = seed
+                    }
             V0.redeemAda pm submitTx passphrase cwalletRedeem

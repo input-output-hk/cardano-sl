08005452f0512b171e03e180adcf38646d272a49
KtorZ <matthias.benkort@gmail.com>
2018-07-09 09:30:17 +0200

    [CO-324] Extend integration test suites for account per-field endpoint
    There was no integration tests for any accounts endpoints :s ... We should work on that in the future as I didn't take care of it
    in this commit / task, but solely add a few one for the newly added endpoints

---
+++ wallet-new/integration/AccountSpecs.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE RankNTypes    #-}
+{-# LANGUAGE TupleSections #-}
+
+module AccountSpecs (accountSpecs) where
+
+import           Universum
+
+import           Cardano.Wallet.API.Indices (accessIx)
+import           Cardano.Wallet.Client.Http
+import           Control.Lens
+import           Pos.Core.Common (mkCoin)
+import           Test.Hspec
+import           Util
+
+import qualified Pos.Core as Core
+import qualified Prelude
+
+
+accountSpecs :: WalletRef -> WalletClient IO -> Spec
+accountSpecs _ wc =
+    describe "Accounts" $ do
+        it "can retrieve only an accounts balance" $ do
+            let zero = V1 (mkCoin 0)
+            (Wallet{..}, Account{..}) <- randomAccount wc
+            eresp <- getAccountBalance wc walId accIndex
+
+            partialAccount <- wrData <$> eresp `shouldPrism` _Right
+            partialAccount `shouldBe` AccountBalance zero
+
+        it "can retrieve only an account's addresses" $ do
+            pair@(Wallet{..}, Account{..}) <- randomAccount wc
+            addresses <- createAddresses wc 10 pair
+            let addr = Prelude.head addresses
+            let tests =
+                    [ PaginationTest (Just 1) (Just 5) NoFilters NoSorts
+                        (expectNAddresses 5)
+                    , PaginationTest (Just 1) (Just 5) (filterByAddress addr) NoSorts
+                        (expectExactlyAddresses [addr])
+                    , PaginationTest (Just 2) (Just 5) (filterByAddress addr) NoSorts
+                        (expectExactlyAddresses [])
+                    ]
+
+            forM_ tests $ \PaginationTest{..} -> do
+                eresp <- getAccountAddresses wc walId accIndex page perPage filters sorts
+                expectations . acaAddresses . wrData =<< eresp `shouldPrism` _Right
+  where
+    filterByAddress :: WalletAddress -> FilterOperations WalletAddress
+    filterByAddress addr =
+        FilterOp (FilterByIndex $ accessIx @_ @(V1 Core.Address) addr) NoFilters
+
+    expectNAddresses :: Int -> [WalletAddress] -> IO ()
+    expectNAddresses n addrs =
+        length addrs `shouldBe` n
+
+    expectExactlyAddresses :: [WalletAddress] -> [WalletAddress] -> IO ()
+    expectExactlyAddresses as bs =
+        sort as `shouldBe` sort bs

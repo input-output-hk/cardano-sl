01d75539f6f5e210655ca6775524004ed7799d57
Alexander Diemand <codieplusplus@apax.net>
2018-07-09 10:04:09 +0200

    safer version of the script
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

--- scripts/haskell/update-cabal-versions.sh
+++ scripts/haskell/update-cabal-versions.sh
@@ -5,15 +5,22 @@ if [ -z "$1" ]; then
     exit
 fi
 
+testSED=$(sed --version &> /dev/null && echo YES || echo NO)
+if [ $testSED != "YES" ]; then
+  echo "don't know if your version of 'sed' works."
+  echo "  on a mac:  install 'gnu-sed' from homebrew/.."
+  exit 1
+fi
+
 newVersion=$1
 
 function updateVersion() {
   sed -E -i -e "s/^(version\\:\\s+)(.+)/\\1$newVersion/" "$1"
 }
 
-for CB in `git ls-files '*/cardano-*.cabal'`; do
+for CB in $(git ls-files '*/cardano-*.cabal'); do
   echo "   ${CB}"
-  updateVersion ${CB}
+  updateVersion "${CB}"
 done
 
 echo "Updated to version $newVersion"

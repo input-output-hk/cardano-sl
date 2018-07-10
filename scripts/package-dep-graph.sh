90446200253b637e76b00057a459c8f5e0c48ed9
Michael Hueschen <michael.hueschen@iohk.io>
2018-07-09 17:44:30 -0700

    [CDEC-429] Flesh out / document `package-dep-graph.sh`
    - Add options to script
    - Document script in `stack.yaml`

--- scripts/package-dep-graph.sh
+++ scripts/package-dep-graph.sh
@@ -1,19 +1,84 @@
 #!/bin/bash -eu
 
+################################################################################
+# Check deps
+################################################################################
+
+if ! command -v dot >/dev/null 2>&1; then
+    echo "\`dot\` not found. It is usually in the \`graphviz\` package."
+    exit 1
+fi
+
+if ! command -v tred >/dev/null 2>&1; then
+    echo "\`tred\` not found. It is usually in the \`graphviz\` package."
+    exit 1
+fi
+
+################################################################################
+# Argument processing
+################################################################################
+
+script=$(basename "$0")
+stack_dot_flags=""
+include_test_bench="0"
+use_tred="1"
+
+while getopts "hetf" opt; do
+  case $opt in
+    h)
+      echo "usage: ./${script} OPTS" >&2
+      echo "OPTS:" >&2
+      echo "    -h : show help" >&2
+      echo "    -e : include external dependencies in graph" >&2
+      echo "    -t : include test+bench packages in graph" >&2
+      echo "    -f : don't use \`tred\` - render full deg graph" >&2
+      exit 0
+      ;;
+    e)
+      stack_dot_flags="${stack_dot_flags} --external"
+      ;;
+    t)
+      include_test_bench="1"
+      ;;
+    f)
+      use_tred="0"
+      ;;
+    ?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+
+################################################################################
+# Generate graph
+################################################################################
+
 tmpdir=$(mktemp -d "${TMPDIR:-/tmp/}$(basename \"$0\").XXXXXXXXXXXX")
+outfile="cardano-sl-pkg-deps.png"
 
 # 'tred' and 'dot' are in the 'graphviz' of most Linux distributions.
 
-prunefiles=$(find . -name \*.cabal -exec basename {} \; \
-    | grep -v stack-work | grep test.cabal \
-    | sed 's/\.cabal//' | tr '\n' ',')
+if [ "${include_test_bench}" = "0" ]; then
+    prunefiles=$(find . -name \*.cabal -exec basename {} \; \
+        | grep -v stack-work | grep "test.cabal\|bench.cabal" \
+        | sed 's/\.cabal//' | tr '\n' ',')
+    stack_dot_flags="${stack_dot_flags} --prune ${prunefiles}"
+fi
 
-stack dot --prune "${prunefiles}" > "${tmpdir}/full-dependencies.dot"
+stack dot ${stack_dot_flags} > "${tmpdir}/full-dependencies.dot"
 
-tred "${tmpdir}/full-dependencies.dot" > "${tmpdir}/direct-dependencies.dot"
+final_dotfile=""
+if [ "${use_tred}" = "1" ]; then
+    final_dotfile="${tmpdir}/direct-dependencies.dot"
+    tred "${tmpdir}/full-dependencies.dot" > ${final_dotfile}
+else
+    final_dotfile="${tmpdir}/full-dependencies.dot"
+fi
 
-dot -Tpng "${tmpdir}/direct-dependencies.dot" -o cardano-sl-pkg-deps.png
+dot -Tpng ${final_dotfile} -o ${outfile}
 
 rm -rf "${tmpdir:?}/"
 
-echo "Generated cardano-sl-pkg-deps.png"
+echo "Generated ${outfile}"

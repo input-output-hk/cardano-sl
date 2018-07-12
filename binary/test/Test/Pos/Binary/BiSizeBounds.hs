eb165e3e07352de8f7b425d208d06baf3ffc0d37
Matt Noonan <mnoonan@kataskeue.com>
2018-07-11 22:00:16 -0400

    Complete tests for Core types.

--- binary/test/Test/Pos/Binary/BiSizeBounds.hs
+++ binary/test/Test/Pos/Binary/BiSizeBounds.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE LambdaCase          #-}
 {-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RankNTypes          #-}
+{-# LANGUAGE RecordWildCards     #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module Test.Pos.Binary.BiSizeBounds
     ( tests
@@ -13,6 +15,7 @@ import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BSL
 import           Data.Map (Map)
 import qualified Data.Map as M
+import           Data.Tagged (Tagged (..))
 import           Data.Text.Lazy (unpack)
 import           Data.Text.Lazy.Builder (toLazyText)
 import           Data.Typeable (TypeRep, typeRep)
@@ -26,106 +29,137 @@ import           Serokell.Data.Memory.Units (Byte)
 bshow :: Buildable a => a -> String
 bshow = unpack . toLazyText . bprint build
 
-testSizeBounds :: (Buildable a, Bi a) => Gen a -> Property
-testSizeBounds = testSizeBoundsWithBuilder bshow
+-- | Configuration for a single test case.
+data TestConfig a = TestConfig
+    { debug    :: a -> String      -- ^ Pretty-print values
+    , lengthOf :: Maybe (a -> Int) -- ^ Compute the length of a value
+    , lengthTy :: TypeRep         -- ^ TypeRep used to represent the length.
+    , gen      :: Gen a           -- ^ Generator
+    , precise  :: Bool            -- ^ Must estimates be exact?
+    }
 
-testListSizeBounds :: (Buildable [a], Bi [a]) => Gen a -> Property
-testListSizeBounds = testSizeBounds . Gen.list (Range.linear 0 300)
+-- | Default configuration, for @Buildable@ types.
+cfg :: forall a. (Typeable a, Buildable a) => TestConfig a
+cfg = TestConfig { debug    = bshow
+                 , lengthOf = Nothing
+                 , lengthTy = typeRep (Proxy @(LengthOf a))
+                 , gen      = Gen.discard
+                 , precise  = False
+                 }
 
-testSizeBoundsWithBuilder :: Bi a => (a -> String) -> Gen a -> Property
-testSizeBoundsWithBuilder debug gen = property $ do
+-- | Default configuration, for @Show@able types.
+scfg :: forall a. (Typeable a, Show a) => TestConfig a
+scfg = TestConfig { debug    = show
+                  , lengthOf = Nothing
+                  , lengthTy = typeRep (Proxy @(LengthOf a))
+                  , gen      = Gen.discard
+                  , precise  = False
+                  }
+
+runTest :: forall a. Bi a => TestConfig a -> Property
+runTest TestConfig{..} = property $ do
     x <- forAllWith debug gen
-    case szVerify x of
-        WithinBounds -> success
-        BoundsAreSymbolic bounds -> do
-            annotate ("Bounds are symbolic: " <> bshow bounds)
-            failure
-        OutOfBounds sz bounds -> do
+
+    let ctx = case lengthOf of
+            Nothing  -> []
+            Just len -> [ (lengthTy, SizeConstant $ fromIntegral (len x)) ]
+
+        badBounds sz bounds = do
             annotate ("Computed bounds: " <> bshow bounds)
             annotate ("Actual size:     " <> show sz)
             annotate ("Value: " <> debug x)
             failure
 
-testSizeBoundsWithLength :: forall a. (Buildable a, Bi a) => (a -> Int) -> Gen a -> Property
-testSizeBoundsWithLength lengthOf gen = property $ do
-    x <- forAllWith bshow gen
-    let ctx = M.fromList [ (typeRep (Proxy @(LengthOf a)),
-                            SizeConstant $ fromIntegral (lengthOf x)) ]
-    case szVerifyCtx ctx x of
-        WithinBounds -> success
+    case szVerify (M.fromList ctx) x of
+        Exact -> success
+        WithinBounds _ _  | not precise -> success
+        WithinBounds sz bounds -> badBounds sz bounds
         BoundsAreSymbolic bounds -> do
             annotate ("Bounds are symbolic: " <> bshow bounds)
             failure
-        OutOfBounds sz bounds -> do
-            annotate ("Computed bounds: " <> bshow bounds)
-            annotate ("Actual size:     " <> show sz)
-            annotate ("Value: " <> bshow x)
-            failure
+        OutOfBounds sz bounds -> badBounds sz bounds
 
-encodedSize :: Bi a => a -> Byte
-encodedSize = fromIntegral . BSL.length . toLazyByteString . encode
-
+-- | The possible results from @szVerify@, describing various ways
+--   a size can or cannot be found within a certain range.
 data ComparisonResult
-    = WithinBounds
-    | BoundsAreSymbolic Size
-    | OutOfBounds Byte (Range Byte)
+    = Exact                          -- ^ Size matched the bounds, and the bounds were exact.
+    | WithinBounds Byte (Range Byte) -- ^ Size matched the bounds, but the bounds are not exact.
+    | BoundsAreSymbolic Size         -- ^ The bounds could not be reduced to a numerical range.
+    | OutOfBounds Byte (Range Byte)  -- ^ The size fell outside of the bounds.
 
-{-
-instance Show ComparisonResult where
-    show = \case
-        WithinBounds         -> "WithinBounds"
-        BoundsAreSymbolic sz -> "BoundsAreSymbolic " <> bshow sz
-        OutOfBounds sz range -> "OutOfBounds " <> show sz <> " " <> bshow range
--}
-
-szVerify :: Bi a => a -> ComparisonResult
-szVerify = szVerifyCtx (M.fromList [])
-
-szVerifyCtx :: Bi a => Map TypeRep SizeOverride -> a -> ComparisonResult
-szVerifyCtx ctx x = case szSimplify (szWithCtx ctx (pure x)) of
+-- | For a given value @x :: a@ with @Bi a@, check that the encoded size
+--   of @x@ falls within the statically-computed size range for @a@.
+szVerify :: Bi a => Map TypeRep SizeOverride -> a -> ComparisonResult
+szVerify ctx x = case szSimplify (szWithCtx ctx (pure x)) of
     Left bounds -> BoundsAreSymbolic bounds
-    Right range | lo range <= sz && sz <= hi range -> WithinBounds
+    Right range | lo range <= sz && sz <= hi range ->
+                      if lo range == hi range
+                      then Exact
+                      else WithinBounds sz range
     Right range | otherwise -> OutOfBounds sz range
   where
-    sz = encodedSize x
+    sz = fromIntegral $ BSL.length $ toLazyByteString $ encode x
 
 tests :: IO Bool
 tests =
-    H.checkParallel $ Group "Encoded size bounds for core types."
-    $ [ ("()"     , testSizeBoundsWithBuilder (const "()") (pure ()))
-      , ("Bool"   , testSizeBounds Gen.bool)
-      , ("Char"   , testSizeBounds Gen.unicode)
-      , ("Char 2" , testSizeBounds Gen.latin1)
-      , ("String" , testListSizeBounds Gen.unicode)
-      , ("String 2" , testListSizeBounds Gen.latin1)
-      , ("Word"   , testSizeBounds (Gen.word   Range.exponentialBounded))
-      , ("Word8"  , testSizeBounds (Gen.word8  Range.exponentialBounded))
-      , ("Word16" , testSizeBounds (Gen.word16 Range.exponentialBounded))
-      , ("Word32" , testSizeBounds (Gen.word32 Range.exponentialBounded))
-      , ("Word64" , testSizeBounds (Gen.word64 Range.exponentialBounded))
-      , ("Int"    , testSizeBounds (Gen.int    Range.exponentialBounded))
-      , ("Float"  , testSizeBounds (Gen.float  (Range.exponentialFloat 0 100000)))
-      , ("Int32"  , testSizeBounds (Gen.int32  Range.exponentialBounded))
-      , ("Int64"  , testSizeBounds (Gen.int64  Range.exponentialBounded))
-      -- , ("Tagged _ Char", testSizeBounds (Tagged <$> Gen.unicode))
-      , ("(Char, Bool)", testSizeBoundsWithBuilder
-                           (\(a,b) -> "(" <> bshow a <> "," <> bshow b <> ")")
-                           ((,) <$> Gen.unicode <*> Gen.bool))
-      , ("(Char, Char, Bool)", testSizeBoundsWithBuilder
-                               (\(a,b,c) -> "(" <> bshow a <> "," <> bshow b <> "," <> bshow c <> ")")
-                               ((,,) <$> Gen.unicode <*> Gen.unicode <*> Gen.bool))
-      , ("(Char, Char, Bool, Bool)", testSizeBoundsWithBuilder
-                               (\(a,b,c,d) -> "(" <> bshow a <> "," <> bshow b <> "," <> bshow c <> "," <> bshow d <> ")")
-                               ((,,,) <$> Gen.unicode <*> Gen.unicode <*> Gen.bool <*> Gen.bool))
-      , ("ByteString"     , testSizeBoundsWithBuilder (show . BS.unpack)  (Gen.bytes (Range.linear 0 1000)))
-      , ("Lazy.ByteString", testSizeBoundsWithBuilder (show . BSL.unpack) (BSL.fromStrict <$> Gen.bytes (Range.linear 0 1000)))
-      , ("Text"           , testSizeBoundsWithLength length (Gen.text (Range.linear 0 1000) Gen.latin1))
-      , ("Text 2"         , testSizeBoundsWithLength length (Gen.text (Range.linear 0 1000) Gen.unicode))
-      --, ("[Word16]"       , testListSizeBounds (Gen.word16 Range.exponentialBounded))
-      --, ("NonEmpty Word16"       , testListSizeBounds (Gen.word16 Range.exponentialBounded))
-      --, ("Either Char Bool"  , testSizeBoundsWithBuilder (either bshow bshow) (Left <$> Gen.unicode))
-      --, ("Either Char Bool 2", testSizeBoundsWithBuilder (either bshow bshow) (Right <$> Gen.bool))
-      , ("Maybe Char"     , testSizeBounds (Gen.maybe Gen.unicode))
-      , ("Maybe Char 2"   , testSizeBounds (Gen.maybe Gen.latin1))
-      ]
+    let listOf = Gen.list (Range.linear 0 300)
+        wordGen = Gen.word Range.exponentialBounded
+    in H.checkParallel $ Group "Encoded size bounds for core types."
+       $ [ ("()"     , runTest $ scfg { gen = pure (), precise = True })
+         , ("Bool"   , runTest $ cfg { gen = Gen.bool, precise = True })
+         , ("Char"   , runTest $ cfg { gen = Gen.unicode })
+         , ("Char 2" , runTest $ cfg { gen = Gen.latin1 })
+         , ("String"   , runTest $ cfg { gen = listOf Gen.unicode
+                                              , lengthOf = Just length
+                                              , lengthTy = typeRep (Proxy @(LengthOf [Char])) })
+         , ("String 2" , runTest $ cfg { gen = listOf Gen.latin1
+                                              , lengthOf = Just length
+                                              , lengthTy = typeRep (Proxy @(LengthOf [Char])) })
+         , ("Word"   , runTest $ cfg { gen = Gen.word   Range.exponentialBounded })
+         , ("Word8"  , runTest $ cfg { gen = Gen.word8  Range.exponentialBounded })
+         , ("Word16" , runTest $ cfg { gen = Gen.word16 Range.exponentialBounded })
+         , ("Word32" , runTest $ cfg { gen = Gen.word32 Range.exponentialBounded })
+         , ("Word64" , runTest $ cfg { gen = Gen.word64 Range.exponentialBounded })
+         , ("Int"    , runTest $ cfg { gen = Gen.int    Range.exponentialBounded })
+         , ("Float"  , runTest $ cfg { gen = Gen.float (Range.exponentialFloat (-1000000) 1000000) })
+         , ("Int32"  , runTest $ cfg { gen = Gen.int32  Range.exponentialBounded })
+         , ("Int64"  , runTest $ cfg { gen = Gen.int64  Range.exponentialBounded })
+         , ("Tagged () Word32", runTest $ (scfg :: TestConfig (Tagged () Word32))
+               { gen = Tagged <$> Gen.word32 Range.exponentialBounded })
+         , ("(Char, Bool)",
+               runTest $ scfg { gen = (,) <$> Gen.unicode <*> Gen.bool })
+         , ("(Char, Char, Bool)",
+               runTest $ scfg { gen = ((,,) <$> Gen.unicode <*> Gen.unicode <*> Gen.bool) })
+         , ("(Char, Char, Bool, Bool)",
+               runTest $ scfg { gen = ((,,,) <$> Gen.unicode <*> Gen.unicode <*> Gen.bool <*> Gen.bool) })
+         , ("ByteString"     , runTest $ (scfg :: TestConfig BS.ByteString)
+               { debug = show . (BS.unpack :: BS.ByteString -> [Word8])
+               , lengthOf = Just (fromIntegral . BS.length)
+               , gen = Gen.bytes (Range.linear 0 1000) })
+         , ("Lazy.ByteString", runTest $ (scfg :: TestConfig BSL.ByteString)
+               { debug = show . (BSL.unpack :: BSL.ByteString -> [Word8])
+               , lengthOf = Just (fromIntegral . BSL.length)
+               , gen = BSL.fromStrict <$> Gen.bytes (Range.linear 0 1000) })
+         , ("Text", runTest $ cfg
+               { lengthOf = Just length
+               , lengthTy = typeRep (Proxy @(LengthOf [Char]))
+               , gen = Gen.text (Range.linear 0 1000) Gen.latin1 })
+         , ("Text 2", runTest $ cfg
+               { lengthOf = Just length
+               , lengthTy = typeRep (Proxy @(LengthOf [Char]))
+               , gen = Gen.text (Range.linear 0 1000) Gen.unicode })
+         , ("[Bool]"       , runTest $ scfg
+               { gen = listOf Gen.bool
+               , lengthOf = Just length
+               , precise = True})
+         , ("NonEmpty Bool", runTest $ scfg
+               { gen = listOf Gen.bool
+               , lengthOf = Just length
+               , precise = True })
+         , ("Either Bool Word", runTest $ (scfg :: TestConfig (Either Bool Word))
+               { gen = Left  <$> Gen.bool })
+         , ("Either Bool Word", runTest $ (scfg :: TestConfig (Either Bool Word))
+               { gen = Right <$> wordGen })
+         , ("Maybe Word"      , runTest $ cfg { gen = wordGen })
+         ]
 

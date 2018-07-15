0483ccc27cce6380f9d3f7af4b1d14146522672e
Erik de Castro Lopo <erikd@mega-nerd.com>
2018-07-16 08:27:35 +1000

    CDEC-438: Move Shutdown from the sin bin to infra

--- sinbin/src/Pos/Sinbin/Shutdown.hs
+++
@@ -1,7 +0,0 @@
-module Pos.Sinbin.Shutdown
-       ( module X
-       ) where
-
-import           Pos.Sinbin.Shutdown.Class as X
-import           Pos.Sinbin.Shutdown.Logic as X
-import           Pos.Sinbin.Shutdown.Types as X

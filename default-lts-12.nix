581e74efc178d86f23a3586b2750b0ee9c95bfdd
Moritz Angermann <moritz.angermann@gmail.com>
2018-07-23 15:07:24 +0800

    bump stackage; alignt default-lts-12 with default

--- default-lts-12.nix
+++ default-lts-12.nix
@@ -1,10 +1,14 @@
-{ system ? builtins.currentSystem, target ? null, n ? 0}:
 let
-  # this is some hack to append spaces to the configureFlags of the
-  # default derivation in GHC.  This allows us to force a rebuild
-  # of all haskell packages.
-  spaces = let repeat = n: c: c + (if n == 0 then "" else repeat (n - 1) c); in repeat n " ";
-  
+  localLib = import ./lib.nix;
+  jemallocOverlay = self: super: {
+    # jemalloc has a bug that caused cardano-sl-db to fail to link (via
+    # rocksdb, which can use jemalloc).
+    # https://github.com/jemalloc/jemalloc/issues/937
+    # Using jemalloc 510 with the --disable-initial-exec-tls flag seems to
+    # fix it.
+    jemalloc = self.callPackage ./nix/jemalloc/jemalloc510.nix {};
+  };
+
   stack-pkgs = import ./stack-pkgs.nix;
 
   # We use some customized libiserv/remote-iserv/iserv-proxy
@@ -25,7 +29,8 @@ let
   #       that way we could pick the proper `lts-12_0` based
   #       on the stack.yaml file, instead of hand-injecting
   #       it here.
-  overlay = self: super: {
+  packageOverlay = self: super: {
+    # the lts-XX_Y is essentially the same as what pkgs/default.nix returns.
     haskellPackages = ((import <stackage> { pkgs = super; }).lts-12_0
       # ontop of the LTS, inject the extra pacakges and source deps.
       { extraDeps = hsPkgs: (stack-pkgs.extraDeps hsPkgs
@@ -34,98 +39,27 @@ let
         { overrides = self: super:
           # global overrides (effect haskellPackages, as well as buildHaskellPackages)
           { libiserv = super.libiserv.override { flags = { network = true; }; }; }; }; };
-
+in
+{ system ? builtins.currentSystem
+, config ? import ./config.nix
+, gitrev ? localLib.commitIdFromGitRepo ./.git
+, buildId ? null
+, target ? null
+,n ? 0 }:
+let
+  # this is some hack to append spaces to the configureFlags of the
+  # default derivation in GHC.  This allows us to force a rebuild
+  # of all haskell packages.
+  spaces = let repeat = n: c: c + (if n == 0 then "" else repeat (n - 1) c); in repeat n " ";
+  
   # configure out general nix setup.
   # 
-  config = {
-    allowUnsupportedSystem = true;
-
-    wine.build = "wine64";
-
-    packageOverrides = ps: with ps; rec {
-      # use the pre-built rocksdb.
-      # We seem to be unable to actually
-      # build rocksdb with mingw64/gcc7
-      # without producing a partial dud.
-      #
-      # Using the pre-built rocksdb, also
-      # means we do not need the gcc7 hack
-      # in our nixpkgs to allow mingw with
-      # libwinpthreads.
-      rocksdb = with ps.stdenv;
-        if hostPlatform.isWindows
-        then pkgs.callPackage ./rocksdb-prebuilt.nix { inherit (buildPackages) fetchurl unzip; }
-        else ps.rocksdb;
-
-      # on windows we have this habit of putting libraries
-      # into `bin`, wheras on unix it's usually `lib`. For
-      # this confuses nix easily. So we'll just move the
-      # .dll's from `bin` into `$out/lib`. Such that they
-      # are trivially found.
-      openssl = ps.openssl.overrideAttrs (drv: {
-        postInstall = with ps.stdenv; drv.postInstall + lib.optionalString hostPlatform.isWindows ''
-          cp $bin/bin/*.dll $out/lib/
-        '';
-      });
-      mfpr = ps.mfpr.overrideAttrs (drv: {
-        configureFlags = with ps.stdenv; (drv.configureFlags or []) ++ lib.optional hostPlatform.isWindows "--enable-static --disable-shared";
-      });
-      libmpc = ps.libmpc.overrideAttrs (drv: {
-        configureFlags = with ps.stdenv; (drv.configureFlags or []) ++ lib.optional hostPlatform.isWindows "--enable-static --disable-shared";
-      });
-
-      haskell = lib.recursiveUpdate ps.haskell {
-        lib = ps.haskell.lib // (with ps.haskell.lib; {
-          # sanity
-          addExtraLibrary'  = ls: drv: addExtraLibrary drv ls;
-          addBuildDepends'  = ds: drv: addBuildDepends drv ds;
-          appendBuildFlags' = fs: drv: appendBuildFlags drv fs;
-          overrideCabal'    = os: drv: overrideCabal drv os;
-          addBuildTools'    = ts: drv: addBuildTools drv ts;
-          addPreBuild'      = x: drv:  overrideCabal drv (drv: { preBuild  = (drv.preBuild or  "") + x; });
-          addPostBuild'     = x: drv:  overrideCabal drv (drv: { postBuild = (drv.postBuild or "") + x; });
-          dropRevision'     = drv: overrideCabal drv (_: { revision = null; editedCabalFile = null; });
-        });
-
-        compiler.ghc843 = (ps.haskell.compiler.ghc843.override {
-          ghcFlavour = if ps.stdenv.targetPlatform == ps.stdenv.hostPlatform then "quick" else "perf-cross-ncg";
-          enableShared = false;
-          enableIntegerSimple = false;
-        }).overrideAttrs (drv: {
-          dontStrip = true;
-          hardeningDisable = [ "stackprotector" "format" ];
-          patches = (drv.patches or []) ++ [
-            ./move-iserv-8.4.2.patch
-            ./hsc2hs-8.4.2.patch
-            ./various-8.4.2.patch
-            ./lowercase-8.4.2.patch
-            ./cabal-exe-ext-8.4.2.patch
-            ./dll-loader-8.4.2.patch
-            ./outputtable-assert-8.4.2.patch
-          ];
-          postPatch = (drv.postPath or "") + ''
-          autoreconf
-          ''; 
-        });
-        packages.ghc843 = (ps.haskell.packages.ghc843.override {
-          overrides = self: super: rec {
-            mkDerivation = drv: super.mkDerivation (drv // {
-              enableLibraryProfiling = false;
-              enableSharedLibraries = false;
-              enableSharedExecutables = false;
-              configureFlags = (drv.configureFlags or []) ++ [ spaces ];
-            });
-          };
-        });
-      };
-    };
-  };
-
+  config = 
   # Combine the Overlay, and Config to produce
   # our package set.
   pkgs = import <nixpkgs> ({
     inherit system;
-    overlays = [ overlay ];
+    overlays = [ jmallocOverlay packageOverlay ];
     config = config;
   } // (if target != null
         then { crossSystem = { win64   = (import <nixpkgs/lib>).systems.examples.mingwW64;

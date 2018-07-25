5fe1219e547341b25d180c8720dfedee3583e8e0
Michael Bishop <michael.bishop@iohk.io>
2018-07-25 20:21:39 -0300

    clean up the fetch functions for most of the source

--- fetch-nixpkgs.nix
+++ fetch-nixpkgs.nix
@@ -1,6 +1,6 @@
 let
   # temporary hack until scripts/nix-shell.sh ceases to use -p
-  pkgs_path = import ./fetchNixpkgs.nix (builtins.fromJSON (builtins.readFile ./nixpkgs-src.json)).nixpkgs;
+  pkgs_path = import ./fetchNixpkgs.nix (builtins.fromJSON (builtins.readFile ./nixpkgs-src.json));
   pkgs = import pkgs_path { config = {}; overlays = []; };
   wrapped = pkgs.runCommand "nixpkgs" {} ''
     ln -sv ${pkgs_path} $out

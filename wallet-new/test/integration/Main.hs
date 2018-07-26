2be3bde670fe6f139ad6bfc5c9ecaff7563f6a3b
KtorZ <matthias.benkort@gmail.com>
2018-07-26 18:13:18 +0200

    [CBR-344] Use similar approach as 'actionWithWallet'
    Turns out that the code I was getting inspiration from (i.e. dbgen) works only when a database has already been initialized.
    Rather than spending too much time trying to understand how to do this properly from haskell code, I've switch my inspiration
    source to 'actionWithWallet' relying on 'runRealWMode'. It seems to work but the process is hanging upon starting the restoration.
    Perhaps the import takes too much time because it connects to a too big blockchain.

--- wallet-new/test/integration/Main.hs
+++ wallet-new/test/integration/Main.hs
@@ -9,32 +9,28 @@ import           System.Environment (lookupEnv)
 import           Test.Hspec (describe, hspec, it, shouldBe)
 
 import           Pos.Client.CLI.NodeOptions (CommonNodeArgs (..), NodeArgs (..))
--- import           Pos.Client.CLI.Options (CommonArgs (..))
 import           Pos.Client.CLI.Params (getNodeParams, gtSscParams)
-import           Pos.Core (ProtocolMagic, Timestamp (..))
-import           Pos.Core.Configuration (defaultGenesisSpec, epochSlots,
-                     withProtocolConstants)
+import           Pos.Core (Timestamp (..))
+import           Pos.Core.Configuration (HasGeneratedSecrets, epochSlots,
+                     generatedSecrets)
 import           Pos.Core.Constants (accountGenesisIndex, wAddressGenesisIndex)
-import           Pos.Core.Genesis (GeneratedGenesisData (..),
-                     GeneratedSecrets (..), GenesisProtocolConstants (..),
-                     GenesisSpec (..), PoorSecret, generateGenesisData,
-                     genesisProtocolConstantsToProtocolConstants,
+import           Pos.Core.Genesis (GeneratedSecrets (..), PoorSecret,
                      poorSecretToEncKey)
-import           Pos.Core.JsonLog.LogEvents (jsonLogConfigFromHandle)
-import           Pos.Core.Reporting (noReporter)
 import           Pos.Crypto.Signing (PassPhrase)
-import           Pos.DB (NodeDBs, closeNodeDBs, openNodeDBs)
 import           Pos.DB.DB (initNodeDBs)
+import           Pos.Infra.Diffusion.Types (Diffusion)
 import           Pos.Launcher (ConfigurationOptions (..), HasConfigurations,
-                     NodeParams (..), NodeResources (..), bracketNodeResources,
-                     withConfigurations)
+                     NodeParams (..), allocateNodeResources,
+                     releaseNodeResources, withConfigurations)
 import           Pos.Txp (txpGlobalSettings)
+import           Pos.Util.CompileInfo (HasCompileInfo, withCompileInfo)
 import           Pos.Util.UserSecret (WalletUserSecret (..), usVss)
+import           Pos.Wallet.WalletMode (WalletMempoolExt)
 import           Pos.Wallet.Web.Methods (importWalletDo)
-import           Pos.Wallet.Web.Mode (WalletWebMode, WalletWebModeContext (..))
+import           Pos.Wallet.Web.Mode (WalletWebMode)
+import           Pos.Wallet.Web.Server.Runner (runWRealMode)
+import           Pos.Wallet.Web.Sockets (closeWSConnections, initWSConnections)
 import           Pos.Wallet.Web.State.Acidic (closeState, openState)
-import           Pos.WorkMode (RealModeContext (..))
-import           System.Wlog (HasLoggerName (askLoggerName))
 
 
 --      INTEGRATION_TESTS_NODE_PATH   = Path to a valid rocksdb database
@@ -44,11 +40,11 @@ import           System.Wlog (HasLoggerName (askLoggerName))
 --      INTEGRATION_TESTS_CONFIG_KEY  = Key to use within that config file (e.g.  --      development, test ...)
 main :: IO ()
 main = do
-    nodePath    <- lookupEnvD "../integration_tests/node" "INTEGRATION_TESTS_NODE_PATH"
-    walletPath  <- lookupEnvD "../integration_tests/wallet" "INTEGRATION_TESTS_WALLET_PATH"
-    dbPath      <- lookupEnvD "../integration_tests/db" "INTEGRATION_TESTS_DB_PATH"
-    configPath  <- lookupEnvD "../lib/configuration.yaml" "INTEGRATION_TESTS_CONFIG_PATH"
-    configKey   <- lookupEnvD "test" "INTEGRATION_TESTS_CONFIG_KEY"
+    nodePath    <- lookupEnvD "state-integration-tests/node"    "INTEGRATION_TESTS_NODE_PATH"
+    walletPath  <- lookupEnvD "state-integration-tests/wallet"  "INTEGRATION_TESTS_WALLET_PATH"
+    dbPath      <- lookupEnvD "state-integration-tests/node-db" "INTEGRATION_TESTS_DB_PATH"
+    configPath  <- lookupEnvD "../lib/configuration.yaml"       "INTEGRATION_TESTS_CONFIG_PATH"
+    configKey   <- lookupEnvD "default"                            "INTEGRATION_TESTS_CONFIG_KEY"
 
     let args =
             ( def
@@ -64,7 +60,8 @@ main = do
                 }
             )
 
-    generateInitialState args defaultGenesisSpec
+    generateInitialState args
+
     hspec $ describe "Integration tests" $ do
         it "A first test" $
             True `shouldBe` True
@@ -72,11 +69,6 @@ main = do
         it "A second test" $
             False `shouldBe` False
   where
-    --lookupEnvE :: String -> IO String
-    --lookupEnvE var = do
-    --    let msg = "Missing required ENV var '" <> var <> "'"
-    --    maybe (fail msg) return =<< lookupEnv var
-
     lookupEnvD :: String -> String -> IO String
     lookupEnvD d var =
         fromMaybe d <$> lookupEnv var
@@ -91,87 +83,23 @@ data ExtraNodeArgs = ExtraNodeArgs
 
 
 -- | Generate an initial state for the integration tests
---
--- Example:
---    import Pos.Core.Configuration(defaultGenesisSpec)
---
---    generateInitialState defaultGenesisSpec
 generateInitialState
     :: (CommonNodeArgs, NodeArgs, ExtraNodeArgs)
-    -> GenesisSpec
     -> IO ()
-generateInitialState args spec = do
-    let wallets = genesisDataToWalletUserSecrets . genesisSpecToGenesisData $ spec
-    runWalletWebMode args $ forM_ wallets (uncurry importWalletDo)
-
-
--- | Run a particular wallet action in a default context.
---
--- Note that a few things are configurable through environment variable:
---
-runWalletWebMode
-    :: (CommonNodeArgs, NodeArgs, ExtraNodeArgs)
-    -> (HasConfigurations => WalletWebMode a)
-    -> IO a
-runWalletWebMode (commonNodeArgs, nodeArgs, ExtraNodeArgs{..}) action = do
-    let configOpts = ConfigurationOptions
-            { cfoFilePath    = _configPath
-            , cfoKey         = _configKey
-            , cfoSystemStart = Just (Timestamp (fromMicroseconds 1512847931))
-            , cfoSeed        = Nothing
-            }
-
-    withConfigurations Nothing configOpts
-        $ \_ protocolMagic -> bracket (openState True _walletPath) closeState
-        $ \walletState -> bracket (openNodeDBs True _nodePath) closeNodeDBs
-        $ \nodeDBs -> do
-            ctx <- WalletWebModeContext
-                <$> pure walletState
-                <*> newTVarIO def
-                <*> liftIO newTQueueIO
-                <*> newRealModeContext protocolMagic nodeDBs
-            action `runReaderT` ctx
+generateInitialState args = runWalletWebMode args $ \_ -> do
+    wallets <- generatedSecretsToWalletSecrets <$> getGeneratedSecrets
+    forM_ wallets (uncurry importWalletDo)
   where
-    -- | Create a new Real Mode context needed to run wallet's actions
-    newRealModeContext
-        :: HasConfigurations
-        => ProtocolMagic
-        -> NodeDBs
-        -> IO (RealModeContext ())
-    newRealModeContext pm dbs = do
-        loggerName <- askLoggerName
-        nodeParams <- getNodeParams loggerName commonNodeArgs nodeArgs
-        let (Just vssSK) = npUserSecret nodeParams ^. usVss
-        let gtParams = gtSscParams commonNodeArgs vssSK (npBehaviorConfig nodeParams)
-        bracketNodeResources @() nodeParams gtParams (txpGlobalSettings pm) (initNodeDBs pm epochSlots)
-            $ \NodeResources{..} -> RealModeContext
-                <$> pure dbs
-                <*> pure nrSscState
-                <*> pure nrTxpState
-                <*> pure nrDlgState
-                <*> jsonLogConfigFromHandle stdout
-                <*> pure loggerName
-                <*> pure nrContext
-                <*> pure noReporter
+    getGeneratedSecrets :: (MonadFail m, HasGeneratedSecrets) => m GeneratedSecrets
+    getGeneratedSecrets = do
+        let msg = "Couldn't find GeneratedSecrets. To fix this, make sure you \
+                  \run the following program with a `TestnetInitializer`."
+        maybe (fail msg) return generatedSecrets
 
+    generatedSecretsToWalletSecrets :: GeneratedSecrets -> [(PassPhrase, WalletUserSecret)]
+    generatedSecretsToWalletSecrets secrets =
+        map poorSecretToWalletUserSecrets (gsPoorSecrets secrets)
 
-genesisSpecToGenesisData :: GenesisSpec -> GeneratedGenesisData
-genesisSpecToGenesisData spec =
-    let
-        gsCst             = gsProtocolConstants spec
-        protocolConstants = genesisProtocolConstantsToProtocolConstants gsCst
-        protocolMagic     = gpcProtocolMagic gsCst
-        initializer       = gsInitializer spec
-        balances          = gsAvvmDistr spec
-    in
-        withProtocolConstants protocolConstants $
-            generateGenesisData protocolMagic initializer balances
-
-
-genesisDataToWalletUserSecrets :: GeneratedGenesisData -> [(PassPhrase, WalletUserSecret)]
-genesisDataToWalletUserSecrets genData =
-    map poorSecretToWalletUserSecrets (gsPoorSecrets . ggdSecrets $ genData)
-  where
     poorSecretToWalletUserSecrets :: PoorSecret -> (PassPhrase, WalletUserSecret)
     poorSecretToWalletUserSecrets secret =
         let
@@ -183,3 +111,31 @@ genesisDataToWalletUserSecrets genData =
                 }
         in
             (mempty, walUserSecret)
+
+
+runWalletWebMode
+    :: forall a
+    .  (CommonNodeArgs, NodeArgs, ExtraNodeArgs)
+    -> ((HasConfigurations, HasCompileInfo) => Diffusion WalletWebMode -> WalletWebMode a)
+    -> IO a
+runWalletWebMode (commonNodeArgs, nodeArgs, ExtraNodeArgs{..}) action = do
+    let configOpts = ConfigurationOptions
+            { cfoFilePath    = _configPath
+            , cfoKey         = _configKey
+            , cfoSystemStart = Just (Timestamp (fromMicroseconds 1512847931))
+            , cfoSeed        = Nothing
+            }
+
+    withCompileInfo $ withConfigurations Nothing configOpts
+        $ \_ pm -> bracket (openState True _walletPath) closeState
+        $ \db   -> bracket initWSConnections closeWSConnections
+        $ \conn -> do
+            params <- getNodeParams "integration-tests" commonNodeArgs nodeArgs
+            let (Just vssSK) = npUserSecret params ^. usVss
+            let ssc          = gtSscParams commonNodeArgs vssSK (npBehaviorConfig params)
+            let txp          = txpGlobalSettings pm
+            let mode         = initNodeDBs pm epochSlots
+            let acquire      = allocateNodeResources @WalletMempoolExt params ssc txp mode
+            bracket acquire releaseNodeResources $ \res -> do
+                queue <- liftIO newTQueueIO
+                runWRealMode pm db conn queue res action

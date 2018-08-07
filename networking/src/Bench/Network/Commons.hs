2e8ebc2c9a5859292d8c6fb4560beb9704c9aba7
Alexander Diemand <codieplusplus@apax.net>
2018-08-07 08:23:36 +0200

    [CBR-97] module 'networking' adapted to new logging
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

--- networking/src/Bench/Network/Commons.hs
+++ networking/src/Bench/Network/Commons.hs
@@ -12,8 +12,6 @@ module Bench.Network.Commons
        , Payload (..)
        , logMeasure
 
-       , loadLogConfig
-
        , Timestamp
        , MeasureEvent (..)
        , MeasureInfo (..)
@@ -23,7 +21,6 @@ module Bench.Network.Commons
        ) where
 
 import           Control.Applicative ((<|>))
-import           Control.Lens (zoom, (?=))
 import           Control.Monad (join)
 import           Control.Monad.Trans (MonadIO (..))
 
@@ -34,21 +31,16 @@ import           Data.Data (Data)
 import           Data.Functor (($>))
 import           Data.Int (Int64)
 import           Data.Monoid ((<>))
-import           Data.Text (Text)
 import           Data.Time.Units (toMicroseconds)
 import           Formatting.Buildable (Buildable (build))
 
 import qualified Formatting as F
 import           GHC.Generics (Generic)
 import           Prelude hiding (takeWhile)
-import           System.Wlog (LoggerConfig (..), errorPlus, fromScratch,
-                     infoPlus, lcTree, ltSeverity, maybeLogsDirB,
-                     parseLoggerConfig, productionB, setupLogging, warningPlus,
-                     zoomLogger)
 
 import           Node (Message (..))
 import           Pos.Util (realTime)
-import           Pos.Util.Trace (Trace, traceWith)
+import           Pos.Util.Trace.Named (TraceNamed, logInfo)
 
 -- * Transfered data types
 
@@ -80,29 +72,10 @@ instance Binary Payload where
 
 -- * Util
 
-logMeasure :: (MonadIO m) => Trace IO Text -> MeasureEvent -> MsgId -> Payload -> m ()
+logMeasure :: (MonadIO m) => TraceNamed IO -> MeasureEvent -> MsgId -> Payload -> m ()
 logMeasure logTrace miEvent miId miPayload = do
     miTime <- toMicroseconds <$> realTime
-    liftIO $ traceWith logTrace $ F.sformat F.build $ LogMessage MeasureInfo{..}
-
-defaultLogConfig :: LoggerConfig
-defaultLogConfig = fromScratch $ zoom lcTree $ do
-    ltSeverity ?= warningPlus
-    zoomLogger "sender" $ do
-        ltSeverity ?= infoPlus
-        commLogger
-    zoomLogger "receiver" $ do
-        ltSeverity ?= infoPlus
-        commLogger
-  where
-    commLogger = zoomLogger "comm" $ ltSeverity ?= errorPlus
-
-loadLogConfig :: MonadIO m => Maybe FilePath -> Maybe FilePath -> m ()
-loadLogConfig logsPrefix configFile = do
-    let cfgBuilder = productionB <> maybeLogsDirB logsPrefix
-    loggerConfig <- maybe (return defaultLogConfig) parseLoggerConfig configFile
-    setupLogging Nothing $ loggerConfig <> cfgBuilder
-
+    liftIO $ logInfo logTrace $ F.sformat F.build $ LogMessage MeasureInfo{..}
 
 -- * Logging & parsing
 
@@ -175,5 +148,5 @@ instance Buildable a => Buildable (LogMessage a) where
 
 logMessageParser :: Parser a -> Parser (Maybe (LogMessage a))
 logMessageParser p = (takeWhile (/= '#') >>) . join $ do
-        (char '#' *> pure (Just . LogMessage <$> p))
+        (char '#' $> (Just . LogMessage <$> p))
     <|> pure (pure Nothing)

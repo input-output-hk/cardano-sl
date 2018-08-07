65c4817451ea380b022e1ac496940e3fb576de01
Alexander Diemand <codieplusplus@apax.net>
2018-08-07 08:22:20 +0200

    [CBR-97] module 'chain' adapted to new logging
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

--- chain/src/Pos/Chain/Ssc/Shares.hs
+++ chain/src/Pos/Chain/Ssc/Shares.hs
@@ -11,7 +11,6 @@ import           Universum hiding (id)
 import           Crypto.Random (drgNewSeed, seedNew, withDRG)
 import qualified Data.HashMap.Strict as HM
 import           Formatting (build, sformat, (%))
-import           System.Wlog (WithLogger, launchNamedPureLog, logWarning)
 
 import           Pos.Binary.Class (AsBinary, asBinary, fromBinary)
 import           Pos.Chain.Ssc.Mem (MonadSscMem, SscGlobalQuery,
@@ -21,33 +20,36 @@ import           Pos.Core.Common (StakeholderId, addressHash)
 import           Pos.Core.Ssc (Commitment (..), getCommitmentsMap)
 import           Pos.Crypto (DecShare, EncShare, VssKeyPair, VssPublicKey,
                      decryptShare, toVssPublicKey)
+import           Pos.Util.Trace.Named (TraceNamed, logWarning)
 
 -- | Decrypt shares (in commitments) that are intended for us and that we can
 -- decrypt.
 getOurShares
-    :: (MonadSscMem ctx m, MonadIO m, WithLogger m)
-    => VssKeyPair -> m (HashMap StakeholderId (NonEmpty DecShare))
-getOurShares ourKey = do
+    :: (MonadSscMem ctx m, MonadIO m)
+    => TraceNamed m -> VssKeyPair -> m (HashMap StakeholderId (NonEmpty DecShare))
+getOurShares logTrace ourKey = do
     randSeed <- liftIO seedNew
     let ourPK = asBinary $ toVssPublicKey ourKey
-    encSharesM <- sscRunGlobalQuery (decryptOurShares ourPK)
+    encSharesM <- sscRunGlobalQuery (decryptOurShares ourPK logTrace)
     let drg = drgNewSeed randSeed
-    res <- launchNamedPureLog (return . fst . withDRG drg) $
-           forM (HM.toList encSharesM) $ \(id, lEncSh) -> do
-              let mEncSh = traverse (rightToMaybe . fromBinary) lEncSh
-              case mEncSh of
-                Just encShares ->
-                    lift $ Just . (id,) <$> mapM (decryptShare ourKey) encShares
-                _             -> do
-                    logWarning $ sformat ("Failed to deserialize share for " % build) id
-                    return Nothing
-    return $ HM.fromList . catMaybes $ res
+    res <-  forM (HM.toList encSharesM) $ \(id, lEncSh) -> do
+        let mEncSh = traverse (rightToMaybe . fromBinary) lEncSh
+        case mEncSh of
+            Just encShares ->
+                (pure . fst . withDRG drg) (Right . (id,) <$> mapM (decryptShare ourKey) encShares)
+            _             -> return (Left id)
+    res' <- forM res $ \case
+        Right ok -> pure (Just ok)
+        Left bad -> do
+            logWarning logTrace $ sformat ("Failed to deserialize share for " %build) bad
+            pure Nothing
+    return $ HM.fromList . catMaybes $ res'
 
 -- | Decrypt shares (in commitments) that we can decrypt.
 decryptOurShares
     :: AsBinary VssPublicKey                           -- ^ Our VSS key
     -> SscGlobalQuery (HashMap StakeholderId (NonEmpty (AsBinary EncShare)))
-decryptOurShares ourPK = do
+decryptOurShares ourPK _= do
     comms <- getCommitmentsMap <$> view sgsCommitments
     opens <- view sgsOpenings
     return . HM.fromList . catMaybes $ checkOpen opens <$> toList comms
--- chain/src/Pos/Chain/Update/Poll/Class.hs
+++ chain/src/Pos/Chain/Update/Poll/Class.hs
@@ -28,7 +28,6 @@ import qualified Data.HashMap.Strict as HM
 import qualified Data.HashSet as HS
 import qualified Data.List as List (find)
 import qualified Ether
-import           System.Wlog (WithLogger, logWarning)
 
 import           Pos.Chain.Update.BlockVersion (applyBVM)
 import           Pos.Chain.Update.Poll.Modifier (PollModifier (..),
@@ -49,7 +48,9 @@ import           Pos.Core.Update (ApplicationName, BlockVersion,
                      SoftwareVersion (..), UpId, UpdateProposal (..))
 import           Pos.Crypto (hash)
 import qualified Pos.Util.Modifier as MM
+import           Pos.Util.Trace.Named (TraceNamed, logWarning)
 import           Pos.Util.Util (ether)
+
 
 ----------------------------------------------------------------------------
 -- Read-only
@@ -57,7 +58,7 @@ import           Pos.Util.Util (ether)
 
 -- | Type class which provides function necessary for read-only
 -- verification of US data.
-class (Monad m, WithLogger m) => MonadPollRead m where
+class Monad m => MonadPollRead m where
     getBVState :: BlockVersion -> m (Maybe BlockVersionState)
     -- ^ Retrieve state of given block version.
     getProposedBVs :: m [BlockVersion]
@@ -72,7 +73,7 @@ class (Monad m, WithLogger m) => MonadPollRead m where
     -- ^ Get numeric component of last confirmed version of application
     getProposal :: UpId -> m (Maybe ProposalState)
     -- ^ Get active proposal
-    getProposalsByApp :: ApplicationName -> m [ProposalState]
+    getProposalsByApp :: TraceNamed IO -> ApplicationName -> m [ProposalState]
     -- ^ Get active proposals for the specified application.
     getConfirmedProposals :: m [ConfirmedProposalState]
     -- ^ Get all known confirmed proposals.
@@ -102,7 +103,7 @@ class (Monad m, WithLogger m) => MonadPollRead m where
     getAdoptedBVData = snd <$> getAdoptedBVFull
 
 instance {-# OVERLAPPABLE #-}
-    (MonadPollRead m, MonadTrans t, Monad (t m), WithLogger (t m)) =>
+    (MonadPollRead m, MonadTrans t, Monad (t m)) =>
         MonadPollRead (t m)
   where
     getBVState = lift . getBVState
@@ -112,7 +113,7 @@ instance {-# OVERLAPPABLE #-}
     getAdoptedBVFull = lift getAdoptedBVFull
     getLastConfirmedSV = lift . getLastConfirmedSV
     getProposal = lift . getProposal
-    getProposalsByApp = lift . getProposalsByApp
+    getProposalsByApp = getProposalsByApp
     getConfirmedProposals = lift getConfirmedProposals
     getEpochTotalStake = lift . getEpochTotalStake
     getRichmanStake e = lift . getRichmanStake e
@@ -133,7 +134,7 @@ class MonadPollRead m => MonadPoll m where
     -- ^ Put state of BlockVersion overriding if it exists.
     delBVState :: BlockVersion -> m ()
     -- ^ Delete BlockVersion and associated state.
-    setAdoptedBV :: BlockVersion -> m ()
+    setAdoptedBV :: TraceNamed IO -> BlockVersion -> m ()
     -- ^ Set last adopted block version. State is taken from competing states.
     setLastConfirmedSV :: SoftwareVersion -> m ()
     -- ^ Set last confirmed version of application.
@@ -153,12 +154,12 @@ class MonadPollRead m => MonadPoll m where
     -- ^ Set proposers.
 
 instance {-# OVERLAPPABLE #-}
-    (MonadPoll m, MonadTrans t, Monad (t m), WithLogger (t m)) =>
+    (MonadPoll m, MonadTrans t, Monad (t m)) =>
         MonadPoll (t m)
   where
     putBVState pv = lift . putBVState pv
     delBVState = lift . delBVState
-    setAdoptedBV = lift . setAdoptedBV
+    setAdoptedBV = setAdoptedBV
     setLastConfirmedSV = lift . setLastConfirmedSV
     delConfirmedSV = lift . delConfirmedSV
     addConfirmedProposal = lift . addConfirmedProposal
@@ -188,7 +189,7 @@ instance (MonadPoll m) => MonadPoll (RollT m) where
         insertIfNotExist bv unChangedBVL getBVState
         delBVState bv
 
-    setAdoptedBV = setValueWrapper unLastAdoptedBVL getAdoptedBV setAdoptedBV
+    setAdoptedBV logTrace = setValueWrapper unLastAdoptedBVL getAdoptedBV (setAdoptedBV logTrace)
 
     setLastConfirmedSV sv@SoftwareVersion{..} = ether $ do
         insertIfNotExist svAppName unChangedSVL getLastConfirmedSV
@@ -299,10 +300,10 @@ instance (MonadPollRead m) =>
         MM.lookupM getLastConfirmedSV appName =<< use pmConfirmedL
     getProposal upId = ether $
         MM.lookupM getProposal upId =<< use pmActivePropsL
-    getProposalsByApp app = ether $ do
+    getProposalsByApp logTrace app = ether $ do
         let eqApp = (== app) . svAppName . upSoftwareVersion . psProposal . snd
         props <- uses pmActivePropsL (filter eqApp . MM.insertions)
-        dbProps <- map (first (hash . psProposal) . join (,)) <$> getProposalsByApp app
+        dbProps <- map (first (hash . psProposal) . join (,)) <$> getProposalsByApp logTrace app
         pure . toList . HM.fromList $ dbProps ++ props -- squash props with same upId
     getConfirmedProposals = ether $
         MM.valuesM
@@ -339,16 +340,16 @@ instance (MonadPollRead m) =>
 
 {-# ANN module ("HLint: ignore Reduce duplication" :: Text) #-}
 
-instance (MonadPollRead m) =>
+instance (MonadIO m, MonadPollRead m) =>
          MonadPoll (PollT m) where
     putBVState bv st = ether $ pmBVsL %= MM.insert bv st
     delBVState bv = ether $ pmBVsL %= MM.delete bv
-    setAdoptedBV bv = ether $ do
+    setAdoptedBV logTrace bv = ether $ do
         bvs <- getBVState bv
         adoptedBVD <- getAdoptedBVData
         case bvs of
             Nothing ->
-                logWarning $ "setAdoptedBV: unknown version " <> pretty bv -- can't happen actually
+                liftIO $ logWarning logTrace $ "setAdoptedBV: unknown version " <> (pretty bv) -- can't happen actually
             Just (bvsModifier -> bvm) ->
                 pmAdoptedBVFullL .= Just (bv, applyBVM bvm adoptedBVD)
     setLastConfirmedSV SoftwareVersion {..} = ether $

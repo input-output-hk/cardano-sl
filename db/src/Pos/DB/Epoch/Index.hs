6badc3eae6de875745c45ffbc99b5e094f4ac443
Rupert Horlick <rupert.horlick@iohk.io>
2018-08-07 17:26:29 -0700

    CDEC-493: Choose DenseBinary impl for epoch index file

---
+++ db/src/Pos/DB/Epoch/Index.hs
@@ -0,0 +1,54 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+module Pos.DB.Epoch.Index
+       ( writeEpochIndex
+       , getEpochBlockOffset
+       , SlotIndexOffset (..)
+       ) where
+
+import           Universum
+
+import           Data.Binary (Binary, decode, encode)
+import qualified Data.ByteString.Builder as B
+import qualified Data.ByteString.Lazy as BL
+import           System.IO (IOMode (..), SeekMode (..), hSeek, withBinaryFile)
+
+import           Pos.Core (LocalSlotIndex (..))
+
+data SlotIndexOffset = SlotIndexOffset
+    { sioSlotIndex :: !Word16
+    , sioOffset    :: !Word64
+    } deriving (Eq, Generic, Show)
+
+instance Binary SlotIndexOffset
+
+-- | Write a list of @SlotIndexOffset@s to a dense @Binary@ representation
+--
+--   To make it dense we pad the list with @maxBound :: Word64@ whenever we see
+--   a missing @LocalSlotIndex@
+writeEpochIndex :: FilePath -> [SlotIndexOffset] -> IO ()
+writeEpochIndex path =
+    withBinaryFile path WriteMode
+        . flip B.hPutBuilder
+        . foldMap (B.lazyByteString . encode . sioOffset)
+        . padIndex
+
+-- | Pad a list of @SlotIndexOffset@s ordered by @LocalSlotIndex@
+padIndex :: [SlotIndexOffset] -> [SlotIndexOffset]
+padIndex = go (flip SlotIndexOffset maxBound <$> [0 .. 21599])
+  where
+    go [] _  = []
+    go xs [] = xs
+    go (x : xs) (y : ys) | sioSlotIndex x == sioSlotIndex y = y : go xs ys
+                         | otherwise                        = x : go xs (y : ys)
+
+getSlotIndexOffsetN :: FilePath -> LocalSlotIndex -> IO Word64
+getSlotIndexOffsetN path (UnsafeLocalSlotIndex i) =
+    withBinaryFile path ReadMode $ \h -> do
+        hSeek h AbsoluteSeek (fromIntegral $ i * 8)
+        decode <$> BL.hGet h 8
+
+getEpochBlockOffset :: FilePath -> LocalSlotIndex -> IO (Maybe Word64)
+getEpochBlockOffset fpath lsi = do
+    off <- getSlotIndexOffsetN fpath lsi
+    pure $ if off == maxBound then Nothing else Just off

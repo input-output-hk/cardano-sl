fb460891d7702cb8e62c484d500841e870e51f5d
Jordan Millar <jordan.millar@iohk.io>
2018-08-21 12:49:16 -0400

    [CDEC-517] Add `parallelize` function to `util` and parallelize tests in `generator`

--- generator/test/Test.hs
+++ generator/test/Test.hs
@@ -1,11 +1,13 @@
 import           Universum
 
-import           Test.Hspec (hspec)
+import           Test.Hspec (hspec, parallel)
 
 import           Spec (spec)
 import           Test.Pos.Configuration (defaultTestConf)
+import           Test.Pos.Util.Parallel.Parallelize (parallelizeAllCores)
 
 main :: IO ()
 main = do
+    parallelizeAllCores
     putText $ "default configuration: " <> show defaultTestConf
-    hspec spec
+    hspec $ parallel spec

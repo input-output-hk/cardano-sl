e63dc5b384073d9c571286cc1edfccd98edcdd58
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2018-08-23 15:35:07 +1000

    [CBR-391] db: add a test case for opening unicode directories

--- db/test/test.hs
+++ db/test/test.hs
@@ -1,7 +1,9 @@
 import           Universum
 
 import qualified Test.Pos.DB.Epoch.Index
+import qualified Test.Pos.DB.Rocks.Functions
 import           Test.Pos.Util.Tripping (runTests)
 
 main :: IO ()
-main = runTests [Test.Pos.DB.Epoch.Index.tests]
+main = runTests [ Test.Pos.DB.Epoch.Index.tests
+                , Test.Pos.DB.Rocks.Functions.tests ]

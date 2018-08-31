284e5597ae8d304ebabce80e0d7d548705813d97
Alexander Diemand <codieplusplus@apax.net>
2018-08-31 16:14:21 +0200

    [CBR-207] counting call sites for logging functions
    Signed-off-by: Alexander Diemand <codieplusplus@apax.net>

--- util/src/Pos/Util/Wlog.hs
+++ util/src/Pos/Util/Wlog.hs
@@ -1,69 +1,69 @@
+-- | an interface to 'log-warper'
+
 module Pos.Util.Wlog
         ( -- * CanLog
           CanLog (..)
         , WithLogger
-        , dispatchEvents
-          -- * Pure logging manipulation
-        , LogEvent (..)
+          -- * Pure logging
+        , dispatchEvents        -- call sites: 1 chain/src/Pos/Chain/Ssc/Toss/Pure.hs
+        , LogEvent (..)         -- call sites: 3 chain/src/Pos/Chain/Ssc/Toss/Pure.hs,db/src/Pos/DB/Update/Poll/Pure.hs,wallet-new/test/unit/UTxO/Verify.hs
         , NamedPureLogger (..)
-        , launchNamedPureLog
-        , runNamedPureLog
+        , launchNamedPureLog    -- call sites: 8   chain,db,explorer
+        , runNamedPureLog       -- call sites: 2   chain,db
+          -- * Setup
+        , setupLogging          -- call sites: 7 generator,lib,networking(bench),tools:keygen|launcher
           -- * Logging functions
         , logDebug
         , logError
         , logInfo
         , logNotice
         , logWarning
-        , logMessage
+        , logMessage            -- call sites: 3  infra,wallet-new
           -- * LoggerName
         , LoggerName (..)
-        , LoggerNameBox (..)
+        , LoggerNameBox (..)    -- call sites: 9  core,db,infra,lib,tools,util
         , HasLoggerName (..)
-        , usingLoggerName
+        , usingLoggerName       -- call sites: 22 db,explorer,infra,lib,networking,node-ipc,tools,wallet,wallet-new
           -- * LoggerConfig
-        , LoggerConfig (..)
-        , lcLogsDirectory
-        , lcTermSeverityOut
-        , lcTree
-        , parseLoggerConfig
+        , LoggerConfig (..)     -- call sites: 13  (mostly together with 'setupLogging')
+        , lcLogsDirectory       -- call sites: 1 tools/src/launcher/Main.hs
+        , lcTermSeverityOut     -- call sites: 1 tools/src/launcher/Main.hs
+        , lcTree                -- call sites: 4 infra,lib,networking,tools
+        , parseLoggerConfig     -- call sites: 2 lib,networking
           -- * Hierarchical tree of loggers (with lenses)
-        , HandlerWrap (..)
-        , fromScratch
-        , hwFilePath
-        , ltFiles
-        , ltSeverity
-        , ltSubloggers
-        , zoomLogger
+        , HandlerWrap (..)      -- call sites: 1 tools/src/launcher/Main.hs
+        , fromScratch           -- call sites: 1 networking/src/Bench/Network/Commons.hs
+        , hwFilePath            -- call sites: 1 infra/src/Pos/Infra/Reporting/Wlog.hs
+        , ltFiles               -- call sites: 2 infra/.../Reporting/Wlog.hs,tools/src/launcher/Main.hs
+        , ltSeverity            -- call sites: 5 networking/src/Bench/Network/Commons.hs,tools/src/launcher/Main.hs
+        , zoomLogger            -- call sites: 3 networking/src/Bench/Network/Commons.hs
+        , ltSubloggers          -- call sites: 1 infra/src/Pos/Infra/Reporting/Wlog.hs
           -- * Builders for 'LoggerConfig'
-        , consoleActionB
-        , maybeLogsDirB
-        , showTidB
-        , productionB
-        , termSeveritiesOutB
+        , consoleActionB        -- call sites: 3 generator/app/VerificationBench.hs,lib/src/Pos/Launcher/Resource.hs
+        , maybeLogsDirB         -- call sites: 2 lib/src/Pos/Launcher/Resource.hs,networking/src/Bench/Network/Commons.hs
+        , showTidB              -- call sites: 1 lib/src/Pos/Launcher/Resource.hs
+        , productionB           -- call sites: 6 lib,networking,tools
+        , termSeveritiesOutB    -- call sites: 2 generator/app/VerificationBench.hs,tools/src/keygen/Main.hs
           -- * Severity
         , Severity (..)
-        , debugPlus
-        , errorPlus
-        , infoPlus
-        , noticePlus
-        , warningPlus
+        , debugPlus             -- call sites: 4 generator/app/VerificationBench.hs,tools:keygen|launcher
+        , errorPlus             -- call sites: 1 networking/src/Bench/Network/Commons.hs
+        , infoPlus              -- call sites: 2 networking/src/Bench/Network/Commons.hs
+        , noticePlus            -- call sites: 1 networking/src/Network/Broadcast/OutboundQueue/Demo.hs
+        , warningPlus           -- call sites: 1 networking/src/Bench/Network/Commons.hs
           -- * Saving Changes
-        , retrieveLogContent
-        , updateGlobalLogger
+        , retrieveLogContent    -- call sites: 1 infra/src/Pos/Infra/Reporting/Wlog.hs
+        , updateGlobalLogger    -- call sites: 1 networking/src/Network/Broadcast/OutboundQueue/Demo.hs
           -- * LogHandler
-        , defaultHandleAction
+        , defaultHandleAction   -- call sites: 2 generator/app/VerificationBench.hs,lib/src/Pos/Launcher/Resource.hs
           -- * Logging messages with a condition
-        , logMCond
-          -- * Utility functions
-        , removeAllHandlers
-          -- * Modifying Loggers
-        , setLevel
-          -- * Launcher
-        , setupLogging
-          -- * Formatter
-        , centiUtcTimeF
+        , logMCond              -- call sites: 1 core/src/Pos/Core/Util/LogSafe.hs
           -- * LogHandler
-        , LogHandlerTag (HandlerFilelike)
+        , LogHandlerTag (HandlerFilelike)  -- call sites: 1 core/src/Pos/Core/Util/LogSafe.hs
+          -- * Utility functions
+        , removeAllHandlers     -- call sites: 2 lib/src/Pos/Launcher/Resource.hs,networking/test/Test/Network/Broadcast/OutboundQueueSpec.hs
+        , centiUtcTimeF         -- call sites: 1 networking/bench/LogReader/Main.hs
+        , setLevel              -- call sites: 1 networking/src/Network/Broadcast/OutboundQueue/Demo.hs
         ) where
 
 import           System.Wlog (CanLog (..), HandlerWrap (..), HasLoggerName (..),
@@ -83,3 +83,4 @@ import           System.Wlog (CanLog (..), HandlerWrap (..), HasLoggerName (..),
                      zoomLogger)
 import           System.Wlog.Formatter (centiUtcTimeF)
 import           System.Wlog.LogHandler (LogHandlerTag (HandlerFilelike))
+

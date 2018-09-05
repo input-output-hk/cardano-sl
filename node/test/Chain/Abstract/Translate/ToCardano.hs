45cd58ddb94f54c28196f06e0a96930eaaf3a572
Marko Dimjašević <marko@dimjasevic.net>
2018-09-05 14:06:10 +0200

    Defines a minimal interpretation monad transformer's type for interpreting from an Abstract chain into Cardano

--- node/test/Chain/Abstract/Translate/ToCardano.hs
+++ node/test/Chain/Abstract/Translate/ToCardano.hs
@@ -7,6 +7,7 @@ import           Chain.Abstract (Addr, SlotId, BlockHash, StakeDistribution)
 import           Control.Lens.TH (makeLenses)
 import           Control.Monad.Except
 import           Universum
+import           UTxO.Context (TransCtxt(..))
 import           UTxO.Interpreter (Interpretation(..), Interpret(..))
 
 {-------------------------------------------------------------------------------
@@ -14,38 +15,17 @@ import           UTxO.Interpreter (Interpretation(..), Interpret(..))
 -------------------------------------------------------------------------------}
 
 -- | Checkpoint (we create one for each block we translate)
-data IntCheckpoint = IntCheckpoint {
-      -- | Slot number of this checkpoint
-      icSlotId    :: !SlotId
+data IntCheckpoint
 
-      -- | Hash of the current block
-    , icBlockHash :: !BlockHash
+-- | Interpretation context
+data IntCtxt (h :: * -> *)
 
-      -- | Running stakes
-    , icStakes    :: !(StakeDistribution Addr)
-
-      -- | Delegation graph. This is instantiated to the identify function.
-    , icDlg       :: Addr -> Addr
-    }
+makeLenses ''IntCtxt
 
 -- | Translation state
-data TransState h = TransState {
-      -- | Transaction map
-      -- _tsTx          :: !(Map (h (DSL.Transaction h Addr)) (Transaction h Addr))
-
-      -- | Checkpoints
-      _tsCheckpoints :: !(NonEmpty IntCheckpoint)
-    }
+data TransState (h :: * -> *)
 
 makeLenses ''TransState
-
--- | Translation context. This the read-only data available to translation.
-data TransCtxt h = TransCtxt
-  { -- | All actors in the system.
-    _tcAddresses :: NonEmpty Addr
-  }
-
-makeLenses ''TransCtxt
 
 {-------------------------------------------------------------------------------
 Errors that may occur during interpretation
@@ -57,22 +37,63 @@ data IntException = IntException deriving Show -- TODO
 instance Exception IntException
 
 {-------------------------------------------------------------------------------
+  Translation monad
+
+  The translation provides access to the translation context as well as some
+  dictionaries so that we can lift Cardano operations to the 'Translate' monad.
+  (Eventually we may wish to do this differently.)
+-------------------------------------------------------------------------------}
+
+-- | Translation environment
+--
+-- NOTE: As we reduce the scope of 'HasConfiguration' and
+-- 'HasUpdateConfiguration', those values should be added into the
+-- 'CardanoContext' instead.
+data TranslateEnv = TranslateEnv {
+      teContext :: TransCtxt
+    }
+
+newtype TranslateT e m a = TranslateT {
+      unTranslateT :: ExceptT e (ReaderT TranslateEnv m) a
+    }
+  deriving ( Functor
+           , Applicative
+           , Monad
+           , MonadError e
+           , MonadIO
+           , MonadFail
+           )
+
+instance MonadTrans (TranslateT e) where
+  lift = TranslateT . lift . lift
+
+type Translate e = TranslateT e Identity
+
+instance Monad m => MonadReader TransCtxt (TranslateT e m) where
+  ask     = TranslateT $ asks teContext
+  local f = TranslateT . local f' . unTranslateT
+    where
+      f' env = env { teContext = f (teContext env) }
+
+{-------------------------------------------------------------------------------
   The interpretation monad
 -------------------------------------------------------------------------------}
 
 -- | Interpretation monad
-newtype TranslateT h e m a = TranslateT {
-    -- untranslateT :: StateT (IntCtxt h) (TranslateT (Either IntException e) m) a  -- from UTxO.Interpreter
-    unTranslateT :: StateT (TransState h) (ReaderT (TransCtxt h) (ExceptT e m)) a -- from Chain.Abstract.Translate.FromUTxO
-    }
-    deriving ( Functor
-             , Applicative
-             , Monad
-             , MonadError e
-             , MonadIO
-             , MonadReader (TransCtxt h)
-             , MonadState (TransState h)
-             )
+newtype IntT h e m a = IntT {
+    unIntT :: StateT (IntCtxt h) (TranslateT (Either IntException e) m) a
+  }
+  deriving ( Functor
+           , Applicative
+           , Monad
+           , MonadReader TransCtxt
+           , MonadError (Either IntException e)
+           )
+
+-- | Evaluate state strictly
+instance Monad m => MonadState (IntCtxt h) (IntT h e m) where
+ get    = IntT $ get
+ put !s = IntT $ put s
 
 {-------------------------------------------------------------------------------
   Translate the Abstract chain definitions to Cardano types
@@ -80,5 +101,5 @@ newtype TranslateT h e m a = TranslateT {
 
 data Abstract2Cardano
 
--- instance Interpretation Abstract2Cardano where
---   type IntCtx Abstract2Cardano = TranslateT
+instance Interpretation Abstract2Cardano where
+  type IntCtx Abstract2Cardano = IntT

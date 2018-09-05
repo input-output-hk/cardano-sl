e630511173e6b5bdfbec9cb7f382e50a394dd423
Damian Nadales <damian.nadales@gmail.com>
2018-09-05 17:17:27 +0200

    Preliminary work towards instantiating the parameters for generating abstract chains. [skip ci]

--- node/test/Infrastructure/Generator.hs
+++ node/test/Infrastructure/Generator.hs
@@ -1,23 +1,35 @@
+{-# LANGUAGE NamedFieldPuns #-}
 module Infrastructure.Generator
   ( GeneratorModel
   , genChainUsingModel
   , simpleModel
+  , simpleGen
   ) where
 
+import           Data.Functor.Identity (runIdentity)
 import qualified Data.Set as Set
-import           Test.QuickCheck
-import Universum
-import UTxO.DSL
-import UTxO.Generator
+import           Test.QuickCheck (Gen)
+import           Universum
+
+import           UTxO.DSL (GivenHash, Hash, Output (Output),
+                     Transaction (Transaction), Value, trExtra, trFee, trFresh,
+                     trHash, trIns, trOuts, trUtxo, utxoRestrictToAddr)
+import           UTxO.Generator (defChainParams, genChain, initTrState)
+
+import           Chain.Abstract (Addr, Chain)
+import           Chain.Abstract.Translate.FromUTxO (ChainValidity, IntException,
+                     translate)
+import qualified UTxO.DSL as DSL
+
 
 data GeneratorModel h a = GeneratorModel
-  { gmBoot :: Transaction h a
+  { gmBoot         :: Transaction h a
   , gmAllAddresses :: [a]
-  , gmEstimateFee :: Int -> [Value] -> Value
+  , gmEstimateFee  :: Int -> [Value] -> Value
   }
 
-genChainUsingModel :: (Hash h a, Ord a) => GeneratorModel h a -> Gen (Chain h a)
-genChainUsingModel GeneratorModel{..} =
+genChainUsingModel :: (Hash h a, Ord a) => GeneratorModel h a -> Gen (DSL.Chain h a)
+genChainUsingModel GeneratorModel{gmBoot, gmAllAddresses, gmEstimateFee} =
     evalStateT (genChain params) initState
   where
     params    = defChainParams gmEstimateFee gmAllAddresses
@@ -50,3 +62,16 @@ simpleModel = GeneratorModel {
 
     initBal :: Value
     initBal = 10000
+
+-- | Instantiate a simple generator
+simpleGen :: Gen (DSL.Chain GivenHash Char)
+simpleGen = genChainUsingModel simpleModel
+
+-- | Simple instance of a 'DSL.Chain' to an 'Abstract.Chain' translation.
+asAbstractChain
+  :: DSL.Chain GivenHash Addr
+  -> Either IntException (Chain GivenHash Addr, ChainValidity)
+asAbstractChain ch = runIdentity $ translate addrs ch [] params
+  where
+    addrs = undefined
+    params = undefined

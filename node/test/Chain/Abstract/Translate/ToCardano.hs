88cdb502713917c11d28dc811bb466466540fe2b
Marko Dimjašević <marko@dimjasevic.net>
2018-09-11 15:56:06 +0200

    Abstract->Cardano: WIP - adds an interpretation of a block

--- node/test/Chain/Abstract/Translate/ToCardano.hs
+++ node/test/Chain/Abstract/Translate/ToCardano.hs
@@ -7,43 +7,132 @@ module Chain.Abstract.Translate.ToCardano (
     IntT
   ) where
 
-import           Cardano.Wallet.Kernel.Types (RawResolvedTx(..), mkRawResolvedTx)
-import           Chain.Abstract (Output(..), Transaction(..), hash, outAddr)
-import           Control.Lens ((%=), ix)
+import           Cardano.Wallet.Kernel.Types
+                 ( RawResolvedBlock(UnsafeRawResolvedBlock)
+                 , RawResolvedTx(..)
+                 , fromRawResolvedBlock
+                 , mkRawResolvedBlock
+                 , mkRawResolvedTx )
+import           Cardano.Wallet.Kernel.DB.InDb (InDb(..))
+import           Cardano.Wallet.Kernel.DB.Resolved
+                 ( ResolvedBlock(..)
+                 , ResolvedTx(..) )
+import           Chain.Abstract
+                 ( Block(..)
+                 , Output(..)
+                 , Transaction(..)
+                 , hash
+                 , outAddr )
+import           Control.Lens ((%=), (.=), ix)
 import           Control.Lens.TH (makeLenses)
 import           Control.Monad.Except
 import           Data.Constraint (Dict(..))
+import           Data.Default (def)
+import qualified Data.HashMap.Strict as HM
 import qualified Data.List.NonEmpty as NE
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
-import           Pos.Chain.Txp (TxAux(..), TxOutAux(..), TxId, TxIn(..), TxOut(..))
+import           Data.Reflection (give)
+import           Pos.Chain.Block
+                 ( BlockHeader(BlockHeaderMain, BlockHeaderGenesis)
+                 , GenesisBlock
+                 , MainBlock
+                 , gbHeader
+                 , genBlockLeaders
+                 , mkGenesisBlock )
+import           Pos.Chain.Lrc (followTheSatoshi)
+import           Pos.Chain.Ssc (defaultSscPayload)
+import           Pos.Chain.Txp
+                 ( TxAux(..)
+                 , TxId
+                 , TxIn(..)
+                 , TxOut(..)
+                 , TxOutAux(..)
+                 , txOutStake )
 import           Pos.Chain.Update (HasUpdateConfiguration)
 import           Pos.Client.Txp.Util (makeMPubKeyTx, makeRedemptionTx)
-import           Pos.Core (HasConfiguration, ProtocolMagic)
-import           Pos.Core.Common (Coin(..), mkCoin)
-import           Pos.Crypto.Signing.Safe (SafeSigner(SafeSigner, FakeSigner))
+import qualified Pos.Core as Core
+                 ( HasConfiguration
+                 , ProtocolConstants
+                 , ProtocolMagic
+                 , SlotId(..) )
+import           Pos.Core.Chrono (OldestFirst (..))
+import           Pos.Core.Common
+                 ( Coin(..)
+                 , SharedSeed(..)
+                 , SlotLeaders
+                 , StakeholderId
+                 , StakesList
+                 , StakesMap
+                 , addCoin
+                 , mkCoin
+                 , subCoin )
+import           Pos.Core.Delegation (DlgPayload(UnsafeDlgPayload))
+import           Pos.Core.Genesis
+                 ( GenesisWStakeholders
+                 , gdBootStakeholders
+                 , gdProtocolConsts
+                 , genesisProtocolConstantsToProtocolConstants )
+import           Pos.Core.Slotting
+                 ( EpochIndex(..)
+                 , addLocalSlotIndex
+                 , crucialSlot
+                 , localSlotIndexMaxBound
+                 , localSlotIndexMinBound )
+import           Pos.Crypto.Signing.Safe (SafeSigner(FakeSigner))
 import qualified Pos.Crypto (hash)
+import           Pos.DB.Block (RawPayload(..), createMainBlockPure)
 import           Universum
-import           UTxO.Context (Addr, AddrInfo(..), TransCtxt(..), resolveAddr, CardanoContext(..))
-import           UTxO.Crypto (SomeKeyPair, TxOwnedInput,
-                   ClassifiedInputs(InputsRegular, InputsRedeem), classifyInputs,
-                   RegularKeyPair(..), RedeemKeyPair(..))
-import qualified UTxO.DSL as DSL (Hash, Input(..), Transaction, Value, outAddr, hash)
-import           UTxO.Interpreter (
-                   Interpretation(..)
-                 , Interpret(..)
-                 )
+import           UTxO.Context
+                 ( Addr
+                 , AddrInfo(..)
+                 , BlockSignInfo(..)
+                 , CardanoContext(..)
+                 , TransCtxt(..)
+                 , blockSignInfoForSlot
+                 , resolveAddr )
+import           UTxO.Crypto
+                 ( ClassifiedInputs(InputsRegular, InputsRedeem)
+                 , RedeemKeyPair(..)
+                 , RegularKeyPair(..)
+                 , SomeKeyPair
+                 , TxOwnedInput
+                 , classifyInputs )
+import qualified UTxO.DSL as DSL
+                 ( Hash
+                 , Input(..)
+                 , Transaction
+                 , Value )
+import           UTxO.Interpreter (Interpretation(..), Interpret(..))
 
 {-------------------------------------------------------------------------------
   Interpretation context
 -------------------------------------------------------------------------------}
 
 -- | Checkpoint (we create one for each block we translate)
-data IntCheckpoint
+data IntCheckpoint = IntCheckpoint {
+      -- | Slot number of this checkpoint
+      icSlotId        :: !Core.SlotId
+
+      -- | Header of the block in this slot
+      --
+      -- Will be initialized to the header of the genesis block.
+    , icBlockHeader   :: !BlockHeader
+
+      -- | Slot leaders for the current epoch
+    , icEpochLeaders  :: !SlotLeaders
+
+      -- | Running stakes
+    , icStakes        :: !StakesMap
+
+      -- | Snapshot of the stakes at the 'crucial' slot in the current epoch; in
+      -- other words, the stakes used to compute the slot leaders for the next epoch.
+    , icCrucialStakes :: !StakesMap
+    }
 
 -- | Information we keep about the transactions we encounter
 data TxMeta h = TxMeta {
-  -- | The DSL transaction itself
+  -- | The abstract transaction itself
   --
   -- We use this for input resolution.
   tmTx   :: !(Transaction h Addr)
@@ -59,6 +148,9 @@ data TxMeta h = TxMeta {
 data IntCtxt h = IntCtxt {
   -- | Transaction map
   _icTx          :: !(Map (h (DSL.Transaction h Addr)) (TxMeta h))
+
+  -- | Checkpoints
+, _icCheckpoints :: !(NonEmpty IntCheckpoint)
 }
 
 makeLenses ''IntCtxt
@@ -67,7 +159,16 @@ makeLenses ''IntCtxt
   Extract some values we need from the translation context
 -------------------------------------------------------------------------------}
 
-magic :: TransCtxt -> ProtocolMagic
+constants :: TransCtxt -> Core.ProtocolConstants
+constants = genesisProtocolConstantsToProtocolConstants
+          . gdProtocolConsts
+          . ccData
+          . tcCardano
+
+weights :: TransCtxt -> GenesisWStakeholders
+weights = gdBootStakeholders . ccData . tcCardano
+
+magic :: TransCtxt -> Core.ProtocolMagic
 magic = ccMagic . tcCardano
 
 {-------------------------------------------------------------------------------
@@ -76,9 +177,20 @@ Errors that may occur during interpretation
 
 -- | Interpretation error
 data IntException
-  = IntUnknownHash      Text
-  | IntExClassifyInputs Text
-  | IntIndexOutOfRange  Text Word32 -- ^ During input resolution (hash and index)
+  = IntExClassifyInputs   Text
+  | IntExCreateBlock      Text
+  | IntIndexOutOfRange    Text Word32 -- ^ During input resolution (hash and index)
+    -- | Coin overflow during stake calculation
+    --
+    -- We record the old stake and the stake we were suppose to add
+  | IntStakeOverflow      StakeholderId Coin Coin
+    -- | Coin underflow during stake calculatino
+    --
+    -- We record the old stake and the stake we were supposed to subtract
+  | IntStakeUnderflow     StakeholderId Coin Coin
+  | IntUnknownHash        Text
+    -- | Unknown stake holder during stake calculation
+  | IntUnknownStakeholder StakeholderId
     deriving Show
 
 instance Exception IntException
@@ -94,7 +206,7 @@ instance Exception IntException
 -- | Translation environment
 data TranslateEnv = TranslateEnv {
       teContext :: TransCtxt
-    , teConfig  :: Dict HasConfiguration
+    , teConfig  :: Dict Core.HasConfiguration
     , teUpdate  :: Dict HasUpdateConfiguration
     }
 
@@ -140,7 +252,7 @@ instance Monad m => MonadState (IntCtxt h) (IntT h e m) where
 
 -- | Lift functions that want the configuration as type class constraints
 withConfig :: Monad m
-           => ((HasConfiguration, HasUpdateConfiguration) => TranslateT e m a)
+           => ((Core.HasConfiguration, HasUpdateConfiguration) => TranslateT e m a)
            -> TranslateT e m a
 withConfig f = do
     Dict <- TranslateT $ asks teConfig
@@ -154,16 +266,28 @@ mapTranslateErrors f (TranslateT ma) = TranslateT $ withExceptT f ma
 
 -- | Convenience function to list actions in the 'Translate' monad
 liftTranslateInt :: Monad m
-                 => (   (HasConfiguration, HasUpdateConfiguration)
+                 => (   (Core.HasConfiguration, HasUpdateConfiguration)
                      => TranslateT IntException m a)
                  -> IntT h e m a
 liftTranslateInt ta =  IntT $ lift $ mapTranslateErrors Left $ withConfig $ ta
 
 {-------------------------------------------------------------------------------
-  Dealing with the transactions
+  Convenience wrappers
 -------------------------------------------------------------------------------}
 
--- | Add transaction into the context
+-- | Increment slot ID
+--
+translateNextSlot :: Monad m => Core.SlotId -> TranslateT e m Core.SlotId
+translateNextSlot (Core.SlotId epoch lsi) = withConfig $
+    return $ case addLocalSlotIndex 1 lsi of
+               Just lsi' -> Core.SlotId epoch       lsi'
+               Nothing   -> Core.SlotId (epoch + 1) localSlotIndexMinBound
+
+{-------------------------------------------------------------------------------
+  Dealing with transactions
+-------------------------------------------------------------------------------}
+
+-- | Add a transaction into the context
 putTx :: (DSL.Hash h Addr, Monad m)
       => Transaction h Addr
       -> TxId
@@ -173,7 +297,7 @@ putTx t txId = icTx %= Map.insert (hash t) TxMeta {
 , tmHash = txId
 }
 
--- | Get transaction from the context
+-- | Get a transaction from the context
 getTx :: (DSL.Hash h Addr, Monad m)
       => h (DSL.Transaction h Addr)
       -> IntT h e m (TxMeta h)
@@ -184,7 +308,7 @@ getTx h = do
       Just txMeta -> return txMeta
 
 -- | Lookup the Cardano hash for the given DSL hash
-intHash :: (Monad m, DSL.Hash h Addr)
+intHash :: (DSL.Hash h Addr, Monad m)
         => h (DSL.Transaction h Addr) -> IntT h e m TxId
 intHash = fmap tmHash . getTx
 
@@ -197,6 +321,142 @@ inpSpentOutput' (DSL.Input h idx) =  do
   case toList (trOuts . tmTx $ txMeta) ^? ix (fromIntegral idx) of
     Nothing  -> throwError $ Left $ IntIndexOutOfRange (pretty h) idx
     Just out -> return out
+
+{-------------------------------------------------------------------------------
+  Dealing with checkpoints
+-------------------------------------------------------------------------------}
+
+-- | Push a new checkpoint
+--
+-- The checkpoint is created by the given function, which gets passed the previous
+-- checkpoint as well as the slot ID that the next checkpoint should have.
+--
+-- The function runs in the underlying 'Translate' monad so that it is not tempted
+-- to use state it shouldn't.
+pushCheckpoint :: Monad m
+                => (    (Core.HasConfiguration, HasUpdateConfiguration)
+                     => IntCheckpoint
+                     -> Core.SlotId
+                     -> TranslateT IntException m (IntCheckpoint, a))
+               -> IntT h e m a
+pushCheckpoint f = do
+    c :| cs  <- use icCheckpoints
+    (c', a)  <- liftTranslateInt $ do
+                  nextSlotId <- translateNextSlot (icSlotId c)
+                  f c nextSlotId
+    icCheckpoints .= c' :| c : cs
+    return a
+
+{-------------------------------------------------------------------------------
+  Constructing checkpoints
+-------------------------------------------------------------------------------}
+
+-- | Creates a checkpoint for a regular block
+mkCheckpoint :: Monad m
+             => IntCheckpoint    -- ^ Previous checkpoint
+             -> Core.SlotId      -- ^ Slot of the new block just created
+             -> RawResolvedBlock -- ^ The block just created
+             -> TranslateT IntException m IntCheckpoint
+mkCheckpoint prev slot raw@(UnsafeRawResolvedBlock block _inputs) = do
+    pc <- asks constants
+    gs <- asks weights
+    let isCrucial = give pc $ slot == crucialSlot (Core.siEpoch slot)
+    newStakes <- updateStakes gs (fromRawResolvedBlock raw) (icStakes prev)
+    return IntCheckpoint {
+        icSlotId        = slot
+      , icBlockHeader   = BlockHeaderMain $ block ^. gbHeader
+      , icEpochLeaders  = icEpochLeaders prev
+      , icStakes        = newStakes
+      , icCrucialStakes = if isCrucial
+                            then newStakes
+                            else icCrucialStakes prev
+      }
+
+-- | Update the stakes map as a result of a block.
+--
+-- We follow the 'Stakes modification' section of the txp.md document.
+updateStakes :: forall m. MonadError IntException m
+             => GenesisWStakeholders
+             -> ResolvedBlock
+             -> StakesMap -> m StakesMap
+updateStakes gs (ResolvedBlock txs _) =
+    foldr (>=>) return $ map go txs
+  where
+    go :: ResolvedTx -> StakesMap -> m StakesMap
+    go (ResolvedTx ins outs) =
+        subStake >=> addStake
+      where
+        subStakes, addStakes :: [(StakeholderId, Coin)]
+        subStakes = concatMap txOutStake' $ toList     (_fromDb ins)
+        addStakes = concatMap txOutStake' $ Map.toList (_fromDb outs)
+
+        subStake, addStake :: StakesMap -> m StakesMap
+        subStake sm = foldM (opStake subCoin IntStakeUnderflow) sm subStakes
+        addStake sm = foldM (opStake addCoin IntStakeOverflow)  sm addStakes
+
+    opStake :: (Coin -> Coin -> Maybe Coin)
+            -> (StakeholderId -> Coin -> Coin -> IntException)
+            -> StakesMap
+            -> (StakeholderId, Coin)
+            -> m StakesMap
+    opStake op exFun sm (hId, c) = do
+        stake <- stakeOf hId sm
+        case stake `op` c of
+          Just stake' -> return $! HM.insert hId stake' sm
+          Nothing     -> throwError $ exFun hId stake c
+
+    stakeOf :: StakeholderId -> StakesMap -> m Coin
+    stakeOf hId sm =
+        case HM.lookup hId sm of
+          Just s  -> return s
+          Nothing -> throwError $ IntUnknownStakeholder hId
+
+    txOutStake' :: (TxIn, TxOutAux) -> StakesList
+    txOutStake' = txOutStake gs . toaOut . snd
+
+-- | Create an epoch boundary block
+--
+-- In between each epoch there is an epoch boundary block (or EBB), that records
+-- the stakes for the next epoch (in the Cardano codebase is referred to as a
+-- "genesis block", and indeed the types are the same; we follow the terminology
+-- from the spec here).
+--
+-- We /update/ the most recent checkpoint so that when we rollback, we effectively
+-- rollback /two/ blocks. This is important, because the DSL has no concept of these
+-- EBBs.
+--
+-- TODO(md): See what changes are needed given that we're dealing with abstract
+--           blocks instead of DSL blocks.
+createEpochBoundary :: Monad m
+                    => IntCheckpoint
+                    -> TranslateT IntException m (IntCheckpoint, GenesisBlock)
+createEpochBoundary ic = do
+    pm    <- asks magic
+    pc    <- asks constants
+    slots <- asks (ccEpochSlots . tcCardano)
+    let newLeaders = give pc $ followTheSatoshi
+                                 slots
+                                 boringSharedSeed
+                                 (HM.toList $ icCrucialStakes ic)
+        ebb = mkGenesisBlock
+                pm
+                (Right     $ icBlockHeader ic)
+                (nextEpoch $ icSlotId      ic)
+                newLeaders
+    return (
+        ic { icEpochLeaders = ebb ^. genBlockLeaders
+           , icBlockHeader  = BlockHeaderGenesis $ ebb ^. gbHeader
+           }
+      , ebb
+      )
+  where
+    -- This is a shared seed which never changes. Obviously it is not an
+    -- accurate reflection of how Cardano works.
+    boringSharedSeed :: SharedSeed
+    boringSharedSeed = SharedSeed "Static shared seed"
+
+    nextEpoch :: Core.SlotId -> EpochIndex
+    nextEpoch (Core.SlotId (EpochIndex i) _) = EpochIndex $ i + 1
 
 {-------------------------------------------------------------------------------
   Translate the Abstract chain definitions to Cardano types
@@ -300,3 +560,66 @@ instance DSL.Hash h Addr => Interpret Abstract2Cardano h (Transaction h Addr) wh
                 (redKpSec kp)
                 (NE.fromList [inp])
                 (NE.fromList outs)
+
+-- | Interpretation of a block of transactions
+instance DSL.Hash h Addr => Interpret Abstract2Cardano h (Block h Addr) where
+  -- The block and the EBB, if any
+  type Interpreted Abstract2Cardano (Block h Addr) = (RawResolvedBlock, Maybe GenesisBlock)
+
+  int :: forall e m. Monad m
+      => Block h Addr -> IntT h e m (RawResolvedBlock, Maybe GenesisBlock)
+  int b = do
+      let (OldestFirst txs) = blockTransactions b
+      (txs', resolvedTxInputs) <- unpack <$> mapM (int @Abstract2Cardano) txs
+      pushCheckpoint $ \prev slot -> do
+        pc    <- asks constants
+        block <- mkBlock
+                   (icEpochLeaders prev)
+                   (icBlockHeader  prev)
+                   slot
+                   txs'
+        let raw = mkRawResolvedBlock block resolvedTxInputs
+        checkpoint <- mkCheckpoint prev slot raw
+        if isEpochBoundary pc slot
+          then second (\ebb -> (raw, Just ebb)) <$> createEpochBoundary checkpoint
+          else return (checkpoint, (raw, Nothing))
+    where
+      unpack :: [RawResolvedTx] -> ([TxAux], [NonEmpty TxOutAux])
+      unpack = unzip . map (rawResolvedTx &&& rawResolvedTxInputs)
+
+      isEpochBoundary :: Core.ProtocolConstants -> Core.SlotId -> Bool
+      isEpochBoundary pc slot = Core.siSlot slot == localSlotIndexMaxBound pc
+
+      mkBlock :: (Core.HasConfiguration, HasUpdateConfiguration)
+              => SlotLeaders
+              -> BlockHeader
+              -> Core.SlotId
+              -> [TxAux]
+              -> TranslateT IntException m MainBlock
+      mkBlock leaders prev slotId ts = mapTranslateErrors IntExCreateBlock $ do
+        -- TODO: empty delegation payload
+        let dlgPayload = UnsafeDlgPayload []
+
+        -- empty update payload
+        let updPayload = def
+
+        -- figure out who needs to sign the block
+        BlockSignInfo{..} <- asks $ blockSignInfoForSlot leaders slotId
+
+        pm <- asks magic
+        createMainBlockPure
+          pm
+          blockSizeLimit
+          prev
+          (Just (bsiPSK, bsiLeader))
+          slotId
+          bsiKey
+          (RawPayload
+              (toList ts)
+              (defaultSscPayload (Core.siSlot slotId)) -- TODO
+              dlgPayload
+              updPayload
+            )
+
+      -- TODO: Get this value from somewhere rather than hardcoding it
+      blockSizeLimit = 2 * 1024 * 1024 -- 2 MB

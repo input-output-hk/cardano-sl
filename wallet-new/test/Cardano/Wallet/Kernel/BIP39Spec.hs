a38e6f2ece67258b4b9f265d5f3028c4be09d2a9
Johannes Lund <johannes.lund@iohk.io>
2018-09-25 12:56:56 +0200

    [CO-373] Update BIP39Spec - Remove references to V0 API - Use RecordWildCards instead of huge tuple

--- wallet-new/test/Cardano/Wallet/Kernel/BIP39Spec.hs
+++ wallet-new/test/Cardano/Wallet/Kernel/BIP39Spec.hs
@@ -1,16 +1,15 @@
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DataKinds       #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Cardano.Wallet.Kernel.BIP39Spec where
 
 import           Universum
 
+import           Cardano.Wallet.Kernel.BIP39
 import           Crypto.Encoding.BIP39 (toEntropy)
 import           Data.Default (def)
 import           Pos.Crypto (AesKey (..))
-import           Pos.Util.Mnemonic
-import           Pos.Wallet.Aeson.ClientTypes ()
-import           Pos.Wallet.Web.ClientTypes.Types (CBackupPhrase (..))
 import           Test.Hspec (Spec, describe, it, shouldBe, shouldReturn,
                      shouldSatisfy)
 import           Test.Hspec.QuickCheck (prop)
@@ -26,27 +25,23 @@ import qualified Data.ByteString.Lazy as BL
 instance Eq CC.XPrv where
     (==) = (==) `on` CC.unXPrv
 
-type TestVector =
-    (
+data TestVector = TestVector
+    {
       -- | Raw JSON encoding (V1)
-      BL.ByteString
-
-      -- | Precursor to legacy (V0) JSON encoding.
-      -- Applying 'jsonV0Compat' to this element should produce valid V0 JSON.
-    , BL.ByteString
+      bytes    :: BL.ByteString
 
       -- | Corresponding Entropy
-    , Entropy (EntropySize 12)
+    , entropy  :: Entropy (EntropySize 12)
 
       -- | Corresponding Mnemonic
-    , Mnemonic 12
+    , mnemonic :: Mnemonic 12
 
       -- | Corresponding Seed
-    , ByteString
+    , seed     :: ByteString
 
       -- | Corresponding AESKey
-    , AesKey
-    )
+    , aesKey   :: AesKey
+    }
 
 
 spec :: Spec
@@ -79,74 +74,52 @@ spec = do
         it "Can generate 128 bits entropy" $
             (length . entropyToByteString <$> genEntropy @128) `shouldReturn` 16
 
-        it "Mnemonic to JSON" $ forM_ testVectors $ \(bytes, _, _, mnemonic, _, _) ->
+        it "Mnemonic to JSON" $ forM_ testVectors $ \TestVector{..} ->
             Aeson.encode mnemonic `shouldBe` bytes
 
-        it "Mnemonic from JSON" $ forM_ testVectors $ \(bytes, _, _, mnemonic, _, _) ->
+        it "Mnemonic from JSON" $ forM_ testVectors $ \TestVector{..} ->
             Aeson.decode bytes `shouldBe` pure mnemonic
 
-        it "CBackupPhrase to JSON" $ forM_ testVectors $ \(bytes, _, _, mnemonic, _, _) ->
-            Aeson.encode (CBackupPhrase mnemonic) `shouldBe` jsonV0Compat bytes
-
-        it "CBackupPhrase from JSON" $ forM_ testVectors $ \(bytes, _, _, mnemonic, _, _) ->
-            Aeson.decode (jsonV0Compat bytes) `shouldBe` pure (CBackupPhrase mnemonic)
-
-        it "CBackupPhrase from legacy JSON" $ forM_ testVectors $ \(_, legacyBytes, _, mnemonic, _, _) ->
-            Aeson.decode (jsonV0Compat legacyBytes) `shouldBe` pure (CBackupPhrase mnemonic)
-
-        it "Mnemonic from legacy JSON" $ forM_ testVectors $ \(_, legacyBytes, _, _, _, _) ->
-            Aeson.decode legacyBytes `shouldSatisfy` isNothing @ (Mnemonic 12)
-
-        it "Mnemonic to Entropy" $ forM_ testVectors $ \(_, _, entropy, mnemonic, _, _) ->
+        it "Mnemonic to Entropy" $ forM_ testVectors $ \TestVector{..} ->
             mnemonicToEntropy mnemonic `shouldBe` entropy
 
-        it "Mnemonic to Seed" $ forM_ testVectors $ \(_, _, _, mnemonic, seed, _) ->
+        it "Mnemonic to Seed" $ forM_ testVectors $ \TestVector{..} ->
             mnemonicToSeed mnemonic `shouldBe` seed
 
-        it "Mnemonic to Seed" $ forM_ testVectors $ \(_, _, _, mnemonic, _, aesKey) ->
+        it "Mnemonic to Seed" $ forM_ testVectors $ \TestVector{..} ->
             mnemonicToAesKey mnemonic `shouldBe` aesKey
 
   where
     testVectors :: [TestVector]
     testVectors =
-        [ ( "[\"abandon\",\"abandon\",\"abandon\",\"abandon\",\"abandon\",\"abandon\",\"abandon\",\"abandon\",\"abandon\",\"abandon\",\"abandon\",\"about\"]"
-          , "[\"abandon abandon\",\"abandon\",\"abandon\",\"abandon abandon abandon\",\"abandon\",\"abandon\",\"abandon abandon\",\"about\"]"
-          , orFail $ mkEntropy'
-              "\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL"
-          , orFail $ mkMnemonic
-              ["abandon","abandon","abandon","abandon","abandon","abandon","abandon","abandon","abandon","abandon","abandon","about"]
-          , "X \223\238d\241\SI\212R\194\136)Q\239d\238\180\&8\128\170C\EOT\253\DC1\DC1\n/\ESC\DC3\145?%\138\157"
-          , AesKey "\148\193\192\136\204\148S\153gyc\n\211\175E\203\217(\DC4\130\141\215\132\207*\161-\249]\ESC\138\254"
-          )
-        , ( "[\"letter\",\"advice\",\"cage\",\"absurd\",\"amount\",\"doctor\",\"acoustic\",\"avoid\",\"letter\",\"advice\",\"cage\",\"above\"]"
-          , "[\"letter advice cage\",\"absurd\",\"amount\",\"doctor\",\"acoustic avoid letter advice\",\"cage\",\"above\"]"
-          , orFail $ mkEntropy'
-              "\128\128\128\128\128\128\128\128\128\128\128\128\128\128\128\128"
-          , orFail $ mkMnemonic
-              ["letter","advice","cage","absurd","amount","doctor","acoustic","avoid","letter","advice","cage","above"]
-          , "X \244o\161D\DC1Z\145qq\RS\237\r\130\206\DEL\159]:\DEL\135/\245`\n\222}\137\206y\194R\145"
-          , AesKey "c\232\DC4?\DC2\199V\157\r\172\177x\141\&8\138\246l\174:^]9\249\192\149\230\180s\165\255,J"
-          )
-        , ( "[\"zoo\",\"zoo\",\"zoo\",\"zoo\",\"zoo\",\"zoo\",\"zoo\",\"zoo\",\"zoo\",\"zoo\",\"zoo\",\"wrong\"]"
-          , "[\"zoo\",\"zoo\",\"zoo zoo\",\"zoo\",\"zoo\",\"zoo\",\"zoo\",\"zoo zoo zoo wrong\"]"
-          , orFail $ mkEntropy'
-              "\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255"
-          , orFail $ mkMnemonic
-              ["zoo","zoo","zoo","zoo","zoo","zoo","zoo","zoo","zoo","zoo","zoo","wrong"]
-          , "X \SO\USL9R\238\128^u\167\246\197\215\SOH\RS\SI{\155\189m\177\212\254r\132\148\207K\134K\179{"
-          , AesKey "h\DC4g\179\&3t%\253\&8\250\&9\131S\FS\161\166!M\233&N\235\171\223\156\155\197\209W\210\STX\180"
-          )
+        [ TestVector "[\"abandon\",\"abandon\",\"abandon\",\"abandon\",\"abandon\",\"abandon\",\"abandon\",\"abandon\",\"abandon\",\"abandon\",\"abandon\",\"about\"]"
+          (orFail $ mkEntropy'
+              "\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL")
+          (orFail $ mkMnemonic
+              ["abandon","abandon","abandon","abandon","abandon","abandon","abandon","abandon","abandon","abandon","abandon","about"])
+          "X \223\238d\241\SI\212R\194\136)Q\239d\238\180\&8\128\170C\EOT\253\DC1\DC1\n/\ESC\DC3\145?%\138\157"
+          $ AesKey "\148\193\192\136\204\148S\153gyc\n\211\175E\203\217(\DC4\130\141\215\132\207*\161-\249]\ESC\138\254"
+        , TestVector "[\"letter\",\"advice\",\"cage\",\"absurd\",\"amount\",\"doctor\",\"acoustic\",\"avoid\",\"letter\",\"advice\",\"cage\",\"above\"]"
+           (orFail $ mkEntropy'
+             "\128\128\128\128\128\128\128\128\128\128\128\128\128\128\128\128")
+           (orFail $ mkMnemonic
+             ["letter","advice","cage","absurd","amount","doctor","acoustic","avoid","letter","advice","cage","above"])
+           "X \244o\161D\DC1Z\145qq\RS\237\r\130\206\DEL\159]:\DEL\135/\245`\n\222}\137\206y\194R\145"
+           (AesKey "c\232\DC4?\DC2\199V\157\r\172\177x\141\&8\138\246l\174:^]9\249\192\149\230\180s\165\255,J")
+        , TestVector
+          "[\"zoo\",\"zoo\",\"zoo\",\"zoo\",\"zoo\",\"zoo\",\"zoo\",\"zoo\",\"zoo\",\"zoo\",\"zoo\",\"wrong\"]"
+          (orFail $ mkEntropy'
+            "\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255\255")
+          (orFail $ mkMnemonic
+            ["zoo","zoo","zoo","zoo","zoo","zoo","zoo","zoo","zoo","zoo","zoo","wrong"])
+          "X \SO\USL9R\238\128^u\167\246\197\215\SOH\RS\SI{\155\189m\177\212\254r\132\148\207K\134K\179{"
+          (AesKey "h\DC4g\179\&3t%\253\&8\250\&9\131S\FS\161\166!M\233&N\235\171\223\156\155\197\209W\210\STX\180")
         ]
       where
         orFail :: Show e => Either e a -> a
         orFail =
             either (error . (<>) "Failed to create golden Mnemonic: " . show) identity
         mkEntropy' = toEntropy @128 @4 @ByteString
-
-    -- | V0 Mnemonics are wrapped in a singleton object with a `bpToList` prop
-    jsonV0Compat :: BL.ByteString -> BL.ByteString
-    jsonV0Compat bytes =
-        "{\"bpToList\":" <> bytes <> "}"
 
     defMnemonic :: [Text]
     defMnemonic = either (error . (<>) "Failed to encode/decode default menmonic " . show) identity

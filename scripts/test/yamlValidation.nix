8a3a5c06897facee021368dba5b8f5dd3f0855cd
Michael Bishop <michael.bishop@iohk.io>
2018-09-25 02:45:37 -0300

    [DEVOPS-1057] also verify that all topology files parse

--- scripts/test/yamlValidation.nix
+++ scripts/test/yamlValidation.nix
@@ -2,6 +2,7 @@
 
 runHaskell "yamlValidation" cardanoPkgs (ps: with ps; [
   cardano-sl-util
+  cardano-sl-infra
   split
 ]) {
   logConfigs = [
@@ -11,8 +12,18 @@ runHaskell "yamlValidation" cardanoPkgs (ps: with ps; [
     ../../log-configs/cluster.yaml
     ../../log-configs/template-demo.yaml
   ];
+  topologyConfigs = [
+    ../../docs/network/example-topologies/mainnet-staging.yaml
+    ../../docs/network/example-topologies/behind-nat-no-dns.yaml
+    ../../docs/network/example-topologies/behind-nat-with-dns.yaml
+    ../../docs/network/example-topologies/p2p.yaml
+    ../../docs/network/example-topologies/static-no-dns.yaml
+    ../../docs/network/example-topologies/static-with-dns.yaml
+    ../../docs/network/example-topologies/traditional.yaml
+  ];
 } ''
   import Pos.Util.Log.LoggerConfig (LoggerConfig)
+  import Pos.Infra.Network.Yaml (Topology)
   import Data.List.Split
   import System.Environment
   import Data.Yaml
@@ -22,6 +33,7 @@ runHaskell "yamlValidation" cardanoPkgs (ps: with ps; [
   main :: IO ()
   main = do
     runTest "logConfigs" checkLogConfig
+    runTest "topologyConfigs" checkTopology
     outpath <- getEnv "out"
     writeFile outpath "done"
 
@@ -38,4 +50,7 @@ runHaskell "yamlValidation" cardanoPkgs (ps: with ps; [
   checkLogConfig :: String -> IO LoggerConfig
   checkLogConfig path = do
     either throwM return =<< decodeFileEither path
+
+  checkTopology :: String -> IO Topology
+  checkTopology path = either throwM return =<< decodeFileEither path
 ''

867cc936e436b95a15de349eddae3155c5084bc7
Johannes Lund <johannes.lund@iohk.io>
2018-09-27 13:26:13 +0200

    [CO-373] Remove tools/dbgen executable and lib
    It had a laudable goal, was mangled and eventually bit-rotted.
    It's not used anymore and heavily rely on V0 and legacy data-layer
    code.
    
    R.I.P

--- tools/test/Test.hs
+++ tools/test/Test.hs
@@ -2,12 +2,9 @@ import           Test.Hspec (hspec)
 
 import           Spec (spec)
 
-import qualified Test.Pos.Tools.Dbgen.Json
 import           Test.Pos.Util.Tripping (runTests)
 
 main :: IO ()
 main = do
     hspec spec
-    runTests
-        [ Test.Pos.Tools.Dbgen.Json.tests
-        ]
+    runTests []

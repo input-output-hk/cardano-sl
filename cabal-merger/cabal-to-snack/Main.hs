af595311fcbff4a12387c1297c74363bd8d171ca
Michael Bishop <michael.bishop@iohk.io>
2018-10-01 05:41:05 -0300

    clean up hnix code

--- cabal-merger/cabal-to-snack/Main.hs
+++ cabal-merger/cabal-to-snack/Main.hs
@@ -2,40 +2,36 @@
 
 module Main (main) where
 
-import Nix.Expr.Types (Binding(NamedVar), NExprF(NSet, NLiteralPath, NList, NStr, NSelect, NSym, NAbs), VarName, NKeyName(StaticKey), Params(Param))
+import Nix.Expr.Types (Binding(NamedVar), NExprF(NSelect), NKeyName(StaticKey), Params(Param), nullPos, NExpr)
 import Nix.Pretty (prettyNix)
 import Data.Fix (Fix(Fix))
-import Text.Megaparsec.Pos (mkPos, SourcePos(SourcePos))
 import qualified Data.List.NonEmpty as NonEmpty
-import Data.Text (unpack)
-import Data.String (fromString)
 import qualified Data.Set as Set
 import Common (Package(pkgSrc, pkgDependencies, pkgExtensions, pkgMainIs, pkgPackages, pkgName), cabalFilesToPackages)
 import           System.Environment (getArgs)
+import Nix.Expr.Shorthands (($=), mkPath, mkStr, mkList, mkSym, mkFunction, mkNonRecSet)
+import Data.Text (unpack)
 
-packageToExpr :: Package -> (Binding (Fix NExprF))
-packageToExpr pkg = NamedVar (NonEmpty.fromList [ StaticKey $ pkgName pkg ]) (Fix set) pos
+packageToExpr :: Package -> (Binding NExpr)
+packageToExpr pkg = NamedVar (NonEmpty.fromList [ StaticKey $ pkgName pkg ]) set nullPos
   where
-    set :: NExprF (Fix NExprF)
-    set = NSet ([
-        mkBinding "src" (NLiteralPath $ pkgSrc pkg)
-      , mkBinding "dependencies" (NList $ Set.toList $ Set.map (Fix . NStr . fromString) (pkgDependencies pkg))
-      , mkBinding "extensions" (NList $ Set.toList $ Set.map (Fix . NStr . fromString) (pkgExtensions pkg))
-      , mkBinding "packages" (NList $ Set.toList $ Set.map toAttrpath (pkgPackages pkg))
+    set :: NExpr
+    set = mkNonRecSet ([
+        "src" $= (mkPath False $ unpack $ pkgSrc pkg)
+      , "dependencies" $= (mkList $ Set.toList $ Set.map mkStr (pkgDependencies pkg))
+      , "extensions" $= (mkList $ Set.toList $ Set.map mkStr (pkgExtensions pkg))
+      , "packages" $= (mkList $ Set.toList $ Set.map toAttrpath (pkgPackages pkg))
       ] <> (makeMain $ pkgMainIs pkg) )
     makeMain (Just mainModule) = [
-        mkBinding "main" ( (NStr . fromString . unpack) mainModule)
+        "main" $= ( mkStr mainModule)
       ]
     makeMain Nothing = []
-    toAttrpath name = Fix (NSelect (Fix $ NSym "self") (NonEmpty.fromList [ StaticKey name ]) Nothing)
-    pos = (SourcePos "file" (mkPos 1) (mkPos 1))
-    mkBinding :: VarName -> f (Fix f) -> Binding (Fix f)
-    mkBinding key value = NamedVar (NonEmpty.fromList [ StaticKey key ]) (Fix value) pos
+    toAttrpath name = Fix (NSelect (mkSym "self") (NonEmpty.fromList [ StaticKey name ]) Nothing)
 
 main :: IO ()
 main = do
   res2 <- getArgs >>= cabalFilesToPackages
   let
-    set2 = NSet $ map packageToExpr res2
-    func = NAbs (Param "self") (Fix set2)
-  writeFile "output" $ (show . prettyNix . Fix) func
+    set2 = mkNonRecSet $ map packageToExpr res2
+    func = mkFunction (Param "self") set2
+  writeFile "output" $ (show . prettyNix) func

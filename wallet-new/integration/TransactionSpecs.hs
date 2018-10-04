f219b682a12a071533bd1739c70a469046044747
KtorZ <matthias.benkort@gmail.com>
2018-10-04 13:49:02 +0200

    [CBR-464] Move TransactionSpec DSL under 'where'
    This makes it available for other tests within this context.

--- wallet-new/integration/TransactionSpecs.hs
+++ wallet-new/integration/TransactionSpecs.hs
@@ -200,57 +200,6 @@ transactionSpecs wRef wc = beforeAll_ (setupLogging "wallet-new_transactionSpecs
                         <> show err
 
         randomTest "fails if you can't cover fee with a transaction" 1 $ run $ do
-            let makePayment
-                    :: Core.Coin
-                    -> (Wallet, Account)
-                    -> Core.Address
-                    -> IO (Either ClientError Transaction)
-                makePayment amount (sourceW, sourceA) destination = do
-                    let payment = Payment
-                            { pmtSource = PaymentSource
-                                { psWalletId     = walId sourceW
-                                , psAccountIndex = accIndex sourceA
-                                }
-                            , pmtDestinations = pure PaymentDistribution
-                                { pdAddress = V1 destination
-                                , pdAmount  = V1 amount
-                                }
-                            , pmtGroupingPolicy   = Nothing
-                            , pmtSpendingPassword = Nothing
-                            }
-                    fmap (fmap wrData) $ postTransaction wc payment
-
-            let getRandomAddress
-                    :: IO Core.Address
-                getRandomAddress = do
-                    wallet <- randomWallet CreateWallet >>= createWalletCheck wc
-                    (_, toAddr) <- firstAccountAndId wc wallet
-                    return (unV1 $ addrId toAddr)
-
-            let fixtureWallet
-                    :: Core.Coin
-                    -> IO (Wallet, Account)
-                fixtureWallet coin = do
-                    genesis <- genesisWallet wc
-                    (genesisAccount, _) <- firstAccountAndId wc genesis
-                    wallet <- randomWallet CreateWallet >>= createWalletCheck wc
-                    (account, address) <- firstAccountAndId wc wallet
-                    txn <- makePayment coin (genesis, genesisAccount) (unV1 $ addrId address) >>= shouldPrismFlipped _Right
-                    pollTransactions wc (walId wallet) (accIndex account) (txId txn)
-                    return (wallet, account)
-
-            let expectFailure
-                    :: Show a
-                    => ClientError
-                    -> Either ClientError a
-                    -> IO ()
-                expectFailure want eresp = do
-                    resp <- eresp `shouldPrism` _Left
-                    want `shouldBe` resp
-
-            --
-            -- Actual test
-            --
             (wallet, account) <- fixtureWallet (Core.mkCoin 42)
             resp <- makePayment (Core.mkCoin 42) (wallet, account) =<< getRandomAddress
             let err = NotEnoughMoney ErrCannotCoverFee
@@ -298,3 +247,52 @@ transactionSpecs wRef wc = beforeAll_ (setupLogging "wallet-new_transactionSpecs
             utxoStatistics <- run $ fmap wrData eresp `shouldPrism` _Right
             let utxoStatisticsExpected = computeUtxoStatistics log10 utxos
             liftIO $ utxoStatistics `shouldBe` utxoStatisticsExpected
+
+  where
+    makePayment
+        :: Core.Coin
+        -> (Wallet, Account)
+        -> Core.Address
+        -> IO (Either ClientError Transaction)
+    makePayment amount (sourceW, sourceA) destination = do
+        let payment = Payment
+                { pmtSource = PaymentSource
+                    { psWalletId     = walId sourceW
+                    , psAccountIndex = accIndex sourceA
+                    }
+                , pmtDestinations = pure PaymentDistribution
+                    { pdAddress = V1 destination
+                    , pdAmount  = V1 amount
+                    }
+                , pmtGroupingPolicy   = Nothing
+                , pmtSpendingPassword = Nothing
+                }
+        fmap (fmap wrData) $ postTransaction wc payment
+
+    getRandomAddress
+        :: IO Core.Address
+    getRandomAddress = do
+        wallet <- randomWallet CreateWallet >>= createWalletCheck wc
+        (_, toAddr) <- firstAccountAndId wc wallet
+        return (unV1 $ addrId toAddr)
+
+    fixtureWallet
+        :: Core.Coin
+        -> IO (Wallet, Account)
+    fixtureWallet coin = do
+        genesis <- genesisWallet wc
+        (genesisAccount, _) <- firstAccountAndId wc genesis
+        wallet <- randomWallet CreateWallet >>= createWalletCheck wc
+        (account, address) <- firstAccountAndId wc wallet
+        txn <- makePayment coin (genesis, genesisAccount) (unV1 $ addrId address) >>= shouldPrismFlipped _Right
+        pollTransactions wc (walId wallet) (accIndex account) (txId txn)
+        return (wallet, account)
+
+    expectFailure
+        :: Show a
+        => ClientError
+        -> Either ClientError a
+        -> IO ()
+    expectFailure want eresp = do
+        resp <- eresp `shouldPrism` _Left
+        want `shouldBe` resp

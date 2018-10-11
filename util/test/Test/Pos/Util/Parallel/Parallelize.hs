915ee1b7a06e9572310e286e3222ce67a2864324
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2018-10-11 15:02:31 +1000

    [DEVOPS-1088] Add option to limit parallelism of test suites

--- util/test/Test/Pos/Util/Parallel/Parallelize.hs
+++ util/test/Test/Pos/Util/Parallel/Parallelize.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 module Test.Pos.Util.Parallel.Parallelize
        ( parallelizeAllCores
        ) where
@@ -5,8 +6,25 @@ module Test.Pos.Util.Parallel.Parallelize
 import           Universum
 
 import           GHC.Conc (getNumProcessors, setNumCapabilities)
+import           System.Environment (lookupEnv)
 
 -- | `parallelizeAllCores` gets the number of processors on a machine and
 -- sets the number of threads equal to the number of processors on the machine.
+-- This setting can be overridden by the @NIX_BUILD_CORES@ environment variable.
 parallelizeAllCores :: IO ()
-parallelizeAllCores = getNumProcessors >>= setNumCapabilities
+parallelizeAllCores = getNumParallel >>= setNumCapabilities
+  where
+    getNumParallel = getNixBuildCores >>= \case
+      Just n -> pure n
+      Nothing -> getNumProcessors
+
+-- | Look up the @NIX_BUILD_CORES@ environment variable, if it
+-- exists. This is set by the Nix builder to control parallelism
+-- within a single build jobs.
+getNixBuildCores :: IO (Maybe Int)
+getNixBuildCores = readBuildCores <$> lookupEnv "NIX_BUILD_CORES"
+  where
+    readBuildCores (Just str) = case readMaybe str of
+      Just n | n > 0 -> Just n
+      _              -> Nothing
+    readBuildCores Nothing    = Nothing

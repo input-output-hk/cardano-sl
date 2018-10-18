c64ba445ae8572ad6183cacadc459fccb012235f
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2018-10-18 11:06:14 +1000

    [DEVOPS-1083] nix: Move stack2nix package to separate file

--- default.nix
+++ default.nix
@@ -237,12 +237,7 @@ let
     };
 
     # Tool for generating ./pkgs/default.nix
-    stack2nix = self.callPackage (pkgs.fetchFromGitHub {
-      owner = "avieth";
-      repo = "stack2nix";
-      rev = "c51db2d31892f7c4e7ff6acebe4504f788c56dca";
-      sha256 = "10jcj33sxpq18gxf3zcck5i09b2y4jm6qjggqdlwd9ss86wg3ksb";
-    }) { };
+    stack2nix = self.callPackage ./nix/stack2nix.nix { };
 
     validateJson = self.callPackage ./tools/src/validate-json {};
 

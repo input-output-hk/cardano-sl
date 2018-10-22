4e6f1810eb27fd5713b26aec1985d39698cd17f8
Ante Kegalj <akegalj@gmail.com>
2018-10-22 17:02:33 +0200

    [CO-412] Pararellize all cores

--- wallet-new/test/state-machine/Spec.hs
+++ wallet-new/test/state-machine/Spec.hs
@@ -4,6 +4,7 @@ module Main (main) where
 
 import           Universum
 
+import           GHC.Conc (getNumProcessors, setNumCapabilities)
 import           Test.Hspec (Spec, after, around, before, describe, hspec, it,
                      parallel)
 import           Test.QuickCheck (expectFailure, withMaxSuccess)
@@ -23,15 +24,19 @@ tests =
                 it "sequential" $ prop_ticketDispenser
                 it "parallel with exclusive lock" $ withMaxSuccess 30 . prop_ticketDispenserParallelOK
                 it "parallel with shared lock" $ expectFailure . prop_ticketDispenserParallelBad
+        -- TODO: test wallet layer which is not in memory. Maybe there are race condition bugs when we are using filesystem persisted db instead of in-memory one
         around (withWalletLayer . curry) $ do
             describe "Wallet" $ do
-                it "sequential" $ prop_wallet
-                it "parallel" $ withMaxSuccess 2 . prop_walletParallel
+                it "sequential" $ withMaxSuccess 30 . prop_wallet
+                it "parallel" $ withMaxSuccess 3 . prop_walletParallel
 
 ------------------------------------------------------------------------
 
+-- TODO: factor out functionality from Parallel.Parallelize module into a lib and reuse
+parallelizeAllCores :: IO ()
+parallelizeAllCores = getNumProcessors >>= setNumCapabilities
+
 main :: IO ()
 main = do
-    -- TODO: enable more cores
-    -- parallelizeAllCores
+    parallelizeAllCores
     hspec tests

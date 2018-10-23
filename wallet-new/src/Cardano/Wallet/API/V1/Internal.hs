5efac079cc2e33225c4880417ad822db43e8a4eb
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2018-10-23 23:21:44 +1000

    Remove wallet-new

--- wallet-new/src/Cardano/Wallet/API/V1/Internal.hs
+++
@@ -1,6 +0,0 @@
-{-# LANGUAGE DeriveGeneric #-}
-
--- | This module contains the top level API definition for frontend-related
--- tasks.  The API endpoints presented here are intended for use with the
--- Daedalus client, and aren't useful for wallets, exchanges, and other users.
-module Cardano.Wallet.API.V1.Internal where
--- wallet-new/src/Cardano/Wallet/Kernel/CoinSelection.hs
+++
@@ -1,6 +0,0 @@
-module Cardano.Wallet.Kernel.CoinSelection (
-    module Exports
-    ) where
-
-import           Cardano.Wallet.Kernel.CoinSelection.FromGeneric as Exports
-import           Cardano.Wallet.Kernel.CoinSelection.Generic as Exports

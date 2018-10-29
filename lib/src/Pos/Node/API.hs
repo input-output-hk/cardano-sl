52a73d7f82b080e373a84760d8eb3bce040d8c2b
parsonsmatt <parsonsmatt@gmail.com>
2018-10-29 10:49:10 -0600

    Pulling the string on node-settings

--- lib/src/Pos/Node/API.hs
+++ lib/src/Pos/Node/API.hs
@@ -1,57 +1,53 @@
-{-# LANGUAGE TypeOperators, DataKinds, RecordWildCards #-}
+{-# LANGUAGE DataKinds       #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeOperators   #-}
+{-# LANGUAGE CPP   #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Pos.Node.API where
 
-import Universum
+import           Universum
 
-import qualified Data.Text as T
-import qualified Data.Text.Encoding as T
-import qualified Network.Transport as NT
-import qualified Data.Map.Strict as Map
-import qualified Data.Char as C
-import Formatting (bprint, build, (%))
-import qualified Formatting.Buildable
-import qualified Pos.Core as Core
+import qualified Prelude
 import           Control.Lens (At, Index, IxValue, at, ix, (?~))
-import           Pos.Infra.Util.LogSafe (BuildableSafeGen (..),
-                     deriveSafeBuildable)
-import           Pos.Infra.Diffusion.Subscription.Status
-                     (SubscriptionStatus (..))
-import           Pos.Util.Servant (CustomQueryFlag, ValidJSON, Flaggable(..), Tags, WalletResponse)
-import           Node (NodeId (..))
-import           Test.QuickCheck
 import           Data.Aeson
 import qualified Data.Aeson.Options as Serokell
 import           Data.Aeson.TH as A
 import           Data.Aeson.Types (Value (..), toJSONKeyText)
+import qualified Data.Char as C
+import qualified Data.Map.Strict as Map
 import           Data.Swagger hiding (Example, example)
 import qualified Data.Swagger as S
 import           Data.Swagger.Declare (Declare, look)
 import           Data.Swagger.Internal.Schema (GToSchema)
 import           Data.Swagger.Internal.TypeShape (GenericHasSimpleShape,
                      GenericShape)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import           Data.Version (Version)
+import           Formatting (bprint, build, (%), shown)
+import qualified Formatting.Buildable
 import           GHC.Generics (Generic, Rep)
+import qualified Network.Transport as NT
+import           Node (NodeId (..))
 import           Servant
+import           Test.QuickCheck
+
+import qualified Pos.Core as Core
+import           Pos.Infra.Diffusion.Subscription.Status
+                     (SubscriptionStatus (..))
+import           Pos.Infra.Util.LogSafe (BuildableSafeGen (..),
+                     deriveSafeBuildable)
+import           Pos.Util.Servant (CustomQueryFlag, Flaggable (..), Tags,
+                     ValidJSON, WalletResponse)
+import qualified Pos.Chain.Update as Core
 
 -- ToJSON/FromJSON instances for NodeId
-import Pos.Infra.Communication.Types.Protocol ()
+import           Pos.Infra.Communication.Types.Protocol ()
 
-import Serokell.Util.Text
-import Pos.Util.Example
-import Pos.Util.UnitsOfMeasure
-
-type API =
-        Tags '["Info"]
-            :> "node-info"
-            :> Summary "Retrieves the dynamic information for this node."
-            :> CustomQueryFlag "force_ntp_check" ForceNtpCheck
-            :> Get '[ValidJSON] (WalletResponse NodeInfo)
-    :<|>
-        Tags '["Settings"]
-            :> "node-settings"
-            :> Summary "Retrieves the static settings for this node."
-            :> Get '[ValidJSON] (WalletResponse NodeSettings)
+import           Pos.Util.Example
+import           Pos.Util.UnitsOfMeasure
+import           Serokell.Util.Text
 
 
 -- TODO: Stuff that was pulled upstream. Likely to be factored out into other modules.
@@ -383,3 +379,167 @@ instance ToSchema NodeId where
 
 instance (Buildable a, Buildable b) => Buildable (a, b) where
     build (a, b) = bprint ("("%build%", "%build%")") a b
+
+-- Types for the NodeSettings API. TODO: Extract to own module.
+
+-- | How many milliseconds a slot lasts for.
+newtype SlotDuration = SlotDuration (MeasuredIn 'Milliseconds Word)
+    deriving (Show, Eq)
+
+mkSlotDuration :: Word -> SlotDuration
+mkSlotDuration = SlotDuration . MeasuredIn
+
+instance Arbitrary SlotDuration where
+    arbitrary = mkSlotDuration <$> choose (0, 100)
+
+instance ToJSON SlotDuration where
+    toJSON (SlotDuration (MeasuredIn w)) =
+        object
+            [ "quantity" .= toJSON w
+            , "unit"     .= String "milliseconds"
+            ]
+
+instance FromJSON SlotDuration where
+    parseJSON = withObject "SlotDuration" $ \sl ->
+        mkSlotDuration <$> sl .: "quantity"
+
+instance ToSchema SlotDuration where
+    declareNamedSchema _ =
+        pure $ NamedSchema (Just "SlotDuration") $ mempty
+            & type_ .~ SwaggerObject
+            & required .~ ["quantity"]
+            & properties .~ (mempty
+                & at "quantity" ?~ (Inline $ mempty
+                    & type_ .~ SwaggerNumber
+                    )
+                & at "unit" ?~ (Inline $ mempty
+                    & type_ .~ SwaggerString
+                    & enum_ ?~ ["milliseconds"]
+                    )
+                )
+
+deriveSafeBuildable ''SlotDuration
+instance BuildableSafeGen SlotDuration where
+    buildSafeGen _ (SlotDuration (MeasuredIn w)) =
+        bprint (build%"ms") w
+
+-- We copy the V1 type here, instead of moving it. It is only used in a single
+-- field of `NodeSettings` thus far, and copying it allows us to avoid orphan
+-- instances by relocating `V1` to some module that does not define the types it
+-- needs.
+newtype V1 a = V1 a deriving (Eq, Ord)
+
+instance Show a => Show (V1 a) where
+    show (V1 a) = Prelude.show a
+
+
+instance ToJSON (V1 Core.ApplicationName) where
+    toJSON (V1 svAppName) = toJSON (Core.getApplicationName svAppName)
+
+instance FromJSON (V1 Core.ApplicationName) where
+    parseJSON = withText "ApplicationName" $ \svAppName ->
+        pure (V1 (Core.ApplicationName svAppName))
+
+instance ToJSON (V1 Core.SoftwareVersion) where
+    toJSON (V1 Core.SoftwareVersion{..}) =
+        object [ "applicationName" .= toJSON (V1 svAppName)
+               -- svNumber is just a type alias to Word32
+               -- so that's fine.
+               , "version" .=  toJSON svNumber
+               ]
+
+instance FromJSON (V1 Core.SoftwareVersion) where
+    parseJSON = withObject "V1SoftwareVersion" $ \o -> do
+        V1 svAppName <- o .: "applicationName"
+        svNumber <- o .: "version"
+        pure $ V1 Core.SoftwareVersion{..}
+
+instance ToSchema (V1 Core.SoftwareVersion) where
+    declareNamedSchema _ =
+        pure $ NamedSchema (Just "V1SoftwareVersion") $ mempty
+            & type_ .~ SwaggerObject
+            & properties .~ (mempty
+                & at "applicationName" ?~ Inline (toSchema (Proxy @Text))
+                & at "version" ?~ Inline (toSchema (Proxy @Word32))
+            )
+            & required .~ ["applicationName", "version"]
+
+instance Arbitrary (V1 Core.SoftwareVersion) where
+    arbitrary = fmap V1 arbitrary
+
+instance Buildable Version where
+    build v = bprint shown v
+
+instance Buildable a => Buildable (V1 a) where
+    build (V1 x) = bprint build x
+
+#if !(MIN_VERSION_swagger2(2,2,2))
+-- See note [Version Orphan]
+instance ToSchema Version where
+    declareNamedSchema _ =
+        pure $ NamedSchema (Just "Version") $ mempty
+            & type_ .~ SwaggerString
+
+-- Note [Version Orphan]
+-- I have opened a PR to add an instance of 'Version' to the swagger2
+-- library. When the PR is merged, we can delete the instance here and remove the warning from the file.
+-- PR: https://github.com/GetShopTV/swagger2/pull/152
+#endif
+
+-- | The @static@ settings for this wallet node. In particular, we could group
+-- here protocol-related settings like the slot duration, the transaction max size,
+-- the current software version running on the node, etc.
+data NodeSettings = NodeSettings
+    { setSlotDuration   :: !SlotDuration
+    , setSoftwareInfo   :: !(V1 Core.SoftwareVersion)
+    , setProjectVersion :: !Version
+    , setGitRevision    :: !Text
+    } deriving (Show, Eq, Generic)
+
+deriveJSON Serokell.defaultOptions ''NodeSettings
+
+instance ToSchema NodeSettings where
+  declareNamedSchema =
+    genericSchemaDroppingPrefix "set" (\(--^) props -> props
+      & ("slotDuration"   --^ "Duration of a slot.")
+      & ("softwareInfo"   --^ "Various pieces of information about the current software.")
+      & ("projectVersion" --^ "Current project's version.")
+      & ("gitRevision"    --^ "Git revision of this deployment.")
+    )
+
+instance Arbitrary NodeSettings where
+    arbitrary = NodeSettings <$> arbitrary
+                             <*> arbitrary
+                             <*> arbitrary
+                             <*> pure "0e1c9322a"
+
+instance Example NodeSettings
+deriveSafeBuildable ''NodeSettings
+instance BuildableSafeGen NodeSettings where
+    buildSafeGen _ NodeSettings{..} = bprint ("{"
+        %" slotDuration="%build
+        %" softwareInfo="%build
+        %" projectRevision="%build
+        %" gitRevision="%build
+        %" }")
+        setSlotDuration
+        setSoftwareInfo
+        setProjectVersion
+        setGitRevision
+
+
+-- The API definition is down here for now due to TH staging restrictions. Will
+-- relocate other stuff into it's own module when the extraction is complete.
+type API =
+        Tags '["Info"]
+            :> "node-info"
+            :> Summary "Retrieves the dynamic information for this node."
+            :> CustomQueryFlag "force_ntp_check" ForceNtpCheck
+            :> Get '[ValidJSON] (WalletResponse NodeInfo)
+    :<|>
+        Tags '["Settings"]
+            :> "node-settings"
+            :> Summary "Retrieves the static settings for this node."
+            :> Get '[ValidJSON] (WalletResponse NodeSettings)
+
+

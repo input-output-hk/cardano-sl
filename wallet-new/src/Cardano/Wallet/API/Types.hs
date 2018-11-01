0ae7292dc81bce2331c951dd46aa8cc44049a469
parsonsmatt <parsonsmatt@gmail.com>
2018-11-01 13:13:32 -0600

    Remove dead code

--- wallet-new/src/Cardano/Wallet/API/Types.hs
+++ wallet-new/src/Cardano/Wallet/API/Types.hs
@@ -16,16 +16,11 @@ module Cardano.Wallet.API.Types
 import           Universum
 
 import           Data.Reflection (Reifies (..))
-import           GHC.TypeLits
 import           Servant
 import qualified Servant.Server.Internal as SI
 
 import           Pos.Util.Servant (ApiLoggingConfig (..), DHeader, DQueryParam,
                      Tags, WithDefaultApiArg)
-
---
--- Utilities
---
 
 -- | `mapRouter` is helper function used in order to transform one `HasServer`
 -- instance to another. It can be used to introduce custom request params type.
@@ -37,72 +32,8 @@ mapRouter
     -> (Proxy api' -> SI.Context ctx -> SI.Delayed env (Server api') -> SI.Router env)
 mapRouter routing f = \_ ctx delayed -> routing Proxy ctx (fmap f delayed)
 
--- | The `UnmaybeArg` type family is necessary to
--- get `a -> b` from `Maybe a -> b` in a way which doesn't make GHC mad.
--- Used in `HasServer` instances for
--- `WithDefaultApiArg`
-type family UnmaybeArg a where
-    UnmaybeArg (Maybe a -> b) = a -> b
-    UnmaybeArg x = TypeError
-        ( 'Text "The parameter to UnmaybeArg must be a function with the shape "
-        ':$$: 'Text "    Maybe a -> b"
-        ':$$: 'Text "You applied it to a type: "
-        ':$$: 'Text "    " ':<>: 'ShowType x
-        )
-
---
--- Specifying API method parameters with default values
---
-
--- | `WithDefaultApiArg` type is used to specify API parameters,
--- which are optional but have default values. It can be applied to every
--- API specifier which yields `Maybe a` as argument type.
--- data WithDefaultApiArg (argType :: * -> *) a
---
--- instance
---     ( HasServer (apiType a :> res) ctx
---     , Server (apiType a :> res) ~ (Maybe c -> d)
---     , UnmaybeArg (Server (apiType a :> res)) ~ (c -> Server res)
---     , Default c
---     )
---     => HasServer (WithDefaultApiArg apiType a :> res) ctx
---   where
---     type ServerT (WithDefaultApiArg apiType a :> res) m =
---         UnmaybeArg (ServerT (apiType a :> res) m)
---     route =
---         mapRouter @(apiType a :> res) route $
---         \f a -> f $ fromMaybe def a
---     hoistServerWithContext
---         :: Proxy (WithDefaultApiArg apiType a :> res)
---         -> Proxy ctx
---         -> (forall x. m x -> n x)
---         -> ServerT (WithDefaultApiArg apiType a :> res) m
---         -- aka
---         -- (UnMaybeArg (ServerT (apiType a :> res) m)
---         -- (UnMaybeArg (Maybe b -> ServerT res m)
---         -- (b -> ServerT res m)
---         -> ServerT (WithDefaultApiArg apiType a :> res) n
---         -- aka
---         -- (UnMaybeArg (ServerT (apiType a :> res) n)
---         -- (UnMaybeArg (Maybe b -> ServerT res n)
---         -- (b -> ServerT res n)
---     hoistServerWithContext _ pc nt s =
-
-
--- | Type aliases for query params and headers which have default values.
--- type DQueryParam s a = WithDefaultApiArg (QueryParam s) a
--- type DHeader s a = WithDefaultApiArg (Header s) a
-
---
--- Types
---
-
 -- | Specifes logging config for servant.
 data WalletLoggingConfig
-
---
--- Instances
---
 
 -- If logger config will ever be determined in runtime, 'Data.Reflection.reify'
 -- can be used.

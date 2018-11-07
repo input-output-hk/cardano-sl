af91001f824a32f6ed3e62a97b9a6eb83b2bbf58
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2018-11-07 17:00:11 +1000

    [DEVOPS-1029] regenerate pkgs/default.nix

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -50604,6 +50604,23 @@ description = "LZMA/XZ compression and decompression";
 license = stdenv.lib.licenses.bsd3;
 
 }) {inherit (pkgs) lzma;};
+"lzma-clib" = callPackage
+({
+  mkDerivation
+, stdenv
+}:
+mkDerivation {
+
+pname = "lzma-clib";
+version = "5.2.2";
+sha256 = "0aed9cb8ef3a2b0e71c429b00161ee3fb342cce2603ccb934f507fb236a09fd5";
+doHaddock = false;
+doCheck = false;
+description = "liblzma C library and headers for use by LZMA bindings";
+license = stdenv.lib.licenses.publicDomain;
+platforms = stdenv.lib.platforms.none;
+
+}) {};
 "lzma-conduit" = callPackage
 ({
   mkDerivation

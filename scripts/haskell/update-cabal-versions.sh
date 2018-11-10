afc61f0cefcdca6c934280b1cfbe8f976cd74cda
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2018-11-10 23:39:55 +1000

    [DEVOPS-1130] Bump

--- scripts/haskell/update-cabal-versions.sh
+++ scripts/haskell/update-cabal-versions.sh
@@ -32,6 +32,6 @@ updateVersion txp/cardano-sl-txp.cabal
 updateVersion update/cardano-sl-update.cabal
 updateVersion util/cardano-sl-util.cabal
 updateVersion wallet/cardano-sl-wallet.cabal
-updateVersion wallet/cardano-sl-wallet-new.cabal
+updateVersion wallet-new/cardano-sl-wallet-new.cabal
 
 echo "Updated to version $newVersion"

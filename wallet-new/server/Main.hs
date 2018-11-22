a464b06a47ef88ca81be6e33844f3d53ae070572
Kosyrev Serge <serge.kosyrev@iohk.io>
2018-11-22 17:10:05 +0300

    Review fixes

--- wallet-new/server/Main.hs
+++ wallet-new/server/Main.hs
@@ -3,7 +3,6 @@ module Main where
 import           Universum
 
 import           Pos.Client.CLI (NodeArgs (..), loggingParams)
-import           Pos.Infra.Statistics.Ekg
 import           Pos.Launcher (launchNode)
 import           Pos.Util.CompileInfo (withCompileInfo)
 
@@ -11,12 +10,14 @@ import           Cardano.Wallet.Action (actionWithWallet)
 import           Cardano.Wallet.Server.CLI (ChooseWalletBackend (..),
                      WalletStartupOptions (..), getWalletNodeOptions)
 
+
 -- | The main entrypoint for the Wallet.
 main :: IO ()
 main = withCompileInfo $ do
     WalletStartupOptions cArgs wArgs <- getWalletNodeOptions
     let lArgs = loggingParams "node" cArgs
     let nArgs = NodeArgs { behaviorConfigPath = Nothing }
+
     putText "Wallet is starting..."
 
     launchNode nArgs cArgs lArgs $ case wArgs of

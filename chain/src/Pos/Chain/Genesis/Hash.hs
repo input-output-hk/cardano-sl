87e2679fb3727ca582977808d6e0ba2ff005c026
Kosyrev Serge <serge.kosyrev@iohk.io>
2018-12-04 18:14:53 +0300

    Remove chain

--- chain/src/Pos/Chain/Genesis/Hash.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE Rank2Types #-}
-
-module Pos.Chain.Genesis.Hash
-       ( GenesisHash (..)
-       ) where
-
-import           Pos.Crypto.Hashing (Hash)
-
-newtype GenesisHash = GenesisHash { getGenesisHash :: forall a . Hash a }

d940239460cdc485d8f97be8c6a7724c792e5558
Erik de Castro Lopo <erikd@mega-nerd.com>
2019-02-22 10:01:30 +1100

    CBR-482: Fix/improve script-runner

--- script-runner/runtest.nix
+++ script-runner/runtest.nix
@@ -5,10 +5,10 @@ let
     cat /etc/nsswitch.conf /etc/protocols > /dev/null
     mkdir $out
     cd $out
-    mkdir poc-state
+    mkdir script-runner/stack-gui
     export SCRIPT=${testname}
-    testcases --configuration-file ${../lib/configuration.yaml} --db-path poc-state/db --keyfile poc-state/secret.key --log-console-off --log-config ${./log-config.yaml} --logs-prefix poc-state/logs --topology ${./topology-local.yaml} --no-brickui --policies ${../scripts/policies/policy_script-runner.yaml}
-    egrep -r --color=always "Processing of proposal|We'll request data for key Tagged|Ignoring data|Proposal .* is confirmed" poc-state/relay-stdout-0
+    testcases --configuration-file ${../lib/configuration.yaml} --db-path script-runner/stack-gui/db --keyfile script-runner/stack-gui/secret.key --log-console-off --log-config ${./log-config.yaml} --logs-prefix script-runner/stack-gui/logs --topology ${./topology-local.yaml} --no-brickui --policies ${../scripts/policies/policy_script-runner.yaml}
+    egrep -r --color=always "Processing of proposal|We'll request data for key Tagged|Ignoring data|Proposal .* is confirmed" script-runner/stack-gui/relay-stdout-0
   '';
 in {
   test41 = runTest "test4.1";

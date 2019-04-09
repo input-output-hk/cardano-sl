d5f405afba9c2ab3989de281a81931a58188140a
Samuel Leathers <samuel.leathers@iohk.io>
2019-04-08 20:36:00 -0400

    bump block verion to 1.0.0

--- script-runner/shell.nix
+++ script-runner/shell.nix
@@ -16,12 +16,13 @@ let
     mkdir -pv states/script-runner/stack-gui-${cfg.name}
     cd states/script-runner/stack-gui-${cfg.name}
 
-    SCRIPT=none $BIN --configuration-file ${cfgFiles}/configuration.yaml --configuration-key ${cfg.key} --log-console-off --db-path db --keyfile secret.key --log-config ${./log-config.yaml} --logs-prefix logs --topology ${./. + "/topology-${cfg.name}.yaml"}
+    SCRIPT=none $BIN --configuration-file ${cfgFiles}/configuration.yaml --configuration-key ${cfg.key} --log-console-off --db-path db --keyfile secret.key --log-config ${./log-config.yaml} --logs-prefix logs --system-start 1544614190 --topology ${./. + "/topology-${cfg.name}.yaml"}
   '';
   mainnet = makeHelper { name = "mainnet"; key = "mainnet_full"; };
   testnet = makeHelper { name = "testnet"; key = "testnet_full"; };
   staging = makeHelper { name = "staging"; key = "mainnet_dryrun_full"; };
+  obft-testnet = makeHelper { name = "obft-testnet"; key = "obft_testnet"; };
 in
   cardanoPkgs.cardano-sl-script-runner.env.overrideAttrs (drv: {
-    buildInputs = drv.buildInputs ++ [ cardanoPkgs.cardano-sl-node-static cardanoPkgs.cardano-sl-tools mainnet testnet staging ];
+    buildInputs = drv.buildInputs ++ [ cardanoPkgs.cardano-sl-node-static cardanoPkgs.cardano-sl-tools mainnet testnet staging obft-testnet ];
   })

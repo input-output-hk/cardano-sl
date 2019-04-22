37b840ad942de9e7e045d220ec93e7ae87df24b3
Michael Bishop <michael.bishop@iohk.io>
2019-04-22 15:07:11 -0400

    fix warnings

--- wallet/FatFinder.hs
+++ wallet/FatFinder.hs
@@ -8,53 +8,29 @@
 
 module Main (main) where
 
-import           Cardano.Crypto.Wallet (unXPrv)
-import           Cardano.Mnemonic (Mnemonic, MnemonicError, mkMnemonic,
-                     mnemonicToSeed)
-import           Cardano.Wallet.Client.Http (BackupPhrase (BackupPhrase),
-                     getAccIndex, unBackupPhrase)
-import           Cardano.Wallet.Kernel.DB.HdWallet (HdRootId, eskToHdRootId,
-                     getHdRootId, isOurs)
-import           Cardano.Wallet.Kernel.DB.InDb (fromDb)
-import           Cardano.Wallet.Kernel.Decrypt (WalletDecrCredentials,
-                     eskToWalletDecrCredentials)
 import           Control.Lens (makeLensesWith)
 import           Control.Monad.Reader ()
-import qualified Data.ByteString as BS
-import           Data.Conduit (mapOutputMaybe, runConduitRes, (.|))
-import qualified Data.Conduit.List as Conduit
-import qualified Data.Text as T
-import qualified Data.Text.Lazy.Builder as T
-import           Formatting (Format, bprint, fprint, hex, later, shown, (%), int, build)
-import           Formatting.Internal.Raw (left)
-import           Pos.Chain.Genesis (GenesisHash, configGenesisData, configGenesisHash)
-import           Pos.Chain.Txp (TxIn, TxOutAux, toaOut, txOutAddress, txpTxs, txInputs,
-                     txOutValue, Tx)
-import           Pos.Core (Coin, IsBootstrapEraAddr (..), addrToBase58,
-                     deriveLvl2KeyPair, mkCoin, unsafeAddCoin)
-import           Pos.Core.NetworkMagic (NetworkMagic (..))
-import           Pos.Crypto (ShouldCheckPassphrase (..), eskPayload,
-                     safeDeterministicKeyGen)
+import           Formatting (fprint, shown, (%), int, build)
+import           Pos.Chain.Genesis (GenesisHash, configGenesisHash)
+import           Pos.Chain.Txp (txpTxs, txInputs, txOutputs, Tx)
 import           Pos.DB (MonadDBRead (dbGet, dbGetSerBlock, dbGetSerBlund, dbGetSerUndo, dbIterSource),
                      NodeDBs, closeNodeDBs, dbGetDefault, dbIterSourceDefault,
                      openNodeDBs, getBlock)
 import           Pos.DB.Block (dbGetSerBlockRealDefault, getFirstGenesisBlockHash, resolveForwardLink,
                      dbGetSerBlundRealDefault, dbGetSerUndoRealDefault)
-import           Pos.DB.Txp.Utxo (utxoSource)
 import           Pos.Util (HasLens (lensOf), postfixLFields)
 import           System.Environment (lookupEnv)
 import           Universum
 import Pos.Launcher
-import           Options.Applicative (Parser, auto, execParser, footerDoc, info, fullDesc)
+import           Options.Applicative (execParser, info, fullDesc)
 import Pos.Client.CLI.Options
 import Pos.Chain.Block
 import           Pos.Util.Trace (Trace, fromTypeclassWlog, Severity(Info))
 import           Data.Functor.Contravariant (contramap)
 import           Pos.Util.Wlog.Compatibility (setupTestLogging)
-import           Pos.Chain.Block (Block, Blund, HeaderHash, Undo, mainBlockSlot)
+import           Pos.Chain.Block (HeaderHash)
 import qualified Data.List.NonEmpty as NE
 import Pos.DB.Block
-import qualified Formatting.Buildable as Buildable
 
 data KeyToyContext = KeyToyContext { _ktNodeDBs :: NodeDBs }
 makeLensesWith postfixLFields ''KeyToyContext
@@ -62,30 +38,11 @@ makeLensesWith postfixLFields ''KeyToyContext
 instance HasLens NodeDBs KeyToyContext NodeDBs where
     lensOf = _ktNodeDBs_L
 
-hexString :: Format r (ByteString -> r)
-hexString = later f
-  where
-    f :: ByteString -> T.Builder
-    f bs = foldl' f2 "" $ BS.unpack bs
-    f2 :: T.Builder -> Word8 -> T.Builder
-    f2 str byte = str <> (left 2 '0' (bprint hex byte))
-
 main :: IO ()
 main = do
-  let
   setupTestLogging
-  putStrLn (""::String)
-
-  cluster <- fromMaybe "mainnet" <$> lookupEnv "CLUSTER"
-  let
-    nm = case cluster of
-      "mainnet" -> NetworkMainOrStage
-      "staging" -> NetworkMainOrStage
-      "testnet" -> NetworkTestnet 1097911063
-      _         -> error "unsupported cluster"
 
   let
-    ix = 0
     logTrace :: Trace IO (Severity, Text)
     logTrace = fromTypeclassWlog
     traceInfo :: Trace IO Text
@@ -98,7 +55,7 @@ main = do
     Nothing -> pure ()
     Just dbPath -> do
       bracket (openNodeDBs False dbPath) closeNodeDBs $ \db -> do
-        withConfigurations traceInfo Nothing Nothing False opts $ \genesisConfig walletConfig txpConfig ntpConfig -> do
+        withConfigurations traceInfo Nothing Nothing False opts $ \genesisConfig _walletConfig _txpConfig _ntpConfig -> do
           let genesisHash = configGenesisHash genesisConfig
           runReaderT (iterateOverAllBlocks genesisHash) (KeyToyContext db)
 
@@ -118,16 +75,19 @@ iterateOverAllBlocks genesisHash = do
     go hh fatTxs = do
       blk <- getBlock genesisHash hh
       newCount <- case blk of
-        Just (Right blk) -> do
+        Just (Right blk') -> do
           let
-            txs = blk ^. mainBlockTxPayload . txpTxs
+            txs = blk' ^. mainBlockTxPayload . txpTxs
             go2 :: Int -> Tx -> KeyToy Int
             go2 count tx = do
               let
-                s = length $ NE.toList (tx ^. txInputs)
+                ins = length $ NE.toList (tx ^. txInputs)
+                outs = length $ NE.toList (tx ^. txOutputs)
+                condition = ins > 300
+                --condition = (ins > 1) && (outs == 1)
               meos <- getHeaderEpochOrSlot hh
-              if s > 300 then do
-                liftIO $ fprint (build % " headerhash: " % shown % " count: " % int % "\n") meos hh s
+              if condition then do
+                liftIO $ fprint (build % " headerhash: " % shown % " ins: " % int % " outs: " % int % "\n") meos hh ins outs
                 pure (count + 1)
               else
                 pure count
@@ -137,7 +97,7 @@ iterateOverAllBlocks genesisHash = do
       case nextHh of
         Nothing -> do
           print fatTxs
-          print "done"
+          print ("done"::String)
         Just nextHh' -> go nextHh' newCount
   genhash <- getFirstGenesisBlockHash genesisHash
   go genhash 0

87d19b93884544cb5f03a4149af7e1e968db3d66
Nicholas Clarke <nick@topos.org.uk>
2019-06-17 14:12:48 +0200

    Bump canonical json
    This is needed to allow byron-proxy in the rewrite to build, since it depends on both this and on the new code.

--- util/src/Pos/Util/Json/Parse.hs
+++ util/src/Pos/Util/Json/Parse.hs
@@ -1,38 +1,44 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Pos.Util.Json.Parse
-       ( tryParseString
-       , wrapConstructor
-       ) where
+    ( tryParseString
+    , wrapConstructor
+    )
+where
 
 import           Universum
 
-import           Data.Typeable (typeRep)
-import           Text.JSON.Canonical (JSValue (..),
-                     ReportSchemaErrors (expected), expectedButGotValue)
+import           Data.Typeable                  ( typeRep )
+import           Text.JSON.Canonical            ( JSValue(..)
+                                                , JSString
+                                                , fromJSString
+                                                , ReportSchemaErrors(expected)
+                                                , expectedButGotValue
+                                                )
 
-tryParseString ::
-       forall a m e. (Typeable a, ReportSchemaErrors m, ToString e)
+tryParseString
+    :: forall a m e
+     . (Typeable a, ReportSchemaErrors m, ToString e)
     => (Text -> Either e a)
     -> JSValue
     -> m a
-tryParseString parser =
-    \case
-        JSString str ->
-            case parser (toText str) of
-                Right res -> pure res
-                Left (toString -> err) ->
-                    expected typeName (Just $ str <> ", err was: " <> err)
-        val -> expectedButGotValue typeName val
-  where
-    typeName = show $ typeRep (Proxy @a)
+tryParseString parser = \case
+    JSString str -> case parser (toAesonStr str) of
+        Right res -> pure res
+        Left  err -> expected
+            typeName
+            (Just $ toAesonStr str <> ", err was: " <> toString err)
+    val -> expectedButGotValue typeName val
+    where typeName = show $ typeRep (Proxy @a)
 
-wrapConstructor ::
-       forall e a m. (Typeable a, ReportSchemaErrors m, ToString e)
+wrapConstructor
+    :: forall e a m
+     . (Typeable a, ReportSchemaErrors m, ToString e)
     => Either e a
     -> m a
-wrapConstructor =
-    \case
-        Left err ->
-            expected typeName (Just $ "error occurred: " <> toString err)
-        Right x -> pure x
-  where
-    typeName = show $ typeRep (Proxy @a)
+wrapConstructor = \case
+    Left  err -> expected typeName (Just $ "error occurred: " <> toString err)
+    Right x   -> pure x
+    where typeName = show $ typeRep (Proxy @a)
+
+toAesonStr :: IsString s => JSString -> s
+toAesonStr = fromString . fromJSString

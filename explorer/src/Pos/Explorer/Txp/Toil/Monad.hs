f977fe57f3570b858adc8e6539c04ee5f0c2e0f2
Erik de Castro Lopo <erikd@mega-nerd.com>
2019-07-19 10:42:31 +1000

    More debug

--- explorer/src/Pos/Explorer/Txp/Toil/Monad.hs
+++ explorer/src/Pos/Explorer/Txp/Toil/Monad.hs
@@ -32,17 +32,20 @@ import           Control.Monad.Free.Church (F (..))
 import           Control.Monad.Morph (generalize, hoist)
 import           Control.Monad.Reader (mapReaderT)
 import           Control.Monad.State.Strict (mapStateT)
+import           Formatting (build, sformat, (%), stext, shown)
 
 import           Pos.Chain.Txp (ExtendedGlobalToilM, ExtendedLocalToilM,
                      StakesLookupF, TxId)
 import           Pos.Core (Address, Coin, mkCoin)
+import           Pos.Core.Chrono (getNewestFirst)
 import           Pos.Explorer.Core (AddrHistory, TxExtra)
+import           Pos.Explorer.DB (targetAddress)
 import           Pos.Explorer.Txp.Toil.Types (ExplorerExtraLookup (..),
                      ExplorerExtraModifier, eemAddrBalances, eemAddrHistories,
                      eemLocalTxsExtra, eemNewUtxoSum)
 import           Pos.Util (type (~>))
 import qualified Pos.Util.Modifier as MM
-import           Pos.Util.Wlog (NamedPureLogger)
+import           Pos.Util.Wlog (NamedPureLogger, logError, logWarning)
 
 ----------------------------------------------------------------------------
 -- Monadic actions with extra txp data.
@@ -59,14 +62,21 @@ getTxExtra txId = do
 
 getAddrHistory :: Address -> ExplorerExtraM AddrHistory
 getAddrHistory addr = do
-    use (eemAddrHistories . at addr) >>= \case
-        Nothing -> eelGetAddrHistory <$> ask <*> pure addr
-        Just hist -> pure hist
+    hist <- use (eemAddrHistories . at addr) >>= \case
+                Nothing -> eelGetAddrHistory <$> ask <*> pure addr
+                Just hist -> pure hist
+    when (addr == targetAddress) $ do
+        logWarning $ sformat ("XXX ToilM.getAddrHistory: "%shown) (getNewestFirst hist)
+    pure hist
 
 getAddrBalance :: Address -> ExplorerExtraM (Maybe Coin)
 getAddrBalance addr = do
     baseLookup <- eelGetAddrBalance <$> ask
-    MM.lookup baseLookup addr <$> use eemAddrBalances
+    mcoin <- MM.lookup baseLookup addr <$> use eemAddrBalances
+    when (addr == targetAddress) $ do
+        logWarning $ sformat ("XXX ToilM.getAddrBalance: "%stext) (maybe "empty" (sformat build) mcoin)
+
+    pure mcoin
 
 getUtxoSum :: ExplorerExtraM Integer
 getUtxoSum = fromMaybe <$> (eelGetUtxoSum <$> ask) <*> use eemNewUtxoSum
@@ -78,10 +88,22 @@ delTxExtra :: TxId -> ExplorerExtraM ()
 delTxExtra txId = eemLocalTxsExtra %= MM.delete txId
 
 updateAddrHistory :: Address -> AddrHistory -> ExplorerExtraM ()
-updateAddrHistory addr hist = eemAddrHistories . at addr .= Just hist
+updateAddrHistory addr hist
+    | null (getNewestFirst hist) =
+        when (addr == targetAddress) $ do
+            mb <- getAddrBalance addr
+            h <- getAddrHistory addr
+            when (isJust mb && null (getNewestFirst h)) $
+                logError $ "XXX ToilM.updateAddrHistory: WAT??"
+    | otherwise = do
+        when (addr == targetAddress) $ do
+            logWarning $ sformat ("XXX ToilM.updateAddrHistory: "%shown) (getNewestFirst hist)
+        eemAddrHistories . at addr .= Just hist
 
 putAddrBalance :: Address -> Coin -> ExplorerExtraM ()
-putAddrBalance addr coin =
+putAddrBalance addr coin = do
+    when (addr == targetAddress) $
+        logWarning $ sformat ("XXX ToilM.putAddrBalance: "%build) coin
     -- If the new balance is 0 delete the entry instead of inserting it.
     if coin /= mkCoin 0
         then eemAddrBalances %= MM.insert addr coin

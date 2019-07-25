4397ad959e02dd58a9492fac7bb67667291b60bb
Jean-Baptiste Giraudeau <jean-baptiste.giraudeau@iohk.io>
2019-07-25 14:16:34 +0200

    Add path and hash of genesis file for each env.

--- lib.nix
+++ lib.nix
@@ -36,26 +36,36 @@ in lib // iohkNix // (rec {
     mainnet = {
       relays = "relays.cardano-mainnet.iohk.io";
       confKey = "mainnet_full";
+      genesisFile = ./lib/mainnet-genesis.json;
+      genesisHash = "5f20df933584822601f9e3f8c024eb5eb252fe8cefb24d1317dc3d432e940ebb";
       private = false;
     };
     staging = {
       relays = "relays.awstest.iohkdev.io";
       confKey = "mainnet_dryrun_full";
+      genesisFile = ./lib/mainnet-genesis-dryrun-with-stakeholders.json;
+      genesisHash = "c6a004d3d178f600cd8caa10abbebe1549bef878f0665aea2903472d5abf7323";
       private = false;
     };
     testnet = {
       relays = "relays.new.cardano-testnet.iohkdev.io";
       confKey = "testnet_full";
+      genesisFile = ./lib/testnet-genesis.json;
+      genesisHash = "96fceff972c2c06bd3bb5243c39215333be6d56aaf4823073dca31afe5038471";
       private = false;
     };
     shelley_staging = {
       relays = "relays.shelley-staging.aws.iohkdev.io";
       confKey = "shelley_staging_full";
+      genesisFile = ./lib/shelley-staging-genesis.json;
+      genesisHash = "82995abf3e0e0f8ab9a6448875536a1cba305f3ddde18cd5ff54c32d7a5978c6";
       private = false;
     };
     shelley_staging_short = {
       relays = "relays.staging-shelley-short.aws.iohkdev.io";
       confKey = "shelley_staging_short_full";
+      genesisFile = ./lib/shelley-staging-short-genesis.json;
+      genesisHash = "d8eb540d96addd8f1f4726cbdf98af6fbff9e4530c76962e454d9ccfa5b5c204";
       private = false;
     };
     demo = {

ec3e8f99c092dec7aaad2010eb6143b090c3df61
Samuel Leathers <samuel.leathers@iohk.io>
2019-08-09 20:29:17 -0400

    disable clock.patch

--- nix/update-iohk-nix.sh
+++ nix/update-iohk-nix.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 
 NIX_DIR=$(dirname "$0")
 
-nix-prefetch-git https://github.com/input-output-hk/iohk-nix \
+nix-prefetch-git https://github.com/input-output-hk/iohk-nix refs/heads/haskell-nix-cardano-sl \
                  > "$NIX_DIR/iohk-nix-src.json"

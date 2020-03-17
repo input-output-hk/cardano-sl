787e72e93c90a97f2b3e470c84abe0f91402319f
Michael Bishop <michael.bishop@iohk.io>
2020-03-17 09:39:09 -0300

    regen nix files

--- nix/.stack.nix/cardano-wallet.nix
+++ nix/.stack.nix/cardano-wallet.nix
@@ -134,6 +134,27 @@
             (hsPkgs.universum)
             ];
           };
+        "export-wallets" = {
+          depends = [
+            (hsPkgs.base)
+            (hsPkgs.aeson)
+            (hsPkgs.aeson-pretty)
+            (hsPkgs.bytestring)
+            (hsPkgs.cardano-crypto)
+            (hsPkgs.cardano-sl)
+            (hsPkgs.cardano-sl-core)
+            (hsPkgs.cardano-sl-crypto)
+            (hsPkgs.cardano-sl-crypto)
+            (hsPkgs.cardano-sl-infra)
+            (hsPkgs.cardano-sl-util)
+            (hsPkgs.cardano-wallet)
+            (hsPkgs.contravariant)
+            (hsPkgs.memory)
+            (hsPkgs.optparse-applicative)
+            (hsPkgs.text)
+            (hsPkgs.universum)
+            ];
+          };
         };
       tests = {
         "unit" = {
